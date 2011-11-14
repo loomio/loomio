513ea4aebb9a23e032f9e6eca0fc4bb2f2bcd536
Jon Lemmon <jonlemmon.public@gmail.com>
2011-11-15 12:05:27 +1300

    Hello Tautoko.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tautoko::Application.config.secret_token = '2b2f4e14bdd5eeb0b26762247773f1b0d228f0ddc9ab8918ab8e8740726908a9a1f9f1d382187e732537e18de3b6de33ddad6077f5190ccfa7e46fd2eb9fa549'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Tautoko::Application.config.session_store :cookie_store, key: '_tautoko_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Tautoko::Application.config.session_store :active_record_store
