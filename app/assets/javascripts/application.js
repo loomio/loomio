085debb632e2d87cd1d2750eec6ac0156b231f74
Jon Lemmon <jonlemmon@gmail.com>
2011-11-15 19:31:29 +1300

    Remove jquery requirements.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,4 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
-//= require jquery
-//= require jquery_ujs
 //= require_tree .
