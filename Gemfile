8502402b7efdde4ea1e9f2a22e672bd58194a12b
Joshua Vial <joshua@enspiral.com>
2011-11-15 13:04:44 +1300

    updating bundler and gems

--- Gemfile
+++ Gemfile
@@ -5,8 +5,7 @@ gem 'rails', '3.1.1'
 # Bundle edge Rails instead:
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
-
+gem 'mysql2'
 
 # Gems used only for assets and not required
 # in production environments by default.
@@ -31,6 +30,10 @@ gem 'devise'
 
 # To use debugger
 # gem 'ruby-debug19', :require => 'ruby-debug'
+
+group :development do
+  gem 'sqlite3'
+end
 
 group :test do
   # Pretty printed test output
--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,9 @@ set :application, "tautoko"
 set :user, application
 set :repository,  "git@github.com:enspiral/tautoko.git"
 
+set :use_sudo, false                                                                                                                                    
+set :rake, "bundle exec rake"
+
 set :scm, :git
 
 task :staging do
@@ -15,14 +18,30 @@ task :staging do
   role :db,  domain, :primary => true 
 end
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+namespace :deploy do
+  task :symlink_configs do                                                                                                                                  
+    run %( cd #{release_path} &&
+      ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml &&
+      ln -nfs #{shared_path}/public/robots.txt #{release_path}/public/robots.txt
+    )
+  end
+
+  desc "bundle gems"                                                                                                                                        
+  task :bundle do
+    run "cd #{release_path} && RAILS_ENV=#{rails_env} && bundle install #{shared_path}/gems/cache --deployment"
+  end 
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
 #   task :start do ; end
 #   task :stop do ; end
 #   task :restart, :roles => :app, :except => { :no_release => true } do
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
-# end
+end
+
+after "deploy:update_code" do
+  deploy.symlink_configs
+  deploy.bundle
+end
+
+require 'airbrake/capistrano'
