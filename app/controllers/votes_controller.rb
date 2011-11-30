d99b4813f6045008b7b0596ca0c1164fa63d1a6e
Jon Lemmon <jonlemmon@gmail.com>
2011-11-30 20:10:06 +1300

    continue work on voting features

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,11 @@
 class VotesController < BaseController
+  # before_filter :ensure_group_member
+  # belongs_to :motion
+
+  # def begin_of_association_chain
+  #   @motion
+  # end
+
   def create
     @vote = Vote.new(position: params[:vote][:position])
     @vote.motion = Motion.find(params[:vote][:motion_id])
@@ -9,6 +16,21 @@ class VotesController < BaseController
                                     id: @vote.motion)
     else
       render :edit
+    end
+  end
+
+  private
+  def ensure_group_member
+    # NOTE: this method is currently duplicated in groups_controller,
+    # and motions_controller. We should figure out a way to DRY this up.
+    group = Motion.find(params[:vote][:motion_id]).group
+    unless group.users.include? current_user
+      if group.requested_users_include?(current_user)
+        flash[:notice] = "Cannot access group yet... waiting for membership approval."
+        redirect_to groups_url
+      else
+        redirect_to request_membership_group_url(group)
+      end
     end
   end
 end
--- app/models/motion.rb
+++ app/models/motion.rb
@@ -2,5 +2,6 @@ class Motion < ActiveRecord::Base
   belongs_to :group
   belongs_to :author, :class_name => 'User'
   belongs_to :facilitator, :class_name => 'User'
+  has_many :votes
   validates_presence_of :name, :group, :author, :facilitator_id
 end
