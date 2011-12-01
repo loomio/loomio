a2b12e1c1526a78e6d0b9eb2d5a7b910147e6f72
Jon Lemmon <jonlemmon@gmail.com>
2011-12-02 11:16:30 +1300

    use create_motion helper in motion spec

--- spec/models/motion_spec.rb
+++ spec/models/motion_spec.rb
@@ -11,17 +11,10 @@ describe Motion do
   it {should have(1).errors_on(:group)}
 
   it "user_has_votes?(user) returns true if the given user has voted on motion" do
-    #This works when running as single spec, but not if in all specs, WTF
-    @user = User.make
-    @user.save!
-    @group = Group.make
-    @group.save!
-    @group.add_member!(@user)
-    @group.save!
-    @motion_to_vote_on = Motion.make(:group => @group, :facilitator => @user, :author => @user)
-    @motion_to_vote_on.save!
-    @vote = Vote.make(:user => @user, :motion => @motion_to_vote_on, :position => "yes")
+    @user = User.make!
+    @motion = create_motion(:author => @user)
+    @vote = Vote.make(:user => @user, :motion => @motion, :position => "yes")
     @vote.save!
-    @motion_to_vote_on.user_has_voted?(@user).should == true
+    @motion.user_has_voted?(@user).should == true
   end
 end
