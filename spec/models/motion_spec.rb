0edbe7cd0492efca6e3675ee48def3d2d6e0cd7f
allansideas <allansideas@gmail.com>
2011-12-01 23:15:01 +1300

    user_has_voted?(user) on motion method

--- spec/models/motion_spec.rb
+++ spec/models/motion_spec.rb
@@ -9,4 +9,19 @@ describe Motion do
   it {should have(1).errors_on(:name)}
   it {should have(1).errors_on(:author)}
   it {should have(1).errors_on(:group)}
+
+  it "user_has_votes?(user) returns true if the given user has voted on motion" do
+    #This works when running as single spec, but not if in all specs, WTF
+    @user = User.make
+    @user.save!
+    @group = Group.make
+    @group.save!
+    @group.add_member!(@user)
+    @group.save!
+    @motion_to_vote_on = Motion.make(:group => @group, :facilitator => @user, :author => @user)
+    @motion_to_vote_on.save!
+    @vote = Vote.make(:user => @user, :motion => @motion_to_vote_on, :position => "yes")
+    @vote.save!
+    @motion_to_vote_on.user_has_voted?(@user).should == true
+  end
 end
