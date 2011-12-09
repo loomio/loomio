5a44c29db56c7c29732fbc13628c4d1cdd6cc21d
allansideas <allansideas@gmail.com>
2011-12-10 09:11:44 +1300

    remove unnecessary code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -63,16 +63,3 @@ def create_motion(*args)
   motion
 end
 
-def get_message_part (mail, content_type)
-  mail.body.parts.find { |p| p.content_type.match content_type }.body.raw_source
-end
-
-shared_examples_for "multipart email" do
-  it "generates a multipart message (plain text and html)" do
-    mail.body.parts.length.should eq(2)
-    mail.body.parts.collect(&:content_type).should == ["text/plain; charset=UTF-8", "text/html; charset=UTF-8"]
-  end
-end
-
-
-
