a7a211eb28dfc4ff182e038f64a50157aa1d5716
allansideas <allansideas@gmail.com>
2011-12-12 14:26:57 +1300

    staging sends emails for testing purposes

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -58,16 +58,20 @@ Tautoko::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-        :address => "127.0.0.1",
-        :enable_starttls_auto => false
-    }
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+        #:address => "127.0.0.1",
+        #:enable_starttls_auto => false
+    #}
+  #config.action_mailer.perform_deliveries = true
+  #config.action_mailer.raise_delivery_errors = true
+  #config.action_mailer.default_url_options = {
+    #:host => 'tautoko.enspiral.info',
+  #}
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = {
-    :host => 'tautoko.enspiral.info',
-  }
+  
   # Send deprecation notices to registered listeners
   config.action_mailer.raise_delivery_errors = true
 end
