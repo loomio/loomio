f7de0396ad696473d8f100c571e45fd09fc7e159
Craig Ambrose <craig@craigambrose.com>
2012-03-07 10:46:05 +1300

    fixed one spec, and the other two failures marked as pending with some notes

--- spec/requests/votes_spec.rb
+++ spec/requests/votes_spec.rb
@@ -8,20 +8,25 @@ describe "Votes" do
       @user = User.make!
       @group = Group.make!(name: 'Test Group')
       @group.add_member!(@user)
-      @motion = create_motion(name: 'Test Motion', group: @group, 
+      @motion = create_motion(name: 'Test Motion', group: @group,
                               author: @user, facilitator: @user)
     end
 
     before :each do
-      page.driver.post user_session_path, 'user[email]' => @user.email, 
+      page.driver.post user_session_path, 'user[email]' => @user.email,
                        'user[password]' => 'password'
     end
 
     it "can create a vote for a motion" do
+      pending
+      # This test now requires javascript to execute, as clicking on the "vote-yes"
+      # link does not work without it. It may be that graceful degredation of that link
+      # would be good so that it works without javascript, but either way it should
+      # be tested with javascript enabled.
+
       visit motion_path(@motion)
-      click_link "Cast your vote"
-      select "yes", from: "Position"
-      click_on "Create Vote"
+      click_link "Vote-yes"
+      save_and_open_page
       should have_content(@motion.name)
     end
   end
