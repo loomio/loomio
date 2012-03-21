be97ae94e06cfc9e049ee4ac96bc56a788bbe48c
Jon Lemmon <jonlemmon@gmail.com>
2012-03-22 12:51:01 +1300

    Upgrate to devise 2.0 and add devise_invitable gem.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120307190505) do
+ActiveRecord::Schema.define(:version => 20120321232109) do
 
   create_table "groups", :force => true do |t|
     t.string   "name"
@@ -73,6 +73,7 @@ ActiveRecord::Schema.define(:version => 20120307190505) do
     t.datetime "updated_at"
     t.boolean  "admin",                                 :default => false
     t.string   "name"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
