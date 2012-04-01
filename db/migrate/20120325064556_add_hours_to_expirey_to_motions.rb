ec7ab75ee951d85976345c5d17ca6a11c13dcb8b
Aaron Thornton <aaronthornton00@gmail.com>
2012-04-02 09:41:49 +1200

    deleted migration

--- db/migrate/20120325064556_add_hours_to_expirey_to_motions.rb
+++
@@ -1,8 +0,0 @@
-class AddHoursToExpireyToMotions < ActiveRecord::Migration
-  def up
-    add_column :motions, :hours_to_expirey, :integer
-  end
-  def down
-    remove_column :motions, :hours_to_expirey
-  end
-end
