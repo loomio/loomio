73dd1fddb3e8f1dd9539a70ad1fe437ef71028ba
Aaron Thornton <aaronthornton00@gmail.com>
2012-04-01 16:43:00 +1200

    implementing yet to voteas a percentage

--- app/controllers/motions_controller.rb
+++ app/controllers/motions_controller.rb
@@ -59,14 +59,12 @@ class MotionsController < GroupBaseController
     end
   end
 
-  def toggle_tag_filter 
+  def toggle_tag_filter
     @motion = Motion.find(params[:id])
     @active_tags = params[:tags]
     @clicked_tag = params[:tag]
-    
     render :partial => "motions/votes_filters", :locals => { clicked_tag: @clicked_tag }, :layout => false, :status => :created
     #render @motion
-  end
 
   private
 
