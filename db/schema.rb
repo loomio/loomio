65f876b2f1beaf1e8066199c0be2bbd5a6f8068d
Aaron Thornton <aaronthornton00@gmail.com>
2012-04-02 12:09:46 +1200

    changes for test

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20120326091356) do
+
   create_table "groups", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
