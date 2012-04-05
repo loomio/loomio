df93a0a47be845ce0ef5a8d2b39f9e0b8969ecf2
Jon Lemmon <jonlemmon@gmail.com>
2012-04-05 14:50:04 +1200

    Make devise accounts http authenticatable

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -55,7 +55,8 @@ Devise.setup do |config|
   # It can be set to an array that will enable http authentication only for the
   # given strategies, for example, `config.http_authenticatable = [:token]` will
   # enable it only for token authentication.
-  # config.http_authenticatable = false
+  config.http_authenticatable = true
+
 
   # If http headers should be returned for AJAX requests. True by default.
   # config.http_authenticatable_on_xhr = true
