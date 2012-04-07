dcd39ca4b82547d899050a6fabe5e4fcdb178dbc
Jon Lemmon <jonlemmon@gmail.com>
2012-04-08 01:34:12 +1200

    Create discussion model and add acts-as-commentable

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,34 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120401221608) do
+ActiveRecord::Schema.define(:version => 20120407071132) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "commentable_id",   :default => 0
+    t.string   "commentable_type", :default => ""
+    t.string   "title",            :default => ""
+    t.text     "body"
+    t.string   "subject",          :default => ""
+    t.integer  "user_id",          :default => 0,  :null => false
+    t.integer  "parent_id"
+    t.integer  "lft"
+    t.integer  "rgt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["commentable_id"], :name => "index_comments_on_commentable_id"
+  add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+
+  create_table "discussions", :force => true do |t|
+    t.integer  "group_id"
+    t.integer  "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "discussions", ["author_id"], :name => "index_discussions_on_author_id"
+  add_index "discussions", ["group_id"], :name => "index_discussions_on_group_id"
 
   create_table "groups", :force => true do |t|
     t.string   "name"
