e04458d059dc91a1d01ead4d5e89df307381da87
Jon Lemmon <jonlemmon@gmail.com>
2012-04-10 15:14:53 +1200

    Add discussions to old motions.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120410011009) do
+ActiveRecord::Schema.define(:version => 20120410030955) do
 
   create_table "comments", :force => true do |t|
     t.integer  "commentable_id",   :default => 0
