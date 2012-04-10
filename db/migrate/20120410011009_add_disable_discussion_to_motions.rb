97e081b5bb42cd34c2124c9958adc781c2ed7c19
Jon Lemmon <jonlemmon@gmail.com>
2012-04-10 14:29:56 +1200

    Add disable_discussion column to motions.

--- db/migrate/20120410011009_add_disable_discussion_to_motions.rb
+++ db/migrate/20120410011009_add_disable_discussion_to_motions.rb
@@ -1,5 +1,5 @@
 class AddDisableDiscussionToMotions < ActiveRecord::Migration
   def change
-    add_column :motions, :disable_discussion, :boolean
+    add_column :motions, :disable_discussion, :boolean, default: false
   end
 end
