2ee5b40e3a4a923f5d502478fc16e6687e806306
Aaron Thornton <aaronthornton00@gmail.com>
2012-04-11 17:28:37 +1200

    migration redo

--- db/migrate/20120411031907_fill_in_name_in_users.rb
+++ db/migrate/20120411031907_fill_in_name_in_users.rb
@@ -3,6 +3,7 @@ class FillInNameInUsers < ActiveRecord::Migration
     User.all.each do |user|
       if user.name == nil
         user.name = user.email
+        user.save
       end
     end
   end
