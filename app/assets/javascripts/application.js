7489fc091daf27b5a48964d5c7b592e867d0dcfc
Jon Lemmon <jonlemmon@gmail.com>
2012-04-12 12:03:13 +1200

    Improve comment/motion text formatting

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 //= require twitter/bootstrap
 //= require jqplot.1.0.0b2_r792
 //= require lib/jquery.tokeninput
+//= require lib/linkify
 //= require underscore
 //= require backbone
 //= require backbone_rails_sync
