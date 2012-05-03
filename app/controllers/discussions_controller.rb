98c977617ceca726c07647e21afff3504e3f57e3
Aaron Thornton <aaronthornton00@gmail.com>
2012-05-03 18:29:30 +1200

    add comment on new discussion

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,17 +1,22 @@
-class DiscussionsController < BaseController
-  load_and_authorize_resource
+class DiscussionsController < GroupBaseController
+  before_filter :check_group_read_permissions
+  load_and_authorize_resource except: :show
 
   def new
-    @discussion = Discussion.new(group: Group.find(params[:group_id]))
-    require 'ap'
-    ap @discussion
+    @discussion = Discussion.new(group: Group.find(params[:discussion][:group_id]))
   end
 
   def create
+    group = Group.find(params[:discussion][:group_id])
+    comment = params[:discussion][:comment]
     @discussion = Discussion.create(params[:discussion])
-    @discusiion.author = current_user
-    if @dissussion.save
-      #redirect_to :back
+    @discussion.author = current_user
+    @discussion.group = group
+    if @discussion.save
+      @discussion.add_comment(current_user, comment)
+      redirect_to @discussion
+    else
+      redirect_to :back
     end
   end
 
@@ -19,4 +24,19 @@ class DiscussionsController < BaseController
     comment = resource.add_comment(current_user, params[:comment])
     redirect_to motion_url(resource.default_motion)
   end
+
+  private
+
+    def group
+      @group ||= find_group
+    end
+
+    def find_group
+      if (params[:id] && (params[:id] != "new"))
+        Discussion.find(params[:id]).group
+      elsif params[:discussion][:group_id]
+        Group.find(params[:discussion][:group_id])
+      end
+    end
+
 end
