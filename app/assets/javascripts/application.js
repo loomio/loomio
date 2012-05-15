d7c35cb4670a1024f9a07a767bfdb5f3b5a622f7
Guillaume Hammadi <guillaume@sogilis.com>
2012-05-15 17:07:02 +1200

    WIP fixing broken views after bootstrap switch

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,8 @@
 // be included in the compiled file accessible from http://example.com/assets/application.js
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
-//
+
+//= require bootstrap
 //= require jquery
 //= require jquery-ui
 //= require jquery_ujs
