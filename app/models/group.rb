698eb4b306887941ccc18031e047cead78390889
Nicolas Wormser <nicolas.wormser@gmail.com>
2012-05-17 22:48:13 +0200

    added :parent_id to attr_accessible

--- app/models/group.rb
+++ app/models/group.rb
@@ -30,7 +30,7 @@ class Group < ActiveRecord::Base
 
   acts_as_tagger
 
-  attr_accessible :name, :viewable_by, :parent
+  attr_accessible :name, :viewable_by, :parent_id, :parent
   attr_accessible :members_invitable_by, :email_new_motion
 
   #
