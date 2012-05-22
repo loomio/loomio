31852a33ef97d4d933e0b8b43ab672bfdf67dbc7
Kiesia Croucher <kiesia@be.autiful.net>
2012-05-22 17:47:08 +1200

    updated incorrect settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,10 @@ Tautoko::Application.configure do
   #config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  # config.assets.compile = false
 
   # Generate digests for assets URLs
-  #config.assets.digest = true
+  config.assets.digest = true
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
