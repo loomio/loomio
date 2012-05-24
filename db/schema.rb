380da56a0c4244fef2adaf77d0d1b30059466b1f
Riccardo Tacconi <rtacconi@gmail.com>
2012-05-24 13:37:04 +0200

    use DB sessions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120521041044) do
+ActiveRecord::Schema.define(:version => 20120524113553) do
 
   create_table "comment_votes", :force => true do |t|
     t.integer  "comment_id"
@@ -105,6 +105,16 @@ ActiveRecord::Schema.define(:version => 20120521041044) do
   end
 
   add_index "motions", ["discussion_id"], :name => "index_motions_on_discussion_id"
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
