4dc5f0b67f036bc203937649e7f5b9d56c75d019
Jon Lemmon <jonlemmon@gmail.com>
2012-05-28 15:39:08 +1200

    Mailers: motions -> proposals

--- app/mailers/motion_mailer.rb
+++ app/mailers/motion_mailer.rb
@@ -8,7 +8,7 @@ class MotionMailer < ActionMailer::Base
     #@group.users.each do |user|
       #email_addresses << user.email unless motion.author == user
     #end
-    mail(to: email, subject: "[Loomio: #{@group.name}] New motion - #{@motion.name}")
+    mail(to: email, subject: "[Loomio: #{@group.name}] New proposal - #{@motion.name}")
   end
 
   def motion_blocked(vote)
@@ -17,6 +17,6 @@ class MotionMailer < ActionMailer::Base
     @motion = vote.motion
     @group = @motion.group
     mail(to: @motion.author.email,
-         subject: "[Loomio: #{@group.name}] Motion blocked - #{@motion.name}")
+         subject: "[Loomio: #{@group.name}] Proposal blocked - #{@motion.name}")
   end
 end
