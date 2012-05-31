bee80212a1e7097f2857856ab428c3e1432c119f
Aaron Thornton <aaronthornton00@gmail.com>
2012-05-31 15:23:26 +1200

    private_or_not_found page added

--- app/controllers/group_base_controller.rb
+++ app/controllers/group_base_controller.rb
@@ -4,12 +4,7 @@ class GroupBaseController < BaseController
   private
     def check_group_read_permissions
       unless group.can_be_viewed_by? current_user
-        if group.requested_users_include?(current_user)
-          flash[:success] = "Cannot access group yet... waiting for membership approval."
-          redirect_to root_url
-        else
-          redirect_to request_membership_group_url(group)
-        end
+        render 'private_or_not_found'
       end
     end
 end
