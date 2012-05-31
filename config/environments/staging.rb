ef99a80500137c114e2e9b74d63988f550aed047
Kiesia Croucher <kiesia@be.autiful.net>
2012-05-31 13:49:26 +1200

    moved app name Tautoko > Loomio

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,4 @@
-Tautoko::Application.configure do
+Loomio::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
