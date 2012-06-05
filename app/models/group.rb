9c5b5c06c1b6924880a70a4b3153ec4f95958a70
Jon Lemmon <jonlemmon@gmail.com>
2012-06-06 11:04:51 +1200

    group.rb: add group.full_name function

--- app/models/group.rb
+++ app/models/group.rb
@@ -28,6 +28,7 @@ class Group < ActiveRecord::Base
 
   delegate :include?, :to => :users, :prefix => true
   delegate :users, :to => :parent, :prefix => true
+  delegate :name, :to => :parent, :prefix => true
 
   acts_as_tagger
 
@@ -54,6 +55,14 @@ class Group < ActiveRecord::Base
 
   def members_invitable_by=(value)
     write_attribute(:members_invitable_by, value.to_s)
+  end
+
+  def full_name(separator= " - ")
+    if parent
+      parent_name + separator + name
+    else
+      name
+    end
   end
 
 
