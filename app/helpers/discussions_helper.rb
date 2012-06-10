d151e9e435ed8460a70c17bb8db38dec3c805e6d
Jon Lemmon <jonlemmon@gmail.com>
2012-06-11 10:38:44 +1200

    Improve styling.

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -26,4 +26,28 @@ module DiscussionsHelper
 
     css_class + suffix
   end
+
+  def link_to_discussion(discussion)
+    title = truncate(discussion.title, :length => 75, :separator => ' ')
+    link = link_to title, discussion_path(discussion), class: "discussion-link"
+    group = discussion.group
+    if group.parent
+      group_link = link_to(group.name, group_path(group), class: "group-link")
+      group_link += content_tag :i, '', class: 'icon-play-tiny'
+      link = group_link + link
+    end
+    link
+  end
+
+  def fancy_discussion_name(discussion)
+    title = truncate(discussion.title, :length => 75, :separator => ' ')
+    text = sanitize(title)
+    group = discussion.group
+    if group.parent
+      group_text = sanitize("#{group.name} ")
+      group_text += content_tag :span, "\u25B6", class: 'name-separator'
+      text = "#{group_text} #{text}".html_safe
+    end
+    text
+  end
 end
