b8553005c5b6fd9cc7be4ef8553f37cf30296e47
Jon Lemmon <jonlemmon@gmail.com>
2012-06-11 15:29:04 +1200

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -108,16 +108,6 @@ ActiveRecord::Schema.define(:version => 20120607190000) do
 
   add_index "motions", ["discussion_id"], :name => "index_motions_on_discussion_id"
 
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
-
   create_table "taggings", :force => true do |t|
     t.integer  "tag_id"
     t.integer  "taggable_id"
