042456fad4a75e4b98beb0c264a07810b56b45d8
Jon Lemmon <jonlemmon@gmail.com>
2012-06-14 19:58:22 +1200

    ability_spec.rb: additional refactoring.

--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -4,6 +4,7 @@ describe "User abilities" do
   let(:user) { User.make! }
   let(:other_user) { User.make! }
   let(:group) { Group.make! }
+  let(:membership_request) { group.add_request!(User.make!) }
   let(:discussion) { create_discussion(group: group) }
   let(:new_discussion) { user.authored_discussions.new(
                          group: group, title: "new discussion") }
@@ -14,32 +15,30 @@ describe "User abilities" do
   subject { ability }
 
   context "member of a group" do
-    before :each do
-      @membership = group.add_member!(user)
-      group.add_member!(other_user)
+    before do
+      @user_membership = group.add_member!(user)
+      @other_user_membership = group.add_member!(other_user)
     end
-
+    it { should_not be_able_to(:update, group) }
     it { should be_able_to(:new_proposal, discussion) }
     it { should be_able_to(:add_comment, discussion) }
     it { should be_able_to(:destroy, user_comment) }
     it { should_not be_able_to(:destroy, another_user_comment) }
-    it { should_not be_able_to(:update, group) }
     it { should be_able_to(:like, user_comment) }
     it { should be_able_to(:like, another_user_comment) }
     it { should be_able_to(:unlike, user_comment) }
     it { should be_able_to(:unlike, another_user_comment) }
     it { should be_able_to(:create, new_discussion) }
-
-    it { should_not be_able_to(:make_admin, @membership) }
+    it { should_not be_able_to(:make_admin, @user_membership) }
+    it { should_not be_able_to(:make_admin, @other_user_membership) }
 
     context "group members invitable_by: members" do
       before do
         group.members_invitable_by = "members"
         group.save
-        @membership = group.add_request!(User.make!)
       end
       it { should be_able_to(:add_members, group) }
-      it { should be_able_to(:approve, @membership) }
+      it { should be_able_to(:approve, membership_request) }
     end
 
     context "group members invitable_by: admins" do
@@ -48,18 +47,17 @@ describe "User abilities" do
         group.save
       end
       it { should_not be_able_to(:add_members, group) }
-      it { should_not be_able_to(:approve, @membership) }
+      it { should_not be_able_to(:approve, membership_request) }
     end
   end
 
   context "admin of a group" do
     before do
       group.add_admin! user
-      @membership = group.add_request!(User.make!)
+      @membership_request = group.add_request!(User.make!)
     end
-
-    it { should be_able_to(:make_admin, @membership) }
-    it { should be_able_to(:remove_admin, @membership) }
+    it { should be_able_to(:make_admin, @membership_request) }
+    it { should be_able_to(:remove_admin, @membership_request) }
     it { should be_able_to(:update, group) }
 
     context "group members invitable_by: admins" do
@@ -68,7 +66,7 @@ describe "User abilities" do
         group.save
       end
       it { should be_able_to(:add_members, group) }
-      it { should be_able_to(:approve, @membership) }
+      it { should be_able_to(:approve, membership_request) }
     end
   end
 
@@ -79,10 +77,7 @@ describe "User abilities" do
                            group: group, title: "new discussion") }
     let(:another_user_comment) { discussion.add_comment(discussion.author, "hello") }
 
-    # Groups
     it { should_not be_able_to(:add_members, group) }
-
-    # Discussions / Comments
     it { should_not be_able_to(:new_proposal, discussion) }
     it { should_not be_able_to(:add_comment, discussion) }
     it { should_not be_able_to(:destroy, another_user_comment) }
