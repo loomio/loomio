3aac7edad4ad60f310fc0fa855b11e55bae6e348
Aaron Thornton <aaronthornton00@gmail.com>
2012-06-14 15:59:19 +1200

    migration added to drop tables and indexes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120611233010) do
+ActiveRecord::Schema.define(:version => 20120614034800) do
 
   create_table "comment_votes", :force => true do |t|
     t.integer  "comment_id"
@@ -115,23 +115,6 @@ ActiveRecord::Schema.define(:version => 20120611233010) do
 
   add_index "motions", ["author_id"], :name => "index_motions_on_author_id"
   add_index "motions", ["discussion_id"], :name => "index_motions_on_discussion_id"
-
-  create_table "taggings", :force => true do |t|
-    t.integer  "tag_id"
-    t.integer  "taggable_id"
-    t.string   "taggable_type"
-    t.integer  "tagger_id"
-    t.string   "tagger_type"
-    t.string   "context",       :limit => 128
-    t.datetime "created_at"
-  end
-
-  add_index "taggings", ["tag_id"], :name => "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], :name => "index_taggings_on_taggable_id_and_taggable_type_and_context"
-
-  create_table "tags", :force => true do |t|
-    t.string "name"
-  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                :default => "",    :null => false
