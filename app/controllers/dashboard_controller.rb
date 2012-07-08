58b9d3bb048ef1a461de11dc94492f608c4a88c0
Aaron Thornton <aaronthornton00@gmail.com>
2012-07-09 11:12:53 +1200

    additions

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,6 @@ class DashboardController < BaseController
     @motions_voted = current_user.motions_voted
     @motions_not_voted = current_user.motions_not_voted
     @motions_closed = current_user.motions_closed
-    #@discussions= current_user.discussions_sorted.page(params[:page]).per(5)
     @groups = GroupDecorator.decorate(current_user.all_root_groups)
   end
 end
