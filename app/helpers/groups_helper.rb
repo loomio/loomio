50466384e7eb40a9520e9885ff8e46e1b70b4caf
Aaron Thornton <aaronthornton00@gmail.com>
2012-07-11 14:01:51 +1200

    group settings page formatting

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,11 +1,4 @@
 module GroupsHelper
-  def group_permissions_label(option)
-    return "Everyone" if option == :everyone
-    return "Members" if option == :members
-    return "Parent group members" if option == :parent_group_members
-    return "Admins" if option == :admins
-  end
-
   def display_subgroups_block?(group)
     group.parent.nil? && (group.subgroups.present? || (current_user && group.users_include?(current_user)))
   end
