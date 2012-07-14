1b31507dfbe81079a516b7ea510a589c47c99359
Jon Lemmon <jonlemmon@gmail.com>
2012-07-14 14:42:28 +1200

    Update migrations.

--- db/migrate/20120710011556_add_creator_id_to_groups.rb
+++ db/migrate/20120710011556_add_creator_id_to_groups.rb
@@ -5,9 +5,15 @@ class AddCreatorIdToGroups < ActiveRecord::Migration
     #for each group assign first admin with membership and assign as creator
     #could there be a Membership level of creator?
     Group.all.each do |group|
-    	mship = Membership.find_by_group_id_and_access_level(group.id, 'admin')
-    	group.creator_id = mship.user_id
-    	group.save!
+      if mship = Membership.find_by_group_id_and_access_level(group.id, 'admin')
+        group.creator_id = mship.user_id
+        group.save!
+      elsif first_user = group.users.first
+        group.creator_id = first_user.id
+        group.save!
+      else
+        group.destroy
+      end
     end
     change_column :groups, :creator_id, :int, :null => false
   end
