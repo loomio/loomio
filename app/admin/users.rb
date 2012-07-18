95047a998f0f626d11ad647b1ef0e52cb3ac9eb8
Jon Lemmon <jonlemmon@gmail.com>
2012-07-18 18:44:09 +1200

    Admin: update users index.

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register User do
-  
+  index do
+    column :name
+    column :email
+    column "Invitation Link" do |user|
+      if user.invitation_token
+        link_to "Invite link (#{user.invitation_token})",
+          accept_invitation_url(user, :invitation_token => user.invitation_token)
+      end
+    end
+    column :created_at
+    column :last_sign_in_at
+    column :is_admin
+  end
 end
