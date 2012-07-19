c50057df986a1c4139628c4e42f106eb0896ccd0
Jon Lemmon <jonlemmon@gmail.com>
2012-07-19 12:43:46 +1200

    Admin: improve edit actions.

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register User do
+  actions :index, :edit, :update
+
   index do
     column :name
     column :email
@@ -11,5 +13,22 @@ ActiveAdmin.register User do
     column :created_at
     column :last_sign_in_at
     column :is_admin
+    default_actions
+  end
+
+  form do |f|
+    f.inputs "Details" do
+      f.input :name, :input_html => { :disabled => true }
+      f.input :email, :input_html => { :disabled => true }
+      f.input :is_admin
+    end
+    f.buttons
+  end
+
+  member_action :update, :method => :put do
+    user = User.find(params[:id])
+    user.is_admin = params[:user][:is_admin]
+    user.save
+    redirect_to admin_users_url, :notice => "User updated"
   end
 end
