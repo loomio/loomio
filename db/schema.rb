83563ade8a16ee6d66c4db5755055ed3fef6fcc4
Andrew Cobb <cobb.andrew@gmail.com>
2012-07-30 14:55:43 -0500

    Update schema file.

--- db/schema.rb
+++ db/schema.rb
@@ -137,30 +137,39 @@ ActiveRecord::Schema.define(:version => 20120718141211) do
   add_index "motions", ["discussion_id"], :name => "index_motions_on_discussion_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                :default => "",    :null => false
-    t.string   "encrypted_password",                   :default => ""
+    t.string   "email",                                      :default => "",    :null => false
+    t.string   "encrypted_password",                         :default => ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                        :default => 0
+    t.integer  "sign_in_count",                              :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "admin",                                :default => false
+    t.boolean  "admin",                                      :default => false
     t.string   "name"
     t.string   "unconfirmed_email"
-    t.string   "invitation_token",       :limit => 60
+    t.string   "invitation_token",             :limit => 60
     t.datetime "invitation_sent_at"
     t.datetime "invitation_accepted_at"
     t.integer  "invitation_limit"
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.datetime "deleted_at"
-    t.boolean  "has_read_system_notice",               :default => false, :null => false
-    t.boolean  "is_admin",                             :default => false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
+    t.string   "avatar_kind"
+    t.string   "uploaded_avatar_file_name"
+    t.string   "uploaded_avatar_content_type"
+    t.integer  "uploaded_avatar_file_size"
+    t.datetime "uploaded_avatar_updated_at"
+    t.boolean  "has_read_system_notice",                     :default => false, :null => false
+    t.boolean  "is_admin",                                   :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
