5686e1d20ca76517aa4e07e1978b5c07f5eed60f
Andrew Cobb <cobb.andrew@gmail.com>
2012-07-31 09:00:16 -0500

    Refactor mailer to use shared context.

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,33 +1,36 @@
 require "spec_helper"
 
 describe UserMailer do
+  shared_examples_for 'email_meta' do
+    it 'renders the receiver email' do
+      @mail.to.should == [@user.email]
+    end
+    
+    it 'assigns correct reply_to' do
+      @mail.reply_to.should == [@group.admin_email]
+    end
+    
+    it 'renders the sender email' do
+      @mail.from.should == ['noreply@loom.io']
+    end
+    
+    it 'assigns the @name variable which appears in the email body' do
+      @mail.body.encoded.should match(@group.full_name)
+    end
+  end
   context 'sending email on membership approval' do
     before :all do
       @user = User.make!
       @group = Group.make!
       @mail = UserMailer.group_membership_approved(@user, @group)
     end
-
+    
+    it_behaves_like 'email_meta'
+    
     it 'renders the subject' do
       @mail.subject.should == "[Loomio: #{@group.full_name}] Membership approved"
     end
-
-    it 'renders the receiver email' do
-      @mail.to.should == [@user.email]
-    end
-
-    it 'assigns correct reply_to' do
-      @mail.reply_to.should == [@group.admin_email]
-    end
-
-    it 'renders the sender email' do
-      @mail.from.should == ['noreply@loom.io']
-    end
-
-    it 'assigns the @name variable which appears in the email body' do
-      @mail.body.encoded.should match(@group.full_name)
-    end
-
+    
     it 'assigns confirmation_url for email body' do
       @mail.body.encoded.should match("http://localhost:3000/groups/#{@group.id}")
     end
@@ -39,24 +42,11 @@ describe UserMailer do
       @group = Group.make!
       @mail = UserMailer.added_to_group(@user, @group)
     end
+    
+    it_behaves_like 'email_meta'
 
     it 'renders the subject' do
       @mail.subject.should match(/been added to a group/)
-    end
-    it 'renders the receiver email' do
-      @mail.to.should == [@user.email]
-    end
-    
-    it 'assigns reply to' do
-      @mail.reply_to.should == [@group.admin_email]
-    end
-    
-    it 'renders the sender email' do
-      @mail.from.should == ['noreply@loom.io']
-    end
-
-    it 'assigns the groups name which appears in the email body' do
-      @mail.body.encoded.should match(@group.full_name)
     end
 
     it 'assigns confirmation_url for email body' do
@@ -71,26 +61,12 @@ describe UserMailer do
       @user = User.invite_and_notify!({email: "test@example.com"}, @inviter, @group)
       @mail = UserMailer.invited_to_loomio(@user, @inviter, @group)
     end
+    
+    it_behaves_like 'email_meta'
 
     it 'renders the subject' do
       @mail.subject.should match(
         /#{@inviter.name} has invited you to #{@group.full_name} on Loomio/)
-    end
-
-    it 'renders the receiver email' do
-      @mail.to.should == [@user.email]
-    end
-
-    it 'renders the sender email' do
-      @mail.from.should == ['noreply@loom.io']
-    end
-    
-    it 'assigns correct reply_to' do
-      @mail.reply_to.should == [@group.admin_email]
-    end
-
-    it 'assigns the groups name which appears in the email body' do
-      @mail.body.encoded.should match(@group.full_name)
     end
 
     it 'assigns inviters name which appears in the email body' do
