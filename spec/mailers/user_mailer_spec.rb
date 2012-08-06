2f48ef17a6a2f810b424fff71f377311e72f5168
Aaron Thornton <aaronthornton00@gmail.com>
2012-08-06 18:01:45 +1200

    fixed failing mailer specs

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -5,17 +5,13 @@ describe UserMailer do
     it 'renders the receiver email' do
       @mail.to.should == [@user.email]
     end
-    
+
     it 'assigns correct reply_to' do
       @mail.reply_to.should == [@group.admin_email]
     end
-    
+
     it 'renders the sender email' do
       @mail.from.should == ['noreply@loom.io']
-    end
-    
-    it 'assigns the @name variable which appears in the email body' do
-      @mail.body.encoded.should match(@group.full_name)
     end
   end
   context 'sending email on membership approval' do
@@ -24,13 +20,13 @@ describe UserMailer do
       @group = Group.make!
       @mail = UserMailer.group_membership_approved(@user, @group)
     end
-    
+
     it_behaves_like 'email_meta'
-    
+
     it 'renders the subject' do
       @mail.subject.should == "[Loomio: #{@group.full_name}] Membership approved"
     end
-    
+
     it 'assigns confirmation_url for email body' do
       @mail.body.encoded.should match("http://localhost:3000/groups/#{@group.id}")
     end
@@ -42,7 +38,7 @@ describe UserMailer do
       @group = Group.make!
       @mail = UserMailer.added_to_group(@user, @group)
     end
-    
+
     it_behaves_like 'email_meta'
 
     it 'renders the subject' do
@@ -61,7 +57,7 @@ describe UserMailer do
       @user = User.invite_and_notify!({email: "test@example.com"}, @inviter, @group)
       @mail = UserMailer.invited_to_loomio(@user, @inviter, @group)
     end
-    
+
     it_behaves_like 'email_meta'
 
     it 'renders the subject' do
