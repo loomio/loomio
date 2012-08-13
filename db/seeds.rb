b57838d3a682915cfec48cedc6cd990ddf1a9eb1
Jon Lemmon <jonlemmon@gmail.com>
2012-08-13 15:27:58 +1200

    Improve wording language

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
 contact_user = User.find_or_initialize_by_email("contact@loom.io")
-contact_user.name = "Loomio helper bot"
+contact_user.name = "Loomio Helper Bot"
 if contact_user.new_record?
   contact_user.password = "password"
 end
-contact_user.save
+contact_user.save!
