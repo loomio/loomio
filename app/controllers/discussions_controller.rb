76779b34ffd5fa7e40686e6ffb3d23faa68ac811
Jon Lemmon <jonlemmon@gmail.com>
2012-08-13 15:34:05 +1200

    Bugfix.

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -53,7 +53,7 @@ class DiscussionsController < GroupBaseController
       @user_already_voted = @current_motion.user_has_voted?(current_user)
     else
       @selected_motion = @discussion.closed_motion(params[:proposal])
-      @user_already_voted = @current_motion.user_has_voted?(current_user)
+      @user_already_voted = @selected_motion.user_has_voted?(current_user)
       if @selected_motion
         @votes_for_graph = @selected_motion.votes_graph_ready
       end
