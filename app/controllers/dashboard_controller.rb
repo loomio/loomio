0486f5bb7a1b38a2f65fd5c43afa6984c50fa5fa
Aaron Thornton <aaronthornton00@gmail.com>
2012-08-23 18:09:26 +1200

    groups dropdown added to modal for dash

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,7 @@ class DashboardController < BaseController
   def show
     @discussions_with_current_motion_voted_on = current_user.discussions_with_current_motion_voted_on
     @discussions_with_current_motion_not_voted_on = current_user.discussions_with_current_motion_not_voted_on
+    @discussion = Discussion.new
     @groups = GroupDecorator.decorate(current_user.all_root_groups)
   end
 end
