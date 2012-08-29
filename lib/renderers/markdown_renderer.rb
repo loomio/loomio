8e4273766a0390c5ed8ddf5680b7253f688ba3f1
Jon Lemmon <jonlemmon@gmail.com>
2012-08-29 17:03:34 +1200

    Codeblock fix and formatting
    Conflicts:
    
    	app/views/comments/_comment.html.haml
    	app/views/discussions/_history.html.haml

--- lib/renderers/markdown_renderer.rb
+++ lib/renderers/markdown_renderer.rb
@@ -3,15 +3,15 @@ require 'open-uri'
 class MarkdownRenderer < Redcarpet::Render::HTML
 	def link(link, title, alt_text)
 	  safelink = URI.escape(link)
-	  "<a target=\"_blank\" href=\"#{safelink}\">#{alt_text}</a>"
+"<a target=\"_blank\" href=\"#{safelink}\">#{alt_text}</a>"
 	end
 
 	def autolink(link, link_type)
 	  safelink = URI.escape(link)
 	  if link_type == :email
-	  	"<a target=\"_blank\" href=\"mailto:#{link}\">#{link}</a>"
+"<a target=\"_blank\" href=\"mailto:#{link}\">#{link}</a>"
 	  else
-	  	"<a target=\"_blank\" href=\"#{safelink}\">#{link}</a>"
+"<a target=\"_blank\" href=\"#{safelink}\">#{link}</a>"
 	  end
 	end
 end
