4f1a5b303470459803d4c2ae6f0c7b8b763dcdfa
James Watling <watling.james@gmail.com>
2012-09-11 20:04:23 +1200

    Added Create Proposal Test Outline and removing uneeded file from last commit

---
+++ features/step_definitions/create_proposal.rb
@@ -0,0 +1,27 @@
+When /^I click create proposal$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^fill in the proposal details$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^a new proposal is created$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^group admin$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I am on a group page$/ do
+
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^on group page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+
+
