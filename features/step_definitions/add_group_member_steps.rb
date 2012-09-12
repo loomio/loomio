b401e4e2cfa567ce321c8d6d71a5a736598806c5
Tiger <tigerwnz@gmail.com>
2012-09-13 10:26:06 +1200

    Expanded the add group member spec to ensure you cannot add the same member twice

--- features/step_definitions/add_group_member_steps.rb
+++ features/step_definitions/add_group_member_steps.rb
@@ -1,4 +1,5 @@
 When /^I complete an invitation$/ do
+  visit '/groups/' + Group.last.id.to_s
   fill_in 'user_email', with: 'new_group_member@example.com'
   click_on 'invite'
 end
@@ -13,4 +14,14 @@ end
 
 When /^no such user is already in the group$/ do
   Membership.where(:group_id => Group.last.id, :user_id => User.last.id).size == 0
+end
+
+Given /^there is a user in the group$/ do
+  visit '/groups/' + Group.last.id.to_s
+  fill_in 'user_email', with: 'new_group_member@example.com'
+  click_on 'invite'
+end
+
+Then /^a member is not added to the group$/ do
+  page.should have_content('new_group_member@example.com is already in the group.')
 end
