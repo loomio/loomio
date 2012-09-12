eb855d53261aff7fcdfaf00953d5bac6da4a4e72
James McCann <jmccnz@gmail.com>
2012-09-12 15:58:32 +1200

    Fixed some display logic, correctly set to action, can now save outcomes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -71,8 +71,8 @@ class Ability
     #
     can :create, Motion, :discussion_id => user.discussion_ids
 
-    can [:destroy, :close_voting, :open_voting], Motion, :author_id => user.id
-    can [:destroy, :close_voting, :open_voting], Motion,
+    can [:destroy, :close_voting, :open_voting, :edit_outcome], Motion, :author_id => user.id
+    can [:destroy, :close_voting, :open_voting, :edit_outcome], Motion,
       :discussion => { :group_id => user.adminable_group_ids }
   end
 end
