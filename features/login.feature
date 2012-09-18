a76c36c7784ca1d2fedb86eb4d1ae79122ba9d84
Jon Lemmon <jonlemmon@gmail.com>
2012-09-18 17:37:25 +1200

    Fix login cukes.

--- features/login.feature
+++ features/login.feature
@@ -1,23 +1,17 @@
 Feature: Login
-In order to use the site
-They must be able to login
+  In order to use the site
+  Users must be able to login
 
-Scenario: Login
-Given I am a registered User
-When enter my login details
-Then I should be logged in
+  Scenario: Login
+    Given "hello@world.com" is a user
+    When I login as "hello@world.com"
+    Then I should be logged in
 
-Scenario: Not A Registered User
-Given I am not a registered User
-When I enter my incorrect login details
-Then I should not be logged in
+  Scenario: Not A Registered User
+    When I login as "hello@world.com"
+    Then I should not be logged in
 
-Scenario: Invalid Email
-Given I am a registered User
-When I enter my email incorrectly
-Then I should not be logged in
-
-Scenario: Invalid Password
-Given I am a registered User
-When I enter my password incorrectly
-Then I should not be logged in
+  Scenario: Invalid Password
+    Given "hello@world.com" is a user
+    When I login as "hello@world.com" with an incorrect password
+    Then I should not be logged in
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -2,47 +2,26 @@ Given /^I am logged in as "(.*?)"$/ do |arg1|
   login(arg1,'password')
 end
 
-When /^enter my login details$/ do
+When /^I login as "(.*?)"$/ do |email|
   visit "/"
   page.should have_content "sign in"
-  fill_in 'user_email', with: "furry@example.com"
+  fill_in 'user_email', with: email
   fill_in 'user_password', with: "password"
   click_on "Sign in"
 end
 
 Then /^I should be logged in$/ do
-  page.should have_content "This page is the Dashboard"
+  page.should have_css "body.dashboard.show"
 end
-
-Given /^I am not a registered User$/ do
-  User.where(:email=>"fake@email.com").size == 0
-end
-
-When /^I enter my incorrect login details$/ do
-  visit "/"
-  page.should have_content "sign in"
-  fill_in 'user_email', with: "fake@email.com"
-  fill_in 'user_password', with: "password"
-  click_on "Sign in"
-end
-
 
 Then /^I should not be logged in$/ do
   page.should have_content "Invalid email or password"
 end
 
-When /^I enter my email incorrectly$/ do
+When /^I login as "(.*?)" with an incorrect password$/ do |email|
   visit "/"
   page.should have_content "sign in"
-  fill_in 'user_email', with: "furry@oops_a_mistake.com"
-  fill_in 'user_password', with: "password"
-  click_on "Sign in"
-end
-
-When /^I enter my password incorrectly$/ do
-  visit "/"
-  page.should have_content "sign in"
-  fill_in 'user_email', with: "furry@example.com"
+  fill_in 'user_email', with: email
   fill_in 'user_password', with: "wrong_password"
   click_on "Sign in"
 end
