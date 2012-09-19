aae0b889ddf0bab321816230d33dc0f98611e255
Aaron Thornton <aaronthornton00@gmail.com>
2012-09-19 17:16:09 +1200

    rdm

---
+++ db/migrate/20120919050300_add_last_viewed_at_to_membership.rb
@@ -0,0 +1,11 @@
+class AddLastViewedAtToMembership < ActiveRecord::Migration
+  def up
+    unless column_exists? :memberships, :last_viewed_at
+      add_column :memberships, :last_viewed_at, :datetime, :default => Time.now()
+    end
+  end
+
+  def down
+    remove_column :memberships
+  end
+end
