370719ba8cd9c535ff2704bf9c1b7c45f0cbd6e9
Jon Lemmon <jonlemmon@gmail.com>
2012-09-27 20:19:22 +1200

    Rip out invite model.

--- app/models/invite.rb
+++
@@ -1,4 +0,0 @@
-class Invite < ActiveRecord::Base
-  belongs_to :inviter
-  attr_accessible :recipient_email, :token
-end
--- spec/factories/invites.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :invite do
-    token "MyString"
-    inviter nil
-    recipient_email "MyString"
-  end
-end
