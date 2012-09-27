e17af16898566995d8c97452b83ea18572cd86b9
Jon Lemmon <jonlemmon@gmail.com>
2012-09-27 12:46:43 +1200

    More changes.

--- app/models/group_request.rb
+++ app/models/group_request.rb
@@ -1,3 +1,12 @@
 class GroupRequest < ActiveRecord::Base
   attr_accessible :admin_email, :description, :expected_size, :name
+
+  def approve!
+    @group = Group.new(:name => name)
+    @group.creator = User.create!(:email => admin_email,
+                                  :name => admin_email,
+                                  :password => SecureRandom.hex(16))
+    @group.save!
+    GroupMailer.new_group_invited_to_loomio
+  end
 end
--- spec/models/group_request_spec.rb
+++ spec/models/group_request_spec.rb
@@ -2,7 +2,29 @@ require 'spec_helper'
 
 describe GroupRequest do
   describe "#approve!" do
-    it "creates a group with the group_request's attributes"
-    it "sends an instructional email to the admin of the group"
+    before do
+      @group = mock_model(Group)
+      @group.stub(:creator=)
+      @group.stub(:save!)
+      Group.stub(:new => @group)
+      GroupMailer.stub(:new_group_invited_to_loomio)
+      @group_request = create :group_request
+    end
+
+    it "creates a group with the group_request's attributes" do
+      Group.should_receive(:new).with(
+        :name => @group_request.name,
+      ).and_return(@group)
+      @group.should_receive(:creator=).with(@group_request.admin_email)
+      @group.should_receive(:save)
+      @group_request.approve!
+    end
+
+    it "sends an instructional email (with invite token) to the admin of the group" do
+      GroupMailer.should_receive(:new_group_invited_to_loomio).
+        with(@group_request.admin_email, @group_request.name)
+        and_return(double("emailer").should_receive(:deliver))
+      @group_request.approve!
+    end
   end
 end
