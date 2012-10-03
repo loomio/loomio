0d0aa0550a664e68b89f8ff6628f62de03829a26
Aaron Thornton <aaronthornton00@gmail.com>
2012-10-03 17:26:28 +1300

    Remove time.now from migration

--- db/migrate/20121003033500_make_group_last_viewed_at_in_membership_non_nullable.rb
+++ db/migrate/20121003033500_make_group_last_viewed_at_in_membership_non_nullable.rb
@@ -1,8 +1,5 @@
 class MakeGroupLastViewedAtInMembershipNonNullable < ActiveRecord::Migration
   def up
-  	default_date = Time.now()
-    Membership.update_all ["group_last_viewed_at = ?", default_date], ["group_last_viewed_at is NULL"]
-
     change_column :memberships, :group_last_viewed_at, :datetime, :default => default_date, :null => false
   end
 
