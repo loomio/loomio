117489718d89759bfd7088d51214d70baea6f3a5
Aaron Thornton <aaronthornton00@gmail.com>
2012-10-03 17:56:02 +1300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121003033500) do
+ActiveRecord::Schema.define(:version => 20120930022453) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
