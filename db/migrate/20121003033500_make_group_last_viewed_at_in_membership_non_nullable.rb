7b7c4c29634da41d4028f823c0725cc72729baea
Aaron Thornton <aaronthornton00@gmail.com>
2012-10-03 18:01:02 +1300

    default removed

--- db/migrate/20121003033500_make_group_last_viewed_at_in_membership_non_nullable.rb
+++ db/migrate/20121003033500_make_group_last_viewed_at_in_membership_non_nullable.rb
@@ -1,6 +1,7 @@
 class MakeGroupLastViewedAtInMembershipNonNullable < ActiveRecord::Migration
   def up
-    change_column :memberships, :group_last_viewed_at, :datetime, :default => default_date, :null => false
+    change_column :memberships, :group_last_viewed_at, :datetime, :null => false
+    change_column_default :memberships, :group_last_viewed_at, nil
   end
 
   def down
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120930022453) do
+ActiveRecord::Schema.define(:version => 20121003033500) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -154,7 +154,7 @@ ActiveRecord::Schema.define(:version => 20120930022453) do
     t.datetime "updated_at"
     t.string   "access_level"
     t.integer  "inviter_id"
-    t.datetime "group_last_viewed_at", :default => '2012-10-03 04:12:44', :null => false
+    t.datetime "group_last_viewed_at", :null => false
   end
 
   add_index "memberships", ["group_id"], :name => "index_memberships_on_group_id"
