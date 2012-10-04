5a1e03c75b8ff43e538de16f678ee5ce1ca2f35b
Aaron Thornton <aaronthornton00@gmail.com>
2012-10-04 13:00:57 +1300

    Migration to remove NULL

--- db/migrate/20121003033500_make_group_last_viewed_at_in_membership_non_nullable.rb
+++ db/migrate/20121003033500_make_group_last_viewed_at_in_membership_non_nullable.rb
@@ -1,9 +1,11 @@
 class MakeGroupLastViewedAtInMembershipNonNullable < ActiveRecord::Migration
   def up
+    Membership.where(:group_last_viewed_at => nil).update_all :group_last_viewed_at => Time.now
     change_column :memberships, :group_last_viewed_at, :datetime, :null => false
     change_column_default :memberships, :group_last_viewed_at, nil
   end
 
   def down
+    change_column :memberships, :group_last_viewed_at, :datetime, :null => true
   end
 end
