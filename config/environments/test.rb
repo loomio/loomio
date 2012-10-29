3a04bf54faaff80c1c8e9af8a9f28a0df297a649
Jon Lemmon <jonlemmon@gmail.com>
2012-10-29 17:10:20 +1300

    Remove unnecessary code.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,7 +27,7 @@ Loomio::Application.configure do
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :test
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
