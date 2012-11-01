604d4f8d27116b5773808654f476f91a5d4add4e
Aaron Thornton <aaronthornton00@gmail.com>
2012-11-01 20:10:33 +1300

    failing helper spec

--- spec/helpers/discussions_helper_spec.rb
+++ spec/helpers/discussions_helper_spec.rb
@@ -1,13 +1,13 @@
-require 'spec_helper'
+ require 'spec_helper'
 
 describe DiscussionsHelper do
   describe "discussion_activity_count_for(discussion, user)" do
     before do
       @discussion = create :discussion
     end
-    it "returns 0 if no user is logged in" do
-      @discussion.stub(:number_of_comments_since_last_looked).and_return(nil)
-      discussion_activity_count_for(@discussion, @user).should == 0
+    it "returns total number of comments if no user is logged in" do
+      @discussion.stub(:number_of_comments_since_last_looked).and_return(4)
+      discussion_activity_count_for(@discussion, @user).should == 4
     end
     it "returns the number of comments since user last looked at the discussion if user is logged in" do
       @user = create :user
