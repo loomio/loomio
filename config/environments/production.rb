1f5401e65e5f413c491c026fbbeec8870bf102e8
Jon Lemmon <jonlemmon@gmail.com>
2012-11-03 12:59:53 +1300

    Send emails to file on staging.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,6 @@ Loomio::Application.configure do
   #config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = {
     :host => 'loom.io',
