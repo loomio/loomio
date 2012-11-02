ae179aa018d8d2530cfcd8ce3275dcaac6365089
Nicolas Wormser <nicolas.wormser@gmail.com>
2012-11-02 10:28:45 +0100

    fixing merge issues with schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-ActiveRecord::Schema.define(:version => 20121030014815) do
-=======
-<<<<<<< HEAD
-ActiveRecord::Schema.define(:version => 20121008010612) do
-=======
 ActiveRecord::Schema.define(:version => 20121010214955) do
->>>>>>> added paper_trail
->>>>>>> added paper_trail
-=======
-ActiveRecord::Schema.define(:version => 20121010214955) do
->>>>>>> fixed failing spec
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -97,21 +85,9 @@ ActiveRecord::Schema.define(:version => 20121010214955) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "title"
-<<<<<<< HEAD
-<<<<<<< HEAD
-    t.integer  "activity",           :default => 0,     :null => false
-=======
-<<<<<<< HEAD
-=======
     t.integer  "activity",        :default => 0, :null => false
->>>>>>> added paper_trail
->>>>>>> added paper_trail
-=======
-    t.integer  "activity",        :default => 0, :null => false
->>>>>>> model specs for the descripition history
     t.datetime "last_comment_at"
     t.text     "description"
-    t.boolean  "has_current_motion", :default => false
   end
 
   add_index "discussions", ["author_id"], :name => "index_discussions_on_author_id"
@@ -131,6 +107,7 @@ ActiveRecord::Schema.define(:version => 20121010214955) do
 
   create_table "group_requests", :force => true do |t|
     t.string   "name"
+    t.integer  "expected_size"
     t.text     "description"
     t.string   "admin_email"
     t.datetime "created_at",                           :null => false
@@ -138,10 +115,6 @@ ActiveRecord::Schema.define(:version => 20121010214955) do
     t.string   "status"
     t.integer  "group_id"
     t.boolean  "cannot_contribute", :default => false
-<<<<<<< HEAD
-    t.string   "expected_size"
-=======
->>>>>>> model specs for the descripition history
   end
 
   add_index "group_requests", ["group_id"], :name => "index_group_requests_on_group_id"
@@ -160,7 +133,6 @@ ActiveRecord::Schema.define(:version => 20121010214955) do
     t.integer  "creator_id",                              :null => false
     t.integer  "memberships_count",    :default => 0,     :null => false
     t.datetime "archived_at"
-    t.integer  "max_size"
   end
 
   add_index "groups", ["parent_id"], :name => "index_groups_on_parent_id"
@@ -172,7 +144,6 @@ ActiveRecord::Schema.define(:version => 20121010214955) do
     t.integer  "group_id"
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
-    t.string   "token",           :null => false
   end
 
   create_table "memberships", :force => true do |t|
@@ -182,8 +153,7 @@ ActiveRecord::Schema.define(:version => 20121010214955) do
     t.datetime "updated_at"
     t.string   "access_level"
     t.integer  "inviter_id"
-    t.datetime "group_last_viewed_at",                :null => false
-    t.integer  "noise_level",          :default => 1, :null => false
+    t.datetime "group_last_viewed_at", :null => false
   end
 
   add_index "memberships", ["group_id"], :name => "index_memberships_on_group_id"
@@ -256,11 +226,10 @@ ActiveRecord::Schema.define(:version => 20121010214955) do
     t.string   "uploaded_avatar_content_type"
     t.integer  "uploaded_avatar_file_size"
     t.datetime "uploaded_avatar_updated_at"
+    t.string   "avatar_initials"
     t.boolean  "has_read_dashboard_notice",                  :default => false, :null => false
     t.boolean  "has_read_group_notice",                      :default => false, :null => false
     t.boolean  "has_read_discussion_notice",                 :default => false, :null => false
-    t.string   "avatar_initials"
-    t.boolean  "receive_emails",                             :default => true,  :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
