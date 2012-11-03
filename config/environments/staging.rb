1455ee721825f81740f698ca7c8f854bfca7ae1a
Jon Lemmon <jonlemmon@gmail.com>
2012-11-03 13:12:35 +1300

    Use sendgrid on staging for now.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -57,21 +57,21 @@ Loomio::Application.configure do
   config.active_support.deprecation = :notify
 
   # Send emails to a file
-  config.action_mailer.delivery_method = :file
-  config.action_mailer.file_settings = {
-    :location => Rails.root.join('tmp/mail')
-  }
+  # config.action_mailer.delivery_method = :file
+  # config.action_mailer.file_settings = {
+  #   :location => Rails.root.join('tmp/mail')
+  # }
 
   # Send emails using SendGrid
-  # ActionMailer::Base.delivery_method = :smtp
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => 'heroku.com'
-  # }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {
