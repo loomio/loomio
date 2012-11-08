bcf8e12a734ac757c41cb43202ec49f44f992638
Jon Lemmon <jonlemmon@gmail.com>
2012-11-08 18:08:55 +1300

    Fix markdown renderer on empty links

--- lib/renderers/markdown_renderer.rb
+++ lib/renderers/markdown_renderer.rb
@@ -2,11 +2,13 @@ require 'open-uri'
 
 class MarkdownRenderer < Redcarpet::Render::HTML
   def link(link, title, alt_text)
+    link ||= "#"
     safelink = URI.escape(link).gsub(/%23/, '#')
 "<a target=\"_blank\" href=\"#{safelink}\">#{alt_text}</a>"
   end
 
   def autolink(link, link_type)
+    link ||= "#"
     safelink = URI.escape(link).gsub(/%23/, '#')
     if link_type == :email
 "<a target=\"_blank\" href=\"mailto:#{link}\">#{link}</a>"
