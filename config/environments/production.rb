66140cf9e284e1a0f13c77da1871252be88da9ec
Jon Lemmon <jonlemmon@gmail.com>
2012-11-11 19:03:21 +1300

    Update mailer url to include www.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,7 +74,7 @@ Loomio::Application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = {
-    :host => 'loomio.org',
+    :host => 'www.loomio.org',
   }
 
   # Store avatars on Amazon S3
