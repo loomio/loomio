3992ffa77b1458b762a03762a0acec8a0b2288b3
Richard D. Bartlett <richarddbartlett@gmail.com>
2012-11-23 03:41:11 +0000

    Work on group requests.

--- app/controllers/group_requests_controller.rb
+++ app/controllers/group_requests_controller.rb
@@ -6,10 +6,14 @@ class GroupRequestsController < BaseController
   end
 
   def create
-    if params[:group_request][:robot_trap].blank?
-      GroupRequest.create!(params[:group_request])
+    if params[:robot_trap].blank?
+      @group_request = GroupRequest.new(params[:group_request])
+      if @group_request.save
+        redirect_to group_request_confirmation_url
+      else
+        render :action => "new"
+      end
     end
-    redirect_to group_request_confirmation_url
   end
 
   def confirmation
