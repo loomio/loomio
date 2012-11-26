b30498d98b5a8799655ef034f38af5205d9ccf2a
Jon Lemmon <jonlemmon@gmail.com>
2012-11-26 18:03:43 +1300

    Fix version history bug.

--- spec/requests/discussions_spec.rb
+++ spec/requests/discussions_spec.rb
@@ -245,7 +245,6 @@ describe "Discussion" do
           assert_modal_flushed
           find("#description-revision-history").find(".revision-title p").should have_content("Edited about #{time_ago_in_words(@discussion.last_versioned_at)} ago by #{@user2.name}")
         end
-        
       end
 
     end
