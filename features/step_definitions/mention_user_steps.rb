4d353f51aca2302b19246846123d5f0ba580cc79
Jon Lemmon <jonlemmon@gmail.com>
2012-11-27 09:39:19 +1300

    Improve at-mentions integration test.

--- features/step_definitions/mention_user_steps.rb
+++ features/step_definitions/mention_user_steps.rb
@@ -3,10 +3,11 @@ When /^I am adding a comment and type in "(.*?)"$/ do |arg1|
 end
 
 When /^I click on "(.*?)" in the menu that pops up$/ do |arg1|
+  wait_until { find("#at-view", visible: true) }
   within('#at-view') do
     page.should have_content(arg1)
     page.find(:css, "li", :text => arg1).click
-  end  
+  end
 end
 
 Then /^I should see "(.*?)" added to the "(.*?)" field$/ do |text, field|
@@ -21,4 +22,4 @@ end
 Then /^I should see a link to "(.*?)"'s user$/ do |user|
   visit(current_path)
   page.should have_link("@#{user}")
-end 
+end
