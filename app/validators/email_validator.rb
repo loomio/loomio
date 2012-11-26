cd521f94e7bf05d3d9e8cbcc2cf8ad55175160d3
Jon Lemmon <jonlemmon@gmail.com>
2012-11-26 17:47:35 +1300

    Fix failing specs.

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,4 +1,4 @@
-class EmailValidator < ActiveModel::EachValidator  
+class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attr_name, value)
     unless value =~ Devise.email_regexp
       record.errors.add(attr_name, :email, options.merge(:value => value))
