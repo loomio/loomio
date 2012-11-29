9c1022181e89ea0825a8e7ae93b4bf0d55a336fa
Aaron Thornton <aaronthornton00@gmail.com>
2012-11-29 21:33:51 +1300

    fix failing cuc spec for edit discussion description

--- features/step_definitions/edit_motion_close_date_steps.rb
+++ features/step_definitions/edit_motion_close_date_steps.rb
@@ -13,7 +13,7 @@ Given /^the proposal is currently open$/ do
 end
 
 When /^I click the 'change close date' button$/ do
-  click_on 'change_close_date'
+  click_on 'change-close-date'
 end
 
 When /^I select the new close date$/ do
@@ -29,6 +29,5 @@ Then /^The proposal close date should be updated$/ do
 end
 
 Then /^I should not see a link to edit the close date$/ do
-  pending
-  page.should_not have_css("change_close_date")
+  page.should_not have_css("change-close-date")
 end
