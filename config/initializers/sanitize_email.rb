0f3d3c5f0e0a5a6b99c24f4e94f566f9da2f660d
Jon Lemmon <jonlemmon@gmail.com>
2012-12-11 15:40:03 +1300

    Start working on this shit.

--- config/initializers/sanitize_email.rb
+++ config/initializers/sanitize_email.rb
@@ -1,6 +1,6 @@
 SanitizeEmail::Config.configure do |config|
   config[:sanitized_to] = 'jon@loomio.org'
-  # sanitize emails from development and test, or set whatever logic should turn sanitize_email on and off here:
+  # sanitize emails from staging, or set whatever logic should turn sanitize_email on and off here:
   config[:activation_proc] = Proc.new { %w(staging).include?(Rails.env) }
   config[:use_actual_email_prepended_to_subject] = true   # or false
   config[:use_actual_email_as_sanitized_user_name] = true # or false
