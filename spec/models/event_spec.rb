aeeb22e4c2f03c81de85b119af1d52fc61b98f3a
Aaron Thornton <aaronthornton00@gmail.com>
2012-12-18 11:12:08 +1300

    fix close_motion! event spec

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -182,32 +182,38 @@ describe Event do
   end
 
   describe "motion_closed!" do
-    let(:user) { stub(:user, :email => 'bill@dave.com') }
+    let(:user) { stub(:user) }
+    let(:closer) { stub(:user, :email => 'bill@dave.com') }
     let(:motion) { stub(:motion, :group => group) }
 
     before do
       motion.stub(:discussion).and_return(discussion)
-      motion.stub(:author).and_return(user)
+      motion.stub(:author).and_return(closer)
+      motion.stub(:group_users).and_return([user])
       MotionMailer.stub_chain(:motion_closed, :deliver)
     end
 
     after do
-      Event.motion_closed!(motion, user)
+      Event.motion_closed!(motion, closer)
     end
 
-    it 'emails group_users_without_motion_author motion_closed' do
-      MotionMailer.should_receive(:motion_closed).with(motion, user.email).and_return(mailer)
+    it 'emails group_users motion_closed' do
+      MotionMailer.should_receive(:motion_closed).with(motion, closer.email).and_return(mailer)
     end
 
     it 'creates a motion_closed event' do
       Event.should_receive(:create!).with(kind: 'motion_closed',
                                           eventable: motion,
-                                          user: user,
+                                          user: closer,
                                           discussion_id: discussion.id).and_return(event)
     end
 
     it 'notifies other group members' do
       event.should_receive(:notify!).with(user)
+    end
+
+    it 'does not notify other the closer' do
+      event.should_not_receive(:notify!).with(closer)
     end
   end
 
