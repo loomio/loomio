d48003f968ac58d80582aa44fbc3aed3f878a4af
Jon Lemmon <jonlemmon@gmail.com>
2013-01-06 17:49:03 +1300

    Turn Group#all_discussions into a query object.

---
+++ app/queries/discussions_query.rb
@@ -0,0 +1,22 @@
+require 'delegate'
+
+class DiscussionsQuery < SimpleDelegator
+  def initialize(user, group)
+    if user
+      relation = Discussion.includes(:group => :memberships)
+        .where("(discussions.group_id = ?
+          OR (groups.parent_id = ? AND groups.archived_at IS NULL
+            AND (groups.viewable_by = 'everyone'
+              OR (groups.viewable_by = 'members' AND memberships.user_id = ?)
+
+              )
+            )
+          )", group.id, group.id, user.id)
+    else
+      relation = Discussion.includes(:group)
+        .where("(discussions.group_id = ? OR (groups.parent_id = ? AND groups.archived_at IS NULL
+          AND groups.viewable_by = 'everyone'))", group.id, group.id)
+    end
+    super(relation)
+  end
+end
