e52a283a57f9a94d6894717d49f1bab07eec9c70
Aaron Thornton <aaronthornton00@gmail.com>
2013-01-08 16:17:48 +1300

    migration to new actimity format

--- db/migrate/20130107033705_fill_in_discussion_id_for_each_event_activity_item.rb
+++ db/migrate/20130107033705_fill_in_discussion_id_for_each_event_activity_item.rb
@@ -1,5 +1,14 @@
 class FillInDiscussionIdForEachEventActivityItem < ActiveRecord::Migration
   class Event < ActiveRecord::Base
+    KINDS = %w[new_discussion discussion_title_edited discussion_description_edited new_comment
+           new_motion new_vote motion_blocked motion_close_date_edited
+           motion_closing_soon motion_closed membership_requested
+           user_added_to_group comment_liked user_mentioned]
+    belongs_to :eventable, :polymorphic => true
+    belongs_to :user
+    validates_inclusion_of :kind, :in => KINDS
+    validates_presence_of :eventable
+    attr_accessible :kind, :eventable, :user, :discussion_id
   end
 
   def up
