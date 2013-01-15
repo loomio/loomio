11bab8b4d2180543f99439fdd31d710cdd3bd982
john irving <whimful@gmail.com>
2013-01-15 14:42:16 +1300

    add cucumber tests to comment markdown settings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130108004633) do
+ActiveRecord::Schema.define(:version => 20130114223848) do
+
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
     t.string   "resource_type", :null => false
@@ -103,7 +104,7 @@ ActiveRecord::Schema.define(:version => 20130108004633) do
     t.string   "title"
     t.datetime "last_comment_at"
     t.text     "description"
-    t.boolean  "uses_markdown",   :default => false, :null => false
+    t.boolean  "uses_markdown",   :default => true, :null => false
   end
 
   add_index "discussions", ["author_id"], :name => "index_discussions_on_author_id"
