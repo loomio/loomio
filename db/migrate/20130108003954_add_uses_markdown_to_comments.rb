1a433a9ce1816fd1d0c37004ae22bdc5a26b304f
Jon Lemmon <jonlemmon@gmail.com>
2013-01-29 23:10:32 +1300

    Make migrations reversable.

--- db/migrate/20130108003954_add_uses_markdown_to_comments.rb
+++ db/migrate/20130108003954_add_uses_markdown_to_comments.rb
@@ -1,6 +1,10 @@
 class AddUsesMarkdownToComments < ActiveRecord::Migration
-  def change
+  def up
     add_column :comments, :uses_markdown, :boolean, default: true, null: false
     change_column :comments, :uses_markdown, :boolean, default: false, null: false
+  end
+
+  def down
+    remove_column :comments, :uses_markdown
   end
 end
--- db/migrate/20130108004432_add_uses_markdown_to_discussions.rb
+++ db/migrate/20130108004432_add_uses_markdown_to_discussions.rb
@@ -1,6 +1,10 @@
 class AddUsesMarkdownToDiscussions < ActiveRecord::Migration
-  def change
+  def up
     add_column :discussions, :uses_markdown, :boolean, default: true, null:false
     change_column :discussions, :uses_markdown, :boolean, default: false, null: false
+  end
+
+  def down
+    remove_column :discussions, :uses_markdown
   end
 end
