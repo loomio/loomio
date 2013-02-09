c79acdb46d861c0e9215f15c03792dedab287c4e
Aaron Thornton <aaronthornton00@gmail.com>
2013-02-09 17:34:41 +0900

    column phase removed from table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130114223848) do
+ActiveRecord::Schema.define(:version => 20130209082913) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -208,13 +208,12 @@ ActiveRecord::Schema.define(:version => 20130114223848) do
     t.integer  "author_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "phase",          :default => "voting", :null => false
-    t.string   "discussion_url", :default => "",       :null => false
+    t.string   "discussion_url", :default => "",   :null => false
     t.datetime "close_date"
     t.integer  "discussion_id"
     t.string   "outcome"
     t.datetime "last_vote_at"
-    t.boolean  "uses_markdown",  :default => true,     :null => false
+    t.boolean  "uses_markdown",  :default => true, :null => false
   end
 
   add_index "motions", ["author_id"], :name => "index_motions_on_author_id"
