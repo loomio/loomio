e31b430e9a4533eecfb456404163213381f34de7
Richard Bartlett <richarddbartlett@gmail.com>
2013-02-19 16:58:38 +1300

    fix spelling mistake

--- app/controllers/motions_controller.rb
+++ app/controllers/motions_controller.rb
@@ -71,7 +71,7 @@ class MotionsController < GroupBaseController
     if motion.set_close_date!((params[:motion][:close_date]).to_datetime, current_user)
       flash[:success] = "Close date successfully changed."
     else
-      flash[:error] = "Invalid close date, it needs to be a furture date."
+      flash[:error] = "Invalid close date, it needs to be a future date."
     end
     redirect_to discussion_url(motion.discussion)
   end
