7b15134b843cd9cd04e8b489ccfaec6487186760
Richard Bartlett <richarddbartlett@gmail.com>
2013-02-19 16:58:38 +1300

    fix spelling mistake

--- spec/controllers/motions_controller_spec.rb
+++ spec/controllers/motions_controller_spec.rb
@@ -87,7 +87,7 @@ describe MotionsController do
         it "displays an error message and returns to the discussion page" do
           motion.should_receive(:set_close_date!).and_return false
           put :edit_close_date, :id => motion.id, :motion => { :close_date => Time.now }
-          flash[:error].should =~ /Invalid close date, it needs to be a furture date./
+          flash[:error].should =~ /Invalid close date, it needs to be a future date./
           response.should redirect_to(discussion)
         end
       end
