ec241f167a96b0dfdc4df8e4d94410333ef83216
Jon Lemmon <jonlemmon@gmail.com>
2013-02-21 20:36:07 +1300

    Fix failing specs.

--- spec/extras/notification_items/user_mentioned_spec.rb
+++ spec/extras/notification_items/user_mentioned_spec.rb
@@ -1,10 +1,11 @@
 describe NotificationItems::UserMentioned do
-  let(:notification) { stub(:notification) }
+  let(:notification) { stub(eventable: eventable) }
+  let(:eventable) { stub(:eventable) }
   let(:item) { NotificationItems::UserMentioned.new(notification) }
 
   it "#actor returns the user who mentioned someone" do
     mentioner = stub(:user)
-    notification.stub_chain(:eventable, :user).and_return(mentioner)
+    eventable.stub(:user => mentioner)
     item.actor.should == notification.eventable.user
   end
 
@@ -13,18 +14,18 @@ describe NotificationItems::UserMentioned do
   end
 
   it "#title returns the discussion title" do
-    notification.stub_chain(:eventable, :discussion_title).and_return("hello")
+    eventable.stub(:discussion_title => "hello")
     item.title.should == notification.eventable.discussion_title
   end
 
   it "#group_full_name returns the discussion's group name including a parent if there is one" do
-    notification.stub_chain(:eventable, :group_full_name).and_return("goob")
+    eventable.stub(:group_full_name => "goob")
     item.group_full_name.should == notification.eventable.group_full_name
   end
 
   it "#link returns a path to the comment" do
     item.stub_chain(:url_helpers, :discussion_path).and_return("/discussions/1/")
-    notification.stub_chain(:eventable, :id).and_return("123")
+    eventable.stub(:id => "123", :discussion => stub(:discussion))
     item.link.should == "/discussions/1/#comment-123"
   end
 end
