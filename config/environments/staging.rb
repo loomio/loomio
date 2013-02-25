775b0748fe493475605a577be695eebcebe91abd
Jon Lemmon <jonlemmon@gmail.com>
2013-02-26 09:37:57 +1300

    Change staging config.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -12,10 +12,10 @@ Loomio::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  #config.assets.compress = true
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  #config.assets.compile = false
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
