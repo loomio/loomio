28245389560506cc75c25adce72bd8d559aea218
Aaron Thornton <aaronthornton00@gmail.com>
2013-02-27 14:42:45 +1300

    show invited users in a seperate panel

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,6 +22,7 @@ class GroupsController < GroupBaseController
     @subgroups = @group.subgroups.accessible_by(current_ability, :show)
     @discussions = Queries::VisibleDiscussions.for(@group, current_user)
     @discussion = Discussion.new(group_id: @group.id)
+    @invited_users = @group.invited_users
   end
 
   def edit
