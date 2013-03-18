b888ad53fcbfbcfa672eaaf066282fe17695dd0a
Aaron Thornton <aaronthornton00@gmail.com>
2013-03-18 16:17:51 +1300

    mirgrations run

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130306223703) do
+ActiveRecord::Schema.define(:version => 20130318031103) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -129,17 +129,19 @@ ActiveRecord::Schema.define(:version => 20130306223703) do
     t.string   "name"
     t.text     "description"
     t.string   "admin_email"
-    t.datetime "created_at",                              :null => false
-    t.datetime "updated_at",                              :null => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.string   "status"
     t.integer  "group_id"
-    t.boolean  "cannot_contribute",    :default => false
+    t.boolean  "cannot_contribute",   :default => false
     t.string   "expected_size"
-    t.integer  "max_size",             :default => 50
+    t.integer  "max_size",            :default => 50
     t.string   "robot_trap"
     t.integer  "distribution_metric"
-    t.string   "sectors_metric"
-    t.string   "other_sectors_metric"
+    t.string   "sectors"
+    t.string   "other_sector"
+    t.string   "admin_name"
+    t.string   "country_name"
   end
 
   add_index "group_requests", ["group_id"], :name => "index_group_requests_on_group_id"
@@ -161,10 +163,11 @@ ActiveRecord::Schema.define(:version => 20130306223703) do
     t.integer  "max_size"
     t.boolean  "cannot_contribute",    :default => false
     t.integer  "distribution_metric"
-    t.string   "sectors_metric"
-    t.string   "other_sectors_metric"
+    t.string   "sectors"
+    t.string   "other_sector"
     t.integer  "discussions_count",    :default => 0,     :null => false
     t.integer  "motions_count",        :default => 0,     :null => false
+    t.string   "country_name"
   end
 
   add_index "groups", ["parent_id"], :name => "index_groups_on_parent_id"
