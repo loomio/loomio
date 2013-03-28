6e5c42c0b05a09c19078377c654ed5080ba64faf
Richard Bartlett <richarddbartlett@gmail.com>
2013-03-28 14:24:10 +1300

    Move discussion feature added

---
+++ extras/discussion_mover.rb
@@ -0,0 +1,25 @@
+class DiscussionMover
+
+  def self.destination_groups(group, user)
+    destinations = []
+    destinations << [group.parent.name, group.parent.id] if group.is_a_subgroup? && user.is_group_admin?(group.parent)
+    destinations += subgroup_destinations(group, user)
+    destinations
+  end
+
+  def self.subgroup_destinations(group, user)
+    subgroup_destinations = []
+    parent = group.parent || group
+    parent.subgroups.each do |subgroup|
+      if user.is_group_admin?(subgroup) && subgroup != group
+        subgroup_destinations << [subgroup.name, subgroup.id]
+      end
+    end
+    subgroup_destinations
+  end
+
+  def self.can_move?(user, destination)
+    return true if user.is_group_admin?(destination)
+    false
+  end
+end
