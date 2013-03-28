00a15685d6b31bb0741176086aac41906c95a926
Richard Bartlett <richarddbartlett@gmail.com>
2013-03-28 14:24:10 +1300

    started

---
+++ features/step_definitions/admin_moves_discussion_steps.rb
@@ -0,0 +1,13 @@
+When /^I select the move discussion link from the discussion dropdown$/ do
+  find("#options-dropdown").click
+  click_on "move-discussion"
+end
+
+When /^I select the destination group$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see the destination group name in the page title$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
