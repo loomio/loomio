761703f17b1eaad77e62d0216de31c878ca98c56
Robert Guthrie <rguthrie@gmail.com>
2013-04-28 16:15:57 +1200

    Make migrations work on old data

--- db/migrate/20120927092327_create_invitations.rb
+++ db/migrate/20120927092327_create_invitations.rb
@@ -1,5 +1,8 @@
 class CreateInvitations < ActiveRecord::Migration
   def change
+    if table_exists? :invitations
+      drop_table :invitations
+    end
     create_table :invitations do |t|
       t.string :recipient_email
       t.string :access_level
