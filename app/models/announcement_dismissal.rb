0a053f5c32405b4a13169d0b213d7654855d6f43
Jon Lemmon <jonlemmon@gmail.com>
2013-05-02 17:15:25 +1200

    Fix up announcement dismissals.

--- app/models/announcement_dismissal.rb
+++ app/models/announcement_dismissal.rb
@@ -1,4 +1,8 @@
 class AnnouncementDismissal < ActiveRecord::Base
+  attr_accessible :user_id, :announcement_id
+
   belongs_to :announcement
   belongs_to :user
+
+  validates_uniqueness_of :user_id, scope: :announcement_id
 end
