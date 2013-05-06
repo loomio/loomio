b43d42d803a9e9510db82d075755867ed46c1edb
Robert Guthrie <rguthrie@gmail.com>
2013-05-07 11:00:00 +1200

    try s3 gzip for assets

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -10,7 +10,7 @@ if defined?(AssetSync)
     # Fail silently.  Useful for environments such as Heroku
     config.fail_silently = true
     config.gzip_compression = true
-    config.existing_remote_files = 'ignore'
+    config.existing_remote_files = 'delete'
 
   end
 end
