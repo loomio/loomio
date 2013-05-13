58bffdc516bfbca2a854d6ff8c7607103ff9b57a
john irving <whimful@gmail.com>
2013-05-14 10:55:07 +1200

    add scopes and single page view to group admin-panel, trim size col

--- app/admin/groups.rb
+++ app/admin/groups.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Group do
   actions :index, :show, :edit
+  before_filter :set_pagination
   filter :name
   filter :creator
   filter :parent
@@ -7,12 +8,37 @@ ActiveAdmin.register Group do
   scope :all, :default => true do |group|
     group.includes [:creator]
   end
+  scope "Parent groups" do |group|
+    group.where(parent_id: nil)
+  end
+  scope "<5 members" do |group|
+    group.where('memberships_count <= ?', 5)
+  end
+  scope "> 85% full" do |group|
+    group.where('max_size > ? AND memberships_count/max_size >= ?', 0, 0.85)
+  end
 
   index :download_links => false do
+    if params[:pagination].blank?
+      div :class => "admin-panel-paginate-toggle" do
+        link_to("single page", 'groups?pagination=false', class: "table_tools_button")
+      end
+    else
+      div :class => "admin-panel-paginate-toggle" do
+        link_to("paginate", 'groups', class: "table_tools_button")
+      end
+    end
+
     column :id
     column :name
-    column :max_size
-    column "Members", :memberships_count
+    column "Size", :sortable => :memberships_count do |group|
+      if group.max_size
+        group_max_size = " (#{group.max_size})"
+      else
+        group_max_size = ""
+      end
+      "#{group.memberships_count}"+group_max_size
+    end
     column "Discussions", :discussions_count
     column "Motions", :motions_count
     column :creator, :sortable => 'users.name'
@@ -28,7 +54,9 @@ ActiveAdmin.register Group do
     f.inputs "Details" do
       f.input :id, :input_html => { :disabled => true }
       f.input :name, :input_html => { :disabled => true }
-      f.input :max_size
+      if :max_size
+        f.input :max_size
+      end
     end
     f.buttons
   end
@@ -60,5 +88,17 @@ ActiveAdmin.register Group do
         end
     end
     active_admin_comments
+  end
+
+  controller do
+    def set_pagination
+      if params[:pagination].blank?
+        @per_page = 40
+      elsif params[:pagination] == 'false'
+        @per_page = 999999999
+      else
+        @per_page = params[:pagination]
+      end
+    end
   end
 end
