cb0cd22ea82da4f2a69c0cab8f777b4266bfd0a6
john irving <whimful@gmail.com>
2013-05-17 15:25:04 +1200

    fix failing woc cucumber step

--- db/schema.rb
+++ db/schema.rb
@@ -183,27 +183,6 @@ ActiveRecord::Schema.define(:version => 20130515123316) do
 
   add_index "group_requests", ["group_id"], :name => "index_group_requests_on_group_id"
 
-  create_table "group_setups", :force => true do |t|
-    t.integer  "group_id"
-    t.string   "group_name"
-    t.text     "group_description"
-    t.string   "viewable_by",            :default => "members"
-    t.string   "members_invitable_by",   :default => "admins"
-    t.string   "discussion_title"
-    t.text     "discussion_description"
-    t.string   "motion_title"
-    t.text     "motion_description"
-    t.date     "close_at_date"
-    t.string   "close_at_time_zone"
-    t.string   "close_at_time"
-    t.string   "admin_email"
-    t.text     "members_list"
-    t.string   "invite_subject"
-    t.text     "invite_body"
-    t.datetime "created_at",                                    :null => false
-    t.datetime "updated_at",                                    :null => false
-  end
-
   create_table "groups", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
