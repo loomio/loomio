3166dc47a8836826fbb58e509166c5d6494cd609
Jon Lemmon <jon@loomio.org>
2013-05-28 14:56:08 +1200

    Fix motion time select

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -10,33 +10,31 @@ describe ApplicationHelper do
     end
   end
   describe "set_title" do
-  	context "for a group" do
- 	  it "has the group name in title" do
-      	@atoz = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' 	  	
- 	  	helper.should_receive(:content_for).with(:title, "the title - #{@atoz}")
- 	  	helper.set_title(@atoz, 'the title')
- 	  end
+    context "for a group" do
+      it "has the group name in title" do
+        @atoz = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+        helper.should_receive(:content_for).with(:title, "the title - #{@atoz}")
+        helper.set_title(@atoz, 'the title')
+      end
 
- 	  it "strips out quotes" do
- 	  	@string = 'onetwoth"ree'
- 	  	helper.should_receive(:content_for).with(:title, "the title - onetwothree")
- 	  	helper.set_title(@string, 'the title')
- 	  end
+      it "strips out quotes" do
+        @string = 'onetwoth"ree'
+        helper.should_receive(:content_for).with(:title, "the title - onetwothree")
+        helper.set_title(@string, 'the title')
+      end
 
- 	  it "strips out brackets" do
- 	   	@string = 'onetwo<three>'
- 	  	helper.should_receive(:content_for).with(:title, "the title - onetwothree")
- 	  	helper.set_title(@string, 'the title')	
- 	  end
+      it "strips out brackets" do
+        @string = 'onetwo<three>'
+        helper.should_receive(:content_for).with(:title, "the title - onetwothree")
+        helper.set_title(@string, 'the title')
+      end
 
- 	  it "includes a parent" do
- 	  	@parent = stub_model(Group, :name => 'Parent Group')
- 	  	@group_name = 'This Group'
- 	  	helper.should_receive(:content_for).with(:title, "the title - Parent Group - This Group")
- 	  	helper.set_title(@group_name, 'the title', @parent)	
- 	  end
-
-  	end
+      it "includes a parent" do
+        @parent = stub_model(Group, :name => 'Parent Group')
+        @group_name = 'This Group'
+        helper.should_receive(:content_for).with(:title, "the title - Parent Group - This Group")
+        helper.set_title(@group_name, 'the title', @parent)
+      end
+    end
   end
 end
-
