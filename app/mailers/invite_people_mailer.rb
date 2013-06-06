b463cd2639b17e0c82bc6013a7b0c44738cb6baa
Robert Guthrie <rguthrie@gmail.com>
2013-06-06 14:09:53 +1200

    minor improvements to the invitation mailer defaults

--- app/mailers/invite_people_mailer.rb
+++ app/mailers/invite_people_mailer.rb
@@ -3,15 +3,17 @@ class InvitePeopleMailer < ActionMailer::Base
     @invitation = invitation
     @message_body = message_body
     mail to: invitation.recipient_email, 
-         from: sender_email,
-         subject: "Approved! Set up your Loomio group: #{@invitation.group_name}"
+         from: 'contact@loomio.org',
+         reply_to: sender_email,
+         subject: "Your Loomio group: #{@invitation.group_name} has been approved!"
   end
 
   def to_join_group(invitation, sender_email, message_body)
     @invitation = invitation
     @message_body = message_body
     mail to: invitation.recipient_email, 
-         from: sender_email,
-         subject: "#{@invitation.inviter.name} invites you to join #{@invitation.group_name} on Loomio"
+         from: 'contact@loomio.org',
+         reply_to: sender_email,
+         subject: "#{@invitation.inviter.name} has invited you to join #{@invitation.group_name} on Loomio"
   end
 end
