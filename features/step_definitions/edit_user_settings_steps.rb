8327ffbdcb884f7df76cb71eae9a8426083b8811
Aaron Thornton <aaronthornton00@gmail.com>
2013-07-02 11:40:04 +1200

    add user email field

--- features/step_definitions/edit_user_settings_steps.rb
+++ features/step_definitions/edit_user_settings_steps.rb
@@ -16,3 +16,24 @@ end
 Then /^I upload a profile image$/ do
   pending "TODO: write test for image upload"
 end
+
+Given(/^I change my email to "(.*?)" and submit the form$/) do |email|
+  fill_in 'user_email', with: email
+  click_on 'user-settings-submit'
+end
+
+When(/^I log out$/) do
+  find('#user').click()
+  click_on('Sign out')
+end
+
+When(/^I log in with "(.*?)"$/) do |email|
+  fill_in 'user_email', with: email
+  fill_in 'user_password', with: @user.password
+  click_on 'sign-in-btn'
+end
+
+Then(/^I should see the logged in homepage$/) do
+  view_screenshot
+  page.should have_css('body.dashboard.show')
+end
