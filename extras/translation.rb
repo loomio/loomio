42b2f648908673e768f3892e43c7349f61519872
Aaron Thornton <aaronthornton00@gmail.com>
2013-07-30 14:56:01 +1200

    add footer

--- extras/translation.rb
+++ extras/translation.rb
@@ -7,12 +7,22 @@ class Translation
                "Nederlands" => "nl"}
   EXPERIMENTAL_LANGUAGES = {"română" => "ro"}
 
+  def self.language(locale)
+    LANGUAGES.index(locale)
+  end
+
   def self.locales
     LANGUAGES.values
   end
 
   def self.experimental_locales
     EXPERIMENTAL_LANGUAGES.values
+  end
+
+  def self.language_link_attributes(language)
+    { href: "?&locale=#{language[1]}"  ,
+      title: "#{I18n.t(:change_language, language: language[0])}",
+      text: "#{language[0]}" }
   end
 
   def self.language_links
