e366156c53d304c6b1ba2e44b2f26406ba461122
Richard Bartlett <rich@loomio.org>
2013-08-13 16:25:05 +1200

    remove subgroups from new group count on admin panel

--- app/admin/dashboards.rb
+++ app/admin/dashboards.rb
@@ -2,8 +2,8 @@ ActiveAdmin::Dashboards.build do
 
   section "Groups", :priority => 1 do
     h1 { Group.count }
-    div { "New today: #{Group.where('created_at >= ?', 24.hours.ago).count}"}
-    div { "New this week: #{Group.where('created_at >= ?', 1.week.ago).count}"}
+    div { "New today: #{Group.where('parent_id IS NULL AND created_at >= ?', 24.hours.ago).count}"}
+    div { "New this week: #{Group.where('parent_id IS NULL AND created_at >= ?', 1.week.ago).count}"}
     div { link_to "See all groups", admin_groups_path }
   end
 
