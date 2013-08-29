dafe4647d61b09aef74430f51eb2b63b0d679446
Richard Bartlett <rich@loomio.org>
2013-08-30 09:03:06 +1200

    clean up dashboard, add counts

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,30 +1,53 @@
+def print_month_totals(model)
+  if model == "users"
+    collection = User.where('created_at > ?', 6.months.ago).group_by{|u| u.created_at.month}
+  elsif model == "groups"
+    collection = Group.where('parent_id IS NULL AND created_at > ?', 6.months.ago).group_by{|g| g.created_at.month}
+  end
+  counts = {}
+  collection.each_pair do |k, v|
+    counts[k] = v.count
+  end
+  month_totals = []
+  counts.each_pair do |k,v|
+    month_name = Date::MONTHNAMES[k]
+    month_totals << "#{month_name}: #{v}"
+  end
+  month_totals.join("<br>")
+end
+
 ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    section "Groups", :priority => 1 do
-    h1 { Group.count }
-    div { "New today: #{Group.where('parent_id IS NULL AND created_at >= ?', 24.hours.ago).count}"}
-    div { "New this week: #{Group.where('parent_id IS NULL AND created_at >= ?', 1.week.ago).count}"}
-    div { link_to "See all groups", admin_groups_path }
-    end
-
-    section "Users", :priority => 2 do
-      h1 { User.count }
-      div { link_to "See all users", admin_users_path }
-    end
-
-    section "Members Per Group (average)", :priority => 3 do
-      h1{ Group.average("memberships_count").round unless Group.count == 0 }
-    end
-
-    section "Discussions", :priority => 4 do
-      h1 { Discussion.count }
-    end
-
-    section "Proposals", :priority => 7 do
-      h1 { Motion.count }
+    columns do
+      column do
+        panel "Groups" do
+          h1 { Group.count }
+          div { "New today: #{Group.where('parent_id IS NULL AND created_at >= ?', 24.hours.ago).count}"}
+          div { print_month_totals("groups").html_safe}
+          div { link_to "See all groups", admin_groups_path }
+        end
+      end
+      column do
+        panel "Users" do
+          h1 { User.count }
+          div { "New today: #{User.where('created_at >= ?', 24.hours.ago).count}"}
+          div { print_month_totals("users").html_safe}
+          div { link_to "See all users", admin_users_path }
+        end
+      end
+      column do
+        panel "Discussions" do
+          h1 { Discussion.count }
+        end
+      end
+      column do
+        panel "Proposals" do
+          h1 { Motion.count }
+        end
+      end
     end
 
     # Here is an example of a simple dashboard with columns and panels.
