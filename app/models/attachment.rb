57e18a8179ac562283839aa193f37271ae1f5668
Robert Guthrie <rob@guthr.ie>
2013-09-10 11:27:34 +1200

    make image attachments less than 1MB display inline

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,6 +3,11 @@ class Attachment < ActiveRecord::Base
   belongs_to :comment, counter_cache: true
 
   validates_presence_of :filename, :location, :user_id
+
+  def is_an_image?
+    extension = self.filename.split('.').last.downcase
+    %w[jpg jpeg png].include?(extension)
+  end
 end
 
 
