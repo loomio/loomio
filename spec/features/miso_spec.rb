d992c5e47ab65e9efbef296c5819af80b2a21e4a
Jon Lemmon <jon@loomio.org>
2013-09-17 17:57:31 +1200

    Be evil. Get sauce work good.

--- spec/features/miso_spec.rb
+++ spec/features/miso_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe "Wikipedia's Miso Page", :sauce => true do
   it "Should mention a favorite type of Miso" do
     visit "http://en.wikipedia.org/"
-    fill_in 'search', :with => "Miso"
+    fill_in 'searchInput', :with => "Miso"
     click_button "searchButton"
     page.should have_content "Akamiso"
   end
--- spec/features/ramen_spec.rb
+++ spec/features/ramen_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 describe "Wikipedia's Ramen Page", :sauce => true do
   it "Should mention the inventor of instant Ramen" do
     visit "http://en.wikipedia.org/"
-    fill_in 'search', :with => "Ramen"
+    fill_in 'searchInput', :with => "Ramen"
     click_button "searchButton"
     page.should have_content "Momofuku Ando"
   end
