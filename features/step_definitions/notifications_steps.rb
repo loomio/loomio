1305c6393e0d8c93750d6ac0caef5d5002db4462
Jon Lemmon <jon@loomio.org>
2013-09-17 19:41:43 +1200

    Fix MembershipRequested notifications from visitors

--- features/step_definitions/notifications_steps.rb
+++ features/step_definitions/notifications_steps.rb
@@ -25,3 +25,20 @@ Then(/^I should see that someone closed the motion$/) do
   find("#notifications-container").should have_content(@closer.name + " " + I18n.t('notifications.motion_closed.by_user'))
 end
 
+Given(/^a visitor has requested membership to the group$/) do
+  params = { name: "Richie", email: "rich@loomio.org" }
+  @membership_request = RequestMembership.to_group(group: @group, params: params)
+end
+
+Then(/^I should see that the visitor requested access to the group$/) do
+  find("#notifications-container").should have_content(@membership_request.name + " " + I18n.t('notifications.membership_requested'))
+end
+
+Given(/^a user has requested membership to the group$/) do
+  @requestor = FactoryGirl.create :user
+  @membership_request = RequestMembership.to_group(group: @group, requestor: @requestor)
+end
+
+Then(/^I should see that the user requested access to the group$/) do
+  step 'I should see that the visitor requested access to the group'
+end
---
+++ spec/models/concerns/avatar_initials_spec.rb
@@ -0,0 +1,52 @@
+require 'spec_helper_lite'
+
+# External dependencies
+require 'active_support/core_ext/object/blank'
+require 'active_support/concern'
+
+require_relative '../../../app/models/concerns/avatar_initials'
+
+class DummyUser
+  include AvatarInitials
+  attr_accessor :name, :email, :deleted_at, :avatar_initials
+end
+
+describe AvatarInitials do
+  let(:user) { DummyUser.new }
+
+  before do
+    user.name = "Rob Gob"
+    user.email = "rob@gob.com"
+  end
+
+  describe "#set_avatar_initials" do
+    it "sets avatar_initials to 'DU' if deleted_at is true (a date is present)" do
+      user.deleted_at = "20/12/2002"
+      user.set_avatar_initials
+      user.avatar_initials.should == "DU"
+    end
+
+    it "sets avatar_initials to the first two characters in all caps of the email if the user's name is email" do
+      user.set_avatar_initials
+      user.avatar_initials.should == "RG"
+    end
+
+    it "returns the first three initials of the stored name" do
+      user.name = "Bob bobby sinclair deebop"
+      user.set_avatar_initials
+      user.avatar_initials.should == "BBS"
+    end
+
+    it "returns the first two characters of their email if they have no name" do
+      user.name = nil
+      user.set_avatar_initials
+      user.avatar_initials.should == "RO"
+    end
+
+    it "works for strange characters" do
+      user.name = "D'Angelo (Loco)"
+      user.set_avatar_initials
+      user.avatar_initials.should == "D("
+    end
+  end
+end
