19855de383ef92209b510a80a3f4452f1db4864c
Robert Guthrie <rob@guthr.ie>
2013-11-19 16:57:33 +1300

    rebuild vote controller

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -1,114 +1,41 @@
 require 'spec_helper'
 
 describe VotesController do
-  context 'a signed in user voting on a motion' do
-    before :each do
-      @user = create(:user)
-      sign_in @user
-      @group = create(:group)
-      @group.add_member!(@user)
-      @discussion = create(:discussion, group: @group)
-      @motion = create(:motion, discussion: @discussion)
-      @motion.save!
-      @previous_url = motion_url(@motion)
-      @vote_args = { motion_id: @motion.id,
-                   vote: {position: 'yes', statement: 'blah'} }
-      request.env["HTTP_REFERER"] = @previous_url
+  let(:user) { create :user }
+  let(:group) { create :group }
+  let(:discussion) { create :discussion, group: group }
+  let(:motion) { create :motion, discussion: discussion }
+
+  before :each do
+    group.add_member!(user)
+    sign_in user
+  end
+
+  describe "casting a vote" do
+    it 'requires the user can vote' do
+      VotesController.any_instance.should_receive(:require_user_can_vote)
+      post :create, motion_id: motion.id, vote: {position: 'yes'}
     end
 
-    context 'proposal is open' do
-
-      describe "casting a vote" do
-        it 'redirects to previous url' do
-          post :create, @vote_args
-          response.should redirect_to(@previous_url)
-        end
-        it "assigns flash success message" do
-          post :create, @vote_args
-          flash[:success].should =~ /Position submitted/
-        end
-        it "assigns instance variables for the view" do
-          post :create, @vote_args
-          assigns(:vote).user.should == @user
-          assigns(:vote).motion.should == @motion
-          assigns(:vote).position.should == 'yes'
-          assigns(:vote).statement.should == 'blah'
-        end
-        it "fires new_vote event" do
-          Events::NewVote.should_receive(:publish!)
-          post :create, @vote_args
-        end
-      end
-
-      describe "casting a 'block' vote" do
-        it "fires motion_blocked event" do
-          Events::MotionBlocked.should_receive(:publish!)
-          post :create, motion_id: @motion.id,
-                   vote: { position: 'block' }
-        end
-      end
-
-      describe "updating a vote" do
-        before do
-          @vote = Vote.new(position: 'yes')
-          @vote.motion = @motion
-          @vote.user = @user
-          @vote.save!
-          @vote_args = {  motion_id: @motion.id, id: @vote.id,
-               vote: {position: 'no', statement: 'blah'} }
-        end
-        it 'creates a new vote' do
-          post :update, @vote_args
-
-          Vote.all.count.should == 2
-          @motion.last_position_by_user(@user).should == 'no'
-        end
-        it "assigns flash success message" do
-          post :update, @vote_args
-          flash[:success].should =~ /Position updated/
-        end
-        it "redirects to motion discussion" do
-          post :update, @vote_args
-          response.should redirect_to(@motion.discussion)
-        end
-        it "fires new_vote event" do
-          Events::NewVote.should_receive(:publish!)
-          post :update, @vote_args
-        end
-      end
-
-      describe "deleting a vote" do
-        before do
-          vote = Vote.new(position: 'yes')
-          vote.motion = @motion
-          vote.user = @user
-          vote.save!
-          delete :destroy, id: vote.id, motion_id: @motion.id
-        end
-        it 'redirects to discussion page' do
-          response.should redirect_to(@motion.discussion)
-        end
-        it "gives flash notice" do
-          flash[:notice].should =~ /destroyed/
-        end
-        it "destroys the vote" do
-          Vote.all.count.should == 0
-        end
-      end
+    it 'saves the vote' do
+      vote = double(:vote).as_null_object
+      vote.should_receive(:save!)
+      Vote.stub(:new).and_return(vote)
+      post :create, motion_id: motion.id, vote: {position: 'yes'}
     end
 
-    context 'proposal is closed' do
-      before :each do
-        @discussion = create(:discussion, group: @group, author: @user)
-        @motion = create(:motion, discussion: @discussion, closed_at: 2.days.ago)
-      end
+    it "assigns flash and redirects to motion" do
+      post :create, motion_id: motion.id, vote: {position: 'yes'}
 
-      it 'cannot vote' do
-        post :create, motion_id: @motion.id,
-             vote: {position: 'yes', statement: 'blah'}
-        response.should be_redirect
-        flash[:error].should =~ /This proposal has closed./
-      end
+      flash[:success].should =~ /Position submitted/
+      response.should redirect_to motion
+    end
+  end
+
+  describe "casting a 'block' vote" do
+    it "fires motion_blocked event" do
+      Events::MotionBlocked.should_receive(:publish!)
+      post :create, motion_id: motion.id, vote: { position: 'block' }
     end
   end
 end
