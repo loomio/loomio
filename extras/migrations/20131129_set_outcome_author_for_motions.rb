2e04224688d7d2426a7ecb705c67a7548c71a64f
john irving <whimful@gmail.com>
2013-12-03 12:19:31 +1300

    fix and tidy

--- extras/migrations/20131129_set_outcome_author_for_motions.rb
+++ extras/migrations/20131129_set_outcome_author_for_motions.rb
@@ -1,13 +1,27 @@
 class SetOutcomeAuthorForMotionsMigration
   def self.now
     Motion.reset_column_information
-    Motion.find_each do |motion|
+    spoofed = 0
+    actual = 0
+
+    Motion.where('closed_at IS NOT NULL').find_each do |motion|
       puts motion.id if (motion.id % 100) == 0
 
       outcome_event = motion.events.where(kind: 'motion_outcome_created').last
-      motion.update_attribute(:outcome_author_id, outcome_event.user.id) unless outcome_event.blank?
+
+      if outcome_event.blank?
+        motion.update_attribute(:outcome_author_id, motion.author.id)
+        spoofed +=1
+      else
+        motion.update_attribute(:outcome_author_id, outcome_event.user.id)
+        actual += 1
+      end
     end
-    puts '*********************************'
+    puts '//////////////////////////////////////////////////////////'
     puts 'UPGRADE DONE'
+    puts "#{Motion.where('closed_at IS NOT NULL').count} targeted motions:"
+    puts "\t- #{actual} with accurate author"
+    puts "\t- #{spoofed} with best guess"
+    puts '//////////////////////////////////////////////////////////'
   end
 end
--- features/migrations/20131129_set_outcome_author_for_motions.feature
+++
@@ -1,7 +0,0 @@
-Feature: migrate motions with outcomes to give them outcome authors
-
-Scenario: Motion with outcome but no outcome author gets outcome author assigned
-
-# Given there is a motion with an outcome but no author
-# When I run the set_motion_outcome_author migration
-# Then that motion now has a motion author
