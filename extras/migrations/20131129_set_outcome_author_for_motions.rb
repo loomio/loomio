cd5731b53a14ee7b198135894f77d2189c6abb73
john irving <whimful@gmail.com>
2013-12-03 17:17:39 +1300

    tidy tidy

--- extras/migrations/20131129_set_outcome_author_for_motions.rb
+++ extras/migrations/20131129_set_outcome_author_for_motions.rb
@@ -4,22 +4,23 @@ class SetOutcomeAuthorForMotionsMigration
     spoofed = 0
     actual = 0
 
-    Motion.where('closed_at IS NOT NULL').find_each do |motion|
+    Motion.where('outcome IS NOT NULL').find_each do |motion|
       puts motion.id if (motion.id % 100) == 0
 
-      outcome_event = motion.events.where(kind: 'motion_outcome_created').last
+      outcome_event = motion.events.where(kind: 'motion_outcome_updated').last ||
+                      motion.events.where(kind: 'motion_outcome_created').last
 
       if outcome_event.blank?
-        motion.update_attribute(:outcome_author_id, motion.author.id)
+        motion.update_attribute(:outcome_author_id, motion.author_id)
         spoofed +=1
       else
-        motion.update_attribute(:outcome_author_id, outcome_event.user.id)
+        motion.update_attribute(:outcome_author_id, outcome_event.user_id)
         actual += 1
       end
     end
     puts '//////////////////////////////////////////////////////////'
     puts 'UPGRADE DONE'
-    puts "#{Motion.where('closed_at IS NOT NULL').count} targeted motions:"
+    puts "#{Motion.where('outcome IS NOT NULL').count} targeted motions:"
     puts "\t- #{actual} with accurate author"
     puts "\t- #{spoofed} with best guess"
     puts '//////////////////////////////////////////////////////////'
