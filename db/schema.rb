ad53630538b63693ec8d1e5f33517f81436e351a
Hannah Salmon <hr.salmon@gmail.com>
2013-12-05 12:12:02 +1300

    Fix group_discussions_viewer_spec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131128014456) do
+ActiveRecord::Schema.define(:version => 20131204223341) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -320,7 +320,7 @@ ActiveRecord::Schema.define(:version => 20131128014456) do
     t.boolean  "next_steps_completed",       :default => false,          :null => false
     t.string   "full_name"
     t.string   "payment_plan",               :default => "undetermined"
-    t.boolean  "viewable_by_parent_members", :default => true,           :null => false
+    t.boolean  "viewable_by_parent_members", :default => false,          :null => false
   end
 
   add_index "groups", ["archived_at", "id"], :name => "index_groups_on_archived_at_and_id"
@@ -418,6 +418,7 @@ ActiveRecord::Schema.define(:version => 20131128014456) do
     t.datetime "closing_at"
     t.integer  "did_not_votes_count"
     t.integer  "votes_count",         :default => 0,    :null => false
+    t.integer  "outcome_author_id"
   end
 
   add_index "motions", ["author_id"], :name => "index_motions_on_author_id"
@@ -493,8 +494,8 @@ ActiveRecord::Schema.define(:version => 20131128014456) do
     t.boolean  "subscribed_to_proposal_closure_notifications",                :default => true,       :null => false
     t.string   "authentication_token"
     t.string   "unsubscribe_token"
-    t.boolean  "uses_markdown",                                               :default => false
     t.integer  "memberships_count",                                           :default => 0,          :null => false
+    t.boolean  "uses_markdown",                                               :default => false
     t.string   "language_preference"
     t.string   "time_zone"
   end
