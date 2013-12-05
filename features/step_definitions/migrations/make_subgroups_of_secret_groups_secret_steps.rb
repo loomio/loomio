be102348b5749ce73b83a02ab6f8655085f7905d
Hannah Salmon <hr.salmon@gmail.com>
2013-12-05 15:18:59 +1300

    Add private groups and logic about secret groups

---
+++ features/step_definitions/migrations/make_subgroups_of_secret_groups_secret_steps.rb
@@ -0,0 +1,15 @@
+Given(/^there is a public subgroup of a secret group$/) do
+  @parent_group = FactoryGirl.create :group, privacy: 'secret'
+  @sub_group = FactoryGirl.build :group, parent: @parent_group, privacy: 'public'
+  @sub_group.save(validate: false)
+end
+
+When(/^I migrate public subgroups of secret groups$/) do
+  require 'extras/migrations/20131129_migrate_subgroups_of_secret_groups_to_be_secret'
+  MakeSubgroupsOfSecretGroupsSecret.now
+end
+
+Then(/^the public subgroup should be secret$/) do
+  @sub_group.reload
+  @sub_group.privacy.should == 'secret'
+end
