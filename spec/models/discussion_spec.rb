0392c63a853cf2751ef01ba77f334f10f6b93904
Richard Bartlett <rich@loomio.org>
2013-12-09 14:10:06 +1300

    archive discussions when group is archived

--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -32,6 +32,17 @@ describe Discussion do
     discussion.last_comment_at.to_s.should == discussion.created_at.to_s
   end
 
+  describe "archive!" do
+    before do
+      discussion.archived_at.should_not be_present
+      discussion.archive!
+    end
+
+    it "sets archived_at on the discussion" do
+      discussion.archived_at.should be_present
+    end
+  end
+
   describe "#search(query)" do
     before { @user = create(:user) }
     it "returns user's discussions that match the query string" do
