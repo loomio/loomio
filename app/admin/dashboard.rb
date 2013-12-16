ecdd1e6249cb77215036cd8a2894b02d6d07f5b7
Richard Bartlett <rich@loomio.org>
2013-12-17 11:27:11 +1300

    messing around with admin panel like a bad person

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,8 +1,10 @@
 def print_month_totals(model)
   if model == "users"
-    collection = User.where('created_at > ?', 6.months.ago).order(:created_at).group_by{|u| u.created_at.month}
+    collection = User.where('created_at > ?', 6.months.ago).order("created_at DESC").group_by{|u| u.created_at.month}
+    divisor = 50
   elsif model == "groups"
-    collection = Group.where('parent_id IS NULL AND created_at > ?', 6.months.ago).order(:created_at).group_by{|g| g.created_at.month}
+    collection = Group.where('parent_id IS NULL AND created_at > ?', 6.months.ago).order("created_at DESC").group_by{|g| g.created_at.month}
+    divisor = 25
   end
   counts = {}
   collection.each_pair do |k, v|
@@ -11,9 +13,9 @@ def print_month_totals(model)
   month_totals = []
   counts.each_pair do |k,v|
     graph = ''
-    (v/50).times {|x| graph << '.'}
-    month_name = Date::MONTHNAMES[k]
-    month_totals << "#{graph} #{month_name}: #{v}"
+    (v/divisor).times {|x| graph << '.'}
+    month_name = Date::ABBR_MONTHNAMES[k]
+    month_totals << "<span style='font-family:monaco'>#{month_name}#{graph}#{v}</span>"
   end
   month_totals.join("<br>")
 end
