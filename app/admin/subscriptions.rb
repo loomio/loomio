a47cf6c6e248a71db3143f979049e3a7c4c83726
Richard Bartlett <rich@loomio.org>
2013-12-31 11:17:43 +1300

    view and edit subscriptions in admin panel

---
+++ app/admin/subscriptions.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register Subscription do
+  actions :index, :show, :edit
+  filter :group_id
+  filter :amount
+  filter :created_at
+  scope :non_zero
+  scope :zero
+
+  index do
+    column :group_id
+    column :group do |s|
+      Group.find(s.group_id).name
+    end
+    column :amount
+    column :created_at
+    column :updated_at
+    default_actions
+  end
+
+  member_action :update, :method => :put do
+    subscription = Subscription.find(params[:id])
+    subscription.amount = params[:subscription][:amount]
+    subscription.save
+    redirect_to admin_subscription_url, :notice => "Subscription updated"
+  end
+end
