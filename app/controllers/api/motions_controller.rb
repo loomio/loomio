d32ecbb868f99df06ea85333986ce344cca299df
Robert Guthrie <rob@guthr.ie>
2014-01-15 12:54:06 +1300

    i am sane

--- app/controllers/api/motions_controller.rb
+++ app/controllers/api/motions_controller.rb
@@ -10,6 +10,7 @@ class Api::MotionsController < Api::BaseController
   end
 
   def vote
+    params[:vote] = {position: params[:position], statement: params[:statement]}
     @vote = Vote.new(permitted_params.vote)
     @vote.user = current_user
     @vote.motion = Motion.find(params[:motion_id])
