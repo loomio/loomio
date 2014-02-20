4671a40a585e80c17cc0f0d32164b91793203ec2
Aaron Thornton <aaronthornton00@gmail.com>
2014-02-21 12:19:49 +1300

    add beta feature conditional into code

--- app/controllers/users/email_preferences_controller.rb
+++ app/controllers/users/email_preferences_controller.rb
@@ -29,7 +29,8 @@ class Users::EmailPreferencesController < BaseController
 
   def set_attributes
     @attributes = permitted_params.email_preferences
-    # @attributes[:days_to_send] = @attributes[:days_to_send].reject(&:blank?)
+    if @email_preferences.user.beta_feature_enabled('activity_summary')
+      @attributes[:days_to_send] = @attributes[:days_to_send].reject(&:blank?)
     @group_email_preferences = @attributes.delete(:group_email_preferences)
   end
 
