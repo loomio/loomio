def4b80f3fc9e27e0c01a11792f2b5b36e1be83c
James Kiesel <james.kiesel@gmail.com>
2014-02-21 12:30:12 +1300

    Add destination dropdown to Contact menu

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -1,5 +1,14 @@
 class ContactMessage < ActiveRecord::Base
+  
+  DESTINATIONS = [
+    ['General Inquiries',                             'contact@loomio.org'],
+    ['Tech support / Bug reports / Feature requests', 'tech@loomio.org'],
+    ['Media inquiries and leads',                     'press@loomio.org'],
+    ['High touch and Loomio Plus sales leads',        'sales@loomio.org'],
+    ['Translation inquiries',                         'translations@loomio.org']
+  ]
+  
   belongs_to :user
-  validates_presence_of :name, :message
+  validates_presence_of :name, :message, :destination
   validates :email, presence: true, email: true
 end
