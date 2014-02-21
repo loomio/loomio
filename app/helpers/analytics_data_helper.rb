6697b11953d61dd253470608e5bc799b505c0955
Simon Tegg <simontegg@roobixtube.local>
2014-02-21 16:36:47 +1300

    analytics data values are now strings

--- app/helpers/analytics_data_helper.rb
+++ app/helpers/analytics_data_helper.rb
@@ -1,27 +1,27 @@
 module AnalyticsDataHelper
 
   def analytics_data_tag
-    tag = {user_id: 'undefined',
-                 cohort: 'undefined'}
+    tag = {"user_id" => 'undefined',
+                 "cohort" => 'undefined'}
 
     if current_user
-     tag.merge!({user_id: current_user.id,
-                       cohort: current_user.created_at.strftime("%Y-%m")})
+     tag.merge!({"user_id" => current_user.id.to_s,
+                       "cohort" => current_user.created_at.strftime("%Y-%m")})
     end
 
     if @discussion.present?
-      tag.merge!({ discussion_id: @discussion.id })
+      tag.merge!({ "discussion_id" => @discussion.id.to_s })
       @group = @discussion.group if @group.nil?
     end
 
     if @group.present? and @group.persisted?
       tag.merge!({
-        group_id: @group.id,
-        group_parent_id: (@group.parent_id ? @group.parent_id : 'undefined'),
-        top_group: (@group.parent_id ? @group.parent_id : @group.id),
-        group_memberships_count: @group.memberships_count,
-        group_privacy: @group.privacy,
-        group_cohort: @group.created_at.strftime("%Y-%m")
+        "group_id" => @group.id.to_s,
+        "group_parent_id" => (@group.parent_id.to_s ? @group.parent_id.to_s : 'undefined'),
+        "top_group" => (@group.parent_id.to_s ? @group.parent_id.to_s : @group.id.to_s),
+        "group_memberships_count" => @group.memberships_count.to_s,
+        "group_privacy" => @group.privacy,
+        "group_cohort" => @group.created_at.strftime("%Y-%m")
       })
     end
 
