430e32dd4230ff53427b60e0194ae1c2944f7fcd
john irving <whimful@gmail.com>
2014-02-24 15:28:22 +1300

    translation part 1

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -1,14 +1,16 @@
 class ContactMessage < ActiveRecord::Base
 
   DESTINATIONS = [
-    ['General enquiries',                             'contact@loomio.org'],
-    ['Tech support / bug reports / feature requests', 'tech@loomio.org'],
-    ['Media enquiries',                               'press@loomio.org'],
-    ['Sales and customer enquiries',                  'sales@loomio.org'],
-    ['Translation enquiries',                         'translations@loomio.org']
+    'contact',
+    'tech',
+    'press',
+    'sales',
+    'translations'
   ]
 
   belongs_to :user
   validates_presence_of :name, :message, :destination
   validates :email, presence: true, email: true
+  validates_inclusion_of :destination, in: DESTINATIONS
+
 end
