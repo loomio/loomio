dd36cb0b7ed39d677eac48e1d9d7e303521daefe
john irving <whimful@gmail.com>
2014-02-24 13:52:36 +1300

    quick language tweak and style consistency fix

--- app/models/contact_message.rb
+++ app/models/contact_message.rb
@@ -1,13 +1,13 @@
 class ContactMessage < ActiveRecord::Base
-  
+
   DESTINATIONS = [
-    ['General Inquiries',                             'contact@loomio.org'],
-    ['Tech support / Bug reports / Feature requests', 'tech@loomio.org'],
-    ['Media inquiries and leads',                     'press@loomio.org'],
-    ['High touch and Loomio Plus sales leads',        'sales@loomio.org'],
-    ['Translation inquiries',                         'translations@loomio.org']
+    ['General enquiries',                             'contact@loomio.org'],
+    ['Tech support / bug reports / feature requests', 'tech@loomio.org'],
+    ['Media enquiries',                               'press@loomio.org'],
+    ['Sales and customer enquiries',                  'sales@loomio.org'],
+    ['Translation enquiries',                         'translations@loomio.org']
   ]
-  
+
   belongs_to :user
   validates_presence_of :name, :message, :destination
   validates :email, presence: true, email: true
