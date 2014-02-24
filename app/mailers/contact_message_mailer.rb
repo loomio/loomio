559065fcd89bdce28fff4ea8f255567760de1292
john irving <whimful@gmail.com>
2014-02-24 15:28:22 +1300

    translation part 1

--- app/mailers/contact_message_mailer.rb
+++ app/mailers/contact_message_mailer.rb
@@ -1,11 +1,12 @@
 class ContactMessageMailer < BaseMailer
   default :to => "contact@loomio.org"
   def contact_message_email(contact_message)
-    @name = contact_message.name
+    # binding.pry
     @email = contact_message.email
+    @name = contact_message.name
     @message = contact_message.message
     @user_id = contact_message.user_id
-    @destination = contact_message.destination
+    @destination = contact_message.destination + '@' + ENV.fetch('CANONICAL_HOST')
     mail(from: "#{@name} <#{@email}>", to: @destination, subject: "Enquiry - #{@name}")
   end
 end
