44f8d73887c59f44413561fedaedca56cb6c55cc
James Kiesel <james.kiesel@gmail.com>
2014-02-27 11:07:50 +1300

    Add environment_variables.yml config file

--- config/initializers/environment_variables.rb
+++ config/initializers/environment_variables.rb
@@ -1,24 +1,16 @@
 unless Rails.env.test?
-  required_env_vars = [] # can be expanded in future iteration                    
-  missing_env_vars = required_env_vars.select { |key| !ENV.has_key?(key.to_s) }
-  error_message = "You are missing the following required environment variables: #{missing_env_vars}\n Please set them to continue."
-  raise error_message if missing_env_vars.any?
-  
-  optional_env_vars = {'SECRET_COOKIE_TOKEN' => '',
-                       'EXCEPTION_RECIPIENT' => '',
-                       'AWS_ACCESS_KEY_ID' => '',
-                       'AWS_SECRET_ACCESS_KEY' => '',
-                       'AWS_ATTACHMENT_BUCKET' => '',
-                       'SENDGRID_USERNAME' => '',
-                       'SENDGRID_PASSWORD' => '',
-                       'BING_TRANSLATION_APPID' => '',
-                       'BING_TRANSLATION_SECRET' => '',
-                       'NAVBAR_LOGO_PATH' => 'top-bar-loomio.png',
-                       'NAVBAR_CONTRIBUTE' => 'show',
-                       'CANONICAL_HOST' => 'loomio.org',
-                       'FOG_DIRECTORY' => '',
-                       'TAG_MANAGER_ID' => '' }
-  optional_env_vars.keys.each do |key|
-    ENV[key] ||= optional_env_vars[key]
+  vars = YAML.load_file("#{Rails.root}/config/environment_variables.yml")
+    
+  missing = ->(array) { array.select { |key| !ENV.has_key? key.to_s } if array.present? }
+  missing_required = missing.call vars['required']
+  missing_optional = missing.call vars['optional']
+
+  raise "You are missing the following required environment variables: #{missing_required}\n" + 
+        "Please set them to continue." if missing_required.present?
+  Rails.logger.info "You are missing the following optional environment variables: #{missing_optional.keys}\n" +
+                    "They have been set to defaults automatically."
+                    
+  missing_optional.keys.each do |key|
+    ENV[key] ||= missing_optional[key]
   end
 end
