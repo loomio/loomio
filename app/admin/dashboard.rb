a7d73c7bb561d9b6e693ef1d50eddb619f53a0b4
Simon Tegg <simon@loomio.org>
2014-03-06 18:37:44 +1300

    monthly active users in admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -20,12 +20,45 @@ def print_month_totals(model)
   month_totals.join("<br>")
 end
 
+
+def print_active_users()
+  counts = Event.connection.select_all(%q{
+    SELECT M.action_year, M.action_month, COUNT(M.user_id) active_users
+      FROM (
+        SELECT DISTINCT user_id, extract(year from created_at) action_year, extract(month from created_at) action_month FROM comments
+        UNION
+        SELECT DISTINCT user_id, extract(year from created_at) action_year, extract(month from created_at) action_month FROM votes
+        ) AS M
+      GROUP BY
+        M.action_year,
+        M.action_month
+      ORDER BY
+        M.action_year DESC,
+        M.action_month DESC
+    })
+  current_month_active = counts[0]['active_users']
+  month_totals = []
+  month_totals << "<h1> #{current_month_active} </h1><table>"
+  counts.each do |m|
+    month = Date.new(m['action_year'].to_i, m['action_month'].to_i, 1).strftime('%Y - %b')
+    active_users = m['active_users']
+    month_totals << "<tr><td> #{month} </td><td> #{active_users} </td><td>"
+  end
+  month_totals << "</table>"
+  month_totals.join('')
+end
+
 ActiveAdmin.register_page "Dashboard" do
 
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
     columns do
+      column do
+        panel "Monthly Active Users" do
+          div { print_active_users().html_safe }
+        end
+      end
       column do
         panel "Groups" do
           h1 { Group.count }
@@ -35,7 +68,7 @@ ActiveAdmin.register_page "Dashboard" do
         end
       end
       column do
-        panel "Users" do
+        panel "User Accounts" do
           h1 { User.count }
           div { "New today: #{User.where('created_at >= ?', 24.hours.ago).count}"}
           div { print_month_totals("users").html_safe}
