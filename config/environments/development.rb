95b668cb339b6c02871a409732d698024725b01a
Robert Guthrie <rob@guthr.ie>
2014-03-09 21:13:13 +1300

    ok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Loomio::Application.configure do
 
   # Show full error reports and disable caching
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = true
+  config.action_controller.perform_caching = false
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
