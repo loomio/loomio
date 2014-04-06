6673e06c9b48c5ee532aaf4e552003b5fa934483
Robert Guthrie <rob@guthr.ie>
2014-04-06 18:21:25 +1200

    add discussion iframe

---
+++ app/models/concerns/beta_features.rb
@@ -0,0 +1,20 @@
+require 'active_support/concern'
+
+module BetaFeatures
+  extend ActiveSupport::Concern
+
+  included do
+    serialize :enabled_beta_features, Array
+    #validates_inclusion_of :enabled_beta_features, in: AVAILABLE_BETA_FEATURES
+  end
+
+  #module ClassMethods
+    #def available_beta_features
+      #AVAILABLE_BETA_FEATURES || []
+    #end
+  #end
+
+  def beta_feature_enabled?(name)
+    enabled_beta_features.include?(name)
+  end
+end
