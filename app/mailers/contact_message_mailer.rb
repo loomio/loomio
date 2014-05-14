863fccaba0ccae1821f893fde3d82618ae3047e0
Richard Bartlett <rich@loomio.org>
2014-05-15 11:03:32 +1200

    send mail to intercom

--- app/mailers/contact_message_mailer.rb
+++ app/mailers/contact_message_mailer.rb
@@ -1,11 +1,10 @@
 class ContactMessageMailer < BaseMailer
-  default :to => "contact@loomio.org"
+  default :to => "#{ENV["INTERCOM_APP_ID"]}@incoming.intercom.io"
   def contact_message_email(contact_message)
     @email = contact_message.email
     @name = contact_message.name
     @message = contact_message.message
     @user_id = contact_message.user_id
-    @destination = contact_message.destination + '@' + ENV.fetch('CANONICAL_HOST', 'loomio.org')
-    mail(from: "#{@name} <#{@email}>", to: @destination, subject: "Enquiry - #{@name}")
+    mail(from: "#{@name} <#{@email}>", subject: "Enquiry - #{@name}")
   end
 end
