675443fd650b6588286633648f33ad37a53aee9d
Robert Guthrie <rob@guthr.ie>
2014-05-20 15:38:59 +1200

    add caching back to themescontroller

--- app/controllers/theme_assets_controller.rb
+++ app/controllers/theme_assets_controller.rb
@@ -4,9 +4,11 @@ class ThemeAssetsController < ApplicationController
   def show
     @theme = Theme.find(params[:id])
     #fresh_when last_modified: @product.published_at.utc, etag: @product
-    respond_to do |format|
-      format.css { render text: @theme.style, content_type: 'text/css' }
-      format.js { render text: @theme.javascript, content_type: 'text/javascript' }
+    if stale?(etag: @theme, last_modified: @theme.updated_at, public: true)
+      respond_to do |format|
+        format.css { render text: @theme.style, content_type: 'text/css' }
+        format.js { render text: @theme.javascript, content_type: 'text/javascript' }
+      end
     end
   end
 end
