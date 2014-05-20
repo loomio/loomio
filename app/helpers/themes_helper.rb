41990170794fb61744a065bab33400ca7cc5d84f
Robert Guthrie <rob@guthr.ie>
2014-05-20 14:41:33 +1200

    add js support to themes

--- app/helpers/themes_helper.rb
+++ app/helpers/themes_helper.rb
@@ -5,7 +5,17 @@ module ThemesHelper
     end
 
     if @group and @group.theme.present?
-      stylesheet_link_tag theme_css_path(@group.theme)
+      stylesheet_link_tag theme_assets_path(@group.theme)
+    end
+  end
+
+  def theme_javascript_include_tag
+    if GroupSubdomainConstraint.matches?(request)
+      @group = Group.published.find_by_subdomain(request.subdomain)
+    end
+
+    if @group and @group.theme.present?
+      javascript_include_tag theme_assets_path(@group.theme)
     end
   end
 end
