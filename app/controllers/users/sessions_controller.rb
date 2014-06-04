a5df04118f50cebb22a93d58cb9830dab8099b1c
Robert Guthrie <rob@guthr.ie>
2014-06-05 09:16:27 +1200

    support logging in users when parse password is correct but different to loomio password

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -18,9 +18,13 @@ class Users::SessionsController < Devise::SessionsController
     if ENV['PARSE_ID'] and request.subdomain == ENV['PARSE_SUBDOMAIN']
       unless warden.authenticate?(auth_options)
         if user_hash = ParseAuth.login_with_email_and_password(params[:user][:email], params[:user][:password])
-          u = User.create!(name: "#{user_hash['firstname']} #{user_hash['lastname']}",
-                           email: params[:user][:email],
-                           password: params[:user][:password])
+
+          unless u = User.find_by_email(params[:user][:email])
+            u = User.create!(name: "#{user_hash['firstname']} #{user_hash['lastname']}",
+                             email: params[:user][:email],
+                             password: params[:user][:password])
+          end
+
           g = Group.find(ENV['PARSE_GROUP_ID'])
           g.add_member!(u)
           flash[:notice] = t(:'devise.sessions.signed_in')
