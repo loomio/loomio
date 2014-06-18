f24b0eae430367577c97fcadcfc8ab88e24a0064
Richard Bartlett <rich@loomio.org>
2014-06-18 15:35:01 +1200

    Add styles to missed-yesterday email

--- app/controllers/wall_controller.rb
+++ app/controllers/wall_controller.rb
@@ -4,7 +4,7 @@ class WallController < BaseController
     # discussions that had activity in the last 24 hours
 
     groups = current_user.inbox_groups
-    @time_since = 3.day.ago
+    @time_since = 1.days.ago
 
     @discussions_by_group = Queries::VisibleDiscussions.new(user: current_user,
                                                             groups: groups).
