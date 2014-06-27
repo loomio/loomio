78647a35b84e125c772dc5a58fde6639666253a3
Elias <elias.westerberg@gmail.com>
2014-06-27 20:58:13 +0200

    Layouts got metatag for responsive view

--- db/schema.rb
+++ db/schema.rb
@@ -347,17 +347,15 @@ ActiveRecord::Schema.define(:version => 20140622232955) do
     t.string   "key"
     t.boolean  "can_start_group",                    :default => true
     t.integer  "category_id"
-    t.text     "enabled_beta_features"
-    t.string   "subdomain"
-    t.integer  "theme_id"
     t.boolean  "is_visible_to_public",               :default => false,          :null => false
     t.boolean  "is_visible_to_parent_members",       :default => false,          :null => false
     t.string   "discussion_privacy_options",                                     :null => false
     t.boolean  "members_can_add_members",            :default => false,          :null => false
     t.string   "membership_granted_upon",                                        :null => false
+    t.text     "enabled_beta_features"
+    t.string   "subdomain"
+    t.integer  "theme_id"
     t.boolean  "email_notification_default",         :default => true,           :null => false
-    t.boolean  "members_can_edit_discussions",       :default => true,           :null => false
-    t.boolean  "motions_can_be_edited",              :default => false,          :null => false
     t.string   "cover_photo_file_name"
     t.string   "cover_photo_content_type"
     t.integer  "cover_photo_file_size"
@@ -426,7 +424,7 @@ ActiveRecord::Schema.define(:version => 20140622232955) do
     t.datetime "archived_at"
     t.integer  "inbox_position",                    :default => 0
     t.boolean  "admin",                             :default => false, :null => false
-    t.boolean  "is_suspended",                      :default => false, :null => false
+    t.boolean  "is_suspended"
   end
 
   add_index "memberships", ["group_id"], :name => "index_memberships_on_group_id"
