738f2831ea67b1262db6de928ee3e80f43b5007b
Robert Guthrie <rob@guthr.ie>
2014-07-21 16:09:28 +1200

    add yahoo for contacts

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -3,7 +3,7 @@ require "omnicontacts"
 Rails.application.middleware.use OmniContacts::Builder do
   importer :gmail, ENV['OMNI_CONTACTS_GOOGLE_KEY'], ENV['OMNI_CONTACTS_GOOGLE_SECRET'], max_results: 1000
   importer :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], max_results: 1000
-  #importer :yahoo, "consumer_id", "consumer_secret", {:callback_path => '/callback'}
+  importer :yahoo, ENV['YAHOO_KEY'], ENV['YAHOO_SECRET']
   #importer :hotmail, "client_id", "client_secret"
 end
 
