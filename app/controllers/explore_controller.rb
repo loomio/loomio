1112f876d682b54983e9a053cb6d3a0c54c8248c
Richard Bartlett <rich@loomio.org>
2014-07-23 17:45:03 +1200

    make explore page navigable and pretty

--- app/controllers/explore_controller.rb
+++ app/controllers/explore_controller.rb
@@ -1,4 +1,6 @@
 class ExploreController < ApplicationController
+  helper_method :selected_category_path, :category_names_and_paths
+
   def search
     @query = params[:query]
     @groups = Group.visible_to_public.
@@ -7,12 +9,27 @@ class ExploreController < ApplicationController
   end
 
   def index
-    @groups = Group.visible_on_explore_front_page.preload(:category)
-    @groups_by_category = @groups.group_by(&:category)
+    @groups = Group.visible_on_explore_front_page
   end
 
   def category
     @category = Category.find(params[:id])
     @groups = Group.visible_to_public.in_category(@category)
+  end
+
+  private
+
+  def category_names_and_paths
+    @categories = Category.all
+    names_and_paths = @categories.map{|c| [I18n.t(c.translatable_name), category_explore_path(c)]}
+    names_and_paths.prepend [t(:"group_categories.all"), explore_path]
+  end
+
+  def selected_category_path
+    if @category
+      category_explore_path(@category)
+    else
+      nil
+    end
   end
 end
