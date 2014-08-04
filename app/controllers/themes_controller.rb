56866f44b0d1226eff08ca74b54b2edcce63bb2d
Robert Guthrie <rob@guthr.ie>
2014-08-04 17:50:49 +1200

    skip authenticity check for themejs,css

--- app/controllers/themes_controller.rb
+++ app/controllers/themes_controller.rb
@@ -1,33 +1,35 @@
 class ThemesController < ApplicationController
-  def new
-    @theme = Theme.new
-  end
+  skip_before_action :verify_authenticity_token
 
-  def create
-    @theme = Theme.new(permitted_params.theme)
-    if @theme.save
-      redirect_to @theme, notice: t(:'Theme created')
-    else
-      render :new
-    end
-  end
+  #def new
+    #@theme = Theme.new
+  #end
+
+  #def create
+    #@theme = Theme.new(permitted_params.theme)
+    #if @theme.save
+      #redirect_to @theme, notice: t(:'Theme created')
+    #else
+      #render :new
+    #end
+  #end
 
   def show
     load_theme
   end
 
-  def edit
-    load_theme
-  end
+  #def edit
+    #load_theme
+  #end
 
-  def update
-    load_theme
-    if @theme.update_attributes(permitted_params.theme)
-      redirect_to @theme, notice: t(:'Theme updated')
-    else
-      render :edit
-    end
-  end
+  #def update
+    #load_theme
+    #if @theme.update_attributes(permitted_params.theme)
+      #redirect_to @theme, notice: t(:'Theme updated')
+    #else
+      #render :edit
+    #end
+  #end
 
   private
   def load_theme
