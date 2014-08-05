6ca47a0e5b294e82e1aaa7537ee8dd66398130b4
Robert Guthrie <rob@guthr.ie>
2014-08-05 12:36:15 +1200

    fix marketing css

--- config/application.rb
+++ config/application.rb
@@ -72,8 +72,6 @@ module Loomio
     # required for heroku
     config.assets.initialize_on_precompile = false
 
-    config.assets.precompile += %w(ie8.js active_admin.css active_admin.js active_admin/print.css marketing.js marketing.css campaigns.js third_party.css unstructured.css missed_yesterday.css email.css)
-
     config.quiet_assets = true
     config.action_mailer.preview_path = "#{Rails.root}/spec/mailers/previews"
   end
