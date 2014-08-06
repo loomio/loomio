92171916d7e62537a948d632206a7515d493a484
Robert Guthrie <rob@guthr.ie>
2014-08-06 16:45:25 +1200

    add rel nofollow to translate links

--- app/helpers/translations_helper.rb
+++ app/helpers/translations_helper.rb
@@ -3,7 +3,7 @@ module TranslationsHelper
   def translate_link_for(model, css='')
     model_name = model.class.to_s.downcase
     translate_link_text = t(:translate_field, default: :translate_this_string, language: current_locale)
-    link_to translate_link_text, translate_path(model_name, model.id_field), method: :post, remote: true, class: "translate-link #{css}", id: "translate-#{model_name}-#{model.id_field}"
+    link_to translate_link_text, translate_path(model_name, model.id_field), method: :post, remote: true, class: "translate-link #{css}", id: "translate-#{model_name}-#{model.id_field}", rel: :nofollow
   end
 
   def get_translation(translation, field, show_error = true)
