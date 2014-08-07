0129b4ca90bb7b11aae222f5443ad10daa7fd73b
Robert Guthrie <rob@guthr.ie>
2014-08-07 16:41:48 +1200

    add a mess, just to clean it up

--- app/models/events/new_comment.rb
+++ app/models/events/new_comment.rb
@@ -1,6 +1,4 @@
 class Events::NewComment < Event
-  after_create :notify_users!
-
   def self.publish!(comment)
     create!(:kind => "new_comment",
             :eventable => comment,
@@ -10,17 +8,4 @@ class Events::NewComment < Event
   def comment
     eventable
   end
-
-  private
-
-  def notify_users!
-    comment.mentioned_group_members.each do |mentioned_user|
-      Events::UserMentioned.publish!(comment, mentioned_user)
-    end
-    comment.non_mentioned_discussion_participants.each do |non_mentioned_user|
-      #notify!(non_mentioned_user)
-    end
-  end
-
-  handle_asynchronously :notify_users!
 end
