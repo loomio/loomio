b5a22255cd897e181696d43882a7ba4912ec4dd1
Robert Guthrie <rob@guthr.ie>
2014-08-08 17:24:22 +1200

    ok

--- spec/models/events/new_discussion_spec.rb
+++ spec/models/events/new_discussion_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
+# Isolated style
 describe Events::NewDiscussion do
-  let(:discussion){ mock_model(Discussion, group: double(:group), touch: true) }
+  let(:author) { double(:author) }
+  let(:discussion){ double(:discussion, author: author, id: 1) }
 
   describe "::publish!" do
-    let(:event) { double(:event, notify_users!: true) }
-    before { Event.stub(:create!).and_return(event) }
+    let(:event) { double(:event) }
+    let(:dr) { double(:discussion_reader, follow!: true) }
+
+    before do
+      Event.stub(:create!).and_return(event)
+      DiscussionReader.stub(:for).and_return(dr)
+    end
 
     it 'creates an event' do
       Event.should_receive(:create!).with(kind: 'new_discussion',
@@ -14,43 +21,18 @@ describe Events::NewDiscussion do
       Events::NewDiscussion.publish!(discussion)
     end
 
-    it 'returns an event' do
+    it 'returns the event' do
       Events::NewDiscussion.publish!(discussion).should == event
     end
-  end
 
-  context "after event has been published" do
-    let(:user){ mock_model(User) }
-    let(:event) { Events::NewDiscussion.new(kind: "new_discussion",
-                                               eventable: discussion,
-                                               discussion_id: discussion.id) }
-    before do
-      discussion.stub(:group_members_without_discussion_author).and_return([user])
-      user.stub(:email_notifications_for_group?).and_return(false)
-      UserMailer.stub_chain(:new_discussion, :deliver)
+    it "enfollows the discussion.author" do
+      expect(DiscussionReader).to receive(:for).with(discussion: discussion,
+                                                     user: user).and_return(dr)
+      expect(dr).to receive(:follow!)
     end
 
-    context 'if user is subscribed to group notification emails' do
-      before do
-        user.should_receive(:email_notifications_for_group?).with(discussion.group).and_return(true)
-      end
-
-      it 'emails group_members_without_motion_author new_motion_created' do
-        UserMailer.should_receive(:new_discussion).with(discussion, user)
-        event.save
-      end
-    end
-
-    context 'if user is not subscribed to group notification emails' do
-      before do
-        user.should_receive(:email_notifications_for_group?).with(discussion.group).and_return(false)
-        event.save
-      end
-
-      it 'does not email new motion created' do
-        UserMailer.should_not_receive(:new_discussion)
-        event.save
-      end
+    it "emails followers of the discussion" do
+      expect(ThreadMailer).to receive(:new_discussion).with(discussion, follower)
     end
   end
 end
