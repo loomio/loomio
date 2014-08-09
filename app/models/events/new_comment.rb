218a06f60b4d1f1e614e769837f25d75b357862b
Robert Guthrie <rob@guthr.ie>
2014-08-09 14:08:06 +1200

    bah.. this is not really that cool at all

--- app/models/events/new_comment.rb
+++ app/models/events/new_comment.rb
@@ -1,40 +1,26 @@
 class Events::NewComment < Event
   def self.publish!(comment)
-    create!(:kind => "new_comment",
-            :eventable => comment,
-            :discussion_id => comment.discussion.id)
+    event = create!(kind: "new_comment",
+                    eventable: comment,
+                    discussion_id: comment.discussion.id)
 
-    # Enfollow the comment author
     DiscussionReader.for(user: comment.author,
                          discussion: comment.discussion).follow!
 
-    event.delay.create_mention_events!
-    event.delay.email_followers!
+    comment.mentioned_users.each do |mentioned_user|
+      Events::UserMentioned.publish!(comment, mentioned_user)
+    end
+
+    comment.followers_without_mentioned_users_or_author.
+            email_followed_threads.each do |follower|
+        ThreadMailer.delay.new_comment(comment, follower)
+    end
+
+    event
   end
 
   def comment
     eventable
-  end
-
-  private
-
-  def create_mention_events!
-    # create any mention events
-    comment.mentioned_group_members.each do |mentioned_user|
-      Events::UserMentioned.publish!(comment, mentioned_user)
-    end
-  end
-
-  def email_followers!
-    followers = comment.discussion.followers -
-                comment.mentioned_group_members -
-                [comment.author]
-    #comment.discussion.followers.where('id not in (?)')
-    followers.each do |follower|
-      if follower.email.followed_threads?
-        ThreadMailer.delay.new_comment(follower, comment)
-      end
-    end
   end
 end
 
