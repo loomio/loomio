ca0a49022a9d30e2a35ff630c893cbf9e6ab3398
Robert Guthrie <rob@guthr.ie>
2014-08-12 12:39:16 +1200

    bump assets again

--- config/application.rb
+++ config/application.rb
@@ -57,7 +57,7 @@ module Loomio
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
+    config.assets.version = '1.2'
 
     config.action_mailer.default_url_options = {
       host: ENV['CANONICAL_HOST']
