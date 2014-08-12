85d368217f2b4c10e8be4bf6992b5c984cd3ba1a
Robert Guthrie <rob@guthr.ie>
2014-08-12 12:52:33 +1200

    does this change anything\?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Loomio::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
-  config.assets.css_compressor = :sass
+  #config.assets.css_compressor = :sass
   config.assets.js_compressor = :uglifier
 
   # Generate digests for assets URLs
