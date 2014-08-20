c1f96e9b164f3213a3aa682c087d6e135b96c44a
Richard Bartlett <rich@loomio.org>
2014-08-20 14:30:14 +1200

    ui

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,19 +1,23 @@
 class DashboardController <  GroupBaseController
+  include ApplicationHelper
+
   def show
     @discussions = GroupDiscussionsViewer.for(user: current_user)
 
-    if params[:following]
+    if sifting_unread?
+      @discussions = @discussions.unread
+    end
+
+    if sifting_followed?
       @discussions = @discussions.following
     end
 
-    
     @discussions = @discussions.joined_to_current_motion.
                                 preload(:current_motion, {:group => :parent}).
                                 order('motions.closing_at ASC, last_comment_at DESC').
                                 page(params[:page]).per(20)
 
     build_discussion_index_caches
-
 
     # on discussion, preload author, current_motion -> author, votes
     # load discussion_readers for discussion, user and store in a hash key: discussion_id
