df5c818e86cf7caad110da9a6dc4222be8ccbeb8
Robert Guthrie <rob@guthr.ie>
2014-08-22 09:39:31 +1200

    fix sum dem cukes

--- app/models/events/motion_name_edited.rb
+++ app/models/events/motion_name_edited.rb
@@ -2,7 +2,7 @@ class Events::MotionNameEdited < Event
   def self.publish!(motion, editor)
     create!(kind: "motion_name_edited",
             eventable: motion,
-            dicussion_id: motion.discussion.id,
+            discussion_id: motion.discussion.id,
             user: editor)
   end
 end
