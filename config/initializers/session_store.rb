0f8bbbd4a919abcfb89754b29d19e0b85998f5a8
Robert Guthrie <rob@guthr.ie>
2014-08-28 10:23:35 +1200

    remove domain from session store thing

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Loomio::Application.config.session_store :cookie_store, key: '_loomio', domain: :all
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Loomio::Application.config.session_store :active_record_store
+Loomio::Application.config.session_store :cookie_store, key: '_loomio'
