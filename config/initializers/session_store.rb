ae251c13e44d2370f9ac88498bc14db17dfcea04
Robert Guthrie <rob@guthr.ie>
2014-08-28 10:30:25 +1200

    fix session tld_length I think

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Loomio::Application.config.session_store :cookie_store, key: '_loomio'
+Loomio::Application.config.session_store :cookie_store, key: '_loomio', domain: :all, tld_length: (ENV['TLD_LENGTH'] || 1).to_i
