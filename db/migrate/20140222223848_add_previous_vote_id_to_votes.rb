269ae23ef2cc9bf69da8aa50e3cc1011953441f9
Robert Guthrie <rob@guthr.ie>
2014-08-28 15:19:43 +1200

    prevent migrations from crying

--- db/migrate/20140222223848_add_previous_vote_id_to_votes.rb
+++ db/migrate/20140222223848_add_previous_vote_id_to_votes.rb
@@ -1,8 +1,7 @@
 class AddPreviousVoteIdToVotes < ActiveRecord::Migration
   def up
-    add_column :votes, :previous_vote_id, :integer
+    add_column :votes, :previous_vote_id, :integer unless column_exists? :votes, :previous_vote_id
   end
-  
   def down
     remove_column :votes, :previous_vote_id
   end
