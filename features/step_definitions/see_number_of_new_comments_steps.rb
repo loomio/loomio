d1be771fba578021e03ba1cf4e0f2e6f440a429c
Robert Guthrie <rob@guthr.ie>
2014-09-15 23:59:17 +1200

    fix de cukes

--- features/step_definitions/see_number_of_new_comments_steps.rb
+++ features/step_definitions/see_number_of_new_comments_steps.rb
@@ -12,7 +12,8 @@ When /^I visit the dashboard$/ do
 end
 
 Then(/^I should see that the discussion has (\d+) unread$/) do |arg1|
-  find(".activity-count").should have_content(arg1)
+  view_screenshot
+  find(".unread-comments-count").should have_content(arg1)
 end
 
 Then /^I should see the number of comments the discussion has$/ do
