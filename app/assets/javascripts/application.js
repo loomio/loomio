a2e4be3deec0d2c70b2a247408257e1b87b7c603
Robert Guthrie <rob@guthr.ie>
2014-10-01 08:07:09 +1300

    fix mobile ie

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -43,3 +43,13 @@
 //= require explore
 //= require keyboard_shortcuts
 //= require locale_selector
+
+if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
+  var msViewportStyle = document.createElement('style')
+  msViewportStyle.appendChild(
+    document.createTextNode(
+      '@-ms-viewport{width:auto!important}'
+    )
+  )
+  document.querySelector('head').appendChild(msViewportStyle)
+}
