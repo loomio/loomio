67dfeaddea4cf65c74b558e7b89cecfe1477c127
Robert Guthrie <rob@guthr.ie>
2014-10-01 14:48:08 +1300

    simplify robots controller

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -1,24 +1,11 @@
 class RobotsController < ActionController::Base
-
   respond_to :text
 
   def show
-    render robot_file
-  end
-
-  def destroy_all_humans
-    false
-    # whew!
-  end
-
-  private
-
-  def robot_file(allow_robots = ENV['ALLOW_ROBOTS'])
-    if allow_robots == true || allow_robots =~ /^y|^t|1/i
-      :public_robots
-    else 
-      :private_robots
+    if ENV['ALLOW_ROBOTS'].to_i == 1
+      render :public_robots
+    else
+      render :private_robots
     end
   end
-
 end
