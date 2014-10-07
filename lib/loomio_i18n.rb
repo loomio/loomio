13aed0dd75f71903a96d7516d841cfbf501ac800
mix irving <whimful@gmail.com>
2014-10-07 14:01:27 +1300

    make zh-HK detectable & offer zh-TW

--- lib/loomio_i18n.rb
+++ lib/loomio_i18n.rb
@@ -3,7 +3,7 @@ module Loomio
 
     SELECTABLE_LOCALES = %i( en be-BY bg-BG ca cs zh-TW de eo es el fr id it hu ja ko ml nl-NL pt-BR ro sr sr-RS sv vi tr uk )
 
-    DETECTABLE_LOCALES = SELECTABLE_LOCALES + %i( be pt zh )
+    DETECTABLE_LOCALES = SELECTABLE_LOCALES + %i( be pt zh zh-HK )
 
     # only for display purposes, please don't depend on this in the system
     TEST_LOCALES = %i( ar cmn hr da fi gl ga-IE km mk mi fa-IR pl pt-PT ru sl te )
@@ -12,7 +12,13 @@ module Loomio
                   :'pt-PT' => :'pt-BR',
                   :be      => :'be-BY',
                   :pt      => :'pt-BR',
-                  :zh      => :'zh-TW'
+                  :zh      => :'zh-TW',
+                  :'zh-HK' => :'zh-TW'
                  }
+
+    # for locales which are only serving as redirects, make sure to add to  :
+    #   i) DETECTABLE_LOCALES
+    #  ii) FALLBACKS
+    # iii) a dummy yaml e.g. config/locales/fallback.zh-HK.yml
   end
 end
