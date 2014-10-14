18b01f10b269f1d3bd71a70f7965b07ac11372a8
mix irving <whimful@gmail.com>
2014-10-15 12:12:16 +1300

    launch da, udpate (de, fr)

--- lib/loomio_i18n.rb
+++ lib/loomio_i18n.rb
@@ -1,12 +1,9 @@
 module Loomio
   module I18n
 
-    SELECTABLE_LOCALES = %i( en be-BY bg-BG ca cs zh-TW de eo es el fr id it hu ja ko ml nl-NL pt-BR ro sr sr-RS sv vi tr uk )
+    SELECTABLE_LOCALES = %i( en be-BY bg-BG ca cs zh-TW da de eo es el fr id it hu ja ko ml nl-NL pt-BR ro sr sr-RS sv vi tr uk )
 
     DETECTABLE_LOCALES = SELECTABLE_LOCALES + %i( be pt zh zh-HK )
-
-    # only for display purposes, please don't depend on this in the system
-    TEST_LOCALES = %i( ar cmn hr da fi gl he ga-IE km mk mi fa-IR pl pt-PT ru sl te )
 
     FALLBACKS = { :ca      => :es,
                   :'pt-PT' => :'pt-BR',
@@ -20,5 +17,10 @@ module Loomio
     #   i) DETECTABLE_LOCALES
     #  ii) FALLBACKS
     # iii) a dummy yaml e.g. config/locales/fallback.zh-HK.yml
+
+
+    # only for display purposes, please don't depend on this in the system
+    TEST_LOCALES = %i( ar cmn hr fi gl he ga-IE km mk mi fa-IR pl pt-PT ru sl te )
+    #
   end
 end
