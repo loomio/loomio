28c0ff0aa0ad729c6756286f65c4cc089d18c28c
James Kiesel <james.kiesel@gmail.com>
2014-10-21 22:14:50 +1300

    Display existing attachments in comments

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -4,8 +4,7 @@ class CommentSerializer < ActiveModel::Serializer
 
   has_one :author, serializer: UserSerializer, root: 'users'
   has_many :likers, serializer: UserSerializer, root: 'users'
-
-  #has_one :parent, serializer: CommentSerializer, root: 'comments'
+  has_many :attachments, serializer: AttachmentSerializer, root: 'attachments'
 
   def filter(keys)
     keys.delete(:parent) unless object.parent.present?
