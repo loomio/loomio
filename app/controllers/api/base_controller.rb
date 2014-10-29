66d230e997041b582472ee6ad76446d593bea532
James Kiesel <james.kiesel@gmail.com>
2014-10-29 19:38:35 +1300

    More WIP

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,5 +1,6 @@
 class API::BaseController < ActionController::Base
   include CurrentUserHelper
+  include ::ProtectedFromForgery
   skip_after_filter :intercom_rails_auto_include
   after_filter :increment_measurement
   respond_to :json
