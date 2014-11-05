a8c52d8bf16428f4a51c940789a79c9946f44daa
Robert Guthrie <rob@guthr.ie>
2014-11-06 11:21:07 +1300

    delete attachments when deleting comments

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -4,6 +4,8 @@ class Attachment < ActiveRecord::Base
 
   validates_presence_of :filename, :location, :user_id
 
+  before_destroy :delete_from_storage
+
   def is_an_image?
     %w[jpg jpeg png gif].include?(filetype)
   end
@@ -19,6 +21,16 @@ class Attachment < ActiveRecord::Base
   def filetype
     filename.split('.').last.downcase
   end
+
+  def delete_from_storage
+    storage = Fog::Storage.new({aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+                                aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+                                provider: 'AWS'})
+    bucket = storage.directories.get(ENV['AWS_ATTACHMENTS_BUCKET'])
+    file = bucket.files.get(URI(URI.encode(self.location)).path.gsub(/^\//, ''))
+    file.destroy
+  end
+
 end
 
 
