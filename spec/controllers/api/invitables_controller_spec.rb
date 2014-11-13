1ec7875184147a138b2e56af5b1698eeafef0306
James Kiesel <james.kiesel@gmail.com>
2014-11-13 14:37:07 +1300

    Improve invitables search some

--- spec/controllers/api/invitables_controller_spec.rb
+++ spec/controllers/api/invitables_controller_spec.rb
@@ -6,8 +6,9 @@ describe API::InvitablesController do
   let(:contact_named_bang) { create :contact, user: user, name: "Bang Wharf" }
   let(:contact_email_biff) { create :contact, user: user, email: "biff@wark.com" }
   let(:user_named_biff) { create :user, name: "Biff Bones" }
+  let(:user_last_named_biff) { create :user, name: "Bones Biff" }
+  let(:user_username_biff) { create :user, username: 'biffcake' }
   let(:user_named_bang) { create :user, name: "Bang Whamfist" }
-  let(:user_email_biff) { create :user, email: "biff@wark.com" }
   let(:alien_named_biff) { create :user, name: "Biff Beef" }
   let(:group) { create :group }
   let(:group_named_biff) { create :group, name: "Biff Arang" }
@@ -18,7 +19,7 @@ describe API::InvitablesController do
     stub_request(:post, "http://localhost:9292/faye").to_return(status: 200)
     group.admins << user
     group.users  << user_named_biff
-    group_named_biff.users << [user, user_named_biff, user_named_bang, user_email_biff]
+    group_named_biff.users << [user, user_named_biff, user_named_bang, user_last_named_biff, user_username_biff]
     group_named_bang.users << user
     sign_in user
   end
@@ -30,66 +31,55 @@ describe API::InvitablesController do
       it 'returns a users contacts filtered by query on name' do
         contact_named_biff; contact_named_bang
         get :index, group_id: group.id, q: 'biff', format: :json
-        json = JSON.parse(response.body)
-        expect(json.keys).to include *(%w[invitables])
-        names     = json['invitables'].map { |i| i['name'] }
-        subtitles = json['invitables'].map { |i| i['subtitle'] }
 
+        names = invitation_names_from response
         expect(names).to     include contact_named_biff.name
         expect(names).to_not include contact_named_bang.name
-        expect(subtitles).to include "<#{contact_named_biff.email}>"
       end
 
       it 'returns a users contacts filtered by query on email' do
         contact_email_biff; contact_named_bang
         get :index, group_id: group.id, q: 'biff', format: :json
-        json = JSON.parse(response.body)
-        expect(json.keys).to include *(%w[invitables])
-        names     = json['invitables'].map { |i| i['name'] }
-        subtitles = json['invitables'].map { |i| i['subtitle'] }
 
+        names = invitation_names_from response
         expect(names).to     include contact_email_biff.name
         expect(names).to_not include contact_named_bang.name
-        expect(subtitles).to include "<#{contact_email_biff.email}>"
       end
 
       it 'returns a users groups filtered by query' do
         get :index, group_id: group.id, q: 'biff', format: :json
-        json = JSON.parse(response.body)
-        expect(json.keys).to include *(%w[invitables])
-        names     = json['invitables'].map { |i| i['name'] }
-        subtitles = json['invitables'].map { |i| i['subtitle'] }
 
+        names = invitation_names_from response
         expect(names).to     include group_named_biff.name
         expect(names).to_not include group_named_bang.name
-        expect(subtitles).to include "Add all members (#{group_named_biff.members.count})"
       end
 
-      it 'returns a users relations filtered by query on name' do
+      it 'returns a users relations filtered by query on first name' do
         user_named_biff; user_named_bang
         get :index, group_id: group.id, q: 'biff', format: :json
-        json = JSON.parse(response.body)
-        expect(json.keys).to include *(%w[invitables])
-        names     = json['invitables'].map { |i| i['name'] }
-        subtitles = json['invitables'].map { |i| i['subtitle'] }
 
+        names = invitation_names_from response
         expect(names).to     include user_named_biff.name
         expect(names).to_not include user_named_bang.name
-        expect(subtitles).to include "I'm a user!"
       end
 
-      it 'returns a users relations filtered by query on email' do
-        user_email_biff; user_named_bang
+      it 'returns a users relations filtered by query on last name' do
+        user_last_named_biff; user_named_bang
         get :index, group_id: group.id, q: 'biff', format: :json
-        json = JSON.parse(response.body)
-        expect(json.keys).to include *(%w[invitables])
-        names     = json['invitables'].map { |i| i['name'] }
-        subtitles = json['invitables'].map { |i| i['subtitle'] }
 
-        expect(names).to     include user_email_biff.name
+        names = invitation_names_from response
+        expect(names).to     include user_last_named_biff.name
         expect(names).to_not include user_named_bang.name
-        expect(subtitles).to include "I'm a user!"
       end
+
+      it 'returns a users relations filtered by query on username' do
+        user_username_biff; user_named_bang
+        get :index, group_id: group.id, q: 'biff', format: :json
+
+        names = invitation_names_from response
+        expect(names).to     include user_username_biff.name
+        expect(names).to_not include user_named_bang.name
+      end       
     end
 
     context 'failure' do
@@ -98,6 +88,12 @@ describe API::InvitablesController do
         expect { get :index, group_id: cant_see_me.id, format: :json }.to raise_error CanCan::AccessDenied
       end
     end
+  end
+
+  def invitation_names_from(response)
+    json = JSON.parse(response.body)
+    expect(json.keys).to include *(%w[invitables])
+    json['invitables'].map { |i| i['name'] }
   end
 
 end
