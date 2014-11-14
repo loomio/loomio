e4e8fab9d45ba865ba5d4817bacb9948d565004a
James Kiesel <james.kiesel@gmail.com>
2014-11-14 19:06:34 +1300

    Add VisibleMembers query

--- extras/queries/visible_members.rb
+++ extras/queries/visible_members.rb
@@ -1,8 +1,8 @@
 class Queries::VisibleMembers < Delegator
 
-  def initialize(group: group, limit: limit)
-    @relation = Membership.where(group: group)
-                          .limit(limit)
+  def initialize(user: nil, group: nil, query: nil, limit: nil)
+    @user, @group, @query, @limit = user, group, query, limit
+    @relation = visible_members_filtered
     super @relation
   end
 
@@ -12,6 +12,28 @@ class Queries::VisibleMembers < Delegator
 
   def __setobj__(obj)
     @relation = obj
+  end
+
+  private
+
+  def visible_members_filtered
+    User.where(id: visible_member_ids)
+        .where("name ilike #{search_term} or username ilike #{search_term}")
+        .limit(@limit)
+  end
+
+  def visible_member_ids
+    @visible_member_ids ||= @user.groups.flat_map { |g| g.member_ids }
+                                        .reject   { |id| group_member_ids.include? id }
+                                        .uniq
+  end
+
+  def group_member_ids
+    @group_member_ids ||= @group.member_ids
+  end
+
+  def search_term
+    @search_term ||= "'%#{@query}%'"
   end
 
 end
