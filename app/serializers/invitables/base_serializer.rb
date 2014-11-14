2847af8c24968885013afb8167fb70a54c22f17d
James Kiesel <james.kiesel@gmail.com>
2014-11-14 22:17:37 +1300

    Remove unneeded cruft from base invitable serializer

--- app/serializers/invitables/base_serializer.rb
+++ app/serializers/invitables/base_serializer.rb
@@ -11,20 +11,10 @@ class Invitables::BaseSerializer < ActiveModel::Serializer
 
   def subtitle
     raise NotImplementedError.new
-    case object
-    when Group   then "Add all members (#{object.members.count})"
-    when Contact then "<#{object.email}>"
-    when User    then "@#{object.username}"
-    end
   end           
 
   def image
     raise NotImplementedError.new
-    case object
-    when Group   then object.logo.try(:url, :original, false)
-    when Contact then "http://placehold.it/40x40"
-    when User    then object.avatar_url
-    end
   end
 
 end
