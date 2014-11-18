ea5c8181804d67aa04db16ce8ab107734d4cbfe6
Robert Guthrie <rob@guthr.ie>
2014-11-19 11:33:28 +1300

    flipper out aye

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -9,6 +9,7 @@ class Memo
   end
 
   def publish!
+    return unless ENV['FAYE_ENABLED']
     puts "PrivatePub.publish_to #{message_channel}, #{ as_hash.inspect }"
     PrivatePub.publish_to message_channel, as_hash
   end
