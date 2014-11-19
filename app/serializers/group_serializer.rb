d72bfba4d4379280b84a82b55471574afcb8afe1
James Kiesel <james.kiesel@gmail.com>
2014-11-20 11:50:19 +1300

    WIP group select for discussion create

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -7,8 +7,17 @@ class GroupSerializer < ActiveModel::Serializer
              :description,
              :members_can_edit_comments,
              :members_can_raise_motions,
-             :members_can_vote
+             :members_can_vote,
+             :flexible_discussion_privacy,
+             :allow_public_discussions
 
+  def flexible_discussion_privacy
+    object.discussion_privacy_options == 'public_or_private_discussions' && !is_visible_to_parent_members?
+  end
+
+  def allow_public_discussions
+    object.discussion_privacy_options != 'private_only'
+  end
 
   has_one :parent, serializer: GroupSerializer, root: 'groups'
 end
