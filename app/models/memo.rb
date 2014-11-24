a99ebf7b85b01e318bf0f695952b7ee36b148f25
Robert Guthrie <rob@guthr.ie>
2014-11-24 16:12:19 +1300

    ok

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -10,7 +10,7 @@ class Memo
 
   def publish!
     return unless ENV['FAYE_ENABLED']
-    puts "PrivatePub.publish_to #{message_channel}, #{ as_hash.inspect }"
+    #puts "PrivatePub.publish_to #{message_channel}, #{ as_hash.inspect }"
     PrivatePub.publish_to message_channel, as_hash
   end
 end
