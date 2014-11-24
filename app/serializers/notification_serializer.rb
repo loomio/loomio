9a8d09e6125e9a822d7c69685412a38c43ebd580
James Kiesel <james.kiesel@gmail.com>
2014-11-25 12:59:06 +1300

    Yeehaw notifications

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,6 +1,6 @@
 class NotificationSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id
+  attributes :id, :viewed_at
 
   has_one :user, serializer: UserSerializer, root: 'users'
   has_one :event, serializer: EventSerializer, root: 'events'
