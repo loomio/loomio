78bd5ed58569a9a7130fe96aac1294599f224993
Robert Guthrie <rob@guthr.ie>
2014-11-24 22:04:25 +1300

    proritize websocket jobs

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -11,6 +11,6 @@ class Memo
   def publish!
     return unless ENV['FAYE_ENABLED'] # remove this soon
     #puts "PrivatePub.publish_to #{message_channel}, #{ as_hash.inspect }"
-    PrivatePub.delay.publish_to message_channel, as_hash
+    PrivatePub.delay(priority: 10).publish_to message_channel, as_hash
   end
 end
