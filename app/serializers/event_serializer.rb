49a37325321aa7582d10acf9f93fc3f4e2034bf0
Richard Bartlett <rich@loomio.org>
2014-11-25 16:22:50 +1300

    notifications working

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -3,9 +3,11 @@ class EventSerializer < ActiveModel::Serializer
   attributes :id, :sequence_id, :kind, :discussion_id, :created_at
 
   has_one :comment, serializer: CommentSerializer
+  has_one :discussion, serializer: DiscussionSerializer
   has_one :proposal, serializer: MotionSerializer, root: :proposals
   has_one :vote, serializer: VoteSerializer
   has_one :comment_vote, serializer: CommentVoteSerializer
+  has_one :actor, serializer: UserSerializer, root: 'users'
 
   %w[comment motion vote comment_vote].each do |kind|
     define_method kind do
@@ -17,11 +19,29 @@ class EventSerializer < ActiveModel::Serializer
     motion
   end
 
+  def comment
+    case object.kind
+    when 'comment_liked' then object.eventable.comment
+    when 'new_comment' then object.eventable
+    end
+  end
+
+  def discussion
+    comment.discussion if comment.present?
+  end
+
   def filter(keys)
-    keys.delete(:comment) unless object.eventable_type == 'Comment'
-    keys.delete(:proposal) unless object.eventable_type == 'Motion'
-    keys.delete(:vote) unless object.eventable_type == 'Vote'
-    keys.delete(:comment_vote) unless object.eventable_type == 'CommentVote'
+    keys.delete(:comment)       unless ["Comment", "CommentVote"].include? object.eventable_type
+    keys.delete(:discussion)    unless ["Comment", "CommentVote"].include? object.eventable_type
+    keys.delete(:proposal)      unless object.eventable_type == 'Motion'
+    keys.delete(:vote)          unless object.eventable_type == 'Vote'
+    keys.delete(:comment_vote)  unless object.eventable_type == 'CommentVote'
     keys
+  end
+
+  def actor
+    case object.kind
+    when 'comment_liked' then object.eventable.user
+    end
   end
 end
