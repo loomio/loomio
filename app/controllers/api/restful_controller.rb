a2b1ca6a052083c5cf910be0b23d831bc2500d46
James Kiesel <james.kiesel@gmail.com>
2014-11-28 09:39:06 +1300

    Add more flash messages

--- app/controllers/api/restful_controller.rb
+++ app/controllers/api/restful_controller.rb
@@ -13,25 +13,37 @@ class API::RestfulController < API::BaseController
 
 
   def create
-    instantiate_resouce
-    service.create({resource_symbol => resource,
-                    actor: current_user})
-    respond_with_resource
+    begin
+      instantiate_resouce
+      service.create({resource_symbol => resource,
+                      actor: current_user})
+      respond_with_resource
+    rescue
+      respond_with_error
+    end
   end
 
   def update
-    load_resource
-    service.update({resource_symbol => resource,
-                    params: resource_params,
-                    actor: current_user})
-    respond_with_resource
+    begin
+      load_resource
+      service.update({resource_symbol => resource,
+                      params: resource_params,
+                      actor: current_user})
+      respond_with_resource
+    rescue
+      respond_with_error
+    end
   end
 
   def destroy
-    load_resource
-    service.destroy({resource_symbol => resource,
-                     actor: current_user})
-    render json: {success: 'success'}
+    begin
+      load_resource
+      service.destroy({resource_symbol => resource,
+                       actor: current_user})
+      render json: {success: 'success'}
+    rescue
+      respond_with_error
+    end
   end
 
   private
@@ -99,6 +111,10 @@ class API::RestfulController < API::BaseController
     else
       render json: resource.errors.full_messages, root: false, status: 400
     end
+  end
+
+  def respond_with_error
+    render json: ['common.errors.aw_crap'], root: false, status: 400
   end
 
 end
