584d953b7b2ffc2ade024a2ebbe99b9388e25d02
James Kiesel <james.kiesel@gmail.com>
2014-11-28 11:27:50 +1300

    VOTE FORM CONFORM

--- app/controllers/api/votes_controller.rb
+++ app/controllers/api/votes_controller.rb
@@ -1,5 +1,4 @@
 class API::VotesController < API::RestfulController
-  load_resource only: [:create]
 
   def index
     @votes = Vote.where(motion: @motion)
@@ -8,19 +7,10 @@ class API::VotesController < API::RestfulController
     respond_with_collection
   end
 
-  def create
-    VoteService.create vote: @vote, actor: current_user
-    respond_with_resource
-  end
-
   def my_votes
     load_and_authorize_discussion
     @votes = @discussion.votes.most_recent.for_user(current_user)
     respond_with_collection
   end
 
-  private
-  def vote_params
-    params.require(:vote).permit(:position, :statement, :motion_id)
-  end
 end
