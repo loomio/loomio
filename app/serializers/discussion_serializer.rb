35b43edef04c11f25dbefcca13a43c5c17845905
James Kiesel <james.kiesel@gmail.com>
2014-12-10 21:37:53 +1300

    Fix circular dependency with EventSerializer (whew!)

--- app/serializers/discussion_serializer.rb
+++ app/serializers/discussion_serializer.rb
@@ -14,17 +14,10 @@ class DiscussionSerializer < EventableSerializer
   has_one :author, serializer: UserSerializer, root: 'users'
   has_one :group, serializer: GroupSerializer, root: 'groups'
   has_one :active_proposal, serializer: MotionSerializer, root: 'proposals'
-  #has_many :events, serializer: EventSerializer
-  #has_many :comments, serialier: CommentSerializer
-  #has_many :proposals, serializer: MotionSerializer, root: 'proposals'
 
   def author
     object.author
   end
-  
-  #def proposals
-    #object.motions
-  #end
 
   def active_proposal
     object.current_motion
