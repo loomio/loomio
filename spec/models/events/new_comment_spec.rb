9a76fe616e88b334cc0ab3fe650875ffbe5cf887
James Kiesel <james.kiesel@gmail.com>
2015-02-03 13:58:22 +1300

    Fix failing spec

--- spec/models/events/new_comment_spec.rb
+++ spec/models/events/new_comment_spec.rb
@@ -2,11 +2,8 @@ require 'rails_helper'
 
 describe Events::NewComment do
   let(:discussion) { create :discussion }
-  let(:parent_user) { create :user, email: 'dave@bill.com' }
   let(:user) { create :user, email: 'bill@dave.com' }
-
-  let(:parent) { create :comment, discussion: discussion, author: parent_user }
-  let(:comment) { create :comment, discussion: discussion, parent: parent, author: user }
+  let(:comment) { create :comment, discussion: discussion, author: user }
 
   describe "::publish!" do
     let(:event) { double(:event, notify_users!: true) }
@@ -24,8 +21,14 @@ describe Events::NewComment do
       Events::NewComment.publish!(comment).should == event
     end
 
+    it 'does not publish a comment replied to event if there is no parent' do
+      Events::CommentRepliedTo.should_not_receive(:publish!).with(comment)
+      Events::NewComment.publish! comment
+    end
+
     it 'publishes a comment replied to event if there is a comment' do
-      Events::CommentRepliedTo.should_receive(:publish!).with(parent)
+      comment.parent = create :comment
+      Events::CommentRepliedTo.should_receive(:publish!).with(comment)
       Events::NewComment.publish! comment
     end
   end
