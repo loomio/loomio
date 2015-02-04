545edb5bf126d963096df97cef2cebdb1a067c31
Robert Guthrie <rob@guthr.ie>
2015-02-05 11:49:35 +1300

    add progress bars to search migration and fix restful respond with resource for events

--- db/migrate/20150108030729_add_search_vectors.rb
+++ db/migrate/20150108030729_add_search_vectors.rb
@@ -19,9 +19,13 @@ class AddSearchVectors < ActiveRecord::Migration
     execute("create index motion_search_vector_index on motion_search_vectors using gin(search_vector)")
     execute("create index comment_search_vector_index on comment_search_vectors using gin(search_vector)")
 
-    Discussion.rebuild_search_index!
-    Motion.rebuild_search_index!
-    Comment.rebuild_search_index!
+    [Discussion, Motion, Comment].each do |model|
+      puts "rebuilding search index for model: #{model.to_s}"
+      progress_bar = ProgressBar.create( format: "(\e[32m%c/%C\e[0m) %a |%B| \e[31m%e\e[0m ",
+                                         progress_mark: "\e[32m/\e[0m",
+                                         total: model.count )
+      model.rebuild_search_index! { progress_bar.increment }
 
+    end
   end
 end
