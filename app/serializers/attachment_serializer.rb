77f23c9c9edc9bfde1780989bfec8a429fd60609
James Kiesel <james.kiesel@gmail.com>
2015-02-04 16:40:21 +1300

    Add attachment directive / filesize filter

--- app/serializers/attachment_serializer.rb
+++ app/serializers/attachment_serializer.rb
@@ -1,6 +1,6 @@
 class AttachmentSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id, :filename, :location, :filesize, :created_at
+  attributes :id, :filename, :location, :filesize, :created_at, :is_an_image?
 
   has_one :comment, serializer: CommentSerializer
 
