4305c437f1fceacf24a0ac8cea1ce5af1a966ead
Robert Guthrie <rob@guthr.ie>
2015-02-06 09:55:35 +1300

    migrations have been run, tests pass

--- db/migrate/20150204230136_rebuild_search_indexes.rb
+++ db/migrate/20150204230136_rebuild_search_indexes.rb
@@ -1,6 +1,6 @@
 class RebuildSearchIndexes < ActiveRecord::Migration
   def change
-    [Discussion, Motion, Comment].each do |model|
+    [Discussion, Motion].each do |model|
       puts "rebuilding search index for model: #{model.to_s}"
       progress_bar = ProgressBar.create( format: "(\e[32m%c/%C\e[0m) %a |%B| \e[31m%e\e[0m ",
                                          progress_mark: "\e[32m/\e[0m",
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150108030729) do
+ActiveRecord::Schema.define(version: 20150204230136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
