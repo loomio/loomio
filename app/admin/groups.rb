bba049457c63da1de4b0a0265b0a55fab3f6ae04
Robert Guthrie <rob@guthr.ie>
2015-02-11 10:47:02 +1300

    stop hiding useful fields in admin group show

--- app/admin/groups.rb
+++ app/admin/groups.rb
@@ -58,7 +58,7 @@ ActiveAdmin.register Group do
     attributes_table do
       row :group_request
       group.attributes.each do |k,v|
-        row k.to_sym if v.present?
+        row k.to_sym
       end
     end
 
