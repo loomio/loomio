3fff5e31bb1263a2d5d0151616bcd2fd17754ecd
Robert Guthrie <rob@guthr.ie>
2015-02-11 14:53:59 +1300

    add vendor.js to deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ bower install
 lineman build
 cd ..
 cp -R lineman/dist/* public/
-git add public/img public/css/app.css public/js/app.js public/fonts
+git add public/img public/css/app.css public/js/app.js public/js/vendor.js public/fonts
 git commit -m "production build commit"
 git push loomio-production production:master -f
 rm -r public/img public/css public/js
