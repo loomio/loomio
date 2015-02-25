5242ddb6d45f065505329391831e97a822fd7d18
Robert Guthrie <rob@guthr.ie>
2015-02-25 19:50:03 +1300

    remove tests that have been removed in the new search branch, that fail here

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -5,7 +5,6 @@ class Discussion < ActiveRecord::Base
 
   include ReadableUnguessableUrls
   include Translatable
-  include Searchable
 
   scope :archived, -> { where('archived_at is not null') }
   scope :published, -> { where(archived_at: nil, is_deleted: false) }
