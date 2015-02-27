5a7946f5e0c81ca7995732b2ca2b29b4631e60fc
Robert Guthrie <rob@guthr.ie>
2015-02-27 18:08:57 +1300

    show off time

--- app/serializers/search_results/discussion_serializer.rb
+++ app/serializers/search_results/discussion_serializer.rb
@@ -1,3 +1,3 @@
 class SearchResults::DiscussionSerializer < SearchResults::BaseSerializer
-  attributes :title, :created_at, :group_name, :group_full_name, :key
+  attributes :title, :created_at, :group_name, :group_full_name, :key, :last_activity_at
 end
