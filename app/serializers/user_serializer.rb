0753fb393394b372f84f335972236fb48c3799a8
mix irving <whimful@gmail.com>
2015-03-06 15:56:56 +1300

    make avatar_url be served more reliabley :fish: fixes #1807

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :username, :avatar_initials, :avatar_kind, :avatar_url, :profile_url
 
   def avatar_url
-    object.avatar_url('medium')
+    object.avatar_url :medium
   end
 
   def profile_url
