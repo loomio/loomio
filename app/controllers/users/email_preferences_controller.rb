b55310673230009bffb0dfd2a9a05322915baffe
Robert Guthrie <rob@guthr.ie>
2015-03-10 10:08:27 +1300

    fix me too please rich

--- app/controllers/users/email_preferences_controller.rb
+++ app/controllers/users/email_preferences_controller.rb
@@ -8,18 +8,6 @@ class Users::EmailPreferencesController < AuthenticateByUnsubscribeTokenControll
   def update
     @user = user
 
-    if any_memberships_have_volume_email? and
-      params[:email_new_discussions_and_proposals].nil?
-      @user.memberships.where(volume: Membership.volumes[:email]).
-                        update_all(volume: Membership.volumes[:normal])
-    end
-
-    if @user.email_on_participation? and
-       params[:email_on_participation].nil?
-      @user.discussion_readers.where(volume: DiscussionReader.volumes[:email]).
-                               update_all(volume: DiscussionReader.volumes[:normal])
-    end
-
     if @user.update_attributes(permitted_params.user)
       flash[:notice] = "Your email settings have been updated."
       redirect_to dashboard_or_root_path
@@ -30,10 +18,6 @@ class Users::EmailPreferencesController < AuthenticateByUnsubscribeTokenControll
   end
 
   private
-  def any_memberships_have_volume_email?
-    @user.memberships.where(volume: Membership.volumes[:email]).any?
-  end
-
   def user
     @restricted_user || current_user
   end
