65fb0f880185262715e3b0ca9090d2e4df6ab9a8
Robert Guthrie <rob@guthr.ie>
2015-03-11 08:28:09 +1300

    remove dumb logic and english from missed yesterday

--- app/extras/discussion_headline.rb
+++ app/extras/discussion_headline.rb
@@ -8,62 +8,16 @@ class DiscussionHeadline
     @time_frame = time_frame
   end
 
-  def discussion_synopsis
-    if new_discussion?
-      case participants.size
-      when 1
-        "#{participants.first.name} started a discussion: #{linked_discussion_title(discussion)}"
-      when 2
-        "#{participants.first.name} and #{participants.second.name} started discussing: #{linked_discussion_title(discussion)}"
-      else
-        "#{participants.first.name} and #{participants.size - 1} others started discussing: #{linked_discussion_title(discussion)}"
-      end
-    else
-      case participants.size
-      when 0
-        ''
-      when 1
-        "#{participants.first.name} discussed: #{linked_discussion_title(discussion)}"
-      when 2
-        "#{participants.first.name} and #{participants.second.name} discussed: #{linked_discussion_title(discussion)}"
-      else
-        "#{participants.first.name} and #{participants.size - 1} others discussed: #{linked_discussion_title(discussion)}"
-      end
-    end
+  def has_motion?
+    new_motion? or ongoing_motion? or motion_closed?
   end
-
-  def motion_synopsis
-    if new_motion?
-      motion = discussion.current_motion
-      "#{motion.author_name} proposed: #{motion.title}"
-    elsif ongoing_motion?
-      motion = discussion.current_motion
-      case voters.size
-      when 0
-        ""
-      when 1
-        "#{voters.first.name} voted on: #{motion.title}"
-      when 2
-        "#{voters.first.name} and #{voters.second.name} voted on: #{motion.title}"
-      else
-        "#{voters.first.name} and #{voters.size - 1} others voted on: #{motion.title}"
-      end
+  
+  def motion
+    if new_motion? or ongoing_motion?
+      discussion.current_motion
     elsif motion_closed?
-      motion = discussion.most_recent_motion
-      "#{motion.title} closed with #{motion.total_votes_count} participants"
+      discussion.most_recent_motion
     end
-  end
-
-  def render
-    if motion_synopsis.present?
-      "#{discussion_synopsis} and #{motion_synopsis}"
-    else
-      discussion_synopsis
-    end
-  end
-
-  def linked_discussion_title(discussion)
-    ActionController::Base.helpers.link_to(discussion.title, Routing.discussion_url(discussion)).html_safe
   end
 
   def new_motion?
@@ -80,17 +34,6 @@ class DiscussionHeadline
 
   def new_discussion?
     is_new?(discussion.created_at)
-  end
-
-  def participants
-    participants = []
-    participants << @discussion.author if new_discussion?
-    participants += @discussion.comments.where(created_at: time_frame).order(:created_at).map(&:author)
-    participants.uniq
-  end
-
-  def voters
-    discussion.current_motion.votes.where(created_at: time_frame).order(:created_at).map(&:author).uniq
   end
 
   private
