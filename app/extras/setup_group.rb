2413d8ea4e94053eee12de30d02253210f34b8cd
Robert Guthrie <rob@guthr.ie>
2015-03-11 16:57:33 +1300

    do not create example discussion in public only group

--- app/extras/setup_group.rb
+++ app/extras/setup_group.rb
@@ -7,7 +7,7 @@ class SetupGroup
     group.is_visible_to_public = false
     group.discussion_privacy_options = 'private_only'
     group.save!
-    SetupGroup.create_example_discussion(group)
+    SetupGroup.create_example_discussion(group) if group.discussion_privacy_options != 'public_only'
     send_invitation_to_start_group(group)
     group
   end
