e4bf16da904971f50162f8ff710ca2358cfdfbec
Robert Guthrie <rob@guthr.ie>
2015-03-11 16:10:56 +1300

    remove old columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150310025335) do
+ActiveRecord::Schema.define(version: 20150311025406) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -180,7 +180,6 @@ ActiveRecord::Schema.define(version: 20150310025335) do
     t.datetime "last_read_at"
     t.integer  "read_comments_count",      default: 0, null: false
     t.integer  "read_items_count",         default: 0, null: false
-    t.boolean  "following"
     t.integer  "last_read_sequence_id",    default: 0, null: false
     t.integer  "read_salient_items_count", default: 0, null: false
     t.integer  "volume"
@@ -407,13 +406,11 @@ ActiveRecord::Schema.define(version: 20150310025335) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "inviter_id"
-    t.boolean  "email_new_discussions_and_proposals", default: true
     t.datetime "archived_at"
-    t.integer  "inbox_position",                      default: 0
-    t.boolean  "admin",                               default: false, null: false
-    t.boolean  "is_suspended",                        default: false, null: false
-    t.boolean  "following_by_default",                default: false, null: false
-    t.integer  "volume",                              default: 2,     null: false
+    t.integer  "inbox_position", default: 0
+    t.boolean  "admin",          default: false, null: false
+    t.boolean  "is_suspended",   default: false, null: false
+    t.integer  "volume",         default: 2,     null: false
   end
 
   add_index "memberships", ["group_id"], name: "index_memberships_on_group_id", using: :btree
@@ -575,44 +572,42 @@ ActiveRecord::Schema.define(version: 20150310025335) do
   add_index "user_deactivation_responses", ["user_id"], name: "index_user_deactivation_responses_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                               limit: 255, default: "",         null: false
-    t.string   "encrypted_password",                  limit: 128, default: ""
-    t.string   "reset_password_token",                limit: 255
+    t.string   "email",                            limit: 255, default: "",         null: false
+    t.string   "encrypted_password",               limit: 128, default: ""
+    t.string   "reset_password_token",             limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                                   default: 0
+    t.integer  "sign_in_count",                                default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",                  limit: 255
-    t.string   "last_sign_in_ip",                     limit: 255
+    t.string   "current_sign_in_ip",               limit: 255
+    t.string   "last_sign_in_ip",                  limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name",                                limit: 255
+    t.string   "name",                             limit: 255
     t.datetime "deactivated_at"
-    t.boolean  "is_admin",                                        default: false
-    t.string   "avatar_kind",                         limit: 255, default: "initials", null: false
-    t.string   "uploaded_avatar_file_name",           limit: 255
-    t.string   "uploaded_avatar_content_type",        limit: 255
+    t.boolean  "is_admin",                                     default: false
+    t.string   "avatar_kind",                      limit: 255, default: "initials", null: false
+    t.string   "uploaded_avatar_file_name",        limit: 255
+    t.string   "uploaded_avatar_content_type",     limit: 255
     t.integer  "uploaded_avatar_file_size"
     t.datetime "uploaded_avatar_updated_at"
-    t.string   "avatar_initials",                     limit: 255
-    t.string   "username",                            limit: 255
-    t.boolean  "email_followed_threads",                          default: true,       null: false
-    t.boolean  "email_when_proposal_closing_soon",                default: false,      null: false
-    t.string   "authentication_token",                limit: 255
-    t.string   "unsubscribe_token",                   limit: 255
-    t.integer  "memberships_count",                               default: 0,          null: false
-    t.boolean  "uses_markdown",                                   default: false,      null: false
-    t.string   "selected_locale",                     limit: 255
-    t.string   "time_zone",                           limit: 255
-    t.string   "key",                                 limit: 255
-    t.string   "detected_locale",                     limit: 255
-    t.boolean  "email_missed_yesterday",                          default: true,       null: false
-    t.string   "email_api_key",                       limit: 255
-    t.boolean  "email_new_discussions_and_proposals",             default: true,       null: false
-    t.boolean  "email_when_mentioned",                            default: true,       null: false
-    t.boolean  "angular_ui_enabled",                              default: false,      null: false
-    t.boolean  "email_on_participation",                          default: true,       null: false
+    t.string   "avatar_initials",                  limit: 255
+    t.string   "username",                         limit: 255
+    t.boolean  "email_when_proposal_closing_soon",             default: false,      null: false
+    t.string   "authentication_token",             limit: 255
+    t.string   "unsubscribe_token",                limit: 255
+    t.integer  "memberships_count",                            default: 0,          null: false
+    t.boolean  "uses_markdown",                                default: false,      null: false
+    t.string   "selected_locale",                  limit: 255
+    t.string   "time_zone",                        limit: 255
+    t.string   "key",                              limit: 255
+    t.string   "detected_locale",                  limit: 255
+    t.boolean  "email_missed_yesterday",                       default: true,       null: false
+    t.string   "email_api_key",                    limit: 255
+    t.boolean  "email_when_mentioned",                         default: true,       null: false
+    t.boolean  "angular_ui_enabled",                           default: false,      null: false
+    t.boolean  "email_on_participation",                       default: true,       null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
