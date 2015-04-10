38cd7fb3f2dd76421ab512061ca27d2e5e36a88b
Robert Guthrie <rob@guthr.ie>
2015-04-10 17:23:34 +1200

    change is_referral to not null

--- db/migrate/20150410051836_change_referral_to_not_null.rb
+++ db/migrate/20150410051836_change_referral_to_not_null.rb
@@ -3,6 +3,6 @@ class ChangeReferralToNotNull < ActiveRecord::Migration
     non_referral_group_ids = GroupRequest.pluck(:group_id).compact;
     Group.parents_only.where('groups.id NOT IN (?)', non_referral_group_ids).update_all(is_referral: true)
     Group.where(is_referral: nil).update_all(is_referral: false)
-    change_column :groups, :is_referral, :boolean, null: false
+    change_column :groups, :is_referral, :boolean, null: false, default: false
   end
 end
