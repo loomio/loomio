840bd73fd4be162a457055c37521e7ce9270a1ac
Robert Guthrie <rob@guthr.ie>
2015-04-19 17:40:28 +1200

    even betterer

--- app/controllers/rbtrace_controller.rb
+++ app/controllers/rbtrace_controller.rb
@@ -8,32 +8,16 @@ class RbtraceController < BaseController
     filename = gen_filename
     filepath = tmp_dir.join(filename)
 
-    Thread.new do
-      require "objspace"
-      require 'net/scp'
-      require 'zlib'
+    require "objspace"
+    require 'net/scp'
+    require 'zlib'
 
-      ObjectSpace.trace_object_allocations_start
-      GC.start()
-      f = Zlib::GzipWriter.open(filepath)
-      ObjectSpace.dump_all(output: f)
+    ObjectSpace.trace_object_allocations_start
+    GC.start()
+    f = Zlib::GzipWriter.open(filepath)
+    ObjectSpace.dump_all(output: f)
 
-      response.headers['Content-Type'] = 'text/event-stream'
-      Net::SCP.upload!(ENV['DUMP_HOST'],
-                       ENV['DUMP_USER'],
-                       filepath,
-                       filename,
-                       ssh: {password: ENV['DUMP_PASS']}) do |update|
-
-        response.stream.write "This is a test Messagen"
-      end
-
-      response.stream.close
-    end.join
-
-
-
-    render text: ["dumped to: #{filepath}", result].inspect
+    send_file(filepath)
   end
 
 
