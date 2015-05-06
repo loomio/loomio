e67b0262a2df2e7e4d18822d7b4485fb53d95242
James Kiesel <james.kiesel@gmail.com>
2015-05-07 05:51:56 +0700

    Whoops! Actually fix failing spec

--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -3,7 +3,7 @@ describe API::UsersController do
 
   let(:user) { create :user }
   let(:another_user) { create :user }
-  let(:user_params) { dashboard_filter: 'show_unread' }
+  let(:user_params) { { dashboard_filter: 'show_unread' } }
 
   before do
     sign_in user
