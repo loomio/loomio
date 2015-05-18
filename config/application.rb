81b6089a1e10788b2681035a32cd06cedc818d3d
Jevon Wright <jevon@jevon.org>
2015-05-19 11:46:44 +1200

    Fix 'no implicit conversion of false into String' init error

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Loomio
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
+        ENV[key] = value.to_s
       end if File.exists?(env_file)
     end
 
