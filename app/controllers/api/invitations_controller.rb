bd79baef114e11ef3df7d73dfdc300ef0d4303c2
James Kiesel <james.kiesel@gmail.com>
2015-06-12 09:45:31 +1200

    MOAR ELEGANCEC

--- app/controllers/api/invitations_controller.rb
+++ app/controllers/api/invitations_controller.rb
@@ -3,16 +3,28 @@ class API::InvitationsController < API::RestfulController
   def create
     load_and_authorize :group, :invite_people
 
-    MembershipService.add_users_to_group invitation_form.new_members
-    InvitationService.invite_to_group    invitation_form.new_emails
+    MembershipService.add_users_to_group new_members
+    InvitationService.invite_to_group    new_emails
 
     respond_with_collection
   end
 
   private
 
-  def invitation_form
-    @invitation_form ||= InvitationForm.new(group: @group, user: current_user, message: params[:invite_message], invitations: params[:invitations])
+  def new_members
+    common_params.merge users: invitation_parser.new_members
+  end
+
+  def new_emails
+    common_params.merge recipient_emails: invitation_parser.new_emails
+  end
+
+  def common_params
+    @common_params ||= { group: @group, inviter: current_user, message: params[:invite_message] }
+  end
+
+  def invitation_parser
+    @invitation_parser ||= InvitationParser.new(params[:invitations])
   end
 
 end
---
+++ app/extras/invitation_parser.rb
@@ -0,0 +1,22 @@
+class InvitationParser = Struct.new(:invitations) do
+
+  def new_emails
+    @new_emails ||= invitations_by_type(:email, :email) + invitations_by_type(:contact, :email)
+  end
+
+  def new_members
+    @new_members ||= User.find(invitations_by_type(:user, :id) + new_group_memberships)
+  end
+
+  private
+
+  def new_group_memberships
+    Membership.where(group_id: invitations_by_type(:group, :id)).pluck(:user_id)
+  end
+
+  def invitations_by_type(type, field)
+    @invitations.select { |invitation| invitation.type == type.to_s }
+                .map    { |invitation| invitation[field.to_s] }
+  end
+
+end
