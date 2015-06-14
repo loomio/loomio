83a74f57a2cab0a9c8046fd105c7c7e72e56bd46
James Kiesel <james.kiesel@gmail.com>
2015-06-15 00:21:44 +1200

    Make language selector work

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,7 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   embed :ids, include: true
 
-  attributes :id, :name, :username, :avatar_initials, :avatar_kind, :avatar_url, :profile_url, :gravatar_md5, :time_zone, :locale
+  attributes :id, :name, :username, :avatar_initials, :avatar_kind, :avatar_url, :profile_url, :gravatar_md5, :time_zone, :selected_locale, :detected_locale
 
   def gravatar_md5
     Digest::MD5.hexdigest(object.email.to_s.downcase) if object.avatar_kind == "gravatar"
@@ -15,7 +15,4 @@ class UserSerializer < ActiveModel::Serializer
     object.avatar_url :large
   end
 
-  def locale
-    object.selected_locale || object.selected_locale
-  end
 end
