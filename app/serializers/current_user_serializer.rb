92e9823741c669b21342be12f4c7a49286d45a50
James Kiesel <james.kiesel@gmail.com>
2015-07-06 16:44:56 -0700

    Flesh out change picture modal

--- app/serializers/current_user_serializer.rb
+++ app/serializers/current_user_serializer.rb
@@ -1,4 +1,9 @@
 class CurrentUserSerializer < UserSerializer
   has_many :memberships, serializer: MembershipSerializer, root: 'memberships'
   attributes :email
+
+  def gravatar_md5
+    Digest::MD5.hexdigest(object.email.to_s.downcase)
+  end
+
 end
