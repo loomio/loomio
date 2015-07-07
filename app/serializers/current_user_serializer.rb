316f026cbeda96a0997abc1f20fbc31ed459197a
James Kiesel <james.kiesel@gmail.com>
2015-07-06 17:56:23 -0700

    Serialize current user gravatar preview

--- app/serializers/current_user_serializer.rb
+++ app/serializers/current_user_serializer.rb
@@ -2,8 +2,7 @@ class CurrentUserSerializer < UserSerializer
   has_many :memberships, serializer: MembershipSerializer, root: 'memberships'
   attributes :email
 
-  def gravatar_md5
-    Digest::MD5.hexdigest(object.email.to_s.downcase)
+  def include_gravatar_md5?
+    true
   end
-
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,7 +4,11 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :username, :avatar_initials, :avatar_kind, :avatar_url, :profile_url, :gravatar_md5, :time_zone, :selected_locale, :detected_locale, :search_fragment
 
   def gravatar_md5
-    Digest::MD5.hexdigest(object.email.to_s.downcase) if object.avatar_kind == "gravatar"
+    Digest::MD5.hexdigest(object.email.to_s.downcase)
+  end
+
+  def include_gravatar_md5?
+    object.avatar_kind == 'gravatar'
   end
 
   def avatar_url
