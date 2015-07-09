1ca87413eaea30868f6189e8cc71c50b3fcc6f3c
Nicolás Reynolds <fauno@endefensadelsl.org>
2015-07-09 13:44:13 -0300

    stop tracking users!

---
+++ db/migrate/20150709164011_remove_user_tracking_fields.rb
@@ -0,0 +1,9 @@
+class RemoveUserTrackingFields < ActiveRecord::Migration
+  def change
+    remove_column :users, :sign_in_count, :integer
+    remove_column :users, :current_sign_in_at, :datetime
+    remove_column :users, :last_sign_in_at, :datetime
+    remove_column :users, :current_sign_in_ip, :string
+    remove_column :users, :last_sign_in_ip, :string
+  end
+end
