3dd46ffaa753e589ed7717178d9ad83b9c43a9c2
Robert Guthrie <rob@guthr.ie>
2015-07-26 11:58:07 +1200

    get more info from rack attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,7 +2,7 @@ Rack::Attack.cache.store = Rails.cache
 Rack::Attack.throttled_response = ->(env) { [429, {}, [ActionView::Base.new.render(file: 'public/429.html')]] }
 
 ActiveSupport::Notifications.subscribe('rack.attack') do |name, start, finish, request_id, req|
-  Airbrake.notify Exception.new(message: "#{req.ip} has been rate limited for url #{req.url}", data: req.env['rack.attack.throttle_data'])
+  Airbrake.notify Exception.new(message: "#{req.ip} has been rate limited for url #{req.url}", data: [name, start, finish, request_id, req])
 end
 
 @config = YAML.load_file("#{Rails.root}/config/rack_attack.yml").with_indifferent_access
