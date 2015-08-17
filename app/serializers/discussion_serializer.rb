4cc7c1b202af947e0c973265289e5d94de87c904
James Kiesel <james.kiesel@gmail.com>
2015-08-17 16:35:27 -0700

    Add attributes_from_reader to discussion serializer

--- app/serializers/discussion_serializer.rb
+++ app/serializers/discussion_serializer.rb
@@ -1,4 +1,13 @@
 class DiscussionSerializer < ActiveModel::Serializer
+
+  def self.attributes_from_reader(*attrs)
+    attrs.each do |attr|
+      define_method attr,                 -> { reader.send(attr) if has_valid_reader? }
+      define_method :"#{attr}_included?", -> { has_valid_reader? }
+    end
+    attributes *attrs
+  end
+
   embed :ids, include: true
   attributes :id,
              :key,
@@ -19,6 +28,15 @@ class DiscussionSerializer < ActiveModel::Serializer
              :first_sequence_id,
              :last_sequence_id
 
+  attributes_from_reader :last_read_at,
+                         :read_comments_count,
+                         :read_items_count,
+                         :read_salient_items_count,
+                         :last_read_sequence_id,
+                         :volume,
+                         :participating,
+                         :starred
+
   has_one :author, serializer: UserSerializer, root: 'users'
   has_one :group, serializer: GroupSerializer, root: 'groups'
   has_one :active_proposal, serializer: MotionSerializer, root: 'proposals'
@@ -34,6 +52,18 @@ class DiscussionSerializer < ActiveModel::Serializer
   def filter(keys)
     keys.delete(:active_proposal) unless object.current_motion.present?
     keys
+  end
+
+  def scope
+    super || {}
+  end
+
+  def reader
+    @reader ||= DiscussionReader.for(user: scope[:user], discussion: object)
+  end
+
+  def has_valid_reader?
+    @has_valid_reader ||= reader.present? && reader.user.present?
   end
 
 end
