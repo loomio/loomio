f60e387521ed4f71baaa550e52f465d1ddce7c7b
Richard Bartlett <rich@loomio.org>
2015-09-01 16:27:55 +1200

    add greyscale to aaarrr table

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,9 +1,10 @@
 module StatsHelper
   def safe_pct(amt, of)
     if of > 0 and amt > 0
-      amt.to_f / of.to_f * 100
+      num = (amt.to_f / of.to_f * 100).round
+      format('%02d', num)
     else
       0
-    end.round
+    end
   end
 end
