1ecde8e8a17e606d2c613fa2b6418b2cb0da04c1
James Kiesel <james.kiesel@gmail.com>
2015-10-09 17:38:45 +0200

    Suppress expect output

--- script/heroku_login.sh
+++ script/heroku_login.sh
@@ -4,6 +4,7 @@ echo "Logging into Heroku..."
 HEROKU_USERNAME=$1
 HEROKU_PASSWORD=$2
 expect -c "
+   log_user 0
    set timeout 60
    spawn `which heroku` login
    expect Email { send $HEROKU_USERNAME\r }
