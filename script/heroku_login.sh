309d6664aa42648b1df05fac2e7c6984d78e2c1a
James Kiesel <james.kiesel@gmail.com>
2015-10-09 16:27:20 +0200

    Add continuous integration

---
+++ script/heroku_login.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Logging into Heroku..."
+
+HEROKU_USERNAME=$1
+HEROKU_PASSWORD=$2
+expect -c "
+   set timeout 60
+   spawn /usr/local/heroku/bin/heroku login
+   expect Email { send $HEROKU_USERNAME\r }
+   expect Password { send $HEROKU_PASSWORD\r }
+   expect $HEROKU_USERNAME
+   exit
+"
