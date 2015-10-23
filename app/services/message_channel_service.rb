cc16f270425ac4c326424c6e6426f3cf8e44a1ea
James Kiesel <james.kiesel@gmail.com>
2015-10-23 10:53:21 +0300

    Hotfix for event channel names

--- app/services/message_channel_service.rb
+++ app/services/message_channel_service.rb
@@ -16,9 +16,10 @@ class MessageChannelService
                     motion_closed_by_user
                     motion_name_edited)
 
-  DISCUSSION_EVENTS = %w(comment_liked
-                         discussion_description_edited
+  DISCUSSION_EVENTS = %w(discussion_description_edited
                          motion_description_edited)
+
+  COMMENT_EVENTS = %w(comment_liked)
 
   def self.subscribe_to(user:, model:)
     raise AccessDeniedError.new unless can_subscribe?(user: user, model: model)
@@ -35,19 +36,19 @@ class MessageChannelService
   end
 
   def self.channel_for(model)
+    return unless model
     "/#{model.class.to_s.downcase}-#{model.key}"
   end
 
-  def self.channel_for_event(event)
-    if GROUP_EVENTS.include? event.kind
-      "/group-#{event.group_key}"
-    elsif DISCUSSION_EVENTS.include? event.kind
-      "/discussion-#{event.discussion_key}"
+  def self.channel_name_for(event)
+    if    GROUP_EVENTS.include?      event.kind then event.eventable.group
+    elsif DISCUSSION_EVENTS.include? event.kind then event.eventable
+    elsif COMMENT_EVENTS.include?    event.kind then event.eventable.comment.discussion
     end
   end
 
   def self.publish_event(event)
-    return unless channel = channel_for_event(event)
+    return unless channel = channel_for(channel_name_for(event))
     publish channel, EventSerializer.new(event).as_json
   end
 
