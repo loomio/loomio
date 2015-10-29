d716be8545c79e7ef90fc7c657af442b19769758
Robert Guthrie <rob@guthr.ie>
2015-10-30 12:56:58 +1300

    make rake db:migrate work from scratch in dev

--- db/migrate/20150225022504_build_search_vectors.rb
+++ db/migrate/20150225022504_build_search_vectors.rb
@@ -3,7 +3,7 @@ class BuildSearchVectors < ActiveRecord::Migration
     progress_bar = ProgressBar.create( format: "(\e[32m%c/%C\e[0m) %a |%B| \e[31m%e\e[0m ",
                                        progress_mark: "\e[32m/\e[0m",
                                        total: Discussion.count )
-    SearchService.index_without_delay! Discussion.all.pluck(:id) do
+    ThreadSearchService.index_without_delay! Discussion.all.pluck(:id) do
       progress_bar.increment
     end
   end
