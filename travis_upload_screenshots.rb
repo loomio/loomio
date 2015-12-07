9a03afaf8af29895d3cd3fd136900c3036204c23
Robert Guthrie <rob@guthr.ie>
2015-12-07 14:34:08 +1300

    ijij

--- travis_upload_screenshots.rb
+++ travis_upload_screenshots.rb
@@ -3,7 +3,7 @@ require 's3_uploader'
 S3Uploader.upload_directory('/tmp/screenshots', 'loomio-travis2',
   { :s3_key => ENV['AWS_KEY'],
     :s3_secret => ENV['AWS_SECRET'],
-    :destination_dir => ENV['TRAVIS_BRANCH'],
+    :destination_dir => "test2#{ENV['TRAVIS_BRANCH']}",
     :region => 'us-west-2',
     :threads => 4,
     :metadata => { 'Cache-Control' => 'max-age=315576000' }
