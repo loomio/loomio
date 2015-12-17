0b4d396419ccd87fa71dd84fee5314ec42e0c690
James Kiesel <james.kiesel@gmail.com>
2015-12-17 21:57:09 +0100

    Fix user mentioned parameter name

--- app/models/events/user_mentioned.rb
+++ app/models/events/user_mentioned.rb
@@ -6,7 +6,7 @@ class Events::UserMentioned < Event
             created_at: comment.created_at).tap { |e| EventBus.broadcast('user_mentioned_event', e, mentioned_user) }
   end
   EventBus.listen('new_comment') do |comment|
-    comment.mentioned_group_members.without(comment.parent.author).each { |user| publish!(comment, mentioned_user) }
+    comment.mentioned_group_members.without(comment.author).each { |user| publish!(comment, user) }
   end
 
   def comment
