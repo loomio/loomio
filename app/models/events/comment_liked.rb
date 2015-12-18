bcbf51adbecad96ef8c13368f278597320abd9bd
James Kiesel <james.kiesel@gmail.com>
2015-12-18 05:14:46 +0100

    Match comment_liked tap to other events

--- app/models/events/comment_liked.rb
+++ app/models/events/comment_liked.rb
@@ -3,7 +3,7 @@ class Events::CommentLiked < Event
 
   def self.publish!(comment_vote)
     create!(kind: "comment_liked",
-            eventable: comment_vote).tap { |event| EventBus.broadcast('comment_liked_event', event) }
+            eventable: comment_vote).tap { |e| EventBus.broadcast('comment_liked_event', e) }
   end
 
   def comment_vote
