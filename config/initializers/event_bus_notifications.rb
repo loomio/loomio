fec02c5cfec65915a7b357a6a0541985b92ba908
James Kiesel <james.kiesel@gmail.com>
2015-12-19 21:12:45 +0100

    Fix comment replied to spec

--- config/initializers/event_bus_notifications.rb
+++ config/initializers/event_bus_notifications.rb
@@ -6,4 +6,4 @@ EventBus.listen('comment_update') do |comment, new_mentions|
   User.where(username: new_mentions).each { |user| Events::UserMentioned.publish!(comment, user) } if new_mentions.present?
 end
 
-EventBus.listen('comment_replied_to_event', 'user_mentioned_event') { |user, event| event.notify!(user) unless event.comment.author == user }
+EventBus.listen('comment_replied_to_event', 'user_mentioned_event') { |user, event| event.notify!(user) }
