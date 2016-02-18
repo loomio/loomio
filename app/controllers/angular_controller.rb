05dd499d420cf117544649f3d88f79363094dfbc
James Kiesel <james.kiesel@gmail.com>
2016-02-18 21:25:26 +1300

    Add angular boot script for static entry point

--- app/controllers/angular_controller.rb
+++ app/controllers/angular_controller.rb
@@ -1,14 +1,16 @@
 class AngularController < ApplicationController
+  include AngularHelper
   before_filter :authenticate_user!
+  before_filter :app_config, only: :boot
   layout 'pages'
+  layout false, only: :boot
 
-  def index
-    @mode = if current_user.angular_ui_enabled?
-             'disable'
-           else
-             'enable'
-           end
+  def toggle
     @enabled = current_user_or_visitor.angular_ui_enabled?
+    @mode    = @enabled ? 'disable' : 'enable'
+  end
+
+  def boot
   end
 
   def on
@@ -19,5 +21,63 @@ class AngularController < ApplicationController
   def off
     current_user.update_attribute :angular_ui_enabled, false
     redirect_to '/'
+  end
+
+
+  def client_asset_path(filename)
+    ['', :client, angular_asset_folder, filename].join('/')
+  end
+  helper_method :client_asset_path
+
+  private
+
+  def app_config
+    @appConfig = {
+      hostedByLoomio: ENV['HOSTED_BY_LOOMIO'],
+      version: Loomio::Version.current,
+      reportErrors: false,
+      environment: Rails.env,
+      loadVideos: (ENV.has_key?('LOOMIO_LOAD_VIDEOS') or Rails.env.production?),
+      flash: flash.to_h,
+      currentUserId: current_user_or_visitor.id,
+      currentUserLocale: current_user_or_visitor.locale,
+      canTranslate: TranslationService.available?,
+      seedRecords: CurrentUserSerializer.new(current_user_or_visitor),
+      permittedParams: PermittedParamsSerializer.new({}),
+      locales: angular_locales,
+      baseUrl: root_url,
+      safeThreadItemKinds: Discussion::THREAD_ITEM_KINDS,
+      intercomAppId: Rails.application.secrets.intercom_app_id,
+      intercomUserHash: Digest::SHA1.hexdigest("#{Rails.application.secrets.intercom_app_secret}#{current_user_or_visitor.id}"),
+      chargify: app_config_chargify
+    }
+  end
+
+  def app_config_chargify
+    return unless Rails.application.secrets.chargify_app_name
+    {
+      appName: Rails.application.secrets.chargify_app_name,
+      host: "https://#{Rails.application.secrets.chargify_app_name}.chargify.com/",
+      plans: {
+        standard: {
+          name: Rails.application.secrets.chargify_standard_plan_name,
+          path: "subscribe/#{Rails.application.secrets.chargify_standard_plan_key}/#{Rails.application.secrets.chargify_standard_plan_name}",
+        },
+        plus: {
+          name: Rails.application.secrets.chargify_plus_plan_name,
+          path: "subscribe/#{Rails.application.secrets.chargify_plus_plan_key}/#{Rails.application.secrets.chargify_plus_plan_name}"
+        }
+      },
+      donation_url: Rails.application.secrets.chargify_donation_url,
+      nagCache: {}
+    }
+  end
+
+  def angular_asset_folder
+    if Rails.env.development?
+      :development
+    else
+      Loomio::Version.current
+    end
   end
 end
--- app/helpers/angular_helper.rb
+++ app/helpers/angular_helper.rb
@@ -1,71 +1,14 @@
 module AngularHelper
-  def app_config
-    @appConfig = {
-      hostedByLoomio: ENV['HOSTED_BY_LOOMIO'],
-      version: Loomio::Version.current,
-      reportErrors: false,
-      environment: Rails.env,
-      loadVideos: (ENV.has_key?('LOOMIO_LOAD_VIDEOS') or Rails.env.production?),
-      flash: flash.to_h,
-      currentUserId: current_user_or_visitor.id,
-      currentUserLocale: current_user_or_visitor.locale,
-      canTranslate: TranslationService.available?,
-      seedRecords: CurrentUserSerializer.new(current_user_or_visitor),
-      permittedParams: PermittedParamsSerializer.new({}),
-      locales: angular_locales,
-      baseUrl: root_url,
-      safeThreadItemKinds: Discussion::THREAD_ITEM_KINDS
-    }
-
-    if Rails.application.secrets.chargify_app_name
-      @appConfig[:chargify] = {
-        appName: Rails.application.secrets.chargify_app_name,
-        host: "https://#{Rails.application.secrets.chargify_app_name}.chargify.com/",
-        plans: {
-          standard: {
-            name: Rails.application.secrets.chargify_standard_plan_name,
-            path: "subscribe/#{Rails.application.secrets.chargify_standard_plan_key}/#{Rails.application.secrets.chargify_standard_plan_name}",
-          },
-          plus: {
-            name: Rails.application.secrets.chargify_plus_plan_name,
-            path: "subscribe/#{Rails.application.secrets.chargify_plus_plan_key}/#{Rails.application.secrets.chargify_plus_plan_name}"
-          }
-        },
-        donation_url: Rails.application.secrets.chargify_donation_url,
-        nagCache: {}
-      }
-    end
-
-    if Rails.application.secrets.intercom_app_id
-      @appConfig[:intercomAppId] = Rails.application.secrets.intercom_app_id
-      @appConfig[:intercomUserHash] = Digest::SHA1.hexdigest("#{Rails.application.secrets.intercom_app_secret}#{current_user.id.to_s}")
-    end
-  end
-
-  def client_asset_path(filename)
-    [:client, angular_asset_folder, filename].join('/')
-  end
 
   def boot_angular_ui
     if browser.ie? && browser.version.to_i < 10
       redirect_to :browser_not_supported and return
     end
-
-    app_config
-    render 'layouts/angular', layout: false
+    render 'angular/index', layout: false
   end
 
   def use_angular_ui?
     current_user_or_visitor.angular_ui_enabled? && request.format == :html
   end
 
-  private
-
-  def angular_asset_folder
-    if Rails.env.development?
-      :development
-    else
-      Loomio::Version.current
-    end
-  end
 end
