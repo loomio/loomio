f02c7c9c9d774d6d201d4ab61a2284adc0e41d4b
James Kiesel <james.kiesel@gmail.com>
2016-02-23 02:29:03 +1300

    Spruce up auth landing page

--- app/models/oauth_application.rb
+++ app/models/oauth_application.rb
@@ -1,3 +1,3 @@
 class OauthApplication < Doorkeeper::Application
-  has_attached_file :logo, styles: {square: "100x100#"}, default_url: 'img/default-logo-medium.png'
+  has_attached_file :logo, styles: {square: "100x100#"}, default_url: '/img/default-logo-medium.png'
 end
