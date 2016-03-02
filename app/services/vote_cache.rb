c626bd02d8e18ce2122457e123e1fb96c9e3f25b
James Kiesel <james.kiesel@gmail.com>
2016-03-03 12:22:38 +1300

    Allow closed proposals to load their own discussions / votes

--- app/services/vote_cache.rb
+++ app/services/vote_cache.rb
@@ -12,7 +12,7 @@ class VoteCache
   end
 
   def get_for(motion)
-    @votes_by_motion_id.fetch(motion.id) { Vote.new(motion: motion, user: user) }
+    @votes_by_motion_id[motion.id]
   end
 
   def clear
