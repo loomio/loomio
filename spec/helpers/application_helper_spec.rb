361083f628df41fdf9382a28eb89fd069e721d8b
James Kiesel <james.kiesel@gmail.com>
2016-03-26 00:11:57 +1300

    Helper specs pass

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -69,43 +69,4 @@ describe ApplicationHelper do
       end
     end
   end
-
-  describe "analytics scope" do
-    it "executes block in production" do
-      Rails.env.stub :production? => true
-      executed = false
-      helper.analytics_scope do
-        executed = true
-      end    
-      expect(executed).to eq  true
-    end
-
-    it "executes block in staging" do
-      Rails.env.stub :staging? => true
-      executed = false
-      helper.analytics_scope do
-        executed = true
-      end    
-      expect(executed).to eq  true
-    end
-
-    it "does not execute block in other environments" do
-      Rails.env.stub :development? => true
-      executed = false
-      helper.analytics_scope do
-        executed = true
-      end    
-      expect(executed).to eq  false
-    end
-
-    it "does not execute block in the searches controller" do
-      Rails.env.stub :production? => true
-      helper.stub :controller_name => "searches"
-      executed = false
-      helper.analytics_scope do
-        executed = true
-      end    
-      expect(executed).to eq  false
-    end
-  end
 end
