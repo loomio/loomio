1264790a11b60a1e813f750b928068dbac904570
James Kiesel <james.kiesel@gmail.com>
2016-04-12 14:43:34 +1200

    Readd group subdomain constraints

---
+++ app/contraints/group_subdomain_constraints.rb
@@ -0,0 +1,5 @@
+class GroupSubdomainConstraint
+  def self.matches?(request)
+    request.subdomain.present? && (request.subdomain != 'www')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -170,6 +170,17 @@ Loomio::Application.routes.draw do
     get  '/contacts/:importer/callback', to: 'contacts#callback'
   end
 
+  constraints(GroupSubdomainConstraint) do
+    get '/' => 'redirect#group_subdomain'
+    get '/d/:id(/:slug)', to: 'redirect#discussion_key'
+    get '/g/:id(/:slug)', to: 'redirect#group_key'
+    get '/m/:id(/:slug)', to: 'redirect#motion_key'
+  end
+
+  get '/discussions/:id', to: 'redirect#discussion_id'
+  get '/groups/:id',      to: 'redirect#group_id'
+  get '/motions/:id',     to: 'redirect#motion_id'  
+
   get "/browser_not_supported", to: "application#browser_not_supported"
 
   devise_for :users, controllers: { sessions: 'users/sessions',
