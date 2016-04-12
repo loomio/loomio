bd9a282137ad8ff5ef91fb2d2fe4f0bb76bbdfd3
James Kiesel <james.kiesel@gmail.com>
2016-04-12 14:46:19 +1200

    Revert robots controller refactor

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -2,6 +2,10 @@ class RobotsController < ActionController::Base
   respond_to :text
 
   def show
-    ENV['ALLOW_ROBOTS'].to_i == 1 ? head(:ok) : render(:private_robots)
+    if ENV['ALLOW_ROBOTS']
+      head :ok
+    else
+      render :private_robots
+    end
   end
 end
