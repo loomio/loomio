63d4d1a63e5981afeb56758c009524820078b534
James Kiesel <james.kiesel@gmail.com>
2016-04-20 11:34:59 +1200

    Revert "Revert "Allow usage of angular email settings page from email unsubscribe link"" (#3223)

---
+++ app/serializers/restricted/membership_serializer.rb
@@ -0,0 +1,5 @@
+class Restricted::MembershipSerializer < ActiveModel::Serializer
+  embed :ids, include: true
+  attributes :id, :volume, :user_id
+  has_one :group, serializer: Restricted::GroupSerializer, root: 'groups'
+end
--- spec/controllers/email_preferences_controller_spec.rb
+++ spec/controllers/email_preferences_controller_spec.rb
@@ -2,14 +2,27 @@ require 'rails_helper'
 
 describe Users::EmailPreferencesController do
   describe "edit" do
-    let(:user) { create (:user) }
+    let(:user) { create :user, angular_ui_enabled: true }
 
-    it 'renders the beta interface when an unsubscribe token is present' do
+    it 'renders the beta interface for betea user when an unsubscribe token is present' do
+      user.update(angular_ui_enabled: false)
       get :edit, unsubscribe_token: user.unsubscribe_token
       expect(response).to render_template(:edit)
     end
 
-    it 'renders the angular interface when no unsubscribe_token is present' do
+    it 'renders the beta interface for beta user when signed in' do
+      user.update(angular_ui_enabled: false)
+      sign_in user
+      get :edit
+      expect(response).to render_template(:edit)
+    end
+
+    it 'renders the angular interface when logged in and unsubscribe_token is present' do
+      get :edit, unsubscribe_token: user.unsubscribe_token
+      expect(response).to render_template(:angular)
+    end
+
+    it 'renders the angular interface when signed in' do
       sign_in user
       get :edit
       expect(response).to render_template(:angular)
