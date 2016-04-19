a931ee51cd94c522d681df7ab4c4fb909bfdcd4c
James Kiesel <james.kiesel@gmail.com>
2016-04-20 11:34:27 +1200

    Revert "Revert "Allow usage of angular email settings page from email unsubscribe link""

---
+++ app/serializers/restricted/user_serializer.rb
@@ -0,0 +1,10 @@
+class Restricted::UserSerializer < ActiveModel::Serializer
+  embed :ids, include: true
+  attributes :id, :restricted, :username, :email, :email_when_proposal_closing_soon, :email_missed_yesterday,
+             :email_when_mentioned, :email_on_participation, :default_membership_volume, :unsubscribe_token
+  has_many :memberships, serializer: Restricted::MembershipSerializer, root: :memberships
+
+  def restricted
+    true
+  end
+end
