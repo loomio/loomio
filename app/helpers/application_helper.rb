0bcf636bddb54497229083466f39607d2d34192c
James Kiesel <james@loomio.org>
2016-04-21 07:57:17 +1200

    Remove more unused code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,5 @@
 module ApplicationHelper
 
-  def time_formatted_relative_to_age(time)
-    current_time = Time.zone.now
-    if time.to_date == Time.zone.now.to_date
-      l(time, format: :for_today)
-    elsif time.year != current_time.year
-      l(time.to_date, format: :for_another_year)
-    else
-      l(time.to_date, format: :for_this_year)
-    end
-  end
-
   def show_login_button?
     controller_name != 'sessions' && !current_user_or_visitor.is_logged_in?
   end
@@ -32,23 +21,6 @@ module ApplicationHelper
       else
         type.to_s
     end
-  end
-
-  def display_title(notifications)
-    notification_size = notifications ? notifications.size : 0
-    result = ""
-    result += "(#{notification_size}) " if notification_size > 0
-    result += content_for(:title) + " | " if content_for?(:title)
-    result += "Loomio"
-    result
-  end
-
-  def set_title(group_name, page_title, parent = nil)
-    title = page_title.blank? ? "" : page_title.to_s
-    title += " - " unless title.blank? || group_name.blank?
-    title += parent.name.to_s+" - " unless !parent || parent.name.blank?
-    title += group_name.to_s unless group_name.blank?
-    content_for :title, title.gsub(/["'<>]/, '')
   end
 
   def render_rich_text(text, md_boolean=true)
@@ -81,20 +53,8 @@ module ApplicationHelper
     Redcarpet::Render::SmartyPants.render(output).html_safe
   end
 
-  def render_help_container
-    ' help-container' if controller_name == 'help'
-  end
-
   def hosted_by_loomio?
     false
-  end
-
-  def logo_path
-    if ENV['APP_LOGO_PATH']
-      ENV['APP_LOGO_PATH']
-    else
-      image_url("navbar-logo-beta.jpg")
-    end
   end
 
   def login_or_signup_path_for_email(email)
--- spec/helpers/email_helper_spec.rb
+++ spec/helpers/email_helper_spec.rb
@@ -12,4 +12,36 @@ describe EmailHelper do
       expect(output).to eq "d=d1&u=1&k=abc123@replyhostname.com"
     end
   end
+
+  describe 'time_formatted_relative_to_age' do
+    let(:time){ Time.parse "2013-01-02 16:55:00 UTC" }
+
+    subject do
+      helper.time_formatted_relative_to_age(time)
+    end
+
+    context 'when time is same day' do
+      it 'displays hours, minutes and meridian' do
+        Timecop.freeze("2013-01-02 12:00:00 UTC") do
+          expect(subject).to eq ' 4:55 pm'
+        end
+      end
+    end
+
+    context 'when it is not the same day' do
+      it 'displays date only' do
+        Timecop.freeze("2013-01-01 12:00:00 UTC") do
+          expect(subject).to eq ' 2 Jan'
+        end
+      end
+    end
+
+    context 'when it is not the same year' do
+      it 'displays date and year' do
+        Timecop.freeze("2014-01-01 12:00:00 UTC") do
+          expect(subject).to eq '2/1/13'
+        end
+      end
+    end
+  end
 end
