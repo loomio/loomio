186b99501af0079722341e222d506739c8fedaaa
Hannah Salmon <hr.salmon@gmail.com>
2016-04-28 13:53:37 +1200

    Fix spec failures

--- app/controllers/api/groups_controller.rb
+++ app/controllers/api/groups_controller.rb
@@ -14,7 +14,7 @@ class API::GroupsController < API::RestfulController
   end
 
   def create
-    instantiate_resource
+    instantiate_resouce
     create_action
     respond_with_resource(scope: {current_user: current_user})
   end
