289cbc412dc02b152e48c33a8ffa207a9dc9bd2f
Robert Guthrie <rob@guthr.ie>
2016-05-05 20:23:18 +1200

    ok

--- app/jobs/locate_users_job.rb
+++ app/jobs/locate_users_job.rb
@@ -1,10 +1,10 @@
 class LocateUsersJob < ActiveJob::Base
   queue_as :low_priority
 
-  def perform(time_range)
+  def perform(active_since)
     # time_range = 30.days.ago..1.day.ago
     Visit.select('DISTINCT(user_id) user_id, country, city, region, started_at').
-          where(started_at: time_range).order('started_at desc').each do |visit|
+          where('started_at > ?', active_since).order('started_at desc').each do |visit|
       User.where(id: visit.user_id).update_all(country: visit.country,
                                                city: visit.city,
                                                region: visit.region)
