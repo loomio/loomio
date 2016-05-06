c26e38653665571f3d30e7fe338233db9f583b8b
Hannah Salmon <hr.salmon@gmail.com>
2016-05-06 15:59:12 +1200

    Fix failing spec

--- app/controllers/api/profile_controller.rb
+++ app/controllers/api/profile_controller.rb
@@ -33,7 +33,7 @@ class API::ProfileController < API::RestfulController
   def save_experience
     raise ActionController::ParameterMissing.new(:experience) unless params[:experience]
     service.save_experience user: current_user, actor: current_user, params: { experience: params[:experience] }
-    head :ok
+    respond_with_resource
   end
 
   private
