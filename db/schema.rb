4e4f7b4972354e0b39a7c9fa03b34282a750899d
James Kiesel <james@loomio.org>
2016-05-07 16:37:03 +1200

    Run migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160505081129) do
+ActiveRecord::Schema.define(version: 20160506000238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
