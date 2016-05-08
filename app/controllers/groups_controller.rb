99e45d094305e13e97aaed346c8299d479faaccc
James Kiesel <james@loomio.org>
2016-05-08 19:57:51 +1000

    Readd group export

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,3 +1,13 @@
 class GroupsController < ApplicationController
   include UsesMetadata
+  skip_before_filter :boot_angular_ui, only: :export
+
+  def export
+    @exporter = GroupExporter.new(load_and_authorize(:group, :export))
+    if request[:format] == 'xls'
+      render content_type: 'application/vnd.ms-excel', layout: false
+    else
+      render layout: false
+    end
+  end
 end
