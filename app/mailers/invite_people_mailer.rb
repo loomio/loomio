ab42f00c6030e7f90f5afe8ce7eccd5155f4e972
HSalmon <hr.salmon@gmail.com>
2016-05-10 17:10:05 +1200

    Resend ignored invitations (#3342)
    * Enable resending of invitations
    
    * Run db migrations
    
    * Use since instead of age_in_hours
    
    * add resend_ignored_invitations_job and insert it into hourly rake task
    
    * Update resend_ignored_invitations_job.rb
    
    * Update resend_ignored_invitations_job.rb

--- app/mailers/invite_people_mailer.rb
+++ app/mailers/invite_people_mailer.rb
@@ -11,15 +11,16 @@ class InvitePeopleMailer < BaseMailer
                      subject_params: {group_name: @invitation.invitable_name}
   end
 
-  def to_join_group(invitation: , locale: )
+  def to_join_group(invitation:, locale:, subject_key: "email.to_join_group.subject")
     @invitation = invitation
     @message_body = invitation.message
     send_single_mail to:   @invitation.recipient_email,
                      locale: locale,
                      from: from_user_via_loomio(invitation.inviter),
                      reply_to: invitation.inviter.name_and_email,
-                     subject_key: "email.to_join_group.subject",
+                     subject_key: subject_key,
                      subject_params: {member: @invitation.inviter.name, group_name: @invitation.invitable_name}
+    invitation.increment!(:send_count)
   end
 
   def after_membership_request_approval(invitation, sender_email, message_body)
