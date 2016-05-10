25e7c08cada608ee015da8ab67228d73a26d676d
Robert Guthrie <rguthrie@gmail.com>
2016-05-10 15:08:04 +1200

    Update resend_ignored_invitations_job.rb

--- app/jobs/resend_ignored_invitations_job.rb
+++ app/jobs/resend_ignored_invitations_job.rb
@@ -1,7 +1,5 @@
 class ResendIgnoredInvitationsJob < ActiveJob::Base
   def perform
     InvitationService.resend_ignored(send_count: 1, since: 1.day.ago)
-    # We're not going to send a day 3 reminder right now. But I'll keep the code here so you get the idea.
-    # InvitationService.resend_ignored(send_count: 2, since: 3.days.ago)
   end
 end
