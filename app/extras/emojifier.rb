5c75eaec18e68d0124346162695c1e7d0168406f
James Kiesel <james@loomio.org>
2016-05-10 15:02:49 +0900

    Use loomio hosted emoji urls in email

--- app/extras/emojifier.rb
+++ app/extras/emojifier.rb
@@ -2,6 +2,8 @@ class Emojifier
   extend JSRuntime
 
   def self.emojify!(text)
-    eval "emojione.shortnameToImage(\"?\")", text
+    root_url = Rails.application.routes.url_helpers.root_url(ActionMailer::Base.default_url_options)
+    eval "(emojione.imagePathPNG = \"#{root_url.chomp('/')}\" + emojione.imagePathPNG) &&
+          (emojione.shortnameToImage(\"?\"))", text
   end
 end
