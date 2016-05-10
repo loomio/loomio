26839fe0c64f19cbff42ae6dcef43d39929e6f4d
Robert Guthrie <rob@guthr.ie>
2016-05-10 17:42:17 +1200

    set default job priority to 10, prioritise mailers, invitations first

--- app/services/contact_message_service.rb
+++ app/services/contact_message_service.rb
@@ -2,7 +2,7 @@ class ContactMessageService
 
   def self.create(contact_message:, actor:)
     contact_message.user = actor
-    ContactMessageMailer.delay.contact_message_email contact_message if contact_message.save
+    ContactMessageMailer.delay(priority: 2).contact_message_email contact_message if contact_message.save
   end
 
 end
--- config/initializers/event_bus.rb
+++ config/initializers/event_bus.rb
@@ -15,7 +15,7 @@ EventBus.configure do |config|
   Event::BULK_MAIL_KINDS.each do |kind|
     config.listen("#{kind}_event") do |event|
       BaseMailer.send_bulk_mail(to: Queries::UsersToEmailQuery.send(kind, event.eventable)) do |user|
-        ThreadMailer.delay.send(kind, user, event)
+        ThreadMailer.delay(priority: 2).send(kind, user, event)
       end
     end
   end
@@ -24,13 +24,13 @@ EventBus.configure do |config|
   # Single mail kinds is only Comment replied to and User mentioned.
   Event::SINGLE_MAIL_KINDS.each do |kind|
     config.listen("#{kind}_event") do |event, user|
-      ThreadMailer.delay.send(kind, user, event) if user.email_when_mentioned
+      ThreadMailer.delay(priority: 2).send(kind, user, event) if user.email_when_mentioned
     end
   end
 
 
   # send individual emails after user events
-  config.listen('membership_request_approved_event') { |event, user| UserMailer.delay.group_membership_approved(user, event.group) }
+  config.listen('membership_request_approved_event') { |event, user| UserMailer.delay(priority: 2).group_membership_approved(user, event.group) }
 
   # send memos to client side after comment change
   config.listen('comment_destroy') { |comment|      Memos::CommentDestroyed.publish!(comment) }
