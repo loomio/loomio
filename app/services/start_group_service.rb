4e258d80642af9c42362592a88a311d93efdea14
Robert Guthrie <rguthrie@gmail.com>
2016-05-10 19:49:51 +1200

    set default job priority to 10, prioritise mailers, invitations first (#3350)

--- app/services/start_group_service.rb
+++ app/services/start_group_service.rb
@@ -14,10 +14,10 @@ class StartGroupService
                                                                 inviter: inviter,
                                                                 recipient_email: email,
                                                                 recipient_name: name)
-                                                                
-    InvitePeopleMailer.delay.to_start_group(invitation: invitation,
-                                            sender_email: inviter.email,
-                                            locale: I18n.locale)
+
+    InvitePeopleMailer.delay(priority: 1).to_start_group(invitation: invitation,
+                                                         sender_email: inviter.email,
+                                                         locale: I18n.locale)
     invitation
   end
 end
