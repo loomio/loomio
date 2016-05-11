b74269200b4221255bbb1e98675a32a3e8bab1d7
James Kiesel <james.kiesel@gmail.com>
2016-05-11 11:16:03 +0900

    Fix export (#3357)
    * Fix export?
    
    * Fix exports

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,12 @@
 class GroupsController < ApplicationController
   include UsesMetadata
-  skip_before_filter :boot_angular_ui, only: :export
   layout false
 
   def export
     @exporter = GroupExporter.new(load_and_authorize(:group, :export))
 
     respond_to do |format|
-      format.xls { render content_type: :"application/vnd.ms-excel", formats: [:html] }
+      format.xls { render formats: [:html] }
       format.html
       format.csv { send_data @exporter.to_csv }
     end
