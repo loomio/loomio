27be966b35b83f5b82410f2286c5aaa5d93cac3c
James Kiesel <james@loomio.org>
2016-05-11 09:54:34 +0900

    Fix exports

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,7 +6,7 @@ class GroupsController < ApplicationController
     @exporter = GroupExporter.new(load_and_authorize(:group, :export))
 
     respond_to do |format|
-      format.xls { render content_type: :"application/vnd.ms-excel", formats: [:html] }
+      format.xls { render formats: [:html] }
       format.html
       format.csv { send_data @exporter.to_csv }
     end
