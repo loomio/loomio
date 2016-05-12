14bb4645ad8c782b55d6c2d675d43b665b887bb0
Hannah Salmon <hr.salmon@gmail.com>
2016-05-12 15:42:58 +1200

    Add migration for existing memberships

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509230613) do
+ActiveRecord::Schema.define(version: 20160512033506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
