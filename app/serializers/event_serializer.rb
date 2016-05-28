1e1ae8c9afe0a386dbcade568ec52885bad58d5d
James Kiesel <james@loomio.org>
2016-05-28 10:22:57 -0700

    Fix proposal closed notification

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -4,11 +4,27 @@ class EventSerializer < ActiveModel::Serializer
 
   has_one :actor, serializer: UserSerializer, root: 'users'
   has_one :eventable, polymorphic: true
+  has_one :proposal, serializer: MotionSerializer, root: 'proposals'
+  has_one :discussion, serializer: DiscussionSerializer
+
+  def eventable_is_proposal?
+    eventable.is_a?(Motion)
+  end
+  alias :include_proposal? :eventable_is_proposal?
+  alias :include_discussion? :eventable_is_proposal?
+
+  def proposal
+    eventable
+  end
+
+  def discussion
+    eventable.discussion
+  end
 
   def eventable
     # comment_liked serializes out a CommentVote, which is worthless to us on the client (and... in general.)
     # so, we'll take the Comment instead.
-    if object.eventable.is_a?(CommentVote)
+    @eventable ||= if object.eventable.is_a?(CommentVote)
       object.eventable.comment
     else
       object.eventable
