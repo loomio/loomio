2c7343803d4b533718ffa6ec76c646f9ec26bcaa
Robert Guthrie <rguthrie@gmail.com>
2016-06-17 15:36:57 +1200

    Update group_subdomain_constraints.rb

--- app/contraints/group_subdomain_constraints.rb
+++ app/contraints/group_subdomain_constraints.rb
@@ -1,6 +1,5 @@
 class GroupSubdomainConstraints
   def self.matches?(request)
-    false
-    # request.subdomain.present? && (request.subdomain != ENV['DEFAULT_SUBDOMAIN'])
+    request.subdomain.present? && (request.subdomain != ENV['DEFAULT_SUBDOMAIN'])
   end
 end
