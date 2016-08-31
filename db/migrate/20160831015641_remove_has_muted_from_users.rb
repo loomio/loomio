04594ce298c738d7746d95dfd17748502afee64e
James Kiesel <james@loomio.org>
2016-08-31 09:06:30 -0500

    Change migration to muting thread

--- db/migrate/20160831015641_remove_has_muted_from_users.rb
+++ db/migrate/20160831015641_remove_has_muted_from_users.rb
@@ -1,7 +1,7 @@
 class RemoveHasMutedFromUsers < ActiveRecord::Migration
   def change
     User.where(has_muted: true).each do |user|
-      user.experienced!("threadMuted")
+      user.experienced!("mutingThread")
     end
     remove_column :users, :has_muted, :boolean
   end
