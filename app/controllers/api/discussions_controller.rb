3512e17a717012281e4ed54e515c774f0d54dc9c
Robert Guthrie <rob@guthr.ie>
2016-09-17 16:44:11 +1200

    load and auth

--- app/controllers/api/discussions_controller.rb
+++ app/controllers/api/discussions_controller.rb
@@ -1,6 +1,6 @@
 class API::DiscussionsController < API::RestfulController
-  load_and_authorize_resource only: [:show, :mark_as_read, :move]
-  load_resource only: [:create, :update, :star, :unstar, :dismiss, :set_volume]
+  load_and_authorize_resource only: [:show, :mark_as_read, :dismiss, :move]
+  load_resource only: [:create, :update, :star, :unstar, :set_volume]
   include UsesDiscussionReaders
 
   def index
