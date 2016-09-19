6e72c2548f9232eefd64a88966470a930eac2e44
James Kiesel <james@loomio.org>
2016-09-19 22:26:07 +0100

    Allow specifying subscription plans with plugins
    angular/core/components/thread_page/thread_page_controller.coffee

--- lib/plugins/base.rb
+++ lib/plugins/base.rb
@@ -4,7 +4,7 @@ module Plugins
   class NoCodeSpecifiedError < Exception; end
   class NoClassSpecifiedError < Exception; end
   class InvalidAssetType < Exception; end
-  Outlet = Struct.new(:plugin, :component, :outlet_name, :experimental)
+  Outlet = Struct.new(:plugin, :component, :outlet_name, :experimental, :plans)
   VALID_ASSET_TYPES = [:coffee, :scss, :haml, :js, :css]
 
   class Base
@@ -70,7 +70,7 @@ module Plugins
 
     def use_component(component, outlet: nil)
       [:coffee, :scss, :haml].each { |ext| use_asset("components/#{component}/#{component}.#{ext}") }
-      Array(outlet).each { |o| @outlets.add Outlet.new(@name, component, o, @config['experimental']) }
+      Array(outlet).each { |o| @outlets.add Outlet.new(@name, component, o, @config['experimental'], @config['plans']) }
     end
 
     def use_client_route(path, component)
