39f847c3e56cf8a8b243003920af0b04a687543c
James Kiesel <james@loomio.org>
2016-09-26 10:27:58 +0100

    Move improvements to comment serializer

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -4,8 +4,18 @@ class CommentSerializer < ActiveModel::Serializer
 
   has_one :author, serializer: UserSerializer, root: :users
   has_one :discussion, serializer: DiscussionSerializer
+  has_many :attachments, serializer: AttachmentSerializer, root: :attachments
+  has_many :likers, serializer: UserSerializer, root: :users
 
   def parent_author_name
     object.parent.author_name if object.parent
+  end
+
+  def include_likers?
+    Hash(scope)[:include_likers]
+  end
+
+  def include_attachments?
+    Hash(scope)[:include_attachments]
   end
 end
