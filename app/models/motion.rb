c11a4090be6fd9d10b1461e5c21e1734af666d47
Robert Guthrie <rob@guthr.ie>
2016-10-03 07:31:25 +1300

    work around duplicate memberships issue to close motion

--- app/models/motion.rb
+++ app/models/motion.rb
@@ -110,8 +110,8 @@ class Motion < ActiveRecord::Base
 
   def close!
     did_not_votes.delete_all
-    non_voters = group_members.uniq - voters
-    DidNotVote.create! non_voters.map { |user| {motion: self, user: user} }
+    non_voters = group_members - voters
+    DidNotVote.create! non_voters.uniq.map { |user| {motion: self, user: user} }
     update(closed_at: Time.now, members_count: group.memberships_count)
   end
 
