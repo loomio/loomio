a65682a3c94b9a86f66139d15d235904ce4263ab
Hannah Salmon <hr.salmon@gmail.com>
2016-10-04 21:23:45 +1300

    Update example threads

--- app/extras/example_content.rb
+++ app/extras/example_content.rb
@@ -3,9 +3,8 @@ ExampleContent = Struct.new(:group) do
 
   def add_to_group!(admin_invite: nil)
     group.add_member!(helper_bot).tap do
-      Events::NewComment.publish!(example_comment)
+      Events::NewDiscussion.publish!(introduction_thread)
       Events::NewMotion.publish!(example_motion)
-      Events::NewVote.publish!(example_vote)
     end.destroy # remove helper bot after s/he has made example content
   end
 
@@ -23,19 +22,9 @@ ExampleContent = Struct.new(:group) do
     @how_it_works_thread ||= group.discussions.create(
       author:        helper_bot,
       title:         I18n.t('how_it_works_thread.title'),
-      description:   I18n.t('how_it_works_thread.description'),
+      description:   I18n.t('how_it_works_thread.description', thread_url: discussion_url(introduction_thread)),
       private:       !!group.discussion_private_default,
       uses_markdown: true
-    )
-  end
-
-  def example_comment
-    @example_comment ||= introduction_thread.comments.create(
-      author:        helper_bot,
-      body:          I18n.t('first_comment.body',
-                             hostname: ENV['CANONICAL_HOST'],
-                             thread_url: discussion_url(introduction_thread),
-                             group_name: group.name)
     )
   end
 
@@ -45,14 +34,6 @@ ExampleContent = Struct.new(:group) do
       name:         I18n.t('first_proposal.name'),
       description:  I18n.t('first_proposal.description'),
       closing_at:  (Time.zone.now + 7.days).at_beginning_of_hour
-    )
-  end
-
-  def example_vote
-    @example_vote ||= example_motion.votes.create(
-      author:       helper_bot,
-      statement:    I18n.t('first_vote.statement'),
-      position:     :yes
     )
   end
 
