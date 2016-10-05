35875c68d717d3f2f53a45eb8b050878c23d490d
James Kiesel <james@loomio.org>
2016-10-05 12:42:00 -0400

    Refactor invitation service method

--- config/initializers/event_bus.rb
+++ config/initializers/event_bus.rb
@@ -6,13 +6,7 @@ EventBus.configure do |config|
   config.listen('motion_create')     { |motion|     Draft.purge(user: motion.author, draftable: motion.discussion, field: :motion) }
   config.listen('vote_create')       { |vote|       Draft.purge(user: vote.user, draftable: vote.motion, field: :vote) }
 
-  config.listen('group_create') do |group, actor|
-    InvitationService.delay.invite_admin_to_group(
-      group: group,
-      name:  actor.name,
-      email: actor.email
-    ) unless actor.is_logged_in?
-  end
+  config.listen('group_create') { |group, actor|    InvitationService.delay.invite_creator_to_group(group: group, creator: actor) }
 
   # Index search vectors after model creation
   config.listen('discussion_create', 'discussion_update') { |discussion| SearchVector.index! discussion.id }
