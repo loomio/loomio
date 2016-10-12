fa723d49e83140defd6220975bac8e9140b5641c
Robert Guthrie <rguthrie@gmail.com>
2016-10-12 15:29:28 +1300

    fix 404 on chargify return (#3778)

--- app/extras/model_locator.rb
+++ app/extras/model_locator.rb
@@ -8,13 +8,19 @@ ModelLocator = Struct.new(:model, :params) do
     elsif model.to_sym == :user
       resource_class.find_by(username: params[:id] || params[:username]) || resource_class.find(params[:id])
     elsif resource_class.respond_to?(:friendly)
-      resource_class.friendly.find params[:key] || params[:id]
+      resource_class.friendly.find key_or_id
     else
-      resource_class.find params[:key] || params[:id]
+      resource_class.find key_or_id
     end
   end
 
   private
+
+  def key_or_id
+    # strip overloaded id's that chargify gives us in the form of
+    # key-number
+    (params[:key] || params[:id]).to_s.split('-')[0]
+  end
 
   def resource_class
     @resource_class ||= model.to_s.camelize.constantize
