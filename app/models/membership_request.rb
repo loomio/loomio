71b5e60886f7058f9115982536793c38eb6a7d1a
Hannah Salmon <hr.salmon@gmail.com>
2016-10-18 15:39:44 +1300

    Tweak language + add character count

--- app/models/membership_request.rb
+++ app/models/membership_request.rb
@@ -8,6 +8,8 @@ class MembershipRequest < ActiveRecord::Base
 
   validates :group, presence: true
 
+  validates_length_of :introduction, maximum: 250
+
   belongs_to :group
   belongs_to :requestor, class_name: 'User'
   belongs_to :user, foreign_key: 'requestor_id' # duplicate relationship for eager loading
