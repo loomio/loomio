f3e0ec7172a051bc97b8a69a1bfc81e5cfbe0326
James Kiesel <james.kiesel@gmail.com>
2016-11-06 13:52:26 -0500

    Fix notification errors (#3849)
    * Fix notification errors
    
    * Fix notification baker errors
    
    * More robust error handling; add specs
    
    * Fix membership.author error

--- app/extras/notification_baker.rb
+++ app/extras/notification_baker.rb
@@ -6,7 +6,8 @@ class NotificationBaker
   # This code isn't very pretty, but it gets the job done and once all users have rebaked their
   # notifications, we can get rid of it.
   def self.bake!(notifications)
-    notifications.reject { |n| n.url && n.translation_values }.each do |notification|
+    notifications.includes(:event).reject { |n| n.url && n.translation_values }.each do |notification|
+      return unless Event::NOTIFICATION_KINDS.include?(notification.kind)
       @event = Events.const_get(notification.kind.camelize).new(notification.event.as_json)
       notification.update(
         url:                @event.send(:notification_url),
