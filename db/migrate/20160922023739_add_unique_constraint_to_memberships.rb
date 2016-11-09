00a7ee1132bfe347bbc0029413d2058cf3bfa053
Robert Guthrie <rob@guthr.ie>
2016-11-09 14:06:16 +1300

    delete dupes

--- db/migrate/20160922023739_add_unique_constraint_to_memberships.rb
+++ db/migrate/20160922023739_add_unique_constraint_to_memberships.rb
@@ -1,5 +1,8 @@
 class AddUniqueConstraintToMemberships < ActiveRecord::Migration
   def change
+    execute "DELETE FROM memberships m1 USING memberships m2 
+  WHERE m1.user_id = m2.user_id AND m1.group_id = m2.group_id AND
+    m1.id < m2.id"
     add_index :memberships, [:group_id, :user_id], unique: true
   end
 end
