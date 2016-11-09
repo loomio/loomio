7508e1b6aacbc701211535877f819c4f6d8c8e01
Robert Guthrie <rob@guthr.ie>
2016-11-09 14:20:50 +1300

    ok

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -138,8 +138,8 @@ ActiveAdmin.register User do
       wanted_user = User.find_by!(email: params[:wanted_user_email])
       MigrateUserService.new(old_id: unwanted_user.id, new_id: wanted_user.id).commit!
       redirect_to admin_user_path(wanted_user), notice: "Merged #{params[:unwanted_user_email]} records into #{params[:wanted_user_email]} successfully"
-    rescue ActiveRecord::RecordNotFound
-      render text: "user not found by email: #{params[:wanted_user_email]}"
+    rescue ActiveRecord::RecordNotFound => e
+      render text: "user not found by email: #{params[:wanted_user_email]}"+e.inspect
     end
   end
 end
