8c8bf997776409c50101f9ca99f99411e444a74c
James Kiesel <james@loomio.org>
2016-11-14 19:55:21 -0500

    Whoops wrote the wrong codeez

--- app/models/events/user_added_to_group.rb
+++ app/models/events/user_added_to_group.rb
@@ -6,7 +6,7 @@ class Events::UserAddedToGroup < Event
   def self.bulk_publish!(memberships, inviter, message = nil)
     memberships.map { |membership| new(kind: 'user_added_to_group', user: inviter, eventable: membership) }
                .tap { |events| import(events) }
-               .tap { |events| events.map { |event| EventBus.broadcast('user_added_to_group_event', event, event.user, message) } }
+               .tap { |events| events.map { |event| EventBus.broadcast('user_added_to_group_event', event, event.eventable.user, message) } }
   end
 
   def notification_actor
