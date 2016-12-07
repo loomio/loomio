7e830967400afbaea1c1b15a3f559490899fbdc5
James Kiesel <james.kiesel@gmail.com>
2016-12-07 05:38:53 +0100

    Only show carousel to new accounts (#3926)
    * Add created_at to user serializer
    
    * Fix failing specs

--- app/models/logged_out_user.rb
+++ app/models/logged_out_user.rb
@@ -9,7 +9,7 @@ class LoggedOutUser
     set_avatar_initials if (@name || @email)
   end
 
-  NIL_METHODS   = [:id, :key, :username, :avatar_url, :selected_locale, :deactivated_at, :time_zone, :default_membership_volume, :unsubscribe_token]
+  NIL_METHODS   = [:id, :key, :username, :avatar_url, :selected_locale, :deactivated_at, :time_zone, :default_membership_volume, :unsubscribe_token, :created_at]
   FALSE_METHODS = [:is_logged_in?, :uses_markdown?, :is_organisation_coordinator?,
                    :email_when_proposal_closing_soon, :email_missed_yesterday, :email_when_mentioned, :email_on_participation, :is_group_admin?]
   EMPTY_METHODS = [:groups, :group_ids, :adminable_group_ids]
