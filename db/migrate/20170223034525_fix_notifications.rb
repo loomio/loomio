47ef41d51575993c2212eeff69547be72f2acad5
James Kiesel <james@loomio.org>
2017-02-23 22:36:22 +1300

    Re-fix notifications

--- db/migrate/20170223034525_fix_notifications.rb
+++ db/migrate/20170223034525_fix_notifications.rb
@@ -1,26 +1,21 @@
 class FixNotifications < ActiveRecord::Migration
   def change
-    Notification.includes(event: :eventable).find_each(batch_size: 100) do |notification|
-      next if notification.translation_values.blank? ||
-              notification.translation_values[:title].present? ||
-              !affected_event_kinds.include?(notification.kind)
-      event = Events.const_get(notification.kind.camelize).new(notification.event.as_json) rescue Event.new
-      notification.update(translation_values: event.send(:notification_translation_values))
+    Event.where(kind: %w(
+      comment_liked
+      comment_replied_to
+      invitation_accepted
+      membership_request_approved
+      membership_requested
+      motion_closed_by_user
+      motion_closed
+      motion_closing_soon
+      motion_outcome_created
+      new_coordinator
+      user_added_to_group
+      user_mentioned
+    )).where('created_at > ?', 1.year.ago).find_each(batch_size: 100) do |event|
+      e = Events.const_get(event.kind.camelize).new(event.as_json) rescue Event.new
+      event.notifications.update_all(translation_values: e.send(:notification_translation_values)) rescue nil
     end
-  end
-
-  def affected_event_kinds
-    %w(comment_liked
-       comment_replied_to
-       invitation_accepted
-       membership_request_approved
-       membership_requested
-       motion_closed_by_user
-       motion_closed
-       motion_closing_soon
-       motion_outcome_created
-       new_coordinator
-       user_added_to_group
-       user_mentioned).freeze
   end
 end
