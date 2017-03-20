26c07b3ef4f2ecf2c3e2b8ac897c4cffd12f5ab0
Robert Guthrie <rguthrie@gmail.com>
2017-03-20 15:08:07 +1300

    remove rack attack entirely

--- config/application.rb
+++ config/application.rb
@@ -34,7 +34,7 @@ module Loomio
     # config.paths.add "extras", eager_load: true
     # config.autoload_paths += Dir["#{config.root}/app/forms/**/"]
 
-    config.middleware.use Rack::Attack
+    # config.middleware.use Rack::Attack
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
