63dc471ed59fe43ad1c8e6efc42274f1c6dd63ef
James Kiesel <james@loomio.org>
2017-03-24 11:12:28 +1300

    Add specs for my polls

--- app/controllers/api/polls_controller.rb
+++ app/controllers/api/polls_controller.rb
@@ -9,7 +9,8 @@ class API::PollsController < API::RestfulController
   def index
     instantiate_collection do |collection|
       collection = collection.where(discussion: @discussion) if load_and_authorize(:discussion, optional: true)
-      collection = collection.active                         if params[:active]
+      collection = collection.where(author: current_user)    if params[:authored_only]
+      collection = collection.send(params[:filter])          if Poll::FILTERS.include?(params[:filter].to_s)
       collection.order(:created_at)
     end
     respond_with_collection
