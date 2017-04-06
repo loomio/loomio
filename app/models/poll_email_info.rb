d87a84c24fd33294f2452be766ab3d03bb92ec08
James Kiesel <james@loomio.org>
2017-04-06 13:50:05 +0900

    Correctly handle time zones in email

--- app/models/poll_email_info.rb
+++ app/models/poll_email_info.rb
@@ -24,12 +24,11 @@ class PollEmailInfo
   end
 
   def time_zone
-    @recipient.time_zone || poll_time_zone
+    @recipient.time_zone || @poll.custom_fields['time_zone']
   end
 
-  def poll_time_zone
-    return unless time_zone = @poll.custom_fields['time_zone']
-    ActiveSupport::TimeZone[time_zone].to_s
+  def formatted_time_zone
+    ActiveSupport::TimeZone[time_zone].to_s if time_zone
   end
 
   def outcome
