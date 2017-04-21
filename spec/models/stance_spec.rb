c370a59d977efa9d0cca5da6772c4d7abb025534
James Kiesel <james@loomio.org>
2017-04-21 12:07:33 +0900

    Add spec for stance

--- spec/models/stance_spec.rb
+++ spec/models/stance_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 describe Stance do
+  describe 'stance choice validation' do
+    let(:poll) { create :poll }
+    let(:proposal) { create :poll_proposal }
+    let(:user) { create :user }
+
+    it 'allows no stance choices for meetings / polls' do
+      expect(Stance.new(poll: poll, participant: user)).to be_valid
+    end
+
+    it 'requires a stance choice for proposals' do
+      expect(Stance.new(poll: proposal, participant: user)).to_not be_valid
+    end
+  end
+
   describe 'choice shorthand' do
     let(:poll) { Poll.create!(poll_type: 'poll', title: 'which pet?', poll_option_names: %w[dog cat], closing_at: 1.day.from_now, author: author)}
     let(:author) { FactoryGirl.create(:user) }
