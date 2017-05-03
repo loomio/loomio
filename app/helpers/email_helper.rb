305ea321f8296a2aa8514486a0d9b303c05a8275
James Kiesel <james.kiesel@gmail.com>
2017-05-03 20:00:06 +0800

    Fix invalid date (#4053)

--- app/helpers/email_helper.rb
+++ app/helpers/email_helper.rb
@@ -1,22 +1,6 @@
 module EmailHelper
   include PrettyUrlHelper
 
-  def formatted_datetime_for(date_string, zone)
-    date_time = DateTime.strptime(date_string.sub('.000Z', 'Z')).in_time_zone(zone)
-    date_time.strftime(date_time.year == Date.today.year ? "%e %b %l:%M %P" : "%e %b %Y %l:%M %P")
-  rescue ArgumentError
-    formatted_date_for(date_string)
-  end
-
-  def formatted_date_for(date_string)
-    date = date_string.to_date
-    date.strftime(date.year == Date.today.year ? "%e %b" : "%e %b %Y")
-  end
-
-  def format_poll_option_name(poll_option, zone)
-    poll_option.poll.dates_as_options ? formatted_datetime_for(poll_option.name, zone) : poll_option.name
-  end
-
   def reply_to_address(discussion: , user: )
     pairs = []
     {d: discussion.id, u: user.id, k: user.email_api_key}.each do |key, value|
