d25e1faf8f668d17349c44fc0b1db472761e7b81
James Kiesel <james@loomio.org>
2017-05-08 14:48:54 +0800

    Don't log in when registering user

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -1,17 +1,15 @@
 class API::RegistrationsController < Devise::RegistrationsController
-  include PendingActionsHelper
-
-  private
-
-  def respond_with(resource, args = {})
-    if resource.persisted?
-      handle_pending_actions
-      flash[:notice] = t(:'devise.registrations.signed_up')
-      render json: BootData.new(resource).data
+  def create
+    build_resource(sign_up_params)
+    if resource.save
+      LoginTokenService.create(actor: resource)
+      head :ok
     else
       render json: { errors: resource.errors }, status: 422
     end
   end
+
+  private
 
   def sign_up_params
     params.require(:user).permit(:name, :email, :recaptcha).tap { |p| p.require(:recaptcha) }
