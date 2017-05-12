00228a279c86b05dfb025818ad0192bb5590815b
James Kiesel <james@loomio.org>
2017-05-12 13:33:15 +0800

    Re add flash message

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,6 +4,7 @@ class API::SessionsController < Devise::SessionsController
   def create
     if user = warden.authenticate(scope: resource_name)
       sign_in(user)
+      flash[:notice] = t(:'devise.sessions.signed_in')
       render json: BootData.new(user).data
     else
       render json: { errors: { password: [t(:"devise.failure.invalid")] } }, status: 401
