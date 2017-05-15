cc0d900388319f5216ace6772a1bd53387a219b7
James Kiesel <james@loomio.org>
2017-05-15 15:02:37 +0800

    Add notify on participate

--- app/models/events/stance_created.rb
+++ app/models/events/stance_created.rb
@@ -1,5 +1,6 @@
 class Events::StanceCreated < Event
   include Events::LiveUpdate
+  include Events::PollEvent
 
   def self.publish!(stance)
     create(kind: "stance_created",
@@ -8,4 +9,16 @@ class Events::StanceCreated < Event
            discussion: stance.poll.discussion,
            created_at: stance.created_at).tap { |e| EventBus.broadcast('stance_created_event', e) }
   end
+
+  private
+
+  def notification_recipients
+    if poll.notify_on_participate?
+      User.where(id: poll.author_id)
+    else
+      User.none
+    end
+  end
+  alias :email_recipients :notification_recipients
+
 end
