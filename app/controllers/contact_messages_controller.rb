a8f05f917bdfe8ae146b8f3eef1dd4b2c911c781
Robert Guthrie <rguthrie@gmail.com>
2017-05-22 12:38:56 +1200

    change contact form to use intercom api (#4096)
    * change intercom
    
    * Update contact_messages_controller.rb
    
    * remove contact form cukes

--- app/controllers/contact_messages_controller.rb
+++ app/controllers/contact_messages_controller.rb
@@ -13,9 +13,8 @@ class ContactMessagesController < BaseController
   	@contact_message = ContactMessage.new(permitted_params.contact_message)
     @contact_message.user = current_user if current_user.is_logged_in?
   	if @contact_message.save
-      ContactMessageMailer.delay(priority: 2).contact_message_email(@contact_message)
+      ContactMessageService.deliver(email: @contact_message.email, message: @contact_message.message )
       flash[:success] = "Thanks! Someone from our team will get back to you shortly!"
-
       redirect_to dashboard_or_root_path
     else
       render :new
