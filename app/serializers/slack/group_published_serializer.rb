07d99b6c22ba1feaac166074115f1e620fc4c5ef
James Kiesel <james@loomio.org>
2017-05-30 03:17:51 +1200

    Add actor to group published

--- app/serializers/slack/group_published_serializer.rb
+++ app/serializers/slack/group_published_serializer.rb
@@ -1,7 +1,7 @@
 class Slack::GroupPublishedSerializer < Slack::BaseSerializer
   def text
     I18n.t(:"slack.join_loomio_group", {
-      author: model.creator&.name || User.helper_bot.name,
+      author: object.user.name,
       name: model.name,
       url: invitation_url(model.shareable_invitation.token, default_url_options)
     })
