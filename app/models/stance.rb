9f6e569414b1d57c0726befef4581b85274f55d7
James Kiesel <james.kiesel@gmail.com>
2017-06-05 08:09:27 +1200

    Count visitors (#4139)
    * Even moar selectors
    
    * Add visitors count to polls
    
    * Bump minor version
    
    * Fix failing specs

--- app/models/stance.rb
+++ app/models/stance.rb
@@ -16,6 +16,7 @@ class Stance < ActiveRecord::Base
   belongs_to :participant, polymorphic: true, required: true
 
   update_counter_cache :poll, :stances_count
+  update_counter_cache :poll, :visitors_count
 
   scope :latest, -> { where(latest: true) }
 
