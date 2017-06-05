51d6e3f440c0728a39208fa3f59684737e17d983
James Kiesel <james@loomio.org>
2017-06-05 15:49:04 +1200

    Moar test fixes...

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -63,6 +63,9 @@ describe Event do
     # set email motion closing soon
     discussion.group.add_member!(user_motion_closing_soon).set_volume! :mute
 
+    # add the loomio group community to poll
+    poll.build_loomio_group_community; poll.save
+
     # create an unsubscription for a poll user
     poll.poll_unsubscriptions.create(user: user_unsubscribed)
   end
