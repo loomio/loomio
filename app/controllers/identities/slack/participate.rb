1f0780048b9f7e039e52cb942b8dda9b29c41292
James Kiesel <james@loomio.org>
2017-06-07 18:11:39 +1200

    Automagic group invite on participate

--- app/controllers/identities/slack/participate.rb
+++ app/controllers/identities/slack/participate.rb
@@ -1,64 +1,52 @@
 module Identities::Slack::Participate
   def participate
-    render json: respond_with_stance      ||
-                 respond_with_poll_closed ||
-                 respond_with_invitation  ||
+    render json: respond_with_poll_closed ||
+                 respond_with_stance      ||
                  respond_with_participate_unauthorized
   end
 
   private
-
-  def respond_with_stance
-    return unless can_participate?
-    ::Slack::Ephemeral::StanceCreatedSerializer.new(StanceService.create(
-      stance: participate_stance,
-      actor: participate_participant
-    ), root: false).as_json
-  end
 
   def respond_with_poll_closed
     return unless !participate_poll.active?
     ::Slack::Ephemeral::PollClosedSerializer.new(participate_poll, root: false).as_json
   end
 
-  def respond_with_invitation
-    return unless participate_invitation&.slack_team_id.present?
-    ::Slack::Ephemeral::GroupInvitationSerializer.new(participate_invitation, scope: {
-      back_to: poll_url(participate_poll),
-      uid:     participate_payload.dig('user', 'id')
-    }, root: false).as_json
+  def respond_with_stance
+    return unless participate_poll&.group&.add_member!(participate_identity&.user)
+    ::Slack::Ephemeral::StanceCreatedSerializer.new(participate_stance_event, root: false).as_json
   end
 
   def respond_with_participate_unauthorized
-      ::Slack::Ephemeral::RequestAuthorizationSerializer.new({
-        url: request_authorization_url(participate_payload['team'])
-      }, root: false).as_json
+    ::Slack::Ephemeral::RequestAuthorizationSerializer.new({
+      url: request_authorization_url(participate_payload['team'])
+    }, root: false).as_json
   end
 
-  def participate_invitation
-    @participate_invitation ||= participate_poll&.group&.shareable_invitation
+  def participate_stance_event
+    @participant_stance_event ||= StanceService.create(
+      stance: participate_stance,
+      actor:  participate_identity.user
+    )
+  end
+
+  def participate_stance
+    @participate_stance ||= Stance.new(
+    poll:   participate_poll,
+    choice: participate_payload.dig('actions', 0, 'name')
+    )
   end
 
   def participate_poll
     @participate_poll ||= Poll.find_by(id: participate_payload['callback_id'])
   end
 
-  def can_participate?
-    participate_participant&.can? :create, participate_stance
-  end
 
-  def participate_stance
-    @participate_stance ||= Stance.new(
-      poll:   participate_poll,
-      choice: participate_payload.dig('actions', 0, 'name')
+  def participate_identity
+    @participate_participant ||= Identities::Base.find_by(
+      identity_type: :slack,
+      uid:           participate_payload.dig('user', 'id')
     )
-  end
-
-  def participate_participant
-    byebug
-    @participate_participant ||= Identities::Base
-      .where(identity_type: :slack, uid: participate_payload.dig('user', 'id'))
-      .first&.user
   end
 
   def participate_payload
