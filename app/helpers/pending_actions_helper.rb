bd32cdaa6fab6a37a1d96817313715d8e610d2cd
James Kiesel <james@loomio.org>
2017-07-09 21:30:48 +1200

    Fix failing spec

--- app/helpers/pending_actions_helper.rb
+++ app/helpers/pending_actions_helper.rb
@@ -5,7 +5,7 @@ module PendingActionsHelper
     return unless user.presence
 
     if pending_invitation
-      InvitationService.redeem(pending_invitation, user)
+      InvitationService.redeem(pending_invitation, user) if !pending_invitation.single_use? && !pending_invitation.accepted?
       session.delete(:pending_invitation_id)
     end
 
