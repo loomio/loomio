c4b7fd50352f5ab6f9e131ff832ed7b8e1852bc6
Robert Guthrie <rguthrie@gmail.com>
2017-07-11 15:22:09 +1200

    save detected locale

--- app/helpers/locales_helper.rb
+++ app/helpers/locales_helper.rb
@@ -20,10 +20,21 @@ module LocalesHelper
   end
 
   def set_application_locale
-    I18n.locale = (Array(locales_from_param)             | # locale from request param
-                   Array(locales_from_user_preference)   | # locale from selected user preference
-                   Array(locales_from_browser_detection) | # locales from browser headers
-                   Array(I18n.default_locale)).first
+    I18n.locale = best_locale
+  end
+
+  def best_locale
+    (Array(locales_from_param)             | # locale from request param
+     Array(locales_from_user_preference)   | # locale from selected user preference
+     Array(locales_from_browser_detection) | # locales from browser headers
+     Array(locales_from_saved_browser_detection)   | # locale from selected user preference
+     Array(I18n.default_locale)).compact.first
+  end
+
+  def save_detected_locale
+    if current_user.is_logged_in? && locales_from_browser_detection.any?
+      current_user.update_detected_locale(locales_from_browser_detection.first)
+    end
   end
 
   private
@@ -37,6 +48,11 @@ module LocalesHelper
     filter_locales(current_user.selected_locale, selectable_locales)
   end
 
+  def locales_from_saved_browser_detection
+    return unless current_user&.is_logged_in?
+    filter_locales(current_user.detected_locale, selectable_locales)
+  end
+
   def locales_from_browser_detection
     parser = HttpAcceptLanguage::Parser.new(request.env["HTTP_ACCEPT_LANGUAGE"])
     locales = parser.user_preferred_languages +
@@ -45,7 +61,7 @@ module LocalesHelper
   end
 
   def filter_locales(input_locales, valid_locales)
-    Array(input_locales).map(&:to_sym) & Array(valid_locales).map(&:to_sym)
+    (Array(input_locales).map(&:to_sym) & Array(valid_locales).map(&:to_sym)).compact.uniq
   end
 
 end
