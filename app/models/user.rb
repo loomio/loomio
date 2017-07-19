b1fa93d489219909d85d3e2770ffaa0a721b9e15
Robert Guthrie <rguthrie@gmail.com>
2017-07-19 14:14:27 +1200

    add user first and last name methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -146,6 +146,14 @@ class User < ActiveRecord::Base
     end
   end
 
+  def first_name
+    name.split(' ').first
+  end
+
+  def last_name
+    name.split(' ').drop(1).join(' ')
+  end
+
   def remember_me
     true
   end
