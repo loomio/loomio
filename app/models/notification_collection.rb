06599af797209bae21a6d175a621fd13d989c78d
James Kiesel <james@loomio.org>
2017-07-27 02:43:05 +1200

    Remove notification baker

--- app/models/notification_collection.rb
+++ app/models/notification_collection.rb
@@ -2,10 +2,10 @@ class NotificationCollection
   attr_accessor :notifications
 
   def initialize(user, limit: 30)
-    @notifications = user.notifications.includes(:actor, :event).order(created_at: :desc).limit(limit)
-
-    # account for old notifications which don't have the url and translation_values fields populated.
-    NotificationBaker.bake!(@notifications)
+    @notifications = user.notifications
+                         .includes(:actor)
+                         .order(created_at: :desc)
+                         .limit(limit)
   end
 
   def serialize!(scope = {})
