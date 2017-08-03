e3e5f56f21b3e3972778fb99aad42ca1d178aecf
James Kiesel <james.kiesel@gmail.com>
2017-08-03 17:03:51 +1200

    Remove gems and spare views (#4230)
    * Remove gems and spare views
    
    * Remove omniauthable
    
    * Remove omniauth from devise
    
    * Remove moar cruft

--- db/migrate/20140708053114_generate_email_api_keys_for_users.rb
+++ db/migrate/20140708053114_generate_email_api_keys_for_users.rb
@@ -1,13 +1,8 @@
 class GenerateEmailAPIKeysForUsers < ActiveRecord::Migration
   def up
-    progress_bar = ProgressBar.create(format: "(\e[32m%c/%C\e[0m) %a |%B| \e[31m%e\e[0m ",
-                                      progress_mark: "\e[32m/\e[0m",
-                                      total: User.count)
-
     User.reset_column_information
     User.find_each do |user|
       user.update_attribute(:email_api_key, SecureRandom.hex(16))
-      progress_bar.increment
     end
   end
 
