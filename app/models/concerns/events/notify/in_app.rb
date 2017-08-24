06a344332221b50600c340b039dbf5f34ad7d838
James Kiesel <james@loomio.org>
2017-08-24 10:49:06 -0700

    Move to reaction_created notification

--- app/models/concerns/events/notify/in_app.rb
+++ app/models/concerns/events/notify/in_app.rb
@@ -53,11 +53,11 @@ module Events::Notify::InApp
   def notification_translation_title
     case eventable
     when PaperTrail::Version then eventable.item.title
-    # TODO: deal with polymorphic reactions here
     when Comment, Discussion then eventable.discussion.title
     when Group, Membership   then eventable.group.full_name
     when Poll, Outcome       then eventable.poll.title
-    when Reaction            then eventable.reactable.discussion
+    # TODO: deal with polymorphic reactions here
+    when Reaction            then eventable.reactable.discussion.title
     end
   end
 end
