0d162295a923af54eeacc9a0e8de4ff9a7ae6a68
James Kiesel <james@loomio.org>
2017-08-31 14:13:59 -0700

    Add lograge for more concise logging

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,8 @@ Loomio::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.lograge.enabled = ENV['ENABLE_LOGRAGE']
+
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
