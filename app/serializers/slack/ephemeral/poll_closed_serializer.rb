767f05d2d456b4ec221dab36ee1db214190e7fa6
James Kiesel <james@loomio.org>
2017-08-30 20:54:45 -0700

    Fix poll closed response

--- app/serializers/slack/ephemeral/poll_closed_serializer.rb
+++ app/serializers/slack/ephemeral/poll_closed_serializer.rb
@@ -4,4 +4,8 @@ class Slack::Ephemeral::PollClosedSerializer < Slack::BaseSerializer
   def text
     I18n.t(:"slack.poll_closed", title: object.title, url: slack_link_for(object))
   end
+
+  def model
+    object
+  end
 end
