67050ea41242d6e2f1193c50cc70caa8058bcdfe
James Kiesel <james@loomio.org>
2017-09-02 22:36:31 -0700

    Add invite link to intro message for slack

--- app/serializers/slack/group_identity_created_serializer.rb
+++ app/serializers/slack/group_identity_created_serializer.rb
@@ -1,6 +1,13 @@
 class Slack::GroupIdentityCreatedSerializer < Slack::BaseSerializer
   private
 
+  def text_options
+    super.merge({
+      url:   slack_link_for(model.group, invitation: true),
+      group: model.group.full_name
+    })
+  end
+
   def include_attachments?
     false
   end
