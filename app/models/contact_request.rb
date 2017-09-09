fc6aabcbef024b97e3646d59eab46aa860768da4
James Kiesel <james@loomio.org>
2017-09-09 16:18:10 -0400

    Add validation; mailer spec

--- app/models/contact_request.rb
+++ app/models/contact_request.rb
@@ -4,9 +4,13 @@ class ContactRequest
   attr_accessor :message, :sender, :recipient_id
 
   def save
-    UserMailer.contact_request(contact_request: self).deliver_now
+    UserMailer.contact_request(contact_request: self).deliver_now if valid?
   end
   alias :save! :save
+
+  def valid?
+    message.present?
+  end
 
   def recipient
     @recipient ||= User.find self.recipient_id
