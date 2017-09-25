921b094ba8b52225b1fe70d3ebd5229b6c8b39c1
Robert Guthrie <rguthrie@gmail.com>
2017-09-25 19:28:13 +1300

    make site name configurable a bit more

--- app/mailers/group_mailer.rb
+++ app/mailers/group_mailer.rb
@@ -34,7 +34,7 @@ class GroupMailer < BaseMailer
     send_single_mail  to: recipient.name_and_email,
                       reply_to: "#{@membership_request.name} <#{@membership_request.email}>",
                       subject_key: "email.membership_request.subject",
-                      subject_params: {who: @membership_request.name, which_group: @group.full_name},
+                      subject_params: {who: @membership_request.name, which_group: @group.full_name, site_name: AppConfig.theme[:site_name]},
                       locale: locale_for(recipient)
   end
 end
