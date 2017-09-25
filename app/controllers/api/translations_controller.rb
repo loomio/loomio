22f32c49be3a9cd138a035ce8960db1d7da6b010
James Kiesel <james.kiesel@gmail.com>
2017-09-25 07:25:31 -0500

    Dynamic translations (#4312)
    * Back to dynamic translations
    
    * Add specs around client translation

--- app/controllers/api/translations_controller.rb
+++ app/controllers/api/translations_controller.rb
@@ -1,4 +1,8 @@
 class API::TranslationsController < API::RestfulController
+  def show
+    render json: ClientTranslationService.new(params[:lang]).as_json
+  end
+
   def inline
     self.resource = service.create(model: load_and_authorize(params[:model]), to: params[:to])
     respond_with_resource
