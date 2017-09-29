1a93e8c981ca7d51ced1fba82b2c56fecb800842
James Kiesel <james.kiesel@gmail.com>
2017-09-28 23:34:25 -0500

    Back to normal user serializer

--- angular/core/models/document_model.coffee
+++ angular/core/models/document_model.coffee
@@ -2,7 +2,7 @@ angular.module('loomioApp').factory 'DocumentModel', (BaseModel, AppConfig) ->
   class DocumentModel extends BaseModel
     @singular: 'document'
     @plural: 'documents'
-    @indices: ['modelId']
+    @indices: ['modelId', 'authorId']
     @serializableAttributes: AppConfig.permittedParams.document
 
     relationships: ->
--- app/serializers/document_serializer.rb
+++ app/serializers/document_serializer.rb
@@ -1,5 +1,5 @@
 class DocumentSerializer < ActiveModel::Serializer
   embed :ids, include: true
-  attributes :id, :title, :icon, :color, :url, :model_id, :model_type, :attachment_id, :created_at
-  has_one :author, serializer: Simple::UserSerializer, root: :users
+  attributes :id, :title, :icon, :color, :url, :model_id, :model_type, :created_at
+  has_one :author, serializer: UserSerializer, root: :users
 end
