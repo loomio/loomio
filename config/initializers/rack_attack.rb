87e63643f5f009445cdc09cc23085d99bdf0e120
Robert Guthrie <rguthrie@gmail.com>
2017-10-01 17:21:31 +1300

    ok I'll leave it now

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,5 +1,19 @@
 class Rack::Attack
-  heavy =  ['/api/v1/groups']
+  heavy =  ['/api/v1/groups', '/api/v1/invitations/bulk_create']
+
+  # considerations
+  # multiple users could be using same ip address (eg coworking space)
+  # does not distinguish between valid and invalid requests (eg: form validation)
+  # - so we need to allow for errors and resubmits without interruption.
+  # our attackers seem to create a group every 1 or 2 minutes, each with max invitations.
+  # so we're mostly interested in the hour and day limits
+
+  {10 => 1.hour,
+   20 => 1.day}.each_pair do |limit, period|
+    Rack::Attack.throttle("groups#create", :limit => limit, :period => period) do |req|
+      req.ip if heavy.any? {|route| req.path.starts_with?(route)} && req.post?
+    end
+  end
 
   medium = ['/api/v1/login_tokens',
             '/api/v1/invitations',
@@ -11,17 +25,10 @@ class Rack::Attack
             '/api/v1/attachments',
             '/api/v1/contact_messages']
 
-  {3  => 1.minute,
-   10 => 1.hour,
-   30 => 1.day}.each_pair do |limit, period|
-    Rack::Attack.throttle("#{limit}#{period}", :limit => limit, :period => period) do |req|
-      req.ip if heavy.any? {|route| req.path.starts_with?(route)} && req.post?
-    end
-  end
-
-  {10  => 1.minute,
-   300 => 1.day}.each_pair do |limit, period|
-    Rack::Attack.throttle("#{limit}#{period}", :limit => limit, :period => period) do |req|
+  {10    => 1.minute,
+   100   => 1.hour,
+   1000  => 1.day}.each_pair do |limit, period|
+    Rack::Attack.throttle("record#create", :limit => limit, :period => period) do |req|
       req.ip if medium.any? {|route| req.path.starts_with?(route)} && req.post?
     end
   end
