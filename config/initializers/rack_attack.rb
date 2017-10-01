81d2cd11cc5f1459b353fea09bf847e03872c789
Robert Guthrie <rguthrie@gmail.com>
2017-10-02 09:18:37 +1300

    explain myself a little more

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -8,6 +8,11 @@ class Rack::Attack
   # our attackers seem to create a group every 1 or 2 minutes, each with max invitations.
   # so we're mostly interested in the hour and day limits
 
+  # group creation and invitation sending is the most common attack we see
+  # usually we get a few new groups per hour, globally.
+  # when attacks happen we see a few groups per minute, usually with the same name
+  # each trying to invite max_invitations with bulk create.
+
   {10 => 1.hour,
    20 => 1.day}.each_pair do |limit, period|
     Rack::Attack.throttle("groups#create", :limit => limit, :period => period) do |req|
@@ -15,21 +20,27 @@ class Rack::Attack
     end
   end
 
-  medium = ['/api/v1/login_tokens',
-            '/api/v1/invitations',
-            '/api/v1/discussions',
-            '/api/v1/polls',
-            '/api/v1/stances',
-            '/api/v1/comments',
-            '/api/v1/reactions',
-            '/api/v1/attachments',
-            '/api/v1/contact_messages']
+  medium = ['login_tokens',
+            'invitations',
+            'discussions',
+            'polls',
+            'stances',
+            'comments',
+            'reactions',
+            'attachments',
+            'registrations',
+            'contact_messages']
 
-  {10    => 1.minute,
-   100   => 1.hour,
-   1000  => 1.day}.each_pair do |limit, period|
+  # throttles all posts to the above endponts
+  # so we're looking at a record creation attack.
+  # the objective of these rules is not to guess what normal behaviour looks like
+  # and pitch abouve that.. but to identify what abusive behaviour certainly is,
+  # and ensure it cannot get really really bad.
+  {100     => 5.minutes,
+   1000    => 1.hour,
+   10000   => 1.day}.each_pair do |limit, period|
     Rack::Attack.throttle("record#create", :limit => limit, :period => period) do |req|
-      req.ip if medium.any? {|route| req.path.starts_with?(route)} && req.post?
+      req.ip if medium.any? {|route| req.path.starts_with?("/api/v1/#{route}")} && req.post?
     end
   end
 end
