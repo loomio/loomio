6355768b6a65f0ebc1e53a6d13f576c954db4cfd
Robert Guthrie <rguthrie@gmail.com>
2017-10-01 16:07:14 +1300

    general throttling plan

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,35 @@
 class Rack::Attack
-  # Throttle group create
-  Rack::Attack.throttle('req/ip', :limit => 10, :period => 1.hour) do |req|
-    req.path == '/api/v1/groups' && req.post?
+  heavy =  ['/api/v1/groups']
+
+  medium = ['/api/v1/login_tokens',
+            '/api/v1/invitations',
+            '/api/v1/discussions',
+            '/api/v1/polls',
+            '/api/v1/stances',
+            '/api/v1/comments',
+            '/api/v1/reactions',
+            '/api/v1/attachments',
+            '/api/v1/contact_messages']
+
+  # heavy throttling - 3 per minute, 10 per hour, 50 per day
+  Rack::Attack.throttle('heavy/minute', :limit => 3, :period => 1.minute) do |req|
+    heavy.any? {|route| req.path.starts_with?(route)} && req.post?
+  end
+
+  Rack::Attack.throttle('heavy/hour', :limit => 10, :period => 1.hour) do |req|
+    heavy.any? {|route| req.path.starts_with?(route)} && req.post?
+  end
+
+  Rack::Attack.throttle('heavy/day', :limit => 50, :period => 1.day) do |req|
+    heavy.any? {|route| req.path.starts_with?(route)} && req.post?
+  end
+
+  # medium 10 per minute, 300 per day
+  Rack::Attack.throttle('medium/minute', :limit => 10, :period => 1.minute) do |req|
+    medium.any? {|route| req.path.starts_with?(route)} && req.post?
+  end
+
+  Rack::Attack.throttle('medium/day', :limit => 300, :period => 1.day) do |req|
+    medium.any? {|route| req.path.starts_with?(route)} && req.post?
   end
 end
