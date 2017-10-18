29e21ce7d54ed43e5e51be2a4f8881bfc63f78f6
Robert Guthrie <rguthrie@gmail.com>
2017-10-18 14:29:58 +1300

    ensure reader_ranges is correctly prepopulated

--- db/migrate/20171013014520_add_read_sequence_id_ranges_to_discussion_readers.rb
+++ db/migrate/20171013014520_add_read_sequence_id_ranges_to_discussion_readers.rb
@@ -1,5 +1,6 @@
 class AddReadSequenceIdRangesToDiscussionReaders < ActiveRecord::Migration
   def change
     add_column :discussion_readers, :read_ranges_string, :string
+    exec("update discussion_readers SET read_ranges_string = ('1,' || last_read_sequence_id)")
   end
 end
