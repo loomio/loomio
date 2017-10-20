8c8d5255eb4d7cac37ce3fa8948f8b1c0c140993
Robert Guthrie <rguthrie@gmail.com>
2017-10-20 15:44:23 +1300

    compile assets on request

--- docker_start.sh
+++ docker_start.sh
@@ -2,7 +2,7 @@
 if [ "$TASK" = "worker" ];
 then
   # build assets
-  bundle exec rake assets:precompile;
+  # bundle exec rake assets:precompile;
   bundle exec rake jobs:work;
 else
   bundle exec puma -C config/puma.rb;
