231a1d4e015f9477c2fe0ef6b576440a086abeb6
Julien <julien@oqp.io>
2017-11-03 04:54:01 +0100

    adds the missing sizes of (optimized) icons and set theme color to the manifest.json (#4379)
    * adds the missing sizes of (optimized) icons and set theme color
    
    * uses AppConfig.theme expanded references for theme icons and color
    
    * optimizes email images, saving space on all mail servers while we‘re at it

--- app/controllers/manifest_controller.rb
+++ app/controllers/manifest_controller.rb
@@ -9,10 +9,30 @@ class ManifestController < ApplicationController
       orientation: 'portrait',
       start_url: '/dashboard',
       background_color: '#ffffff',
-      theme_color: '#ffffff',
+      theme_color: AppConfig.theme[:accent_color],
       icons: [{
-        src:   [root_url.chomp('/'), AppConfig.theme[:icon_src]].join(''),
+        src:   [root_url.chomp('/'), AppConfig.theme[:icon32_src]].join(''),
+        sizes: '32x32',
+        type:  'image/png'
+        }, {
+        src:   [root_url.chomp('/'), AppConfig.theme[:icon48_src]].join(''),
+        sizes: '48x48',
+        type:  'image/png'
+        }, {
+        src:   [root_url.chomp('/'), AppConfig.theme[:icon128_src]].join(''),
+        sizes: '128x128',
+        type:  'image/png'
+        }, {
+        src:   [root_url.chomp('/'), AppConfig.theme[:icon144_src]].join(''),
         sizes: '144x144',
+        type:  'image/png'
+        }, {
+        src:   [root_url.chomp('/'), AppConfig.theme[:icon192_src]].join(''),
+        sizes: '192x192',
+        type:  'image/png'
+        }, {
+        src:   [root_url.chomp('/'), AppConfig.theme[:icon512_src]].join(''),
+        sizes: '512x512',
         type:  'image/png'
       }]
     }
