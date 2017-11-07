d49898a8ef9355dd4676403f95a29fa0a255578e
James Kiesel <james.kiesel@gmail.com>
2017-11-08 12:29:37 +1300

    Skip long migration on deploy

--- db/migrate/20171103062701_update_discussion_readers_count.rb
+++ db/migrate/20171103062701_update_discussion_readers_count.rb
@@ -1,5 +1,5 @@
 class UpdateDiscussionReadersCount < ActiveRecord::Migration
   def change
-    Discussion.find_each(batch_size: 100) { |d| d.update_seen_by_count }
+    # Discussion.find_each(batch_size: 100) { |d| d.update_seen_by_count }
   end
 end
