15217d39bb45fb1244b01755e1736062703fd68f
Robert Guthrie <rguthrie@gmail.com>
2017-11-15 12:21:44 +1300

    tidy

--- angular/core/services/chronological_event_window.coffee
+++ angular/core/services/chronological_event_window.coffee
@@ -22,34 +22,16 @@ angular.module('loomioApp').factory 'ChronologicalEventWindow', (BaseEventWindow
       @max = false
       @loader.loadMore(@min)
 
-    rootsAndOrphans: (events) =>
-      _.filter events, (event) => !event.parentId? || !@inWindow(event.parent())
-
     fewerDiscussionEditedEvents: (events) ->
       _.reject events, (event) =>
         event.kind == "discussion_edited" &&
         (event.position == 0 || (event.previous() || {}).kind == "discussion_edited")
 
-    replaceOrphansWithParents: (events) =>
-      _.map events, (event) =>
-        if event.parentId? && event.parent().kind != "new_discussion" &&!@inWindow(event.parent())
-          event.parent()
-        else
-          event
-
     events: =>
-      # i think we want to memoize this method eventually
       query =
         sequenceId:
           $between: [@min, (@max || Number.MAX_VALUE)]
         discussionId: @discussion.id
 
       events = Records.events.collection.chain().find(query).simplesort('id').data()
-      # _.uniq( @replaceOrphansWithParents( @rootsAndOrphans( @fewerDiscussionEditedEvents( events))))
       _.uniq(@fewerDiscussionEditedEvents( events))
-
-    inWindow: (event) ->
-      event.sequenceId >= @min && ((@max == false) || event.sequenceId <= @max)
-
-    isLastInWindow: (event) ->
-      _.last(@events()) == event
--- app/controllers/dev/discussions_controller.rb
+++ app/controllers/dev/discussions_controller.rb
@@ -11,24 +11,24 @@ class Dev::DiscussionsController < Dev::BaseController
   def test_some_read
     discussion = create_discussion_with_nested_comments
     sign_in discussion.author
-    event = discussion.items.order(id: :asc).first(5).last
-    EventService.mark_as_read(event: event, actor: discussion.author)
+    read_ids = discussion.items.order(sequence_id: :asc).limit(5).pluck(:sequence_id)
+    DiscussionReader.for_model(discussion, discussion.author).viewed!(read_ids)
     redirect_to discussion_url(discussion)
   end
 
   def test_most_read
     discussion = create_discussion_with_nested_comments
     sign_in discussion.author
-    event = discussion.items.order(id: :asc).last(5).first
-    EventService.mark_as_read(event: event, actor: discussion.author)
+    read_ids = discussion.items.order(sequence_id: :desc).limit(5).pluck(:sequence_id)
+    DiscussionReader.for_model(discussion, discussion.author).viewed!(read_ids)
     redirect_to discussion_url(discussion)
   end
 
   def test_all_read
     discussion = create_discussion_with_nested_comments
     sign_in discussion.author
-    event = discussion.items.order(id: :asc).last
-    EventService.mark_as_read(event: event, actor: discussion.author)
+    read_ids = discussion.items.order(sequence_id: :asc).pluck(:sequence_id)
+    DiscussionReader.for_model(discussion, discussion.author).viewed!(read_ids)
     redirect_to discussion_url(discussion)
   end
 
