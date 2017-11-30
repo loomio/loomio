72c3cb537927f1f3c1e97da8bbcae3ea0bcddfd0
James Kiesel <james.kiesel@gmail.com>
2017-12-01 08:09:13 +1300

    Attempt to fix discussion deserialization errors (#4440)
    * Attempt to fix discussion deserialization errors
    
    * dont need to recount read items

--- app/jobs/recalculate_read_items_counts_job.rb
+++
@@ -1,8 +0,0 @@
-class RecalculateReadItemsCountsJob < ActiveJob::Base
-  queue_as :low_priority
-  def perform(discussion)
-    DiscussionReader.where(discussion_id: discussion.id).find_each do |dr|
-      dr.update_attribute(:read_items_count, dr.calculate_read_items_count)
-    end
-  end
-end
