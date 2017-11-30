42006efdcab7367454a98f65c270aca28b3d4931
Robert Guthrie <rguthrie@gmail.com>
2017-12-01 12:18:37 +1300

    dont serialize out created event unless full discussion serializer

--- app/serializers/discussion_serializer.rb
+++ app/serializers/discussion_serializer.rb
@@ -37,7 +37,6 @@ class DiscussionSerializer < ActiveModel::Serializer
 
   has_one :author, serializer: UserSerializer, root: :users
   has_one :group, serializer: GroupSerializer, root: :groups
-  has_one :created_event, serializer: Events::SimpleSerializer, root: :events
   has_many :active_polls, serializer: Simple::PollSerializer, root: :polls
 
   def active_polls
