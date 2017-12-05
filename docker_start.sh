3ed65f09061636567770fc1de53cc0d463482eac
James Kiesel <james.kiesel@gmail.com>
2017-12-05 18:04:44 +1300

    Docker and heroku both use same build command

--- docker_start.sh
+++ docker_start.sh
@@ -3,6 +3,6 @@ if [ "$TASK" = "worker" ];
 then
   bundle exec rake jobs:work;
 else
-  bundle exec rake deploy:build_client
+  bundle exec rake plugins:fetch[plugins.docker] plugins:install deploy:build
   bundle exec puma -C config/puma.rb;
 fi
