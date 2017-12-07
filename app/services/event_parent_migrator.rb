519226589ce089369df3211312b14b3050c46bfa
Robert Guthrie <rguthrie@gmail.com>
2017-12-07 14:20:35 +1300

    handle ambiguous eventable types

--- app/services/event_parent_migrator.rb
+++ app/services/event_parent_migrator.rb
@@ -39,13 +39,15 @@ class EventParentMigrator
 
   def self.assign_edit_parents(group)
     group.discussions.find_each do |discussion|
-      discussion.items.where(kind: ["poll_expired"])
+      discussion.items.where(kind: ["discussion_edited", "poll_edited", "poll_expired"])
                       .where(parent_id: nil).find_each do |event|
-        event.update(parent: event.eventable.created_event)
-      end
-      discussion.items.where(kind: ["discussion_edited", "poll_edited"])
-                      .where(parent_id: nil).find_each do |event|
-        event.update(parent: event.eventable&.created_event || event.eventable.item&.created_event)
+        if event.eventable
+          if event.eventable&.created_event
+            event.update(parent: event.eventable.created_event)
+          elsif event.eventable&.item && event.eventable.item&.created_event
+            event.update(parent: event.eventable.item.created_event)
+          end
+        end
       end
     end
   end
