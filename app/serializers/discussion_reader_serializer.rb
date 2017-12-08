9feb11e24c35b1bfe7f1b379a86980722f82732a
Robert Guthrie <rguthrie@gmail.com>
2017-12-08 13:36:15 +1300

    send created_event with discussion reader serializer

--- app/serializers/discussion_reader_serializer.rb
+++ app/serializers/discussion_reader_serializer.rb
@@ -3,20 +3,32 @@ class DiscussionReaderSerializer < ActiveModel::Serializer
 
   attributes :id,
              :discussion_reader_id,
+             :ranges_string,
              :read_ranges_string,
              :last_read_at,
-             :seen_by_count
+             :seen_by_count,
+             :volume,
+             :dismissed_at
 
+  has_one :created_event, serializer: Events::BaseSerializer, root: :events
+
+  def created_event
+    object.discussion.created_event
+  end
 
   def id
     object.discussion_id
   end
 
-   def discussion_reader_id
-     object.id
-   end
+  def ranges_string
+    object.discussion.ranges_string
+  end
 
-   def seen_by_count
-     object.discussion.seen_by_count
-   end
+  def discussion_reader_id
+    object.id
+  end
+
+  def seen_by_count
+    object.discussion.seen_by_count
+  end
 end
