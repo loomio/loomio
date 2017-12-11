3738294e02bc14e54bc16bd3f5ec5785f83bb28f
James Kiesel <james.kiesel@gmail.com>
2017-12-11 17:25:34 +1300

    Update gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,8 +4,6 @@ Loomio::Application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
-  config.lograge.enabled = ENV['ENABLE_LOGRAGE']
-
   # Log error messages when you accidentally call methods on nil.
   config.whiny_nils = true
 
