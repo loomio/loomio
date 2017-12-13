f848001f4ba7f8e113b26f629a0510c2712e1dd1
James Kiesel <james.kiesel@gmail.com>
2017-12-14 11:10:33 +1300

    Fix ability

---
+++ app/models/ability/announcement.rb
@@ -0,0 +1,10 @@
+module Ability::Announcement
+  def initialize(user)
+    super(user)
+
+    can :create, Announcement do |announcement|
+      user_is_author_of?(announcement.announceable) ||
+      user_is_admin_of?(announcement.announceable.group_id)
+    end
+  end
+end
