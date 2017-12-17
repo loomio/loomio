2b18c5bc33dde5b8e976304ae31b2ec56f89641c
James Kiesel <james.kiesel@gmail.com>
2017-12-18 11:48:19 +1300

    Rename rather than drop/add closed_at

--- db/migrate/20171213050734_open_closed_discussion_count.rb
+++ db/migrate/20171213050734_open_closed_discussion_count.rb
@@ -2,9 +2,8 @@ class OpenClosedDiscussionCount < ActiveRecord::Migration
   def change
     add_column :groups, :open_discussions_count, :integer, default: 0, null: false
     add_column :groups, :closed_discussions_count, :integer, default: 0, null: false
-    add_column :discussions, :closed_at, :datetime
+    rename_column :discussions, :archived_at, :closed_at
     remove_column :discussions, :is_deleted, :boolean
-    remove_column :discussions, :archived_at, :datetime
     remove_column :discussions, :closed, :boolean
   end
 end
