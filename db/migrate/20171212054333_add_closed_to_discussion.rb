ea5500a1ff018df3fd8f1276987e50872388a239
James Kiesel <james.kiesel@gmail.com>
2017-12-18 13:14:36 +1300

    Remove unused addition of 'closed' to discussions

--- db/migrate/20171212054333_add_closed_to_discussion.rb
+++
@@ -1,5 +0,0 @@
-class AddClosedToDiscussion < ActiveRecord::Migration
-  def change
-    add_column :discussions, :closed, :boolean, default: false, null: false
-  end
-end
--- db/migrate/20171213050734_open_closed_discussion_count.rb
+++ db/migrate/20171213050734_open_closed_discussion_count.rb
@@ -4,6 +4,5 @@ class OpenClosedDiscussionCount < ActiveRecord::Migration
     add_column :groups, :closed_discussions_count, :integer, default: 0, null: false
     rename_column :discussions, :archived_at, :closed_at
     remove_column :discussions, :is_deleted, :boolean
-    remove_column :discussions, :closed, :boolean
   end
 end
