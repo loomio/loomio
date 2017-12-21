db5b4e16845f46aa57bd3f3141009628dd9da108
Robert Guthrie <rguthrie@gmail.com>
2017-12-22 12:16:46 +1300

    fix automatic migration of versions -> eventables

--- app/services/migrate_events_service.rb
+++ app/services/migrate_events_service.rb
@@ -4,9 +4,9 @@ module MigrateEventsService
                 eventable_type: "PaperTrail::Version").find_each do |event|
       version = event.eventable
       event.update_columns(eventable_type: version.item_type,
-                              eventable_id: version.item_id,
-                              custom_fields: {version_id: version.id,
-                                              changed_keys: Hash(version.object_changes).keys})
+                             eventable_id: version.item_id,
+                             custom_fields: {version_id: version.id,
+                                             changed_keys: Hash(version.object_changes).keys})
 
 
     end
