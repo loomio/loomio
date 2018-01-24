ecf4c26a0a34a908fd54fa0771b33d4c6c12f0be
Robert Guthrie <rguthrie@gmail.com>
2018-01-24 16:51:06 +1300

    add tld length to cookie domain setting..

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,2 @@
 # Be sure to restart your server when you modify this file.
-if ENV[:NO_COOKIES_DOMAIN_ALL]
-  Loomio::Application.config.session_store :cookie_store, key: '_loomio'
-else
-  Loomio::Application.config.session_store :cookie_store, key: '_loomio', domain: :all
-end
+Loomio::Application.config.session_store :cookie_store, key: '_loomio', domain: :all, tld_length: (ENV['TLD_LENGTH'] || 1).to_i 
