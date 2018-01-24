82a4c9608b2a56b6524f1ef31d16599ba3c6e8bc
Robert Guthrie <rguthrie@gmail.com>
2018-01-24 16:44:11 +1300

    add env switch for cookies domain setting

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,6 @@
 # Be sure to restart your server when you modify this file.
-Loomio::Application.config.session_store :cookie_store, key: '_loomio', domain: :all
+if ENV[:NO_COOKIES_DOMAIN_ALL]
+  Loomio::Application.config.session_store :cookie_store, key: '_loomio'
+else
+  Loomio::Application.config.session_store :cookie_store, key: '_loomio', domain: :all
+end
