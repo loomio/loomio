191849554c693f03f6bb0585ec5b55de59f82e54
Robert Guthrie <rguthrie@gmail.com>
2018-02-08 18:58:01 +1300

    ensure we get user id name email and visit id

--- app/helpers/sentry_raven_helper.rb
+++ app/helpers/sentry_raven_helper.rb
@@ -1,6 +1,10 @@
 module SentryRavenHelper
   def set_raven_context
-    Raven.user_context(id: current_user.id) # or anything else in session
-    Raven.extra_context(params: params.to_unsafe_h, url: request.url)
+    Raven.user_context(id: current_user.id,
+                       email: current_user.email,
+                       name: current_user.name)
+    Raven.extra_context(params: params.to_unsafe_h,
+                        visit: current_visit.id,
+                        url: request.url)
   end
 end
