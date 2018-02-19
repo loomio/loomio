4c01840e00e02662bbefef8ff95c76dcd8d34116
Robert Guthrie <rguthrie@gmail.com>
2018-02-19 14:26:08 +1300

    disable bootsnap

--- config/boot.rb
+++ config/boot.rb
@@ -4,4 +4,4 @@ require 'rubygems'
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
-require 'bootsnap/setup'
+#require 'bootsnap/setup'
