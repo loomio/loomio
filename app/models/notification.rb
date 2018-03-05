42e90140eb0a074fd9ec3567b8407bfc141dfda7
James Kiesel <james.kiesel@gmail.com>
2018-03-06 01:02:57 +1300

    Reaction notification (#4565)
    * Display correct model for reaction notifications
    
    * Add migration for previous notifications
    
    * Run migration

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -10,6 +10,7 @@ class Notification < ApplicationRecord
 
   delegate :kind, to: :event, allow_nil: true
   delegate :eventable, to: :event, allow_nil: true
+  delegate :locale, to: :user
   delegate :message_channel, to: :user
 
   scope :user_mentions, -> { joins(:event).where("events.kind": :user_mentioned) }
---
+++ db/migrate/20180305031829_add_notification_model.rb
@@ -0,0 +1,10 @@
+class AddNotificationModel < ActiveRecord::Migration[5.1]
+  def change
+    Notification.joins(:event).where("events.kind": :reaction_created).each do |n|
+      I18n.with_locale(n.locale) do
+        e = Events::ReactionCreated.find(n.event_id)
+        n.update(translation_values: e.send(:notification_translation_values))
+      end
+    end
+  end
+end
