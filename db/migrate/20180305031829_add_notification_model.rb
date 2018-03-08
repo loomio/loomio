fbadd60acffd8e78055de9c8be1026fd9d248c82
Robert Guthrie <rguthrie@gmail.com>
2018-03-08 14:51:20 +1300

    skip recalculating notifications where eventable is long gone

--- db/migrate/20180305031829_add_notification_model.rb
+++ db/migrate/20180305031829_add_notification_model.rb
@@ -1,6 +1,7 @@
 class AddNotificationModel < ActiveRecord::Migration[5.1]
   def change
     Notification.joins(:event).where("events.kind": :reaction_created).each do |n|
+      next unless n.eventable
       I18n.with_locale(n.locale) do
         e = Events::ReactionCreated.find(n.event_id)
         n.update(translation_values: e.send(:notification_translation_values))
