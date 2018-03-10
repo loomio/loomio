0abb4e333c08ecc8b704c418a99f9c8bc1f8fc96
James Kiesel <james.kiesel@gmail.com>
2018-03-10 17:21:03 +1300

    Ensure complete group on group page (#4579)

--- app/serializers/full/group_serializer.rb
+++ app/serializers/full/group_serializer.rb
@@ -1,5 +1,10 @@
 class Full::GroupSerializer < GroupSerializer
+  attribute :complete
   has_one :slack_group_identity, serializer: GroupIdentitySerializer, root: :group_identities
+
+  def complete
+    true
+  end
 
   def slack_group_identity
     object.identity_for(:slack)
