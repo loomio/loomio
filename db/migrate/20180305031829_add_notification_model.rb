ea603ecd75e76e65b4bf838416f9c8b863b6e399
Robert Guthrie <rguthrie@gmail.com>
2018-03-12 13:19:26 +1300

    scope down notifications migration so it runs on prod

--- db/migrate/20180305031829_add_notification_model.rb
+++ db/migrate/20180305031829_add_notification_model.rb
@@ -1,6 +1,6 @@
 class AddNotificationModel < ActiveRecord::Migration[5.1]
   def change
-    Notification.joins(:event).where("events.kind": :reaction_created).each do |n|
+    Notification.joins(:event).where("events.kind": :reaction_created).where("events.created_at > ?", 30.days.ago).each do |n|
       next unless n.eventable
       I18n.with_locale(n.locale) do
         e = Events::ReactionCreated.find(n.event_id)
