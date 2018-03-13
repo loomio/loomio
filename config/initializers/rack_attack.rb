965147562a6cfc1ab8d855e9b256f891b2f6d209
James Kiesel <james.kiesel@gmail.com>
2018-03-14 08:46:01 +1300

    Rack attack initializer cleanup

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,4 @@
 class Rack::Attack
-  heavy =  ['/api/v1/groups', '/api/v1/invitations/bulk_create']
-
   # considerations
   # multiple users could be using same ip address (eg coworking space)
   # does not distinguish between valid and invalid requests (eg: form validation)
@@ -13,34 +11,54 @@ class Rack::Attack
   # when attacks happen we see a few groups per minute, usually with the same name
   # each trying to invite max_invitations with bulk create.
 
-  {10 => 1.hour,
-   20 => 1.day}.each_pair do |limit, period|
-    Rack::Attack.throttle("groups#create", :limit => limit, :period => period) do |req|
-      req.ip if heavy.any? {|route| req.path.starts_with?(route)} && req.post?
-    end
-  end unless Rails.env.test?
-
-  medium = ['login_tokens',
-            'invitations',
-            'discussions',
-            'polls',
-            'stances',
-            'comments',
-            'reactions',
-            'documents',
-            'registrations',
-            'contact_messages']
-
   # throttles all posts to the above endponts
   # so we're looking at a record creation attack.
   # the objective of these rules is not to guess what normal behaviour looks like
   # and pitch abouve that.. but to identify what abusive behaviour certainly is,
   # and ensure it cannot get really really bad.
-  {100     => 5.minutes,
-   1000    => 1.hour,
-   10000   => 1.day}.each_pair do |limit, period|
-    Rack::Attack.throttle("record#create", :limit => limit, :period => period) do |req|
-      req.ip if medium.any? {|route| req.path.starts_with?("/api/v1/#{route}")} && req.post?
+
+  RATE_LIMITS = [
+    {
+      name: :heavy,
+      endpoints: [
+        'groups',
+        'invitations/bulk_create'
+      ],
+      limits: {
+        10 => 1.hour,
+        20 => 1.day
+      }
+    }, {
+      name: :medium,
+      endpoints: [
+        'login_tokens',
+        'invitations',
+        'discussions',
+        'polls',
+        'stances',
+        'comments',
+        'reactions',
+        'documents',
+        'registrations',
+        'contact_messages'
+      ],
+      limits:   {
+        100   => 5.minutes,
+        1000  => 1.hour,
+        10000 => 1.day
+      }
+    }
+  ].freeze
+
+  RATE_LIMITS.each do |limit_set|
+    limit_set[:limits].each do |limit, period|
+      throttle(limit_set[:name], limit: limit, period: period) do |req|
+        req.ip if should_limit?(req, limit_set[:endpoints])
+      end
     end
-  end unless Rails.env.test?
+  end if ENV['USE_RACK_ATTACK']
+
+  def self.should_limit?(req, endpoints)
+    req.post? && endpoints.any? { |r| req.path.starts_with?("/api/v1/#{r}") }
+  end
 end
