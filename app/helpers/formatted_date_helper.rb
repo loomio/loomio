4072d308c33f71070f338d28d0d4d8f07087237c
James Kiesel <james.kiesel@gmail.com>
2018-04-03 15:08:53 +1200

    Apply formatting to meeting chart thead

--- app/helpers/formatted_date_helper.rb
+++ app/helpers/formatted_date_helper.rb
@@ -1,5 +1,27 @@
 module FormattedDateHelper
+  def display_time(value, zone)
+    apply_format parse_date_string(value, zone), '%l:%M %P'
+  end
+
+  def display_day(value, zone)
+    apply_format parse_date_string(value, zone), '%a'
+  end
+
+  def display_date(value, zone)
+    apply_format parse_date_string(value, zone), '%-d %b'
+  end
+
   private
+
+  def parse_date_string(value, zone)
+    date_time(value).in_time_zone(zone)
+  rescue ArgumentError
+    value.to_date
+  end
+
+  def apply_format(date, format)
+    date.strftime(format).strip
+  end
 
   def formatted_datetime(value, zone)
     date_time(value).in_time_zone(zone).strftime(date_time_format(value)).strip
