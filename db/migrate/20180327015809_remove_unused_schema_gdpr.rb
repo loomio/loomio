93525bdf89cd6832b96d2b410b3c0d6a1c8cb53c
Robert Guthrie <rguthrie@gmail.com>
2018-04-12 08:51:29 +1200

    Update 20180327015809_remove_unused_schema_gdpr.rb

--- db/migrate/20180327015809_remove_unused_schema_gdpr.rb
+++ db/migrate/20180327015809_remove_unused_schema_gdpr.rb
@@ -1,7 +1,6 @@
 class RemoveUnusedSchemaGdpr < ActiveRecord::Migration[5.1]
   def change
     remove_column :comments, :liker_ids_and_names
-    drop_table    :attachments
     drop_table    :contact_messages
     drop_table    :group_hierarchies
     drop_table    :poll_references
