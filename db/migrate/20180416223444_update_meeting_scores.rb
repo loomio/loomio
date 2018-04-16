102bb68d07d4c04db5fe01c387c6285b37adf1e1
James Kiesel <james.kiesel@gmail.com>
2018-04-17 11:02:37 +1200

    Add update stance data to meeting score migration

--- db/migrate/20180416223444_update_meeting_scores.rb
+++ db/migrate/20180416223444_update_meeting_scores.rb
@@ -4,5 +4,6 @@ class UpdateMeetingScores < ActiveRecord::Migration[5.1]
       .where("polls.poll_type": :meeting)
       .where(score: 1)
       .update_all(score: 2)
+    Poll.where(poll_type: :meeting).map(&:update_stance_data)
   end
 end
