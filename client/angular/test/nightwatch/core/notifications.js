22a52ad3f6179a13fc20a94ec808705fa3e110eb
Robert Guthrie <rguthrie@gmail.com>
2018-04-26 14:57:18 +1200

    remove unnecessary test

--- client/angular/test/nightwatch/core/notifications.js
+++ client/angular/test/nightwatch/core/notifications.js
@@ -24,18 +24,5 @@ module.exports = {
     page.expectText('.notifications__activity', notificationTexts.length, 100000)
     page.click('.notifications__button')
     notificationTexts.map((text) => { page.expectText('.notifications__dropdown', text) })
-  },
-
-  'notifies inviter when invitation is accepted': (test) => {
-    page = pageHelper(test)
-
-    page.loadPath('setup_group')
-    page.click('.members-card__invite-members-btn')
-    page.fillIn('.invitation-form__email-addresses', 'max@example.com')
-    page.click('.invitation-form__submit')
-    page.pause()
-    page.loadPath('accept_last_invitation')
-    page.click('.notifications__button', 6000)
-    page.expectText('.notifications__dropdown', 'Max Von Sydow accepted your invitation to join Dirty Dancing Shoes')
   }
 }
