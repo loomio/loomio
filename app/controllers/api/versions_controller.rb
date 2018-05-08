70fdee3bf8b13c0f8eb5bd058ac9fc4eaf871876
Edward Dalley <endalley11@gmail.com>
2018-05-08 14:24:33 +1200

    Better versions (#4638)
    * Add diff match patch for more better diffing
    
    * revision history modal coffee only
    
    * Updated Revision history modal with mocked data.
    
    * versions displayed for comments
    
    * Advanced Revision history to accomodate discussion and comment
    
    * Styling tweaks, move components to their own folders
    
    * Polls fixup for release readiness
    
    * Add e2es for revision history
    
    * Fix failing specs
    
    * Make tests focusable
    
    * fix test selector
    
    * adjust time display & translations

--- app/controllers/api/versions_controller.rb
+++ app/controllers/api/versions_controller.rb
@@ -1,4 +1,8 @@
 class API::VersionsController < API::RestfulController
+  def show
+    self.resource = service.reduce(model:model, index:params[:index].to_i)
+    respond_with_resource
+  end
 
   private
 
@@ -6,13 +10,11 @@ class API::VersionsController < API::RestfulController
     PaperTrail::Version
   end
 
-  def accessible_records
-    records = load_and_authorize(params[:model]).versions.order(created_at: :desc)
-    records = records.where(event: 'update') if params[:model] == 'discussion'
-    records
+  def model
+    load_and_authorize(:group, optional:true)||
+    load_and_authorize(:discussion, optional:true)||
+    load_and_authorize(:comment, optional:true) ||
+    load_and_authorize(:poll, optional:false)
   end
 
-  def default_page_size
-    100
-  end
 end
