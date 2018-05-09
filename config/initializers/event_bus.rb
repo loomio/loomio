0795a704db24948c4268e63fd83a1b0d9982c075
Robert Guthrie <rguthrie@gmail.com>
2018-05-09 09:47:45 +0700

    stupid persistence check. what will we learn next?

--- config/initializers/event_bus.rb
+++ config/initializers/event_bus.rb
@@ -25,7 +25,7 @@ EventBus.configure do |config|
   config.listen('group_create',
                 'discussion_create',
                 'comment_create',
-                'poll_create') { |model, actor| model.perform_draft_purge!(actor) }
+                'poll_create') { |model, actor| model.perform_draft_purge!(actor) if model.persisted? }
 
   # Add creator to group on group creation
   config.listen('group_create') do |group, actor|
