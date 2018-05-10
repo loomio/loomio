529bda56a4b638c47837f6360a315ba3941f397b
Robert Guthrie <rguthrie@gmail.com>
2018-05-10 13:24:11 +0700

    delete very old membership requests which are now invalid (#4666)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180509014045) do
+ActiveRecord::Schema.define(version: 20180510012449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
