59c5f7363aea0bca0228ed00c4bcde2ed84c514a
James Kiesel <james.kiesel@gmail.com>
2018-05-18 22:06:54 +1200

    Fix search result stylign

--- app/models/search_vector.rb
+++ app/models/search_vector.rb
@@ -40,7 +40,7 @@ class SearchVector < ApplicationRecord
     query = connection.quote(query)
 
     joins(discussion: :group)
-   .select(:discussion_id, :search_vector, 'groups.full_name as result_group_name')
+   .select(:discussion_id, :search_vector, 'groups.name as result_group_name')
    .select("ts_rank_cd('{#{WEIGHT_VALUES.join(',')}}', search_vector, plainto_tsquery(#{query})) * #{recency_multiplier} as rank")
    .where("search_vector @@ plainto_tsquery(#{query})")
    .order('rank DESC')
