e35d64bcf8ddcfe305de0725b5d5766e722b78b7
Robert Guthrie <rguthrie@gmail.com>
2018-05-20 12:57:40 +0800

    retry create if event not unique

--- app/models/events/discussion_closed.rb
+++ app/models/events/discussion_closed.rb
@@ -2,11 +2,10 @@ class Events::DiscussionClosed < Event
   include Events::LiveUpdate
 
   def self.publish!(discussion, actor)
-    create(kind: "discussion_closed",
-           eventable: discussion,
-           user: actor,
-           parent: discussion.created_event,
-           discussion: discussion,
-           created_at: discussion.closed_at).tap { |e| EventBus.broadcast('discussion_closed', e) }
+    super discussion
+          user: actor,
+          parent: discussion.created_event,
+          discussion: discussion,
+          created_at: discussion.closed_at
   end
 end
