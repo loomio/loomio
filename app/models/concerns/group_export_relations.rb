c2fd158344051a0f62373615eec1031a4603073f
Gregory Kan <kan.gregory@gmail.com>
2018-10-19 15:40:04 +1300

    feat(polls): only surface non-anonymous polls and related data

--- app/models/concerns/group_export_relations.rb
+++ app/models/concerns/group_export_relations.rb
@@ -4,52 +4,55 @@ module GroupExportRelations
   included do
     # polls
     has_many :discussion_polls, through: :discussions
-    has_many :poll_options, through: :polls
-    has_many :poll_unsubscriptions, through: :polls
-    has_many :poll_did_not_votes, through: :polls
-    has_many :outcomes, through: :polls
-    has_many :stances, through: :polls
-    has_many :stance_choices, through: :stances
+    has_many :exportable_polls, -> { where anonymous: false }, class_name: 'Poll', foreign_key: :group_id
+    
+    has_many :exportable_poll_options,          through: :exportable_polls, source: :poll_options
+    has_many :exportable_poll_unsubscriptions,  through: :exportable_polls, source: :poll_unsubscriptions
+    has_many :exportable_poll_did_not_votes,    through: :exportable_polls, source: :poll_did_not_votes
+    has_many :exportable_outcomes,              through: :exportable_polls, source: :outcomes
+    has_many :exportable_stances,               through: :exportable_polls, source: :stances
+    has_many :exportable_stance_choices,        through: :exportable_stances, source: :stance_choices
+
 
     # documents
     has_many :discussion_documents,        through: :discussions,        source: :documents
-    has_many :poll_documents,              through: :polls,              source: :documents
+    has_many :exportable_poll_documents,   through: :exportable_polls,   source: :documents
     has_many :comment_documents,           through: :comments,           source: :documents
     has_many :public_discussion_documents, through: :public_discussions, source: :documents
     has_many :public_poll_documents,       through: :public_polls,       source: :documents
     has_many :public_comment_documents,    through: :public_comments,    source: :documents
 
     # reactions
-    has_many :discussion_reactions,        -> { joins(:user) }, through: :discussions, source: :reactions
-    has_many :poll_reactions,              -> { joins(:user) }, through: :polls,       source: :reactions
-    has_many :stance_reactions,            -> { joins(:user) }, through: :stances,     source: :reactions
-    has_many :comment_reactions,           -> { joins(:user) }, through: :comments,    source: :reactions
-    has_many :outcome_reactions,           -> { joins(:user) }, through: :outcomes,    source: :reactions
+    has_many :discussion_reactions,         -> { joins(:user) }, through: :discussions,         source: :reactions
+    has_many :exportable_poll_reactions,    -> { joins(:user) }, through: :exportable_polls,    source: :reactions
+    has_many :exportable_stance_reactions,  -> { joins(:user) }, through: :exportable_stances,  source: :reactions
+    has_many :comment_reactions,            -> { joins(:user) }, through: :comments,            source: :reactions
+    has_many :exportable_outcome_reactions, -> { joins(:user) }, through: :exportable_outcomes, source: :reactions
 
     # readers
     has_many :discussion_readers,  through: :discussions
 
     # guest groups
-    has_many :discussion_guest_groups,     through: :discussions,        source: :guest_group
-    has_many :poll_guest_groups,           through: :polls,              source: :guest_group
+    has_many :discussion_guest_groups,      through: :discussions,        source: :guest_group
+    has_many :exportable_poll_guest_groups, through: :exportable_polls,   source: :guest_group
 
     # users
-    has_many :discussion_authors,      through: :discussions, source: :author
+    has_many :discussion_authors,         through: :discussions,                    source: :author
     # has_many :discussion_reader_users, through: :discussion_readers, source: :user
-    has_many :comment_authors,         through: :comments,           source: :user
-    has_many :poll_authors,            through: :polls,              source: :author
-    has_many :outcome_authors,         through: :outcomes,           source: :author
-    has_many :stance_authors,          through: :stances,            source: :participant
-    has_many :reader_users,            through: :discussion_readers, source: :user
-    has_many :non_voters,              through: :poll_did_not_votes, source: :user
+    has_many :comment_authors,            through: :comments,                       source: :user
+    has_many :exportable_poll_authors,    through: :exportable_polls,               source: :author
+    has_many :exportable_outcome_authors, through: :exportable_outcomes,            source: :author
+    has_many :exportable_stance_authors,  through: :exportable_stances,             source: :participant
+    has_many :reader_users,               through: :discussion_readers,             source: :user
+    has_many :non_voters,                 through: :exportable_poll_did_not_votes,  source: :user
 
     # events
-    has_many :membership_events,       through: :memberships, source: :events
-    has_many :discussion_events,       through: :discussions, source: :events
-    has_many :comment_events,          through: :comments,    source: :events
-    has_many :poll_events,             through: :polls,       source: :events
-    has_many :outcome_events,          through: :outcomes,    source: :events
-    has_many :stance_events,           through: :stances,     source: :events
+    has_many :membership_events,          through: :memberships,          source: :events
+    has_many :discussion_events,          through: :discussions,          source: :events
+    has_many :comment_events,             through: :comments,             source: :events
+    has_many :exportable_poll_events,     through: :exportable_polls,     source: :events
+    has_many :exportable_outcome_events,  through: :exportable_outcomes,  source: :events
+    has_many :exportable_stance_events,   through: :exportable_stances,   source: :events
   end
 
   def all_groups
@@ -57,7 +60,7 @@ module GroupExportRelations
       Group.where(id: self.id),
       self.subgroups,
       self.discussion_guest_groups,
-      self.poll_guest_groups
+      self.exportable_poll_guest_groups
     ])
   end
 
@@ -66,9 +69,9 @@ module GroupExportRelations
       self.members,
       self.discussion_authors,
       self.comment_authors,
-      self.poll_authors,
-      self.outcome_authors,
-      self.stance_authors,
+      self.exportable_poll_authors,
+      self.exportable_outcome_authors,
+      self.exportable_stance_authors,
       self.reaction_users,
       self.reader_users,
       self.non_voters
@@ -80,9 +83,9 @@ module GroupExportRelations
       self.membership_events,
       self.discussion_events,
       self.comment_events,
-      self.poll_events,
-      self.outcome_events,
-      self.stance_events
+      self.exportable_poll_events,
+      self.exportable_outcome_events,
+      self.exportable_stance_events
     ])
   end
 
@@ -94,7 +97,7 @@ module GroupExportRelations
     Queries::UnionQuery.for(:documents, [
       self.documents,
       self.discussion_documents,
-      self.poll_documents,
+      self.exportable_poll_documents,
       self.comment_documents
     ])
   end
@@ -102,10 +105,10 @@ module GroupExportRelations
   def all_reactions
     Queries::UnionQuery.for(:reactions, [
       self.discussion_reactions,
-      self.poll_reactions,
-      self.stance_reactions,
+      self.exportable_poll_reactions,
+      self.exportable_stance_reactions,
       self.comment_reactions,
-      self.outcome_reactions
+      self.exportable_outcome_reactions
     ])
   end
 
