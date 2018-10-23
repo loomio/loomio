d028687a7887bb540932add16bffb694ed8f1d6d
Gregory Kan <kan.gregory@gmail.com>
2018-10-24 11:18:16 +1300

    fix(document): new, open, close before save

--- app/jobs/group_export_job.rb
+++ app/jobs/group_export_job.rb
@@ -5,7 +5,11 @@ class GroupExportJob < ActiveJob::Base
     groups = actor.groups.where(id: group.all_groups)
     filename = GroupExportService.export_filename_for(group)
     GroupExportService.export(groups, filename)
-    document = Document.create(author: actor, file: File.open(filename, 'r'), title: filename)
+    document = Document.new(author: actor, title: filename)
+    file = File.open(filename)
+    document.file = file
+    file.close
+    document.save!
     UserMailer.group_export_ready(actor, group, document).deliver
     document.delay(:run_at => 1.week.from_now).destroy!
   end
