bc77888f7f4a2c40eba15eaae7dd99aa48730011
Robert Guthrie <rob@guthrie.nz>
2018-11-09 14:55:37 +1300

    make group handle case insensitive

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180724135748) do
+ActiveRecord::Schema.define(version: 20181109014324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
