3684d4ca30f7e7aefcc2340925b45b684be08601
Gregory Kan <kan.gregory@gmail.com>
2018-11-15 15:32:32 +1300

    chore(robfactor): robfactored queries and added a couple of new ones for organisation-wide counts

--- app/extras/queries/admin_group_page.rb
+++ app/extras/queries/admin_group_page.rb
@@ -1,32 +1,50 @@
 class Queries::AdminGroupPage
-  def self.visits(group_id)
-    query = "select date_trunc('day', created_at) date, count(id) from organisation_visits where organisation_id = #{group_id} group by date order by date;"
-    records = self.execute(query)
-    self.massage(records)
+  def self.visits_per_day_sql(group)
+    "select date_trunc('day', created_at) date, count(id) from organisation_visits where organisation_id = #{group.id} group by date order by date"
   end
 
-  def self.members(group_id)
-    query = "select date_trunc('day', created_at) date, count(distinct user_id) from memberships where group_id = #{group_id} group by date order by date;"
-    records = self.execute(query)
-    self.massage(records)
+  def self.members_per_day_sql(group)
+    "select date_trunc('day', created_at) date, count(distinct user_id) from memberships where group_id = #{group.id} group by date order by date"
   end
 
-  def self.threads(group_id)
-    query = "select date_trunc('day', created_at) date, count(id) from discussions where group_id = #{group_id} group by date order by date;"
-    records = self.execute(query)
-    self.massage(records)
+  def self.threads_per_day_sql(group)
+    "select date_trunc('day', created_at) date, count(id) from discussions where group_id IN (#{group.id_and_subgroup_ids.join(',')}) group by date order by date"
   end
 
-  def self.execute(query_string)
-    ActiveRecord::Base.connection.execute(query_string).to_a
+  def self.execute(sql)
+    ActiveRecord::Base.connection.execute(sql)
+  end
+
+  def self.run_per_day(sql)
+    massage execute(sql)
   end
 
   def self.massage(records)
-    records
-      .map { |record| { date: record["date"], count: record["count"] }  }
+    records.to_a.map { |record| { date: record["date"], count: record["count"] }  }
   end
 
-  def self.fetch_data(group_id)
-    { visits: self.visits(group_id), members: self.members(group_id), threads: self.threads(group_id) }.to_json
+  def self.fetch_data(group)
+    {
+      visits: run_per_day(visits_per_day_sql(group)),
+      members: run_per_day(members_per_day_sql(group)),
+      threads: run_per_day(threads_per_day_sql(group)),
+    }
   end
+
+  def self.visits_count(group)
+    OrganisationVisit.where(organisation_id: group.id).count
+  end
+
+  def self.thread_items_count(group)
+    Discussion.where(group_id: group.id_and_subgroup_ids).sum(:items_count)
+  end
+
+  def self.discussions_count(group)
+    Discussion.where(group_id: group.id_and_subgroup_ids).count
+  end
+
+  def self.polls_count(group)
+    Poll.where(group_id: group.id_and_subgroup_ids).count
+  end
+
 end
