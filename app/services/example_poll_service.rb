2cf07dfd1169beda10002e2494d46dd8e7a9f801
Gregory Kan <kan.gregory@gmail.com>
2018-11-28 16:26:32 +1300

    chore(refactor): missing @

--- app/services/example_poll_service.rb
+++ app/services/example_poll_service.rb
@@ -1,6 +1,6 @@
 module ExamplePollService
   def self.cleanup
     Poll.where(example: true).where('created_at < ?', 1.day.ago).destroy_all
-    User.where('email ilike ?', '%example.com').where('created_at < ?', 1.day.ago).destroy_all
+    User.where('email ilike ?', '%@example.com').where('created_at < ?', 1.day.ago).destroy_all
   end
 end
