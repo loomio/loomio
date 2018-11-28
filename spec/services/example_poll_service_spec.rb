31aa737e33c22f22f50592506e896a40a8deb03d
Gregory Kan <kan.gregory@gmail.com>
2018-11-28 16:48:06 +1300

    chore(rspec): added spec to test example poll service

---
+++ spec/services/example_poll_service_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe ExamplePollService do
+  let!(:poll) { FactoryBot.create(:poll) }
+  let!(:example_poll) { FactoryBot.create(:poll, example: true, created_at: 2.day.ago) }
+  let!(:user) { FactoryBot.create(:user) }
+  let!(:example_user) { FactoryBot.create(:user, email: "something@example.com", created_at: 2.day.ago) }
+
+  it('deletes example poll but does not delete poll') do
+    ExamplePollService.cleanup
+    expect { example_poll.reload }.to raise_error ActiveRecord::RecordNotFound
+    expect(poll.reload.persisted?).to eq true
+  end
+
+  it('deletes example user but does not delete user') do
+    ExamplePollService.cleanup
+    expect { example_user.reload }.to raise_error ActiveRecord::RecordNotFound
+    expect(user.reload.persisted?).to eq true
+  end
+end
