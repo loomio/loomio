a81ce312337316cfcdf4ab55105a481bfc2915e2
Robert Guthrie <rob@guthrie.nz>
2018-12-03 17:25:20 +1300

    tested

--- lib/version.rb
+++ lib/version.rb
@@ -2,7 +2,7 @@ module Loomio
   module Version
     def self.current
       [`git show -s --format=%ci`.split(' ').first,
-      `git rev-parse HEAD`.strip.first(7)].join("_")
+      `git rev-parse HEAD`.strip.first(7)].join("-")
     end
   end
 end
