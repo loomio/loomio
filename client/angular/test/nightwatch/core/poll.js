39fe9ff918ad97e4af03acf2e34e3656f2113a39
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2018-12-04 15:01:37 +1300

    added e2e test for poll poll type

--- client/angular/test/nightwatch/core/poll.js
+++ client/angular/test/nightwatch/core/poll.js
@@ -7,7 +7,7 @@ module.exports = {
     page.loadPath('setup_start_poll_form_from_url')
     page.expectValue('.poll-common-form-fields__title', "testing title")
   },
-  'can_start_a_poll_in_a_group': (test) => {
+  'can_start_a_proposal_in_a_group': (test) => {
     page = pageHelper(test)
 
     page.loadPath('test_discussion', { controller: 'polls' })
@@ -54,6 +54,35 @@ module.exports = {
 
     page.scrollTo('.poll-common-stance-choice--count', () => {
       page.expectText('.poll-common-stance-choice__option-name', 'Yes')
+      page.expectText('.poll-common-votes-panel__stance-reason', 'A reason')
+    })
+  },
+  'can_start_a_poll_in_a_group': (test) => {
+    page = pageHelper(test)
+
+    page.loadPath('test_discussion', { controller: 'polls' })
+    page.click('.decision-tools-card__poll-type--poll')
+    page.click(".poll-common-tool-tip__collapse")
+    page.fillIn('.poll-common-form-fields__title', 'A new proposal')
+    page.fillIn('.poll-common-form-fields textarea', 'Some details')
+    page.fillIn('.poll-poll-form__add-option-input  ', 'An option')
+    page.click('.poll-poll-form__option-button')
+    // page.fillIn('.poll-poll-form__add-option-input  ', 'Another option')
+    // page.click('[aria-label="Remove option"]')
+    page.click('.poll-common-form__submit')
+    page.expectElement('.announcement-form__submit')
+    page.click('.dismiss-modal-button')
+    page.expectNoElement('.poll-common-modal')
+
+    page.expectText('.poll-common-card__title', 'A new proposal')
+    page.expectText('.poll-common-details-panel__details', 'Some details')
+
+    page.click('.poll-common-vote-form__button')
+    page.fillIn('.poll-common-vote-form__reason textarea', 'A reason')
+    page.click('.poll-common-vote-form__submit')
+
+    page.scrollTo('.poll-common-votes-panel__stance-name-and-option', () => {
+      page.expectText('.poll-common-stance-choice--poll', 'An option')
       page.expectText('.poll-common-votes-panel__stance-reason', 'A reason')
     })
   },
