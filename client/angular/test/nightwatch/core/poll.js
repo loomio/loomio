9ab174575eb3dc45ae8d4131c553de08bcfed017
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2018-12-05 14:28:36 +1300

    add e2e test for ranked choice poll

--- client/angular/test/nightwatch/core/poll.js
+++ client/angular/test/nightwatch/core/poll.js
@@ -185,6 +185,37 @@ module.exports = {
       page.expectText('.poll-common-votes-panel__stance-reason', 'A reason')
     })
   },
+  'can_start_a_ranked_choice_in_a_group': (test) => {
+    page = pageHelper(test)
+
+    page.loadPath('test_discussion', { controller: 'polls' })
+    page.click('.decision-tools-card__poll-type--ranked_choice')
+    page.click('.poll-common-tool-tip__collapse')
+    page.fillIn('.poll-common-form-fields__title', 'A new proposal')
+    page.fillIn('.poll-common-form-fields textarea', 'Some details')
+
+    page.fillIn('.poll-poll-form__add-option-input', 'An option')
+    page.click('.poll-poll-form__option-button')
+    page.fillIn('.poll-poll-form__add-option-input', 'Another option')
+    page.click('.poll-common-form__options .poll-poll-form__option-button:last-child')
+
+    page.click('.poll-common-form__submit')
+    page.expectElement('.announcement-form__submit')
+    page.click('.dismiss-modal-button')
+    page.expectNoElement('.poll-common-modal')
+
+    page.expectText('.poll-common-card__title', 'A new proposal')
+    page.expectText('.poll-common-details-panel__details', 'Some details')
+
+    page.fillIn('.poll-common-vote-form__reason textarea', 'A reason')
+
+    page.click('.poll-common-vote-form__submit')
+
+    page.scrollTo('.poll-common-votes-panel__stance-name-and-option', () => {
+      page.expectText('.poll-common-votes-panel__stance-name-and-option .poll-common-stance-choice--ranked-choice:first-child', 'An option')
+      page.expectText('.poll-common-votes-panel__stance-reason', 'A reason')
+    })
+  },
 
   'can set an outcome': (test) => {
     page = pageHelper(test)
