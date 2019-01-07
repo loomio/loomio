58845ee5f12eef7f256a227abf009c3b18da11ff
Robert Guthrie <rob@guthrie.nz>
2019-01-07 15:02:23 +1300

    save record that indeed should be saved

--- app/controllers/identities/slack_controller.rb
+++ app/controllers/identities/slack_controller.rb
@@ -50,7 +50,7 @@ class Identities::SlackController < Identities::BaseController
   end
 
   def user_to_join
-    @user_to_join ||= current_user.presence || identity_to_join&.user || identity_to_join&.create_user!
+    @user_to_join ||= current_user.presence || identity_to_join&.user || identity_to_join&.create_user!.tap(&:save)
   end
 
   def identity_to_join
