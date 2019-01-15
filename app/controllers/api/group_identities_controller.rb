0861dff98d7bf78c8943ec12ac0905b0ff88c3c2
James Kiesel <james.kiesel@gmail.com>
2019-01-16 08:22:44 +1300

    Add specs for group identity create

--- app/controllers/api/group_identities_controller.rb
+++ app/controllers/api/group_identities_controller.rb
@@ -3,9 +3,9 @@ class API::GroupIdentitiesController < API::RestfulController
 
   def instantiate_resource
     super.tap do |resource|
-      resource.identity = resource.group.identity_for(resource_params[:identity_type])&.identity ||
-                          current_user.identity_for(resource_params[:identity_type]) ||
-                          generate_webhook_identity
+      resource.identity = generate_webhook_identity ||
+                          resource.group.identity_for(resource_params[:identity_type])&.identity ||
+                          current_user.identity_for(resource_params[:identity_type])
     end
   end
 
@@ -14,6 +14,6 @@ class API::GroupIdentitiesController < API::RestfulController
       identity_type: resource_params[:identity_type],
       access_token:  resource.webhook_url,
       uid:           resource.webhook_url
-    )
+    ) if resource.webhook_url
   end
 end
