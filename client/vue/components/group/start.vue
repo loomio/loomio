1eab32fb0b045ed0d63fa8dfa5f7498d874594c4
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2019-01-30 17:46:21 +1300

    tweaks to new group modal styling

--- client/vue/components/group/start.vue
+++ client/vue/components/group/start.vue
@@ -1,6 +1,3 @@
-<style lang="scss">
-</style>
-
 <script lang="coffee">
 Records       = require 'shared/services/records'
 LmoUrlService = require 'shared/services/lmo_url_service'
@@ -31,33 +28,24 @@ module.exports =
   #       # LmoUrlService.goTo LmoUrlService.group(group)
 </script>
 
-<template>
-  <v-card class="group-modal">
-    <!-- <div v-show="isDisabled" class="lmo-disabled-form"></div> -->
-    <v-card-title>
-      <div class="md-toolbar-tools lmo-flex__space-between">
-        <i class="mdi mdi-account-multiple"></i>
-        <div class="group-form__title">
-          <div v-if="dcurrentStep == 'create'" class="group-form__group-title">
-            <h1 v-if="dgroup.isNew() && dgroup.parentId" v-t="'group_form.start_subgroup_heading'" class="lmo-h1"></h1>
-            <h1 v-if="dgroup.isNew() && !dgroup.parentId" v-t="'group_form.start_group_heading'" class="lmo-h1"></h1>
-            <h1 v-if="!dgroup.isNew()" v-t="'group_form.edit_group_heading'" class="lmo-h1"></h1>
-          </div>
-          <div v-if="dcurrentStep == 'announce'" class="group-form__invitation-title">
-            <h1 v-t="'announcement.form.group_announced.title'" class="lmo-h1"></h1>
-          </div>
-        </div>
-        <dismiss-modal-button :close="close"></dismiss-modal-button>
-      </div>
-    </v-card-title>
-    <v-card-text class="md-body-1 lmo-slide-animation">
-      <group-form v-if="dcurrentStep == 'create'" :group="dgroup" :modal="true" class="animated"></group-form>
-      <announcement-form v-if="dcurrentStep == 'announce'" :announcement="announcement" class="animated"></announcement-form>
-      <!-- <dialog_scroll_indicator></dialog_scroll_indicator> -->
-    </v-card-text>
-    <v-card-actions>
-      <group-form-actions v-if="dcurrentStep == 'create'" :group="dgroup" :successFn="completeGroupCreation"></group-form-actions>
-      <!-- <announcement_form_actions v-if="dcurrentStep = 'announce'" :announcement="announcement"></announcement_form_actions> -->
-    </v-card-actions>
-  </v-card>
+<template lang="pug">
+v-card.group-modal
+  // <div v-show="isDisabled" class="lmo-disabled-form"></div>
+  v-card-title
+    v-layout(justify-space-between style="align-items: center")
+      v-icon mdi-account-multiple
+      .group-form__group-title(v-if="dcurrentStep == 'create'")
+        h1.headline(v-if='dgroup.isNew() && dgroup.parentId', v-t="'group_form.start_subgroup_heading'")
+        h1.headline(v-if='dgroup.isNew() && !dgroup.parentId', v-t="'group_form.start_group_heading'")
+        h1.headline(v-if='!dgroup.isNew()', v-t="'group_form.edit_group_heading'")
+      .group-form__invitation-title(v-if="dcurrentStep == 'announce'")
+        h1.headline(v-t="'announcement.form.group_announced.title'")
+      dismiss-modal-button(:close='close')
+  v-card-text.md-body-1
+    group-form(v-if="dcurrentStep == 'create'", :group='dgroup', :modal='true')
+    announcement-form.animated(v-if="dcurrentStep == 'announce'", :announcement='announcement')
+    // <dialog_scroll_indicator></dialog_scroll_indicator>
+  v-card-actions
+    group-form-actions(v-if="dcurrentStep == 'create'", :group='dgroup', :successfn='completeGroupCreation')
+    // <announcement_form_actions v-if="dcurrentStep = 'announce'" :announcement="announcement"></announcement_form_actions>
 </template>
