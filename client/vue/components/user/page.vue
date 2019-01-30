b181650535690c0d987bcd0d009d8a55136984e3
iainkirkpatrick <kirkpatrick.iain@gmail.com>
2019-01-30 17:18:41 +1300

    towards functional user page, group loading and displaying not working yet tho

--- client/vue/components/user/page.vue
+++ client/vue/components/user/page.vue
@@ -34,16 +34,16 @@ module.exports =
       # EventBus.broadcast $rootScope, 'pageError', error
   methods:
     setUser: ->
-      return if @user
+      # return if @user
       if @user = (Records.users.find(@$route.params.key) or Records.users.find(username: @$route.params.key))[0]
-        # EventBus.broadcast $rootScope, 'currentComponent', {title: @user.name, page: 'userPage'}
+      # EventBus.broadcast $rootScope, 'currentComponent', {title: @user.name, page: 'userPage'}
         @loadGroupsFor(@user)
 
     contactUser: ->
       ModalService.open 'ContactRequestModal', user: => @user
 
     loadGroupsFor: (user) ->
-      Records.memberships.fetchByUser(user)
+      Records.memberships.fetchByUser(user).then =>
   computed:
     canContactUser: ->
       AbilityService.canContactUser(@user)
@@ -55,36 +55,27 @@ module.exports =
       _sortBy @user.formalGroups(), 'fullName'
 </script>
 
-<template>
-  <div class="loading-wrapper container main-container lmo-one-column-layout">
-    <loading v-if="isEmptyUser"></loading>
-    <main v-if="!isEmptyUser" class="user-page main-container lmo-row">
-      <div class="lmo-card user-page__profile">
-        <div layout="row" class="user-page__content lmo-flex">
-          <div class="user-page__avatar lmo-margin-right">
-            <user-avatar :user="user" size="featured"></user-avatar>
-            <v-btn v-if="canContactUser()" @click="contactUser()" v-t="{ path: 'user_page.contact_user', args: { name: user.firstName() } }" class="md-block md-primary md-raised user-page__contact-user"></v-btn>
-          </div>
-          <div class="user-page__info">
-            <h1 class="lmo-h1">{{user.name}}</h1>
-            <div class="lmo-hint-text">@{{user.username}}</div>
-            <p>{{user.shortBio}}</p>
-            <div v-t="{ path: 'user_page.locale_field', args: { value: user.localeName() } }" v-if="user.localeName()"></div>
-            <div v-t="{ path: 'user_page.location_field', args: { value: user.location } }" v-if="user.location"></div>
-            <div v-t="{ path: 'user_page.online_field', args: { value: fromNow(user.lastSeenAt) } }" v-if="user.lastSeenAt"></div>
-            <div class="user-page__groups">
-              <h3 v-t="'common.groups'" class="lmo-h3 user-page__groups-title"></h3>
-              <v-list>
-                <v-list-tile v-for="group in orderedFormalGroups" :key="group.id" class="user-page__group lmo-flex lmo-flex__center">
-                  <img :src="group.logoUrl()" class="md-avatar lmo-box--small lmo-margin-right">
-                  <router-link :to="urlFor(group)">{{group.fullName}}</router-link>
-                </v-list-tile>
-              </v-list>
-              <!-- <loading v-if="loadGroupsForExecuting"></loading> -->
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  </div>
+<template lang="pug">
+.loading-wrapper.container.main-container.lmo-one-column-layout
+  loading(v-if='isEmptyUser')
+  main.user-page.main-container.lmo-row(v-if='!isEmptyUser')
+    .lmo-card.user-page__profile
+      .user-page__content.lmo-flex(layout='row')
+        .user-page__avatar.lmo-margin-right
+          user-avatar(:user='user', size='featured')
+          v-btn.md-block.md-primary.md-raised.user-page__contact-user(v-if='canContactUser', @click='contactUser()', v-t="{ path: 'user_page.contact_user', args: { name: user.firstName() } }")
+        .user-page__info
+          h1.lmo-h1 {{user.name}}
+          .lmo-hint-text @{{user.username}}
+          p {{user.shortBio}}
+          div(v-t="{ path: 'user_page.locale_field', args: { value: user.localeName() } }", v-if='user.localeName()')
+          div(v-t="{ path: 'user_page.location_field', args: { value: user.location } }", v-if='user.location')
+          div(v-t="{ path: 'user_page.online_field', args: { value: fromNow(user.lastSeenAt) } }", v-if='user.lastSeenAt')
+          .user-page__groups
+            h3.lmo-h3.user-page__groups-title(v-t="'common.groups'")
+            v-list
+              v-list-tile.user-page__group.lmo-flex.lmo-flex__center(v-for='group in user.formalGroups()', :key='group.id')
+                img.md-avatar.lmo-box--small.lmo-margin-right(:src='group.logoUrl()')
+                router-link(:to='urlFor(group)') {{group.fullName}}
+            // <loading v-if="loadGroupsForExecuting"></loading>
 </template>
