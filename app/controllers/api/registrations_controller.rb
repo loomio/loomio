81d076aab671838cda1dd507ca391ca439909030
Robert Guthrie <rob@guthrie.nz>
2019-03-07 14:17:36 +1300

    fix register path

--- app/controllers/api/registrations_controller.rb
+++ app/controllers/api/registrations_controller.rb
@@ -4,10 +4,17 @@ class API::RegistrationsController < Devise::RegistrationsController
   before_action :permission_check, only: :create
 
   def create
-    build_resource(sign_up_params)
+    @verified_email = email_is_verified?
+
+    if self.resource = user_from_membership
+      resource.attributes=(sign_up_params)
+    else
+      build_resource(sign_up_params)
+    end
+
     if UserService.create(user: resource)
       save_detected_locale(resource)
-      if email_is_verified?
+      if @verified_email
         sign_in resource
         flash[:notice] = t(:'devise.sessions.signed_in')
         render json: { success: :ok, signed_in: true }
@@ -21,6 +28,10 @@ class API::RegistrationsController < Devise::RegistrationsController
   end
 
   private
+  def user_from_membership
+    pending_membership.present? && pending_membership.user
+  end
+
   def permission_check
     AppConfig.app_features[:create_user] || pending_membership
   end
