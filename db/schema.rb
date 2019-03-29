479e4e2050483efdb44abfb674a991ad38e3bb3c
Robert Guthrie <rob@guthrie.nz>
2019-03-29 13:55:17 +1300

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2019_03_21_022626) do
-ActiveRecord::Schema.define(version: 2019_03_10_214604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
