5e0d5606f1585570514a1cd3c5bfbbcf576406ba
Gregory Kan <kan.gregory@gmail.com>
2019-05-13 10:03:32 +1200

    chore(refactor): convert to pug

--- vue/src/components/poll/common/card.vue
+++ vue/src/components/poll/common/card.vue
@@ -23,21 +23,21 @@ export default
 <template lang="pug">
 v-card
   // <div v-if="isDisabled" class="lmo-disabled-form"></div>
-  loading(v-if='!poll.complete')
-  .lmo-blank(v-if='poll.complete')
-    poll-common-card-header.px-4(:poll='poll')
-    v-card-text
-      poll-common-set-outcome-panel(:poll='poll')
-      h1.poll-common-card__title(v-if='!poll.translation') {{poll.title}}
-      h3.lmo-h1.poll-common-card__title(v-if='poll.translation')
-        translation(:model='poll', :field='title')
-      poll-common-outcome-panel(:poll='poll', v-if='poll.outcome()')
-      poll-common-details-panel(:poll='poll')
-      .poll-common-card__results-shown(v-if='showResults()')
-        poll-common-directive(:poll='poll', name='chart-panel')
-        poll-common-add-option-button(:poll='poll')
-        poll-common-percent-voted(:poll='poll')
-    poll-common-action-panel(:poll='poll')
+  //- loading(v-if='!poll.complete')
+  //- .lmo-blank(v-if='poll.complete')
+  poll-common-card-header.px-4(:poll='poll')
+  v-card-text
+    poll-common-set-outcome-panel(:poll='poll')
+    h1.poll-common-card__title(v-if='!poll.translation') {{poll.title}}
+    h3.lmo-h1.poll-common-card__title(v-if='poll.translation')
+      translation(:model='poll', :field='title')
+    poll-common-outcome-panel(:poll='poll', v-if='poll.outcome()')
+    poll-common-details-panel(:poll='poll')
     .poll-common-card__results-shown(v-if='showResults()')
-      poll-common-votes-panel(:poll='poll')
+      poll-common-directive(:poll='poll', name='chart-panel')
+      poll-common-add-option-button(:poll='poll')
+      poll-common-percent-voted(:poll='poll')
+  poll-common-action-panel(:poll='poll')
+  .poll-common-card__results-shown(v-if='showResults()')
+    poll-common-votes-panel(:poll='poll')
 </template>
