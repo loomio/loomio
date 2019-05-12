35bba004fcd97d65407c6f523e0aa37947a53954
Gregory Kan <kan.gregory@gmail.com>
2019-05-13 10:04:08 +1200

    chore(refactor): remove steps; convert to pug

--- vue/src/components/poll/common/start_modal.vue
+++ vue/src/components/poll/common/start_modal.vue
@@ -18,30 +18,17 @@ export default
       iconFor(@poll)
 
 </script>
-<template>
-  <v-card class="poll-common-modal">
-    <!-- <div ng-show="isDisabled" class="lmo-disabled-form"></div> -->
-    <v-card-title>
-      <div class="md-toolbar-tools lmo-flex__space-between"><i :class="'mdi '+ icon()"></i>
-        <h1 class="lmo-h1">
-          <span v-if="currentStep == 'choose'" v-t="'poll_common.start_poll'"></span>
-          <span v-if="currentStep == 'save'" v-t="'poll_common.start_poll'"></span>
-          <span v-if="currentStep == 'announce'" v-t="'announcement.form.poll_announced.title'"></span>
-        </h1>
-        <dismiss-modal-button :close="close"></dismiss-modal-button>
-      </div>
-    </v-card-title>
-    <v-card-text>
-      <div class="lmo-slide-animation">
-        <poll-common-choose-type v-if="currentStep == 'choose'" :poll="poll" class="animated"></poll-common-choose-type>
-        <poll-common-directive v-if="currentStep == 'save'" :poll="poll" name="form" :modal="true" class="animated"></poll-common-directive>
-        <announcement-form v-if="currentStep == 'announce'" :announcement="announcement" class="animated"></announcement-form>
-      </div>
-      <!-- <dialog_scroll_indicator></dialog_scroll_indicator> -->
-    </v-card-text>
-    <v-card-actions v-if="currentStep != 'choose'" class="lmo-slide-animation">
-      <poll-common-form-actions v-if="currentStep == 'save'" :poll="poll" :close="close" class="animated"></poll-common-form-actions>
-      <announcement-form-actions v-if="currentStep == 'announce'" :announcement="announcement" class="animated"></announcement-form-actions>
-    </v-card-actions>
-  </v-card>
+<template lang="pug">
+  v-card.poll-common-modal
+    // <div ng-show="isDisabled" class="lmo-disabled-form"></div>
+    v-card-title
+      h1.lmo-h1
+        span(v-t="'poll_common.start_poll'")
+      dismiss-modal-button(:close='close')
+    v-card-text
+      .lmo-slide-animation
+        poll-common-directive.animated(:poll='poll', name='form', :modal='true')
+      // <dialog_scroll_indicator></dialog_scroll_indicator>
+    v-card-actions.lmo-slide-animation
+      poll-common-form-actions.animated(:poll='poll', :close='close')
 </template>
