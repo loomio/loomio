2027ab69797478b576fa48aa6dd1297807d35a49
Robert Guthrie <rob@guthrie.nz>
2019-05-22 09:38:11 +1200

    rewrite membershiprequests page with vuetify

--- vue/src/components/membership_requests/page.vue
+++ vue/src/components/membership_requests/page.vue
@@ -1,39 +1,3 @@
-<style lang="scss">
-@import 'mixins';
-@import 'lmo_card';
-@import 'variables';
-.membership-requests-page {
-  @include lmoRow;
-}
-
-.membership-requests-page__pending-requests,
-.membership-requests-page__previous-requests {
-  @include card;
-  ul {
-    list-style: none;
-    padding-left: 0px;
-  }
-}
-
-.membership-requests-page__actions {
-  height: 60px;
-  margin-top: 5px;
-  button { margin-right: 10px; }
-}
-
-.membership-requests-page__pending-request-email,
-.membership-requests-page__pending-request-date,
-.membership-requests-page__previous-request-email,
-.membership-requests-page__previous-request-response {
-  color: $grey-on-white;
-  font-size: 12px;
-}
-
-.membership-requests-page__previous-request {
-  margin: 16px 0;
-}
-</style>
-
 <script lang="coffee">
 import AppConfig      from '@/shared/services/app_config'
 import Records        from '@/shared/services/records'
@@ -91,36 +55,39 @@ export default
     main.membership-requests-page(v-if='!isEmptyGroup')
       .lmo-group-theme-padding
       group-theme(:group='group')
-      .membership-requests-page__pending-requests
-        h2.lmo-h2(v-t="'membership_requests_page.heading'")
-        ul(v-if='pendingRequests.length')
-          li.lmo-flex.membership-requests-page__pending-request(layout='row', v-for='request in pendingRequests', :key='request.id')
-            user-avatar.lmo-margin-right(:user='request.actor()', size='medium')
-            .lmo-flex(layout='column')
-              span.membership-requests-page__pending-request-name
-                strong {{request.actor().name}}
-              .membership-requests-page__pending-request-email {{request.email}}
-              .membership-requests-page__pending-request-date
-                time-ago(:date='request.createdAt')
-              .membership-requests-page__pending-request-introduction {{request.introduction}}
-              .membership-requests-page__actions
-                v-btn.membership-requests-page__ignore(@click='ignore(request)', v-t="'membership_requests_page.ignore'")
-                v-btn.md-primary.md-raised.membership-requests-page__approve(@click='approve(request)', v-t="'membership_requests_page.approve'")
-        .membership-requests-page__no-pending-requests(v-if='pendingRequests.length == 0', v-t="'membership_requests_page.no_pending_requests'")
-      .membership-requests-page__previous-requests
-        h3.lmo-card-heading(v-t="'membership_requests_page.previous_requests'")
-        ul(v-if='previousRequests.length')
-          li.lmo-flex.membership-requests-page__previous-request(layout='row', v-for='request in previousRequests', :key='request.id')
-            user-avatar.lmo-margin-right(:user='request.actor()', size='medium')
-            .lmo-flex(layout='column')
-              span.membership-requests-page__previous-request-name
-                strong {{request.actor().name}}
-              .membership-requests-page__previous-request-email {{request.email}}
-              .membership-requests-page__previous-request-response
-                span(v-t="{ path: 'membership_requests_page.previous_request_response', args: { response: request.formattedResponse(), responder: request.responder().name } }")
-                span ·
-                time-ago(:date='request.respondedAt')
+
+      v-list.membership-requests-page__pending-requests(subheader)
+        v-subheader(v-if='pendingRequests.length' v-t="'membership_requests_page.heading'")
+        v-subheader.membership-requests-page__no-pending-requests(v-if='pendingRequests.length == 0' v-t="'membership_requests_page.no_pending_requests'")
+        v-list-tile.membership-requests-page__pending-request(v-for='request in pendingRequests' :key='request.id' avatar)
+          v-list-tile-avatar
+            user-avatar(:user='request.actor()', size='medium')
+          v-list-tile-content
+            v-list-tile-title.membership-requests-page__pending-request-name {{request.actor().name}} ({{request.email}})
+            v-list-tile-sub-title.membership-requests-page__pending-request-introduction
+              span {{request.introduction}}
+              time-ago(:date='request.createdAt')
+          v-list-tile-action
+            //- , v-t="'membership_requests_page.ignore'"
+            //- , v-t="'membership_requests_page.approve'"
+            v-btn.membership-requests-page__ignore(flat icon @click='approve(request)')
+              v-icon mdi-check
+          v-list-tile-action
+            v-btn.membership-requests-page__approve(flat icon @click='ignore(request)')
+              v-icon mdi-close
+
+      v-list.membership-requests-page__previous-requests(subheader three-line)
+        v-subheader(v-if='previousRequests.length' v-t="'membership_requests_page.previous_requests'")
+        v-subheader.membership-requests-page__no-previous-requests(v-if='previousRequests.length == 0' v-t="'membership_requests_page.no_previous_requests'")
+        v-list-tile.membership-requests-page__previous-request(v-for='request in previousRequests' :key='request.id' avatar)
+          v-list-tile-avatar
+            user-avatar(:user='request.actor()', size='medium')
+          v-list-tile-content
+            v-list-tile-title.membership-requests-page__previous-request-name {{request.actor().name}} {{request.email}}
+            v-list-tile-sub-title.membership-requests-page__previous-request-response
+              span(v-t="{ path: 'membership_requests_page.previous_request_response', args: { response: request.formattedResponse(), responder: request.responder().name } }")
+              span ·
+              time-ago(:date='request.respondedAt')
               .membership-requests-page__previous-request-introduction {{request.introduction}}
-        .membership-requests-page__no-previous-requests(v-if='previousRequests.length == 0', v-t="'membership_requests_page.no_previous_requests'")
 
 </template>
