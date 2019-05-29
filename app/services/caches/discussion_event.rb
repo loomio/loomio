b572f756143a196f18943c09a74554a30c1d832f
Robert Guthrie <rob@guthrie.nz>
2019-05-29 12:57:36 +1200

    ensure discussion created event when using discussion event cache

--- app/services/caches/discussion_event.rb
+++ app/services/caches/discussion_event.rb
@@ -9,6 +9,10 @@ class Caches::DiscussionEvent < Caches::Base
     :eventable
   end
 
+  def default_values_for(discussion)
+    [discussion.created_event]
+  end
+
   def collection_from(parents)
     super.where(kind: ['new_discussion', 'discussion_forked'])
   end
