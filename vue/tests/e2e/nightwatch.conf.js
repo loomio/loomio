a0d63852aced53fea6b58ea11b49eca178079933
Robert Guthrie <rob@guthrie.nz>
2019-05-31 15:20:25 +1200

    resize browser to fix a test maybe

--- vue/tests/e2e/nightwatch.conf.js
+++ vue/tests/e2e/nightwatch.conf.js
@@ -4,7 +4,8 @@ var chromedriver = require('chromedriver');
 var chromeOptions = []
 if (process.env.RAILS_ENV == 'test') {
   console.log("WORD UP!!!!!!!!! running in HEADLESS MODE")
-  chromeOptions = [ "headless" ]
+  chromeOptions = [ "headless", "window-size=1280,800" ]
+
 }
 
 module.exports = {
