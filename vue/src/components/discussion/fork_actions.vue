c4c06ae144b3f066f1211862aa9cd54572edb6e6
Gregory Kan <kan.gregory@gmail.com>
2019-06-04 09:28:44 +1200

    chore(pug): pugify

--- vue/src/components/discussion/fork_actions.vue
+++ vue/src/components/discussion/fork_actions.vue
@@ -24,13 +24,11 @@ export default
       @openForkedDiscussionModal(@discussion)
 </script>
 
-<template>
-  <div class="discussion-fork-actions lmo-drop-animation">
-    <div v-show="discussion.isForking()" class="discussion-fork-actions__notice animated lmo-card lmo-flex--row lmo-flex__center">
-      <i class="mdi mdi-call-split mdi-18px lmo-margin-right"></i>
-      <span v-t="'discussion_fork_actions.helptext'" class="lmo-flex__grow"></span>
-      <v-btn @click="discussion.forkedEventIds = []" v-t="'common.action.cancel'" class="md-accent"></v-btn>
-      <v-btn @click="submit()" v-t="'common.action.fork'" class="md-primary md-raised discussion-fork-actions__submit"></v-btn>
-    </div>
-  </div>
+<template lang='pug'>
+.discussion-fork-actions.lmo-drop-animation
+  .discussion-fork-actions__notice.animated.lmo-card.lmo-flex--row.lmo-flex__center(v-show='discussion.isForking()')
+    v-icon.mdi.mdi-18px.lmo-margin-right mdi-call-split
+    span.lmo-flex__grow(v-t="'discussion_fork_actions.helptext'")
+    v-btn.md-accent(@click='discussion.forkedEventIds = []', v-t="'common.action.cancel'")
+    v-btn.discussion-fork-actions__submit(@click='submit()', v-t="'common.action.fork'")
 </template>
