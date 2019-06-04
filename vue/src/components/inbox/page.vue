b9379bce9fe59752aa53bf81a65be6a1272c9299
Robert Guthrie <rob@guthrie.nz>
2019-06-04 19:10:03 +1200

    show inbox if unread threads present while loading

--- vue/src/components/inbox/page.vue
+++ vue/src/components/inbox/page.vue
@@ -69,10 +69,10 @@ export default
 v-container.lmo-main-container.inbox-page(grid-list-lg)
   .thread-preview-collection__container
     h1.lmo-h1-medium.inbox-page__heading(v-t="'inbox_page.unread_threads'")
-    section.dashboard-page__loading(v-if='loading', aria-hidden='true')
+    section.dashboard-page__loading(v-if='unreadCount == 0 && loading', aria-hidden='true')
       .thread-previews-container
         loading-content.thread-preview(:lineCount='2' v-for='(item, index) in [1,2,3,4,5,6,7,8,9,10]' :key='index')
-    section.inbox-page__threads(v-if='!loading')
+    section.inbox-page__threads(v-if='unreadCount > 0 || !loading')
       .inbox-page__no-threads(v-show='unreadCount == 0')
         span(v-t="'inbox_page.no_threads'")
         span 🙌
