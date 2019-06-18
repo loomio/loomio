7b8d135539aec1ed5b284c74df2b56048a7b4f4f
Gregory Kan <kan.gregory@gmail.com>
2019-06-18 16:13:50 +1200

    feat(thread): members panel and polls panel

--- vue/src/components/thread/polls_panel.vue
+++ vue/src/components/thread/polls_panel.vue
@@ -1,6 +1,37 @@
 <script lang="coffee">
+import Records       from '@/shared/services/records'
+import LmoUrlService from '@/shared/services/lmo_url_service'
+import WatchRecords from '@/mixins/watch_records'
+import { applyLoadingFunction } from '@/shared/helpers/apply'
+
+export default
+  mixins: [WatchRecords]
+  data: ->
+    discussion: null
+    loading: false
+    polls: []
+  created: ->
+    @discussion = Records.discussions.find(@$route.params.key)
+    @fetchRecords()
+    @watchRecords
+      collections: ['polls']
+      query: (store) =>
+        @polls = store.polls.collection.chain()
+          .find(discussionId: @discussion.id)
+          .compoundsort([['latest', true], ['createdAt', true]])
+          .data()
+  methods:
+    fetchRecords: ->
+      @loading = true
+      Records.polls.fetchFor(@discussion).then =>
+        @loading = false
+
 </script>
+
 <template lang="pug">
-.thread-polls-panel
-  h1.headline polls-panel
+.polls-panel
+  p.text-xs-center(v-if="!polls.length" v-t="'group_polls_panel.no_polls'") 
+  v-list.poll-common-index-card__polls(two-line v-if='polls.length')
+    poll-common-preview(v-for='poll in polls', :key='poll.id', :poll='poll')
+  loading(v-if="loading")
 </template>
