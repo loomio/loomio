4832c4fe8c827e9c850c7bb01daca2791a569271
Robert Guthrie <rob@guthrie.nz>
2019-07-06 14:46:52 +1200

    create thread-item-slot and record own height

--- vue/src/components/thread/activity_panel.vue
+++ vue/src/components/thread/activity_panel.vue
@@ -13,21 +13,9 @@ import WatchRecords from '@/mixins/watch_records'
 import { print } from '@/shared/helpers/window'
 import { compact, snakeCase, camelCase, min, max, times, map, without, uniq, throttle, debounce, range, difference, isNumber, isEqual } from 'lodash'
 import RangeSet from '@/shared/services/range_set'
-import ThreadActivityMixin from '@/mixins/thread_activity'
-import NewComment from '@/components/thread/item/new_comment.vue'
-import PollCreated from '@/components/thread/item/poll_created.vue'
-import StanceCreated from '@/components/thread/item/stance_created.vue'
-import OutcomeCreated from '@/components/thread/item/outcome_created.vue'
 
 export default
-  mixins: [ AuthModalMixin, WatchRecords, ThreadActivityMixin]
-
-  components:
-    NewComment: NewComment
-    PollCreated: PollCreated
-    StanceCreated: StanceCreated
-    OutcomeCreated: OutcomeCreated
-    ThreadItem: -> import('@/components/thread/item.vue')
+  mixins: [ AuthModalMixin, WatchRecords]
 
   props:
     discussion: Object
@@ -40,7 +28,6 @@ export default
     visibleSlots: []
     missingPositions: []
     pageSize: 10
-
 
   created: -> @init()
 
@@ -64,8 +51,6 @@ export default
             find(discussionId: @discussion.id).
             find(depth: 1).data().forEach (event) =>
               @eventsBySlot[event.position] = event
-
-          console.log 'eventsBySlot', @eventsBySlot
 
       @scrollToInitialPosition()
 
@@ -181,13 +166,8 @@ export default
 
 <template lang="pug">
 .activity-panel
-  .activity_panel__slot(v-for="(event, slot) in eventsBySlot" :id="'position-'+slot" :key="slot" v-observe-visibility="(isVisible, entry) => slotVisible(isVisible, entry, slot, event)" )
-    v-sheet.ma-4.pa-4(v-if="!event" color="grey lighten-3" height="200")
-      | {{slot}} {{event && event.kind}}
-
-    component(v-if="event" :is="componentForKind(event.kind)" :event='event')
+  thread-item-slot(v-for="(event, slot) in eventsBySlot" :id="'position-'+slot" :key="slot" :event="event" :position="slot" v-observe-visibility="(isVisible, entry) => slotVisible(isVisible, entry, slot, event)" )
   thread-actions-panel(:discussion="discussion")
-
 </template>
 <style lang="scss">
 @import 'variables';
---
+++ vue/src/components/thread/item_slot.vue
@@ -0,0 +1,36 @@
+<script lang="coffee">
+import NewComment from '@/components/thread/item/new_comment.vue'
+import PollCreated from '@/components/thread/item/poll_created.vue'
+import StanceCreated from '@/components/thread/item/stance_created.vue'
+import OutcomeCreated from '@/components/thread/item/outcome_created.vue'
+import ThreadActivityMixin from '@/mixins/thread_activity'
+
+export default
+  mixins: [ThreadActivityMixin]
+  props:
+    event: Object
+    position: String
+
+  components:
+    NewComment: NewComment
+    PollCreated: PollCreated
+    StanceCreated: StanceCreated
+    OutcomeCreated: OutcomeCreated
+    ThreadItem: -> import('@/components/thread/item.vue')
+
+  data: ->
+    height: 200
+
+  watch:
+    event: ->
+      if @event
+        setTimeout => @height = @$refs.box.clientHeight
+
+</script>
+<template lang="pug">
+.thread-item-slot(ref="box")
+  v-sheet.ma-4.pa-4(v-if="!event" color="grey lighten-3" :height="height")
+    .grey--text.text-xs-center.display-1 {{position}}
+
+  component(v-if="event" :is="componentForKind(event.kind)" :event='event')
+</template>
