0790b2453a211333e1bbbcb5ca8d82560648ffda
Robert Guthrie <rob@guthrie.nz>
2019-07-06 12:54:24 +1200

    support for collapsing/expanding thread items

--- vue/src/components/common/mid_dot.vue
+++ vue/src/components/common/mid_dot.vue
@@ -1,3 +1,3 @@
 <template>
-<span aria-hidden='true'>&nbsp;·&nbsp;</span>
+<span><span aria-hidden='true'>&nbsp;·&nbsp;</span><slot></slot></span>
 </template>
--- vue/src/components/thread/item.vue
+++ vue/src/components/thread/item.vue
@@ -21,6 +21,8 @@ export default
   data: ->
     isDisabled: false
     isFocused: @updateIsFocused()
+    collapsed: false
+    hover: false
 
   methods:
     updateIsFocused: ->
@@ -42,6 +44,7 @@ export default
     camelCase: camelCase
 
   computed:
+    iconSize: -> if @isNested then 36 else 48
     canRemoveEvent: ->
       AbilityService.canRemoveEventFromThread(@event)
 
@@ -68,12 +71,35 @@ export default
 
 <template lang="pug">
 div
-  .thread-item(:class="{'thread-item--unread': isUnread, 'thread-item--focused': isFocused}" v-observe-visibility="{callback: viewed}")
+  .thread-item.thread-item--collapsed(v-if="collapsed")
+    v-layout(:id="'sequence-' + event.sequenceId" :class="{'thread-item--indent': indent}")
+      .thread-item__avatar.mr-3.mt-2.d-flex(@mouseover="hover = true" @mouseleave="hover = false")
+        v-btn(v-if="hover" icon :width="iconSize" :height="iconSize"  @click="collapsed = false")
+          v-icon mdi-arrow-expand-vertical
+        user-avatar(v-if='!hover && !event.isForkable() && event.actor()' :user='event.actor()' :size='iconSize')
+      h3.thread-item__title.body-2.my-1.d-flex.align-center.wrap(:id="'event-' + event.id")
+        //- div
+          | sid {{event.sequenceId}}
+          | pos {{event.position}}
+          //- | pid: {{event.model().parentId}}
+          | depth: {{event.depth}}
+        slot(name="headline")
+          span(v-html='headline')
+        mid-dot
+        router-link.thread-item__link(:to='link')
+          time-ago.timeago--inline(:date='event.createdAt')
+        mid-dot(v-if="event.childCount")
+          span(v-if="event.childCount" v-t="{path: 'thread_preview.replies_count', args: {count: event.childCount}}")
+
+  .thread-item(v-if="!collapsed" :class="{'thread-item--unread': isUnread, 'thread-item--focused': isFocused}" v-observe-visibility="{callback: viewed}")
     v-layout.lmo-action-dock-wrapper(:id="'sequence-' + event.sequenceId" :class="{'thread-item--indent': indent}")
       .lmo-disabled-form(v-show='isDisabled')
       .thread-item__avatar.mr-3.mt-2
-        user-avatar(v-if='!event.isForkable() && event.actor()' :user='event.actor()' :size='isNested ? "thirtysix" : "medium"')
-        v-checkbox.thread-item__is-forking(v-if="event.isForkable()" :disabled="!event.canFork()" @change="event.toggleFromFork()" v-model="event.isForking()")
+        div(@mouseover="hover = true" @mouseleave="hover = false")
+          v-btn(v-if="hover" :width="iconSize" :height="iconSize"  icon @click="collapsed = true")
+            v-icon mdi-arrow-collapse-vertical
+          user-avatar(v-if='!hover && !event.isForkable() && event.actor()' :user='event.actor()' :size='iconSize')
+          v-checkbox.thread-item__is-forking(v-if="!hover && event.isForkable()" :disabled="!event.canFork()" @change="event.toggleFromFork()" v-model="event.isForking()")
       v-layout.thread-item__body(column)
         v-layout
           h3.thread-item__title.body-2.my-1.d-flex.align-center.wrap(:id="'event-' + event.id")
@@ -90,7 +116,7 @@ div
           button.md-button--tiny(v-if='canRemoveEvent', @click='removeEvent()')
             i.mdi.mdi-delete
         slot
-  template(v-if='event.isSurface()')
+  template(v-if='!collapsed && event.isSurface()')
     event-children(:parent-event='event')
 </template>
 
