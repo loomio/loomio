2bce613d614b53efc943cf8f6de6d9fea6f19f85
Robert Guthrie <rob@guthrie.nz>
2019-07-12 17:27:14 +1200

    import moment

--- vue/src/components/poll/meeting/form.vue
+++ vue/src/components/poll/meeting/form.vue
@@ -1,6 +1,7 @@
 <script lang="coffee">
 import AppConfig from '@/shared/services/app_config'
 import EventBus  from '@/shared/services/event_bus'
+import * as moment from 'moment'
 
 import { pull } from 'lodash'
 
