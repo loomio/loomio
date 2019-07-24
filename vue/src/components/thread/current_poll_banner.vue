a225b4d5f6ef71f32ae5e6e38820bb56cb02d89e
Robert Guthrie <rob@guthrie.nz>
2019-07-24 18:33:29 +1200

    fix banner cos the v-one was poor

--- vue/src/components/thread/current_poll_banner.vue
+++ vue/src/components/thread/current_poll_banner.vue
@@ -17,26 +17,43 @@ export default
         @poll = find @discussion.activePolls(), (poll) ->
           !store.stances.findOrNull(latest: true, pollId: poll.id, participantId: AppConfig.currentUserId)
         @event = @poll.createdEvent() if @poll
+  computed:
+    styles: ->
+      { bar, top } = @$vuetify.application
+      return
+        display: 'flex'
+        position: 'sticky'
+        top: "#{bar + top}px"
+        zIndex: 1
 
 </script>
 <template lang="pug">
-
-v-banner.current-poll-banner.mb-4(v-if="event && $route.params.sequence_id != event.sequenceId" single-line sticky :elevation="3")
-  v-layout(align-center)
-    v-avatar.mr-4(:size="36")
-      poll-common-chart-preview(:poll='poll' :size="36")
+v-card.current-poll-banner.mb-4.py-2.px-4(:style="styles" v-if="event && $route.params.sequence_id != event.sequenceId" :elevation="3")
+  v-avatar.mr-4(:size="36")
+    poll-common-chart-preview(:poll='poll' :size="36")
+  .current-poll-banner__title.mr-4
     span {{poll.title}}
-    mid-dot
-    poll-common-closing-at.caption(approximate :poll="poll")
-  template(v-slot:actions)
-    v-btn(outlined color="primary" :to="urlFor(event)" v-t="'poll_common.vote'")
+    space
+    v-chip(small outlined)
+      poll-common-closing-at.caption(approximate :poll="poll")
+  v-spacer
+  v-btn(color="primary" :to="urlFor(event)" v-t="'poll_common.vote'")
 </template>
 
 <style lang="sass">
 .current-poll-banner
-  .v-banner__content
-    overflow: visible
-.v-banner--single-line
-  .v-banner__text
-    overflow: visible !important
+  display: flex
+  align-items: center
+.current-poll-banner__title
+  white-space: nowrap
+  overflow: hidden
+  text-overflow: ellipsis
+
+  // position: sticky top: 64px
+  // z-index: 1
+//   .v-banner__content
+//     overflow: visible
+// .v-banner--single-line
+//   .v-banner__text
+// overflow: visible !important
 </style>
