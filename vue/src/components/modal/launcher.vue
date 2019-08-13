276cd5dba4d4906f53c065e68e6d611520e7f31d
Gregory Kan <kan.gregory@gmail.com>
2019-08-14 10:50:03 +1200

    chore(onEsc): remove close dialog on escape for now

--- vue/src/components/modal/launcher.vue
+++ vue/src/components/modal/launcher.vue
@@ -70,7 +70,7 @@ export default
 </script>
 
 <template lang="pug">
-v-dialog(v-model="isOpen" max-width="600px" persistent :fullscreen="$vuetify.breakpoint.smAndDown" @keydown.esc="closeModal()")
+v-dialog(v-model="isOpen" max-width="600px" persistent :fullscreen="$vuetify.breakpoint.smAndDown")
   component(:is="componentName" :key="componentKey()" v-bind="componentProps" :close="closeModal")
 
 </template>
