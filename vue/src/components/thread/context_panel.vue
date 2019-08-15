881fda4dfbac94b464075e283e43fd1454e9c004
Robert Guthrie <rob@Roberts-MacBook-Pro.local>
2019-08-16 08:50:54 +1200

    fix(contextpanel): better check for translations

--- vue/src/components/thread/context_panel.vue
+++ vue/src/components/thread/context_panel.vue
@@ -56,7 +56,7 @@ export default
 
   h1.headline.context-panel__heading.px-3#sequence-0(v-observe-visibility="{callback: titleVisible}")
     span(v-if='!discussion.translation.title') {{discussion.title}}
-    span(v-if='discussion.translation')
+    span(v-if='discussion.translation.title')
       translation(:model='discussion', field='title')
     i.mdi.mdi-pin.context-panel__heading-pin(v-if="status == 'pinned'")
 
