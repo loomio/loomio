9c940deb9fc28b6877f5bcdc5618839fb7698578
Robert Guthrie <rob@guthrie.nz>
2019-08-16 16:24:43 +1200

    fix(sidebar) so it display orgs when you only belong to a subgroup

--- vue/src/components/common/sidebar.vue
+++ vue/src/components/common/sidebar.vue
@@ -25,6 +25,7 @@ export default
     group: null
     version: AppConfig.version.split('.').slice(-1)[0]
     myGroups: []
+    organizations: []
     unreadCounts: {}
 
   created: ->
@@ -40,14 +41,10 @@ export default
     @watchRecords
       collections: ['groups', 'memberships']
       query: (store) =>
-        if @organization
-          @myGroups = intersection Session.user().formalGroups(), @organization.subgroups()
+        @organizations = Session.user().parentGroups().concat(Session.user().orphanParents())
 
-        @groups = Session.user().parentGroups().concat(Session.user().orphanSubgroups()).filter (g) -> g.type == "FormalGroup"
-        # @groups = Session.user().formalGroups().filter (group) ->
-        #   group.isParent() or !Session.user().isMemberOf(group.parent())
-        # @ = uniq compact concat(groups, map(groups, (group) -> group.parent()))
-        @groups = sortBy @groups, 'fullName'
+        if @group
+          @myGroups = intersection Session.user().formalGroups(), @organization.subgroups()
 
     @watchRecords
       collections: ['groups', 'discussions']
@@ -69,27 +66,13 @@ export default
 
 
   methods:
-    sortGroups: (groups) -> sortBy groups, 'fullName'
-    availableGroups: -> Session.user().formalGroups()
-
-    currentGroup: ->
-      return head(@availableGroups()) if @availableGroups().length == 1
-      find(@availableGroups(), (g) => g.id == (AppConfig.currentGroup or {}).id) || Records.groups.build()
-
     unreadThreadCount: ->
       InboxService.unreadCount()
 
     canViewPublicGroups: -> AbilityService.canViewPublicGroups()
-    isCurrentOrganization: (group) ->
-      if AppConfig.currentGroup
-        AppConfig.currentGroup.parentOrSelf().id == group.id
 
   computed:
     user: -> Session.user()
-    parentGroups: -> Session.user().parentGroups()
-    siteName: -> AppConfig.theme.site_name
-    logoUrl: -> AppConfig.theme.app_logo_src
-    subgroups: -> @organization.subgroups()
     canStartSubGroup: -> true
 
 </script>
@@ -101,11 +84,14 @@ v-navigation-drawer.sidenav-left(app v-model="open")
     v-navigation-drawer(stateless mini-variant mini-variant-width="56" :value="open")
       v-list-item(to="/dashboard")
         v-list-item-avatar(:size="28")
-          user-avatar(no-link :user="user")
-      v-list-item(v-for='group in parentGroups' :key='group.id' :to='urlFor(group)' dense)
+          user-avatar(no-link :user="user" v-on="on")
+      v-list-item(v-for='group in organizations' :key='group.id' :to='urlFor(group)' dense)
         v-list-item-avatar(size="28px")
-          v-avatar(tile size="28px")
-            img.sidebar__list-item-group-logo(:src='group.logoUrl()')
+          v-tooltip
+            template(v-slot:activator="{ on }")
+              v-avatar(tile size="28px" v-on="on")
+                img.sidebar__list-item-group-logo(:src='group.logoUrl()')
+            span {{group.name}}
       v-list-item
         v-list-item-avatar(:size="28")
           v-icon(:size="28" tile) mdi-plus
@@ -130,13 +116,13 @@ v-navigation-drawer.sidenav-left(app v-model="open")
       div(v-if="myGroups.length > 0")
         v-divider
         v-list-item.sidebar__list-item-button--recent(dense exact :to='urlFor(organization, null, {subgroups: "mine"})')
-          v-list-item-title My groups
+          v-list-item-title(v-t="'sidebar.my_groups'")
         v-list-item(dense v-for="group in myGroups" :key="group.id" :to="urlFor(group)")
           v-list-item-title(v-if="unreadCounts[group.id]") {{ group.name }} ({{ unreadCounts[group.id] }})
           v-list-item-title(v-if="!unreadCounts[group.id]") {{ group.name }}
       v-divider
       v-list-item.sidebar__list-item-button--recent(dense exact :to="urlFor(organization, 'subgroups')")
-        v-list-item-title(v-t="'sidebar.all_groups'")
+        v-list-item-title(v-t="'sidebar.more_groups'")
 
       div(v-if="organization.subscriptionPlan == 'trial'")
         v-divider
