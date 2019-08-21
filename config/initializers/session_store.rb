891a5ac4b8e643740bf2306927615e4ce96f961a
Robert Guthrie <rob@guthrie.nz>
2019-08-22 08:34:03 +1200

    make secure cookie optional via FORCE_SSL env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
 # Be sure to restart your server when you modify this file.
-Loomio::Application.config.session_store :cookie_store, key: '_loomio', domain: ENV.fetch('SESSION_DOMAIN', ENV['CANONICAL_HOST']), secure: Rails.env.production?
+Loomio::Application.config.session_store :cookie_store, key: '_loomio', domain: ENV.fetch('SESSION_DOMAIN', ENV['CANONICAL_HOST']), secure: (ENV['FORCE_SSL'].to_i == 1)
