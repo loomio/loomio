99fb17a02f2e1379df0cfe47729d0568a3e61091
Robert Guthrie <rob@guthrie.nz>
2019-08-28 11:51:55 +1200

    run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_25_221208) do
+ActiveRecord::Schema.define(version: 2019_08_27_234738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
