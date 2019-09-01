a487a381124764ec87d0cf1a99d4f580475c837f
Robert Guthrie <rob@guthrie.nz>
2019-09-01 13:34:59 +1200

    start work on delux notifications experience

--- vue/src/components/common/change_volume_form.vue
+++ vue/src/components/common/change_volume_form.vue
@@ -12,28 +12,43 @@ export default
       default: true
       type: Boolean
   data: ->
+    includeInCatchUp: true
     volumeLevels: ["loud", "normal", "quiet"]
     isDisabled: false
-    applyToAll: if @model.isA('user') then true else false
-    volume: if @model.isA('user') then "" else @defaultVolume()
+    applyToAll: @defaultApplyToAll()
+    volume: @defaultVolume()
+
   mounted: ->
     @submit = submitForm @, @model,
       submitFn: (model) =>
         model.saveVolume(@volume, @applyToAll)
       flashSuccess: 'change_volume_form.saved'
       successCallback: => @closeModal()
+
+  computed:
+    formChanged: ->
+      (@volume != @defaultVolume()) || (@applyToAll != @defaultApplyToAll())
+
   methods:
+    defaultApplyToAll: ->
+      if @model.isA('user') then true else false
+
+    defaultVolume: ->
+      switch @model.constructor.singular
+        when 'discussion' then @model.volume()
+        when 'membership' then @model.volume
+        when 'user'       then null
+
+    labelFor: (volume) ->
+      @$t("change_volume_form.simple.#{volume}")+' - '+@$t("change_volume_form.simple.#{volume}_explain")
+
     translateKey: (key) ->
       if @model.isA('user')
         "change_volume_form.all_groups"
       else
         "change_volume_form.#{key || @model.constructor.singular}"
 
-    defaultVolume: ->
-      switch @model.constructor.singular
-        when 'discussion' then @model.volume()
-        when 'membership' then @model.volume
-        when 'user'       then @model.defaultMembershipVolume
+
     groupName: ->
       if @model.groupName
         @model.groupName()
@@ -42,6 +57,10 @@ export default
     openGroupVolumeModal: ->
       @closeModal()
       setTimeout => GroupService.actions(@model.group(), @).change_volume.perform()
+
+    openUserPreferences: ->
+      @closeModal()
+      @$router.push('/email_preferences')
 
 </script>
 <template lang="pug">
@@ -53,14 +72,20 @@ v-card.change-volume-form
       v-spacer
       dismiss-modal-button(v-if="showClose" :close="close")
     v-card-text
-      v-radio-group(v-model='volume')
-        v-radio(v-for='level in volumeLevels', :value='level', :class="'volume-' + level", :key="'volume-' + level", :label="$t(translateKey() + '.' + level + '_description')")
-      p(v-if="model.isA('discussion')")
-        span(v-t="'change_volume_form.discussion.only_this_thread'")
-        space
+      p.mt-4(v-if="model.isA('membership')")
+      v-radio-group(hide-details v-model='volume')
+        v-radio.volume-loud(value='loud' :label="labelFor('loud')")
+        v-radio.volume-normal(value='normal' :label="labelFor('normal')")
+        v-radio.volume-quiet(value='quiet' :label="labelFor('quiet')")
+
+      div(v-if="model.isA('membership') && model.group().parentOrSelf().hasSubgroups()")
+        v-checkbox#apply-to-all.mb-4(v-if="model.isA('membership')" v-model='applyToAll', :label="$t('change_volume_form.membership.apply_to_organization', { organization: model.group().parentOrSelf().name })" hide-details)
+
+      p.mt-4(v-if="model.isA('discussion')")
         a(@click="openGroupVolumeModal()" v-t="'change_volume_form.discussion.group'")
-      v-checkbox#apply-to-all.change-volume-form__apply-to-all(v-if="model.isA('membership')" v-model='applyToAll', :label="$t('change_volume_form.membership.apply_to_organization', { organization: model.group().parentOrSelf().name })")
-    v-card-actions
+      p.mt-4(v-if="model.isA('membership')")
+        a(@click="openUserPreferences()") Change user email preferences
+    v-card-actions(align-center)
       v-spacer
-      v-btn.change-volume-form__submit(type='button', :disabled='!volume', v-t="'common.action.update'" @click='submit()' color="primary")
+      v-btn.change-volume-form__submit(type='button', :disabled='!formChanged', v-t="'common.action.update'" @click='submit()' color="primary")
 </template>
