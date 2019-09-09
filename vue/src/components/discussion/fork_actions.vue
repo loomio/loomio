6b45c09225f8056e266c569c58d00a92bef73be9
Gregory Kan <kan.gregory@gmail.com>
2019-09-09 15:53:58 +1200

    feat(comment): fork actions shows two buttons

--- vue/src/components/discussion/fork_actions.vue
+++ vue/src/components/discussion/fork_actions.vue
@@ -1,15 +1,33 @@
 <script lang="coffee">
 import Records      from '@/shared/services/records.coffee'
-import DiscussionModalMixin from '@/mixins/discussion_modal.coffee'
+# import DiscussionModalMixin from '@/mixins/discussion_modal.coffee'
+import openModal      from '@/shared/helpers/open_modal'
 
 export default
-  mixins: [DiscussionModalMixin]
+  # mixins: [DiscussionModalMixin]
   props:
     discussion: Object
   methods:
     submit: ->
-      @openForkedDiscussionModal(@discussion)
-      @discussion.forkedEventIds = []
+      # @openForkedDiscussionModal(@discussion)
+      # @discussion.forkedEventIds = []
+      openModal
+        component: 'MoveCommentModal'
+        props:
+          discussion: @discussion
+
+      openEditDiscussionModal: ->
+        openModal
+          component: 'DiscussionForm'
+            props:
+              discussion: @discussion
+      openNewDiscussionModal: ->
+        openModal
+          component: 'DiscussionForm'
+            props:
+              discussion: Records.discussions.build
+                groupId:        @discussion.groupId
+                forkedEventIds: @discussion.forkedEventIds
 </script>
 
 <template lang='pug'>
@@ -18,5 +36,7 @@ v-banner.discussion-fork-actions(sticky v-model='discussion.isForking()' icon="m
   template(v-slot:actions)
     v-btn(@click='discussion.forkedEventIds = []')
       v-icon mdi-close
-    v-btn.discussion-fork-actions__submit(@click='submit()', v-t="'common.action.fork'" color="primary")
+    v-btn(@click="openEditDiscussionModal()") Move comments to existing thread
+    v-btn(@click="openNewDiscussionModal()") Move comments to new thread
+    //- v-btn.discussion-fork-actions__submit(@click='submit()', v-t="'common.action.fork'" color="primary")
 </template>
