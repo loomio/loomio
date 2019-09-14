0b89f044cbd37e10c9992d9b45739f1aa1dbd2c8
James Kiesel <james.kiesel@gmail.com>
2019-09-14 18:46:12 +1200

    Tweak to show_via_saml ability

--- app/models/ability/group.rb
+++ app/models/ability/group.rb
@@ -14,9 +14,9 @@ module Ability::Group
 
     can [:show_via_saml], ::Group do |group|
       if saml_url = group.parent_or_self.identity_for(:saml)&.saml_url
-        user.identity_for(:saml)&.saml_url == saml_url
+        user.saml_identity&.saml_url == saml_url
       else
-        user.ability.can?(:show, group)
+        true
       end
     end
 
