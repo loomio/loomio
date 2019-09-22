cb358053b4681b712fb157425dc418825a5ac0ac
Gregory Kan <kan.gregory@gmail.com>
2019-09-23 11:48:20 +1200

    chore(tooltips): added tooltips to text editor buttons

--- vue/src/components/thread/current_poll_banner.vue
+++ vue/src/components/thread/current_poll_banner.vue
@@ -36,7 +36,7 @@ v-card.current-poll-banner.mb-4.py-2.px-4.lmo-no-print(:style="styles" v-if="eve
     span {{poll.title}}
   v-spacer
   poll-common-closing-at.caption.mr-4(approximate :poll="poll")
-  v-btn(color="primary" :to="urlFor(event)" v-t="'poll_common.vote'")
+  v-btn(color="primary" :to="urlFor(event)" v-t="'poll_common.view'")
 </template>
 
 <style lang="sass">
