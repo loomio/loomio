364693127216396f6eaeaf3a5f1bea4c2fab2888
Gregory Kan <kan.gregory@gmail.com>
2019-09-25 09:11:04 +1200

    fix(e2e): fix dev setup

--- app/controllers/dev/polls_controller.rb
+++ app/controllers/dev/polls_controller.rb
@@ -16,16 +16,16 @@ class Dev::PollsController < Dev::BaseController
 
   def test_discussion
     group = create_group_with_members
-    discussion = saved fake_discussion(group: group)
-    DiscussionService.create(discussion: discussion, actor: discussion.author)
     sign_in group.admins.first
+    discussion = saved fake_discussion(group: group, author: group.admins.first)
+    DiscussionService.create(discussion: discussion, actor: discussion.author)
     redirect_to discussion_path(discussion)
   end
 
   def test_poll_in_discussion
     group = create_group_with_members
     sign_in group.admins.first
-    discussion = saved fake_discussion(group: group)
+    discussion = saved fake_discussion(group: group, author: group.admins.first)
     DiscussionService.create(discussion: discussion, actor: discussion.author)
     poll = saved fake_poll(discussion: discussion)
     stance = saved fake_stance(poll: poll)
