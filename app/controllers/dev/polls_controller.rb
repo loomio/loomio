bf1db7ffd2d84ba0d7ea556b8f909f1a671a4a8c
Gregory Kan <kan.gregory@gmail.com>
2019-09-24 14:40:59 +1200

    fix(dev): discussion service

--- app/controllers/dev/polls_controller.rb
+++ app/controllers/dev/polls_controller.rb
@@ -17,6 +17,7 @@ class Dev::PollsController < Dev::BaseController
   def test_discussion
     group = create_group_with_members
     discussion = saved fake_discussion(group: group)
+    DiscussionService.create(discussion: discussion, actor: discussion.author)
     sign_in group.admins.first
     redirect_to discussion_path(discussion)
   end
@@ -25,6 +26,7 @@ class Dev::PollsController < Dev::BaseController
     group = create_group_with_members
     sign_in group.admins.first
     discussion = saved fake_discussion(group: group)
+    DiscussionService.create(discussion: discussion, actor: discussion.author
     poll = saved fake_poll(discussion: discussion)
     stance = saved fake_stance(poll: poll)
     StanceService.create(stance: stance, actor: group.members.last)
@@ -48,6 +50,7 @@ class Dev::PollsController < Dev::BaseController
     group = saved fake_group
     group.add_admin! user
     discussion = saved fake_discussion(group: group)
+    DiscussionService.create(discussion: discussion, actor: discussion.author
 
     sign_in user
     create_activity_items(discussion: discussion, actor: user)
