3080a8fcca17cc747594f833a83b6aa202a88ccf
Robert Guthrie <rob@guthrie.nz>
2019-10-07 21:38:04 +1300

    remove circular indicator from thread page

--- vue/src/components/common/loading.vue
+++ vue/src/components/common/loading.vue
@@ -9,7 +9,7 @@ export default
 
 <template lang="pug">
 div
-  v-layout.page-loading(justify-center v-if="!until")
+  v-layout.my-4.page-loading(justify-center v-if="!until")
     v-progress-circular(indeterminate color='primary')
   slot(v-if="until")
 </template>
--- vue/src/components/thread/page.vue
+++ vue/src/components/thread/page.vue
@@ -1,23 +1,11 @@
 <script lang="coffee">
-import LmoUrlService     from '@/shared/services/lmo_url_service'
-import Session           from '@/shared/services/session'
 import Records           from '@/shared/services/records'
-import RecordLoader      from '@/shared/services/record_loader'
 import EventBus          from '@/shared/services/event_bus'
 import AbilityService    from '@/shared/services/ability_service'
-import WatchRecords from '@/mixins/watch_records'
-
-import { registerKeyEvent } from '@/shared/helpers/keyboard'
-import {compact} from 'lodash'
 
 export default
-  mixins: [WatchRecords]
-
   data: ->
     discussion: null
-    activePolls: []
-    per: 25
-    threadPercentage: 0
 
   created: -> @init()
 
@@ -26,12 +14,7 @@ export default
     '$route.params.comment_id': 'init'
 
   methods:
-    openThreadNav: -> EventBus.$emit('toggleThreadNav')
-
     init: ->
-      EventBus.$on 'threadPositionUpdated', (position) =>
-        @threadPercentage = parseInt(position / @discussion.createdEvent().childCount * 100)
-
       Records.discussions.findOrFetchById(@$route.params.key).then (discussion) =>
           @discussion = discussion
           EventBus.$emit 'currentComponent',
@@ -52,6 +35,4 @@ loading(:until="discussion")
     thread-current-poll-banner(:discussion="discussion")
     discussion-fork-actions(:discussion='discussion' v-show='discussion.isForking' :key="'fork-actions'+ discussion.id")
     thread-card(:discussion='discussion' :key="discussion.id")
-    v-btn.thread-page__open-thread-nav(fab fixed bottom right @click="openThreadNav()")
-      v-progress-circular(color="accent" :value="threadPercentage")
 </template>
