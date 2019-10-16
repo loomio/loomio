013d432cbbbba1afe93a100b5fd2ded90e4d4945
Robert Guthrie <rob@guthrie.nz>
2019-10-16 16:33:36 +1300

    support svg assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
 Rails.application.config.assets.precompile += %w(basic/all.css
   active_admin.css slack.css active_admin.js active_admin/print.css angular_ahoy.js
   marketing.js email.css poll_mailer.css marketing/index.scss admin.css admin.js marketing/marketing.js
-  *.png *.jpg *.jpeg *.gif)
+  *.png *.jpg *.jpeg *.gif *.svg)
