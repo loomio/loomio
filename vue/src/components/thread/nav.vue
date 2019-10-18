d93fd6bc10f37b1c4a4db6be9dad0dbd060a6334
Robert Guthrie <rob@guthrie.nz>
2019-10-18 21:30:47 +1300

    we have a slider

--- vue/src/components/thread/nav.vue
+++ vue/src/components/thread/nav.vue
@@ -1,132 +1,75 @@
 <script lang="coffee">
 import EventBus from '@/shared/services/event_bus'
 import Records from '@/shared/services/records'
-import AnnouncementModalMixin from '@/mixins/announcement_modal'
 import ChangeVolumeModalMixin from '@/mixins/change_volume_modal'
 import { debounce, truncate } from 'lodash'
 
 export default
-  mixins: [AnnouncementModalMixin, ChangeVolumeModalMixin]
+  mixins: [ChangeVolumeModalMixin]
   data: ->
     discussion: null
     open: null
-    keyEvents: []
-    inversePosition: 0
+    offset: 0
+    max: 200
+    dragging: false
 
   mounted: ->
-    # threadPositionRequest (use slides the slider, tell others)
-    # threadPositionUpdated (slider position needs updating)
-    EventBus.$on 'toggleThreadNav', => @open = !@open
-    EventBus.$on 'threadPositionUpdated', debounce (position) =>
-      @inversePosition = 0 - position
-    ,
-      250
-
     EventBus.$on 'currentComponent', (options) =>
       @discussion = options.discussion
       return unless @discussion
 
-      Records.events.fetch
-        params:
-          discussion_id: @discussion.id
-          pinned: true
-          per: 200
-
-      @watchRecords
-        key: @discussion.id
-        collections: ["events"]
-        query: (records) =>
-          return unless @discussion
-          @keyEvents = records.events.collection.chain()
-                        .find({
-                          pinned: true
-                          discussionId: @discussion.id
-                        })
-                        .simplesort('sequenceId')
-                        .data()
-  computed:
-    childCount: -> @discussion.createdEvent().childCount
-    position: -> 0 - @inversePosition
-    thumbLabel: -> "#{@position} / #{@childCount}"
+      # Records.events.fetch
+      #   params:
+      #     discussion_id: @discussion.id
+      #     pinned: true
+      #     per: 200
 
   methods:
-    emitPosition: ->
-      EventBus.$emit 'threadPositionRequest', @position
+    onMouseDown: ->
+      onMouseMove = (event) =>
+        offset = event.clientY - @$refs.slider.getBoundingClientRect().top;
 
-    scrollTo: (selector) ->
-      @$vuetify.goTo(selector, duration: 0)
-
-    title: (model) ->
-      if model.title or model.statement
-        truncate (model.title || model.statement || model.body).replace(///<[^>]*>?///gm, ''), 50
-      else
-        parser = new DOMParser()
-        doc = parser.parseFromString(model.statement || model.body, 'text/html')
-        if el = doc.querySelector('h1,h2,h3')
-          el.textContent
+        if offset < 0
+          @offset = 0
+        else if offset > @max
+          @offset = @max
         else
-          truncate (model.body).replace(///<[^>]*>?///gm, ''), 50
+          @offset = offset
 
-    refreshThread: debounce ->
-      EventBus.$emit('updateThreadPosition', 0 - @requestedPosition)
-    ,
-      250
+      onMouseUp = ->
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
 
-    addPeople: ->
-      records = Records
-      announcement = Records.announcements.buildFromModel(@discussion)
-      @openAnnouncementModal(Records.announcements.buildFromModel(@discussion))
+      document.addEventListener 'mousemove', onMouseMove
+      document.addEventListener 'mouseup', onMouseUp
+
+    dragend: ->
+      @dragging = false
 
 </script>
 
 <template lang="pug">
-v-navigation-drawer.lmo-no-print(v-if="discussion" v-model="open" :permanent="$vuetify.breakpoint.mdAndUp" width="210px" app fixed right clipped)
-  .thread-nav
-    v-list(dense)
-      v-list-item(:to="urlFor(discussion)" @click="scrollTo('#context')")
-        v-list-item-avatar(:size="20")
-          v-icon(:size="20") mdi-format-vertical-align-top
-        v-list-item-title(v-t="'activity_card.beginning'")
-      //- v-slider(color="accent" track-color="accent" thumb-color="accent" thumb-size="64" v-model="inversePosition" vertical :max="0" :min="0 - discussion.createdEvent().childCount" thumb-label @change="emitPosition()")
-      //-   template(v-slot:thumb-label)
-      //-     | {{thumbLabel}}
-      v-list-item(:to="urlFor(discussion)+'/'+(discussion.firstUnreadSequenceId() || '')" :disabled="!discussion.isUnread()")
-        v-list-item-avatar(:size="20")
-          v-icon(:size="20") mdi-bookmark-outline
-        v-list-item-title(v-t="{path: 'activity_card.unread', args: {count: discussion.unreadItemsCount()}}")
-      v-list-item(v-for="event in keyEvents" :key="event.id" :to="urlFor(discussion)+'/'+event.sequenceId")
-        v-list-item-avatar(:size="20")
-          user-avatar(v-if="event.kind == 'new_comment'" :user="event.actor()" :size="20")
-          poll-common-chart-preview(v-if="event.kind == 'poll_created'" :poll='event.model()' :size="20" :showMyStance="false")
-        v-list-item-title
-          span {{title(event.model())}}
-      v-list-item(@click="scrollTo('#add-comment')")
-        v-list-item-avatar(:size="20")
-          v-icon(:size="20") mdi-format-vertical-align-bottom
-        v-list-item-title(v-t="'activity_card.end'")
-    v-divider
-
-    v-list(dense)
-      v-list-item(two-line @click="openChangeVolumeModal(discussion)")
-        v-list-item-content
-          v-list-item-title(v-t="'change_volume_form.simple.label'")
-          v-list-item-subtitle
-            span(v-t="'change_volume_form.simple.' + discussion.volume()")
-          //- v-list-item-subtitle
-          //-   span(v-t="'change_volume_form.simple.' + discussion.volume() + '_explain'")
-
-
-
-    //- v-list(dense)
-    //-   v-subheader Navigation
-    //-   v-list-item(:to="urlFor(discussion)")
-    //-     v-list-item-title Context
-    //-   v-list-item(:to="urlFor(discussion)+'/'+discussion.firstSequenceId()" :disabled="!discussion.firstSequenceId()")
-    //-     v-list-item-title First
-    //-   v-list-item(:disabled="!discussion.firstUnreadSequenceId()" :to="urlFor(discussion)+'/'+discussion.firstUnreadSequenceId()")
-    //-     v-list-item-title Unread
-    //-   v-list-item(:to="urlFor(discussion)+'/'+discussion.lastSequenceId()" :disabled="!discussion.lastSequenceId()")
-    //-     v-list-item-title Latest
-    //-   v-list-item(@click="scrollTo('.activity-panel__actions')")
-    //-     v-list-item-title Add comment
+v-navigation-drawer.thread-nav.lmo-no-print(v-if="discussion" :permanent="$vuetify.breakpoint.mdAndUp" width="210px" app fixed right clipped)
+  | offset {{offset}}
+  .thread-nav__track(ref="slider")
+    .thread-nav__knob(:class="{'thread-nav__knob-hide': dragging}" ref="knob" @mousedown="onMouseDown" :style="{top: offset+'px'}")
 </template>
+
+<style lang="sass">
+.thread-nav__knob-hide
+  background-color: red !important
+  transform: translateX(-9999px)
+
+.thread-nav__track
+  height: 200px
+  border: 1px solid black
+  width: 0
+  margin: 16px
+
+.thread-nav__knob
+  position: relative
+  height: 16px
+  width: 24px
+  background-color: #999
+  cursor: ns-resize
+</style>
