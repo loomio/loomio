c4445bc12a5ee016c9586bf5517c34433cc2544f
Robert Guthrie <rob@guthrie.nz>
2019-10-18 21:57:15 +1300

    bit better

--- vue/src/components/thread/nav.vue
+++ vue/src/components/thread/nav.vue
@@ -3,6 +3,7 @@ import EventBus from '@/shared/services/event_bus'
 import Records from '@/shared/services/records'
 import ChangeVolumeModalMixin from '@/mixins/change_volume_modal'
 import { debounce, truncate } from 'lodash'
+import { approximate } from '@/shared/helpers/format_time'
 
 export default
   mixins: [ChangeVolumeModalMixin]
@@ -10,7 +11,8 @@ export default
     discussion: null
     open: null
     offset: 0
-    max: 200
+    max: 300
+    thumbSize: 64
     dragging: false
 
   mounted: ->
@@ -46,30 +48,31 @@ export default
     dragend: ->
       @dragging = false
 
+  computed:
+    topDate: -> approximate(@discussion.createdAt)
+    bottomDate: -> approximate(@discussion.lastActivityAt)
+
 </script>
 
 <template lang="pug">
 v-navigation-drawer.thread-nav.lmo-no-print(v-if="discussion" :permanent="$vuetify.breakpoint.mdAndUp" width="210px" app fixed right clipped)
-  | offset {{offset}}
-  .thread-nav__track(ref="slider")
-    .thread-nav__knob(:class="{'thread-nav__knob-hide': dragging}" ref="knob" @mousedown="onMouseDown" :style="{top: offset+'px'}")
+  p offset {{offset}}
+  span {{topDate}}
+  .thread-nav__track(ref="slider" :style="{height: max+thumbSize+'px'}")
+    .thread-nav__knob(:style="{top: offset+'px', height: thumbSize+'px'}" :class="{'thread-nav__knob-hide': dragging}" ref="knob" @mousedown="onMouseDown")
+  span {{bottomDate}}
 </template>
 
 <style lang="sass">
-.thread-nav__knob-hide
-  background-color: red !important
-  transform: translateX(-9999px)
-
 .thread-nav__track
-  height: 200px
   border: 1px solid black
   width: 0
   margin: 16px
 
 .thread-nav__knob
   position: relative
-  height: 16px
-  width: 24px
+  left: -8px
+  width: 16px
   background-color: #999
   cursor: ns-resize
 </style>
