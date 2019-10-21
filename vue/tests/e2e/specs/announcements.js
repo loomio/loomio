b818254268cb6980e16217214c31afc1b8e6e282
Gregory Kan <kan.gregory@gmail.com>
2019-10-22 10:26:48 +1300

    comment out all e2e suites except for auth for now

--- vue/tests/e2e/specs/announcements.js
+++ vue/tests/e2e/specs/announcements.js
@@ -1,111 +1,111 @@
-require('coffeescript/register')
-pageHelper = require('../helpers/pageHelper.coffee')
-
-module.exports = {
-  'invite_to_group': (test) => {
-    page = pageHelper(test)
-    page.loadPath('setup_group')
-    page.click('.group-page-members-tab')
-    page.click('.membership-card__invite', 1000)
-    page.fillIn('.announcement-form__input input', 'test@example.com')
-    page.expectText('.announcement-chip__content', 'test@example.com')
-    page.click('.announcement-chip__content')
-    page.click('.headline')
-    page.click('.announcement-form__submit')
-    page.expectFlash('1 notifications sent')
-  },
-
-  'new_discussion': (test) => {
-    page = pageHelper(test)
-
-    page.loadPath('setup_group')
-    page.click('.discussions-panel__new-thread-button')
-    page.fillIn('.discussion-form__title-input input', 'Immannounce dis')
-    page.click('.discussion-form__submit')
-    page.expectFlash('Thread started')
-    page.expectElement('.announcement-form')
-    page.click('.announcement-form__audience')
-    page.click('.announcement-form__submit', 1000)
-    page.expectFlash('2 notifications sent')
-  },
-
-  'discussion_edited': (test) => {
-    page = pageHelper(test)
-
-    page.loadPath('setup_discussion')
-    page.click('.action-dock__button--edit_thread')
-    page.fillIn('.discussion-form__title-input', 'Yo reliability, whatsup? Its me, ya boi, testing')
-    page.click('.discussion-form__submit')
-    page.expectElement('.announcement-form')
-    page.click('.announcement-form__audience')
-    page.click('.announcement-form__submit')
-    page.expectFlash('2 notifications sent')
-  },
-
-  // TODO: GK: need to 'ensure' the thread-nav
-  'announcement_created': (test) => {
-    page = pageHelper(test)
-
-    page.loadPath('setup_discussion')
-    page.pause(500)
-    page.ensureThreadNav()
-    page.click('.thread-nav__add-people')
-    page.expectElement('.announcement-form')
-    page.pause(500)
-    page.fillIn('.announcement-form__input input', 'test@example.com')
-    page.expectText('.announcement-chip__content', 'test@example.com')
-    page.click('.announcement-chip__content')
-    page.click('.headline')
-    page.click('.announcement-form__submit')
-    page.expectFlash('1 notifications sent')
-  },
-
-  'poll_created': (test) => {
-    page = pageHelper(test)
-
-    page.loadPath('setup_discussion')
-    page.click('.activity-panel__add-poll')
-    page.click('.decision-tools-card__poll-type--proposal')
-    page.fillIn('.poll-common-form-fields__title input', 'A new proposal')
-    page.fillIn('.poll-common-form-fields .ProseMirror', 'Some details')
-    page.click('.poll-common-form__submit')
-    page.expectFlash('Proposal started')
-    page.expectElement('.announcement-form')
-    page.pause(500)
-    page.fillIn('.announcement-form__input input', 'test@example.com')
-    page.expectText('.announcement-chip__content', 'test@example.com')
-    page.click('.announcement-chip__content')
-    page.click('.headline')
-    page.click('.announcement-form__submit', 1000)
-    page.expectFlash('1 notifications sent')
-  },
-
-  'poll_edited': (test) => {
-    page = pageHelper(test)
-
-    page.loadPath('test_poll_in_discussion', { controller: 'polls' })
-    page.click('.action-dock__button--edit_poll')
-    page.fillIn('.poll-common-form-fields__title', 'Yo reliability, whatsup? Its me, ya boi, testing')
-    page.click('.poll-common-form__submit')
-    page.expectElement('.announcement-form')
-    page.click('.announcement-form__audience')
-    page.click('.announcement-form__submit')
-    page.expectFlash('1 notifications sent')
-  },
-
-  // 'outcome_created': (test) => {
-  //   page = pageHelper(test)
-  //
-  //   page.loadPath('test_proposal_poll_closed', { controller: 'polls' })
-  //   page.click('.poll-common-set-outcome-panel__submit')
-  //   page.fillIn('.poll-common-outcome-form__statement textarea', 'Immannounce all yall')
-  //   page.click('.poll-common-outcome-form__submit')
-  //   page.expectFlash('Outcome created')
-  //   page.expectElement('.announcement-form')
-  //   page.selectFromAutocomplete('.announcement-form__invite input', 'test@example.com')
-  //   page.expectText('.announcement-chip__content', 'test@example.com')
-  //   page.click('.announcement-form__submit')
-  //   page.pause(3000)
-  //   page.expectFlash('1 notifications sent')
-  // }
-}
+// require('coffeescript/register')
+// pageHelper = require('../helpers/pageHelper.coffee')
+//
+// module.exports = {
+//   'invite_to_group': (test) => {
+//     page = pageHelper(test)
+//     page.loadPath('setup_group')
+//     page.click('.group-page-members-tab')
+//     page.click('.membership-card__invite', 1000)
+//     page.fillIn('.announcement-form__input input', 'test@example.com')
+//     page.expectText('.announcement-chip__content', 'test@example.com')
+//     page.click('.announcement-chip__content')
+//     page.click('.headline')
+//     page.click('.announcement-form__submit')
+//     page.expectFlash('1 notifications sent')
+//   },
+//
+//   'new_discussion': (test) => {
+//     page = pageHelper(test)
+//
+//     page.loadPath('setup_group')
+//     page.click('.discussions-panel__new-thread-button')
+//     page.fillIn('.discussion-form__title-input input', 'Immannounce dis')
+//     page.click('.discussion-form__submit')
+//     page.expectFlash('Thread started')
+//     page.expectElement('.announcement-form')
+//     page.click('.announcement-form__audience')
+//     page.click('.announcement-form__submit', 1000)
+//     page.expectFlash('2 notifications sent')
+//   },
+//
+//   'discussion_edited': (test) => {
+//     page = pageHelper(test)
+//
+//     page.loadPath('setup_discussion')
+//     page.click('.action-dock__button--edit_thread')
+//     page.fillIn('.discussion-form__title-input', 'Yo reliability, whatsup? Its me, ya boi, testing')
+//     page.click('.discussion-form__submit')
+//     page.expectElement('.announcement-form')
+//     page.click('.announcement-form__audience')
+//     page.click('.announcement-form__submit')
+//     page.expectFlash('2 notifications sent')
+//   },
+//
+//   // TODO: GK: need to 'ensure' the thread-nav
+//   'announcement_created': (test) => {
+//     page = pageHelper(test)
+//
+//     page.loadPath('setup_discussion')
+//     page.pause(500)
+//     page.ensureThreadNav()
+//     page.click('.thread-nav__add-people')
+//     page.expectElement('.announcement-form')
+//     page.pause(500)
+//     page.fillIn('.announcement-form__input input', 'test@example.com')
+//     page.expectText('.announcement-chip__content', 'test@example.com')
+//     page.click('.announcement-chip__content')
+//     page.click('.headline')
+//     page.click('.announcement-form__submit')
+//     page.expectFlash('1 notifications sent')
+//   },
+//
+//   'poll_created': (test) => {
+//     page = pageHelper(test)
+//
+//     page.loadPath('setup_discussion')
+//     page.click('.activity-panel__add-poll')
+//     page.click('.decision-tools-card__poll-type--proposal')
+//     page.fillIn('.poll-common-form-fields__title input', 'A new proposal')
+//     page.fillIn('.poll-common-form-fields .ProseMirror', 'Some details')
+//     page.click('.poll-common-form__submit')
+//     page.expectFlash('Proposal started')
+//     page.expectElement('.announcement-form')
+//     page.pause(500)
+//     page.fillIn('.announcement-form__input input', 'test@example.com')
+//     page.expectText('.announcement-chip__content', 'test@example.com')
+//     page.click('.announcement-chip__content')
+//     page.click('.headline')
+//     page.click('.announcement-form__submit', 1000)
+//     page.expectFlash('1 notifications sent')
+//   },
+//
+//   'poll_edited': (test) => {
+//     page = pageHelper(test)
+//
+//     page.loadPath('test_poll_in_discussion', { controller: 'polls' })
+//     page.click('.action-dock__button--edit_poll')
+//     page.fillIn('.poll-common-form-fields__title', 'Yo reliability, whatsup? Its me, ya boi, testing')
+//     page.click('.poll-common-form__submit')
+//     page.expectElement('.announcement-form')
+//     page.click('.announcement-form__audience')
+//     page.click('.announcement-form__submit')
+//     page.expectFlash('1 notifications sent')
+//   },
+//
+//   // 'outcome_created': (test) => {
+//   //   page = pageHelper(test)
+//   //
+//   //   page.loadPath('test_proposal_poll_closed', { controller: 'polls' })
+//   //   page.click('.poll-common-set-outcome-panel__submit')
+//   //   page.fillIn('.poll-common-outcome-form__statement textarea', 'Immannounce all yall')
+//   //   page.click('.poll-common-outcome-form__submit')
+//   //   page.expectFlash('Outcome created')
+//   //   page.expectElement('.announcement-form')
+//   //   page.selectFromAutocomplete('.announcement-form__invite input', 'test@example.com')
+//   //   page.expectText('.announcement-chip__content', 'test@example.com')
+//   //   page.click('.announcement-form__submit')
+//   //   page.pause(3000)
+//   //   page.expectFlash('1 notifications sent')
+//   // }
+// }
--- vue/tests/e2e/specs/emailSettings.js
+++ vue/tests/e2e/specs/emailSettings.js
@@ -1,86 +1,86 @@
-require('coffeescript/register')
-pageHelper = require('../helpers/pageHelper.coffee')
-
-module.exports = {
-  'lets_you_update_email_settings_while_logged_in': (test) => {
-    page = pageHelper(test)
-
-    page.loadPath('setup_group')
-    page.ensureSidebar()
-    page.click('.sidebar__user-dropdown')
-    page.click('.user-dropdown__list-item-button--email-settings')
-    testUpdate(page)
-  },
-
-  'lets_you_set_default_email_settings_for_all_new_memberships_while_logged_in': (test) => {
-    page = pageHelper(test)
-
-    page.loadPath('setup_group')
-    page.ensureSidebar()
-    page.click('.sidebar__user-dropdown')
-    page.click('.user-dropdown__list-item-button--email-settings')
-    testDefaultUpdate(page)
-  },
-
-  'lets_you_update_email_settings_for_all_current_memberships_while_logged_in': (test) => {
-    page = pageHelper(test)
-
-    page.loadPath('setup_group')
-    page.ensureSidebar()
-    page.click('.sidebar__user-dropdown')
-    page.click('.user-dropdown__list-item-button--email-settings')
-    testMembershipUpdate(page)
-  },
-
-  'lets_you_update_email_settings_while_logged_out': (test) => {
-    page = pageHelper(test)
-
-    page.loadPath('email_settings_as_restricted_user')
-    testUpdate(page)
-  },
-
-  'lets_you_set_default_email_settings_for_all_new_memberships_while_logged_out': (test) => {
-    page = pageHelper(test)
-
-    page.loadPath('email_settings_as_restricted_user')
-    testDefaultUpdate(page)
-  },
-
-  'lets_you_update_email_settings_for_all_current_memberships_while_logged_out': (test) => {
-    page = pageHelper(test)
-
-    page.loadPath('email_settings_as_restricted_user')
-    testMembershipUpdate(page)
-  },
-
-  'displays_the_email_settings_as_a_restricted_user': (test) => {
-    page = pageHelper(test)
-
-    page.loadPath('email_settings_as_logged_in_user')
-    page.expectNoElement('.navbar__sign-in')
-  }
-}
-
-testUpdate = (page) => {
-  page.click('.email-settings-page__daily-summary label')
-  page.click('.email-settings-page__update-button')
-  page.expectFlash('Email settings updated')
-}
-
-testDefaultUpdate = (page) => {
-  page.click('.email-settings-page__change-default-link')
-  page.expectText('.change-volume-form__title', 'Email settings for new groups')
-  page.click('.volume-loud')
-  page.click('.change-volume-form__submit')
-  page.expectFlash('You will be emailed all activity in new groups.')
-  page.expectText('.email-settings-page__default-description', 'When you join a new group, you will be emailed whenever there is activity.')
-}
-
-testMembershipUpdate = (page) => {
-  page.click('.email-settings-page__change-default-link')
-  page.click('.volume-loud')
-  page.click('.change-volume-form__apply-to-all .v-input--selection-controls__input')
-  page.click('.change-volume-form__submit')
-  page.expectFlash('You will be emailed all activity in all your groups.')
-  page.expectText('.email-settings-page__membership-volume', 'All activity')
-}
+// require('coffeescript/register')
+// pageHelper = require('../helpers/pageHelper.coffee')
+//
+// module.exports = {
+//   'lets_you_update_email_settings_while_logged_in': (test) => {
+//     page = pageHelper(test)
+//
+//     page.loadPath('setup_group')
+//     page.ensureSidebar()
+//     page.click('.sidebar__user-dropdown')
+//     page.click('.user-dropdown__list-item-button--email-settings')
+//     testUpdate(page)
+//   },
+//
+//   'lets_you_set_default_email_settings_for_all_new_memberships_while_logged_in': (test) => {
+//     page = pageHelper(test)
+//
+//     page.loadPath('setup_group')
+//     page.ensureSidebar()
+//     page.click('.sidebar__user-dropdown')
+//     page.click('.user-dropdown__list-item-button--email-settings')
+//     testDefaultUpdate(page)
+//   },
+//
+//   'lets_you_update_email_settings_for_all_current_memberships_while_logged_in': (test) => {
+//     page = pageHelper(test)
+//
+//     page.loadPath('setup_group')
+//     page.ensureSidebar()
+//     page.click('.sidebar__user-dropdown')
+//     page.click('.user-dropdown__list-item-button--email-settings')
+//     testMembershipUpdate(page)
+//   },
+//
+//   'lets_you_update_email_settings_while_logged_out': (test) => {
+//     page = pageHelper(test)
+//
+//     page.loadPath('email_settings_as_restricted_user')
+//     testUpdate(page)
+//   },
+//
+//   'lets_you_set_default_email_settings_for_all_new_memberships_while_logged_out': (test) => {
+//     page = pageHelper(test)
+//
+//     page.loadPath('email_settings_as_restricted_user')
+//     testDefaultUpdate(page)
+//   },
+//
+//   'lets_you_update_email_settings_for_all_current_memberships_while_logged_out': (test) => {
+//     page = pageHelper(test)
+//
+//     page.loadPath('email_settings_as_restricted_user')
+//     testMembershipUpdate(page)
+//   },
+//
+//   'displays_the_email_settings_as_a_restricted_user': (test) => {
+//     page = pageHelper(test)
+//
+//     page.loadPath('email_settings_as_logged_in_user')
+//     page.expectNoElement('.navbar__sign-in')
+//   }
+// }
+//
+// testUpdate = (page) => {
+//   page.click('.email-settings-page__daily-summary label')
+//   page.click('.email-settings-page__update-button')
+//   page.expectFlash('Email settings updated')
+// }
+//
+// testDefaultUpdate = (page) => {
+//   page.click('.email-settings-page__change-default-link')
+//   page.expectText('.change-volume-form__title', 'Email settings for new groups')
+//   page.click('.volume-loud')
+//   page.click('.change-volume-form__submit')
+//   page.expectFlash('You will be emailed all activity in new groups.')
+//   page.expectText('.email-settings-page__default-description', 'When you join a new group, you will be emailed whenever there is activity.')
+// }
+//
+// testMembershipUpdate = (page) => {
+//   page.click('.email-settings-page__change-default-link')
+//   page.click('.volume-loud')
+//   page.click('.change-volume-form__apply-to-all .v-input--selection-controls__input')
+//   page.click('.change-volume-form__submit')
+//   page.expectFlash('You will be emailed all activity in all your groups.')
+//   page.expectText('.email-settings-page__membership-volume', 'All activity')
+// }
