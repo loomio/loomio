3d02353982b4e40f233ec9a48385c212c663f457
Gregory Kan <kan.gregory@gmail.com>
2019-10-23 12:43:23 +1300

    notifications e2es

--- vue/tests/e2e/specs/notifications.js
+++ vue/tests/e2e/specs/notifications.js
@@ -1,29 +1,30 @@
-// require('coffeescript/register')
-// pageHelper = require('../helpers/pageHelper.coffee')
-//
-// notificationTexts = [
-//   'accepted your invitation to join',
-//   'added you to the group',
-//   'approved your request',
-//   'requested membership to',
-//   'mentioned you in',
-//   'replied to your comment',
-//   'shared an outcome',
-//   'Poll is closing soon',
-//   'started a Poll',
-//   'reacted to your comment',
-//   'made you a coordinator',
-//   'participated in',
-//   'added options to'
-// ]
-//
-// module.exports = {
-//   'has all the notifications': (test) => {
-//     page = pageHelper(test)
-//
-//     page.loadPath('setup_all_notifications')
-//     page.expectText('.notifications__activity', notificationTexts.length, 100000)
-//     page.click('.notifications__button')
-//     notificationTexts.map((text) => { page.expectText('.notifications__dropdown', text) })
-//   }
-// }
+require('coffeescript/register')
+pageHelper = require('../helpers/pageHelper.coffee')
+
+notificationTexts = [
+  'accepted your invitation to join',
+  'added you to the group',
+  'approved your request',
+  'requested membership to',
+  'mentioned you in',
+  'replied to your comment',
+  'shared an outcome',
+  'poll is closing soon',
+  'started a poll',
+  'reacted 😄 to your comment',
+  'made you an admin',
+  'participated in',
+  'added options to'
+]
+
+module.exports = {
+  'has all the notifications': (test) => {
+    page = pageHelper(test)
+
+    page.loadPath('setup_all_notifications')
+    page.pause(500)
+    page.expectText('.notifications__activity', notificationTexts.length, 100000)
+    page.click('.notifications__button')
+    notificationTexts.map((text) => { page.expectText('.notifications__dropdown', text) })
+  }
+}
