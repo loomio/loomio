0ee4ff218cca55f72c6ac4874943eb68d49db167
Robert Guthrie <rob@guthrie.nz>
2019-10-24 08:35:10 +1300

    timeline is great! proportional to thread, animated, mostly correct

--- vue/src/components/thread/nav.vue
+++ vue/src/components/thread/nav.vue
@@ -14,53 +14,90 @@ export default
     discussion: null
     open: null
     knobOffset: 0
-    max: 300
-    thumbSize: 32
-    dragging: false
-    position: 0
-    mouseY: null
-    boxY: null
+    knobHeight: 32
+    trackHeight: 100
+    position: 1
+    unitHeight: 1
 
   mounted: ->
     EventBus.$on 'currentComponent', (options) =>
       @discussion = options.discussion
       return unless @discussion
 
+      if @discussion.newestFirst
+        @topPosition = @discussion.createdEvent().childCount
+        @topDate = @discussion.lastActivityAt
+
+        @bottomPosition = 1
+        @bottomDate = @discussion.createdAt
+      else
+        @topPosition = 1
+        @topDate = @discussion.createdAt
+
+        @bottomDate = @discussion.lastActivityAt
+        @bottomPosition = @discussion.createdEvent().childCount
+
+      @watchRecords
+        key: 'thread-nav'+@discussion.id
+        collections: ["events"]
+        query: =>
+          return unless @discussion
+          @presets = []
+          @presets.push({position: @topPosition, title: timeline(@topDate)})
+
+          Records.events.collection.chain()
+            .find({pinned: true, parentId: @discussion.createdEvent().id})
+            .simplesort('position').data().forEach (event) =>
+              @presets.push({position: event.position, title: @title(event.model())})
+
+          @presets.push({position: @bottomPosition, title: timeline(@bottomDate)})
+
+      Records.events.fetch
+        params:
+          discussion_id: @discussion.id
+          pinned: true
+          per: 200
+
     EventBus.$on 'visibleSlots', (slots) =>
       unless slots.length == 0 && @discussion
-        totalSlots = @discussion.createdEvent().childCount
-        @knobOffset = @offsetFor(first(slots))
+        if @discussion.newestFirst
+          @position = last(slots) || 1
+        else
+          @position = first(slots) || 1
 
-      # Records.events.fetch
-      #   params:
-      #     discussion_id: @discussion.id
-      #     pinned: true
-      #     per: 200
+        @trackHeight = @discussion.createdEvent().childCount * 14
+        @unitHeight = @trackHeight / @discussion.createdEvent().childCount
+        @knobOffset = @offsetFor(@position)
+        @knobHeight = @unitHeight * (last(slots) - first(slots) + 1)
+
+        # console.log 'knob position, offset, height. unit height', @position, @knobOffset, @knobHeight, @unitHeight
+        # console.log 'visibleSlots', slots
 
   methods:
     title: (model) ->
       if model.title or model.statement
-        truncate (model.title || model.statement || model.body).replace(///<[^>]*>?///gm, ''), 50
+        (model.title || model.statement || model.body).replace(///<[^>]*>?///gm, '')
       else
         parser = new DOMParser()
         doc = parser.parseFromString(model.statement || model.body, 'text/html')
         if el = doc.querySelector('h1,h2,h3')
           el.textContent
         else
-          truncate (model.body).replace(///<[^>]*>?///gm, ''), 50
+          (model.body || '').replace(///<[^>]*>?///gm, '')
 
-    updateThreadPosition: ->
+    scrollToClick: (event) ->
+      offset = event.clientY - @$refs.slider.getBoundingClientRect().top
+      @knobOffset = offset
+      @position = @positionFor(@knobOffset)
 
     onMouseDown: ->
       onMouseMove = (event) =>
-        @mouseY = event.clientY
-        @boxY = @$refs.slider.getBoundingClientRect().top
         offset = event.clientY - @$refs.slider.getBoundingClientRect().top
 
         if offset < 0
           @knobOffset = 0
-        else if offset > @max
-          @knobOffset = @max
+        else if offset > @trackHeight
+          @knobOffset = @trackHeight
         else
           @knobOffset = offset
 
@@ -69,112 +106,116 @@ export default
       onMouseUp = =>
         document.removeEventListener('mousemove', onMouseMove);
         document.removeEventListener('mouseup', onMouseUp);
-        @$router.replace(query: {p: @position})
+        @goToPosition(@position)
 
       document.addEventListener 'mousemove', onMouseMove
       document.addEventListener 'mouseup', onMouseUp
 
+    goToPosition: (position) ->
+      @$router.replace(query: {p: position})
+
     offsetFor: (position) ->
+      return 0 unless @discussion
       parseInt if @discussion.newestFirst
-          @max - ((position) * @positionSize)
+          @trackHeight - ((position) * @unitHeight)
         else
-          (position - 1) * @positionSize
+          (position - 1) * @unitHeight
 
     positionFor: (offset) ->
+      return 1 unless @discussion
+      position = parseInt(offset / @unitHeight)
       if @discussion.newestFirst
-        Math.round(offset / @positionSize) + 1
+        @discussion.createdEvent().childCount - position
       else
-        @discussion.createdEvent().childCount - Math.round(offset / @positionSize) + 1
-
-  computed:
-    # presets: -> [
-    #   position: @bottomPosition
-    #   title: timeline(@bottomDate)
-    # ,
-    #   position: @topPosition
-    #   title: timeline(@topDate)
-    # ]
-    presets: -> []
-    positionSize: -> @max / (@discussion.createdEvent().childCount - 1)
-    thumbLabel: -> "#{@position} / #{@childCount}"
+        position
 
   watch:
     'discussion.newestFirst':
       immediate: true
       handler: ->
-        if @discussion.newestFirst
-          @topPosition = @discussion.createdEvent().childCount
-          @topDate = @discussion.lastActivityAt
-
-          @bottomPosition = 1
-          @bottomDate = @discussion.createdAt
-        else
-          @topPosition = 1
-          @topDate = @discussion.createdAt
-
-          @bottomDate = @discussion.lastActivityAt
-          @bottomPosition = @discussion.createdEvent().childCount
+        return unless @discussion
 
 </script>
 
 <template lang="pug">
 v-navigation-drawer.lmo-no-print.disable-select(v-if="discussion" :permanent="$vuetify.breakpoint.mdAndUp" width="210px" app fixed right clipped color="transparent" floating)
-  div
-    //- | mouseY {{mouseY}}
-    //- | boxY {{boxY}}
-    //- | offset {{knobOffset}}
-    //- | max {{max}}
-    //- | position {{position}}
   .thread-nav
-    .thread-nav__inner
-      .thread-nav__track(ref="slider" :style="{height: max+thumbSize+'px'}")
-      .thread-nav__preset(v-for="preset in presets" :style="{top: offsetFor(preset.position)+'px'}")
-        .thread-nav__preset--line
-        .thread-nav__preset--title {{preset.title}}
-      .thread-nav__knob(:style="{top: knobOffset+'px', height: thumbSize+'px'}" :class="{'thread-nav__knob-hide': dragging}" ref="knob" @mousedown="onMouseDown")
-          span {{knobOffset}} / {{position}} / {{discussion.createdEvent().childCount}}
+    .thread-nav__track(ref="slider" :style="{height: trackHeight+'px'}" @click="scrollToClick")
+      .thread-nav__track-line
+    .thread-nav__preset(v-for="preset in presets" :style="{top: offsetFor(preset.position)+'px'}")
+      .thread-nav__preset--line
+      router-link.thread-nav__preset--title(:to="{query:{p: preset.position}}") {{preset.title}}
+    .thread-nav__knob(:style="{top: knobOffset+'px', height: knobHeight+'px'}" ref="knob" @mousedown="onMouseDown")
 </template>
 
 <style lang="sass">
 .thread-nav
   margin-top: 32px
+  position: relative
 
 .thread-nav__preset
-  float: left
   display: flex
-  position: relative
-  width: 160px
-  left: -12px
-  align-items: center
+  position: absolute
+  width: 200px
+  align-items: flex-start
+
+.thread-nav__preset--title
+  font-size: 14px
+  margin-top: -10px
+  width: 250px
+  white-space: nowrap
+  overflow: hidden
+  text-overflow: ellipsis
+
+.thread-nav__preset--title:hover
+  overflow: visible !important
+  white-space: normal !important
+  // text-overflow: inherit
+  // overflow: visible
+  // white-space: normal
+  background-color: #eee
+  z-index: 1000
 
 .thread-nav__preset--line
-  border: 1px solid black
-  height: 1px
+  height: 2px
+  width: 24px
   min-width: 24px
-  margin-right: 16px
+  background-color: #aaa
+  margin-right: 8px
+  // margin-left: 12px
+  // margin-right: 8px
+  // margin-left: 11px
+  // left: 11px
 
 .thread-nav__track
-  float: left
-  position: relative
-  border: 1px solid var(--v-accent-base)
-  width: 0
-  margin: 0 16px
-  left: 16px
+  position: absolute
+  width: 24px
+  cursor: pointer
+
+.thread-nav__track-line
+  height: 100%
+  position: absolute
+  background-color: var(--v-accent-base)
+  width: 2px
+  margin: 0 11px
+
+// 123456789012345678901234
+// ------------------------ track 24
+// -----------||----------- track-line 11 2 11
+// -------##########------- knob 8 8 8
+// ######################## preset-line 0 24 0
 
 .thread-nav__knob
-  float: left
-  position: relative
+  position: absolute
   display: flex
   flex-direction: column
-  align-items: center
-  justify-content: center
-  left: -4px
-  width: 100px
-  border-left: 8px solid var(--v-accent-base)
-  background-color: var(--v-accent-lighten5)
+  width: 8px
+  background-color: var(--v-accent-base)
   cursor: ns-resize
-  border-top-right-radius: 16px
-  border-bottom-right-radius: 16px
+  border-radius: 4px
+  transition: top 0.3s ease, height 2s ease
+  margin: 0 8px
+  // transition: scale 0.5s ease
 
 .disable-select
   user-select: none
