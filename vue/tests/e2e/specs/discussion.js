b244f3bef9e2d4bd30adb0adb8d11e36e55472dd
Gregory Kan <kan.gregory@gmail.com>
2019-10-29 10:51:57 +1300

    discussion suite e2es should all be passing now

--- vue/tests/e2e/specs/discussion.js
+++ vue/tests/e2e/specs/discussion.js
@@ -107,20 +107,6 @@ module.exports = {
   //   page.expectElement('.thread-item--unread')
   // },
 
-  // 'lets_you_mute_and_unmute': (test) => {
-  //   page = pageHelper(test)
-  //
-  //   page.loadPath('setup_multiple_discussions')
-  //   page.click('.context-panel-dropdown')
-  //   page.click('.context-panel-dropdown__option--mute')
-  //   page.click('.confirm-modal__submit')
-  //   page.expectFlash('Thread muted')
-  //
-  //   page.click('.context-panel-dropdown')
-  //   page.click('.context-panel-dropdown__option--unmute')
-  //   page.expectFlash('Thread unmuted')
-  // },
-
   'marks_a_discussion_as_seen': (test) => {
     page = pageHelper(test)
 
@@ -292,35 +278,31 @@ module.exports = {
     page.click('.confirm-modal__submit')
     page.expectNoText('.activity-panel', 'original comment right thur')
   },
-  //
-  // // TODO: GK: need help with this one
-  // 'invites_a_user_to_a_discussion': (test) => {
-  //   page = pageHelper(test)
-  //
-  //   page.loadPath('setup_discussion_mailer_new_discussion_email')
-  //   page.click('.thread-mailer__subject a', 2000)
-  //   page.expectText('.context-panel__heading', 'go to the moon')
-  //   page.expectText('.context-panel__description', 'A description for this discussion')
-  //   page.fillIn('.comment-form .ProseMirror', 'Hello world!')
-  //   page.click('.comment-form__submit-button')
-  //   page.expectText('.thread-item__title', 'Jennifer Grey', 10000)
-  //   page.expectText('.thread-item__body', 'Hello world!')
-  //   page.expectText('.group-cover-name', 'Girdy Dancing Shoes')
-  //   page.ensureSidebar()
-  //   page.expectNoElement('.sidebar__list-item-button--group')
-  // },
 
-  // TODO: GK: need help with this one
-  // 'invites_an_email_to_a_discussion': (test) => {
-  //   page = pageHelper(test)
-  //
-  //   page.loadPath('setup_discussion_mailer_invitation_created_email')
-  //   page.click('.thread-mailer__subject a', 2000)
-  //   page.expectValue('.auth-email-form__email input', 'jen@example.com')
-  //   page.signUpViaInvitation("Jennifer")
-  //   page.expectFlash('Signed in successfully')
-  //   page.expectText('.context-panel__heading', 'go to the moon', 10000)
-  //   page.expectText('.context-panel__description', 'A description for this discussion')
-  //   page.expectText('.new-comment__body', 'body of the comment')
-  // },
+  'invites_a_user_to_a_discussion': (test) => {
+    page = pageHelper(test)
+
+    page.loadPath('setup_discussion_mailer_new_discussion_email')
+    page.click('.thread-mailer__subject a', 2000)
+    page.expectText('.context-panel__heading', 'go to the moon')
+    page.expectText('.context-panel__description', 'A description for this discussion')
+    page.fillIn('.comment-form .ProseMirror', 'Hello world!')
+    page.click('.comment-form__submit-button')
+    page.expectText('.thread-item__title', 'Jennifer Grey', 10000)
+    page.expectText('.thread-item__body', 'Hello world!')
+    page.expectText('.context-panel__breadcrumbs', 'Girdy Dancing Shoes')
+  },
+
+  'invites_an_email_to_a_discussion': (test) => {
+    page = pageHelper(test)
+
+    page.loadPath('setup_discussion_mailer_invitation_created_email')
+    page.click('.thread-mailer__subject a', 2000)
+    page.expectValue('.auth-email-form__email input', 'jen@example.com')
+    page.signUpViaInvitation("Jennifer")
+    page.expectFlash('Signed in successfully')
+    page.expectText('.context-panel__heading', 'go to the moon', 10000)
+    page.expectText('.context-panel__description', 'A description for this discussion')
+    page.expectText('.new-comment__body', 'body of the comment')
+  },
 }
