cb775a16150953f77b2cbcc6d787c57c32766590
Gregory Kan <kan.gregory@gmail.com>
2019-10-30 15:12:36 +1300

    router-links for discussions nav

--- vue/src/components/group/discussions_nav.vue
+++ vue/src/components/group/discussions_nav.vue
@@ -16,21 +16,7 @@ export default
       @group = options.group
       return unless @group
 
-  methods:
-    selectFilter: (filter) ->
-      params = if filter == "open"
-        omit @$route.query, ['t']
-      else
-        Object.assign({}, @$route.query, {t: filter})
-
-      @$router.replace(query: params)
-
-      @filter = filter
-
   computed:
-    tags: ->
-      intersection([@filter], @groupTags)
-
     groupTags: ->
       @group && @group.parentOrSelf().tagNames || []
 
@@ -43,14 +29,14 @@ export default
 <template lang="pug">
 v-navigation-drawer.lmo-no-print.disable-select.thread-sidebar(:permanent="$vuetify.breakpoint.mdAndUp" width="230px" app fixed right clipped color="transparent" floating)
   ul
-    li(label outlined value="open" @click="selectFilter('open')")
-      span(v-t="'discussions_panel.open'")
-    li(label outlined value="unread" @click="selectFilter('unread')")
-      span(v-t="{ path: 'discussions_panel.unread', args: { count: unreadCount }}")
-    li(label outlined value="closed" @click="selectFilter('closed')")
-      span(v-t="'discussions_panel.closed'")
-  ul
-    li(v-for="tag in groupTags" :key="tag" :value="tag" @click="selectFilter(tag)" outlined) {{tag}}
+    li
+      router-link(:to="{query: {t: 'open'}}" v-t="'discussions_panel.open'")
+    li
+      router-link(:to="{query: {t: 'unread'}}" v-t="{ path: 'discussions_panel.unread', args: { count: unreadCount }}")
+    li
+      router-link(:to="{query: {t: 'closed'}}" v-t="'discussions_panel.closed'")
+    li(v-for="tag in groupTags" :key="tag")
+      router-link(:to="{query: {t: tag}}") {{tag}}
 
 </template>
 
