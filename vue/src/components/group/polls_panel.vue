e79d39c2b5181b7fe1ec5d24bf6d6ce29b4ab6fb
Robert Guthrie <rob@guthrie.nz>
2019-11-04 13:20:38 +1300

    use route approach in polls panel

--- vue/src/components/group/polls_panel.vue
+++ vue/src/components/group/polls_panel.vue
@@ -8,129 +8,118 @@ import { debounce, some, every, compact, omit, values, keys, intersection } from
 
 export default
   data: ->
-    group: Records.groups.fuzzyFind(@$route.params.key)
+    group: null
     polls: []
-    searchQuery: ''
     loader: null
     per: 50
     from: 0
-    status: null
-    pollType: null
-    subgroups: 'mine'
     pollTypes: AppConfig.pollTypes
 
   created: ->
-    EventBus.$emit 'currentComponent',
-      page: 'groupPage'
-      title: @group.name
-      group: @group
-      search:
-        placeholder: @$t('navbar.search_polls', name: @group.parentOrSelf().name)
+    Records.groups.findOrFetch(@$route.params.key).then (group) =>
+      @group = group
 
-    @loader = new RecordLoader
-      collection: 'polls'
-      path: 'search'
+      EventBus.$emit 'currentComponent',
+        page: 'groupPage'
+        title: @group.name
+        group: @group
+        search:
+          placeholder: @$t('navbar.search_polls', name: @group.parentOrSelf().name)
 
-    @watchRecords
-      collections: ['polls', 'groups', 'memberships']
-      query: => @query()
-    @refresh()
+      @loader = new RecordLoader
+        collection: 'polls'
+        path: 'search'
+
+      @watchRecords
+        collections: ['polls', 'groups', 'memberships']
+        query: => @findRecords()
+
+      @refresh()
 
   methods:
-    refresh: ->
-      @fetch()
-      @query()
+    onQueryInput: (val) -> @$router.replace(@mergeQuery(q: val))
 
-    query: (store) ->
-      groupIds = switch @subgroups
+    refresh: debounce ->
+      @fetch()
+      @findRecords()
+    , 500
+
+    findRecords: (store) ->
+      groupIds = switch @$route.query.subgroups
         when 'none' then [@group.id]
-        when 'mine' then intersection(@group.organisationIds(), Session.user().groupIds())
         when 'all' then @group.organisationIds()
+        else
+          intersection(@group.organisationIds(), Session.user().groupIds())
 
       chain = Records.polls.collection.chain()
       chain = chain.find(groupId: {$in: groupIds})
 
-      switch
-        when @status == null
-          true # noop
-        when @status == 'active'
+      switch @$route.query.status
+        when 'active'
           chain = chain.find({'closedAt': null})
-        when @status == 'closed'
+        when 'closed'
           chain = chain.find({'closedAt': {$ne: null}})
 
-      if @pollType
-        chain = chain.find({'pollType': @pollType})
+      if @$route.query.poll_type
+        chain = chain.find({'pollType': @$route.query.poll_type})
 
-      if @searchQuery
+      if @$route.query.q
         chain = chain.where (poll) =>
           some [poll.title, poll.details], (field) =>
-            every @searchQuery.split(' '), (frag) -> RegExp(frag, "i").test(field)
+            every @$route.query.q.split(' '), (frag) -> RegExp(frag, "i").test(field)
 
       @polls = chain.simplesort('-createdAt').limit(@loader.numRequested).data()
 
-    fetch: debounce ->
+    fetch: ->
       @loader.fetchRecords
         group_key: @group.key
         status: @$route.query.status
         poll_type: @$route.query.poll_type
         per: @per
         from: @from
-        query: @searchQuery
-        subgroups: @subgroups
-    ,
-      300
+        query: @$route.query.q
+        subgroups: @$route.query.subgroups
 
   watch:
-    '$route.query':
-      immediate: true
-      handler: (query) ->
-        @status = query.status
-        @pollType = query.poll_type
-        @subgroups = query.subgroups || 'mine'
-        @searchQuery = query.q
-        @refresh()
-
-    searchQuery: debounce (val) ->
-      @$router.replace(query: { q: val })
-    , 500
-
-    subgroups: -> @refresh()
+    '$route.query': 'refresh'
 
 </script>
 
 <template lang="pug">
 .polls-panel
-  v-layout.py-2(align-center)
-    v-menu
-      template(v-slot:activator="{ on }")
-        v-btn.mr-2.text-lowercase(v-on="on" text)
-          span(v-if="status == 'active'" v-t="'polls_panel.open'")
-          span(v-if="status == 'closed'" v-t="'polls_panel.closed'")
-          span(v-if="!status" v-t="'polls_panel.any_status'")
-          v-icon mdi-menu-down
-      v-list(dense)
-        v-list-item(:to="mergeRouteQuery({status: null })" v-t="'polls_panel.any_status'")
-        v-list-item(:to="mergeRouteQuery({status: 'active'})" v-t="'polls_panel.open'")
-        v-list-item(:to="mergeRouteQuery({status: 'closed'})" v-t="'polls_panel.closed'")
-    v-menu
-      template(v-slot:activator="{ on }")
-        v-btn.mr-2.text-lowercase(v-on="on" text)
-          //- span(v-t="{path: filterName(filter), args: {count: unreadCount}}")
-          span(v-if="pollType" v-t="'poll_types.'+pollType")
-          span(v-if="!pollType" v-t="'polls_panel.any_type'")
-          v-icon mdi-menu-down
-      v-list(dense)
-        v-list-item(:to="mergeRouteQuery({poll_type: null})" )
-          v-list-item-title(v-t="'polls_panel.any_type'")
-        v-list-item(v-for="pollType in pollTypes" :key="pollType" :to="mergeRouteQuery({poll_type: pollType})" )
-          v-list-item-title(v-t="'poll_types.'+pollType")
-    v-text-field(clearable hide-details solo v-model="searchQuery" :placeholder="$t('navbar.search_polls', {name: group.name})" append-icon="mdi-magnify")
-  v-card(outlined)
-    v-list(two-line avatar v-if='polls.length')
-      poll-common-preview(:poll='poll', v-for='poll in polls', :key='poll.id')
+  loading(v-if="!group")
+  div(v-if="group")
+    v-layout.py-2(align-center)
+      v-menu
+        template(v-slot:activator="{ on }")
+          v-btn.mr-2.text-lowercase(v-on="on" text)
+            span(v-if="$route.query.status == 'active'" v-t="'polls_panel.open'")
+            span(v-if="$route.query.status == 'closed'" v-t="'polls_panel.closed'")
+            span(v-if="!$route.query.status" v-t="'polls_panel.any_status'")
+            v-icon mdi-menu-down
+        v-list(dense)
+          v-list-item(:to="mergeQuery({status: null })" v-t="'polls_panel.any_status'")
+          v-list-item(:to="mergeQuery({status: 'active'})" v-t="'polls_panel.open'")
+          v-list-item(:to="mergeQuery({status: 'closed'})" v-t="'polls_panel.closed'")
+      v-menu
+        template(v-slot:activator="{ on }")
+          v-btn.mr-2.text-lowercase(v-on="on" text)
+            //- span(v-t="{path: filterName(filter), args: {count: unreadCount}}")
+            span(v-if="$route.query.poll_type" v-t="'poll_types.'+$route.query.poll_type")
+            span(v-if="!$route.query.poll_type" v-t="'polls_panel.any_type'")
+            v-icon mdi-menu-down
+        v-list(dense)
+          v-list-item(:to="mergeQuery({poll_type: null})" )
+            v-list-item-title(v-t="'polls_panel.any_type'")
+          v-list-item(v-for="pollType in pollTypes" :key="pollType" :to="mergeQuery({poll_type: pollType})" )
+            v-list-item-title(v-t="'poll_types.'+pollType")
+      v-text-field(clearable hide-details solo :value="$route.query.q" @input="onQueryInput" :placeholder="$t('navbar.search_polls', {name: group.name})" append-icon="mdi-magnify")
+    v-card(outlined)
+      v-list(two-line avatar v-if='polls.length')
+        poll-common-preview(:poll='poll', v-for='poll in polls', :key='poll.id')
 
-    v-alert(v-if='polls.length == 0 && !loader.loading' :value="true" color="grey" outlined icon="info" v-t="'polls_panel.no_polls'")
+      v-alert(v-if='polls.length == 0 && !loader.loading' :value="true" color="grey" outlined icon="info" v-t="'polls_panel.no_polls'")
 
-    v-layout(justify-center)
-      v-btn.my-2(outlined color='accent' v-if="!loader.exhausted" :loading="loader.loading" @click="loader.loadMore()" v-t="'common.action.load_more'")
+      v-layout(justify-center)
+        v-btn.my-2(outlined color='accent' v-if="!loader.exhausted" :loading="loader.loading" @click="loader.loadMore()" v-t="'common.action.load_more'")
 </template>
