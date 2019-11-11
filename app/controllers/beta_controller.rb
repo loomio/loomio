81cd368c79b9046492611d044d7fc6288b7434b4
Robert Guthrie <rob@guthrie.nz>
2019-11-11 16:24:18 +1300

    add not loading link to boot screen

--- app/controllers/beta_controller.rb
+++ app/controllers/beta_controller.rb
@@ -6,10 +6,12 @@ class BetaController < ApplicationController
 
   def update
     if params[:enable] # don't use old client
+      current_user.experiences.delete('vue_client')
       current_user.experiences.delete('old_client')
       current_user.save
       redirect_to '/dashboard'
     else #use old client
+      current_user.experiences.delete('vue_client')
       current_user.experiences['old_client'] = true
       current_user.save
       redirect_to '/dashboard?old_client=1'
