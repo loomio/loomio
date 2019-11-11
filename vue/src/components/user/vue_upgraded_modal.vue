7ce34efc5b4ef7a48aced0217a09e8d2021c77fc
Gregory Kan <kan.gregory@gmail.com>
2019-11-11 13:40:56 +1300

    updated what's new link

--- vue/src/components/user/vue_upgraded_modal.vue
+++ vue/src/components/user/vue_upgraded_modal.vue
@@ -16,6 +16,6 @@ v-card.vue-upgraded-modal
     v-layout(column)
       v-img(src='https://www.loomio.org/assets/loomio_org/high-five-819621bc73330bb853dd3cb1a5a9b273e2a01004d955678257dbfc139618528f.gif')
       h1.mb-8(v-t="'vue_upgraded_modal.title'")
-      div(v-html="$t('vue_upgraded_modal.body', {link: 'https://blog.loomio.org/2019/09/18/what-is-new-in-loomio-2.0-and-beta-release/'})")
+      div(v-html="$t('vue_upgraded_modal.body', {link: 'https://help.loomio.org/en/user_manual/whats_new_loomio_2/'})")
       v-btn.mt-4(block color="accent" v-t="'common.ok_got_it'" @click="close()")
 </template>
