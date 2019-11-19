c25755b6088ea9a137036f35728280deac6f2ccd
Gregory Kan <kan.gregory@gmail.com>
2019-11-19 15:16:38 +1300

    scaffolding the tree

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,9 +1,13 @@
 class DiscussionsController < ApplicationController
   include UsesMetadata
   include LoadAndAuthorize
+  include EmailHelper
+  helper :email
 
   def export
     @discussion = load_and_authorize(:discussion, :show)
+    @info = DiscussionEmailInfo.new(recipient: current_user, event: @discussion.created_event, action_name: 'new_discussion')
+    byebug
     respond_to do |format|
       format.html
       # format.csv { send_data @exporter.to_csv, filename:@exporter.file_name }
