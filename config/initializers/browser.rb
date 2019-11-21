08ab77e4363465fba1ad2af631ec3d42c75fadd5
Robert Guthrie <rob@guthrie.nz>
2019-11-22 08:30:28 +1300

    fiddle the conditions for 417 redirect

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,3 +1,3 @@
 Rails.configuration.middleware.use Browser::Middleware do
-  redirect_to '/417' if !(request.params['old_client']) && !request.xhr? && browser.ie?
+  redirect_to '/417' if browser.ie? && !request.params['old_client'] && !request.xhr? 
 end
