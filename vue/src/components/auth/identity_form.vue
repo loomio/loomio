87b1d8e81704ad3cffbcbfff2d4978825b8f2d5a
Robert Guthrie <rob@guthrie.nz>
2019-11-21 20:41:03 +1300

    quick scrub up of the auth identity form

--- vue/src/components/auth/identity_form.vue
+++ vue/src/components/auth/identity_form.vue
@@ -23,17 +23,18 @@ export default
 </script>
 <template lang="pug">
 .auth-identity-form(@keyup.ctrl.enter="submit()" @keydown.meta.enter.stop.capture="submit()")
-  h2.lmo-h2(v-t="{ path: 'auth_form.hello', args: { name: user.name || user.email } }")
-  auth-avatar(:user='user')
+  .mb-4.text-center
+    auth-avatar(:user='user')
+    h2.headline(v-t="{ path: 'auth_form.hello', args: { name: user.name || user.email } }")
   .auth-identity-form__options
-    .auth-identity-form__new-account
+    .auth-identity-form__new-account.mb-8
       p(v-t="{ path: 'auth_form.new_to_loomio', args: { site_name: siteName } }")
       v-btn(color="primary" @click='createAccount()', v-t="'auth_form.create_account'")
     .auth-identity-form__existing-account
       p(v-t="{ path: 'auth_form.already_a_user', args: { site_name: siteName }}")
       .auth-email-form__email
         label(v-t="'auth_form.email'")
-        v-text-field#email.lmo-primary-form-input(name='email', type='text', v-autofocus='true', :placeholder="'auth_form.email_placeholder'", v-model='email')
-        //- validation_errors(subject='user', field='email')
-      v-btn(@click='submit()', v-t="'auth_form.link_accounts'")
+        v-text-field#email.lmo-primary-form-input(name='email' type='text' v-autofocus='true' :placeholder="$t('auth_form.email_address_of_existing_account')" v-model='email')
+        validation-errors(:subject='user' :field='email')
+      v-btn(@click='submit()' v-t="'auth_form.link_accounts'")
 </template>
