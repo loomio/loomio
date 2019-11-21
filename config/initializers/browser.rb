c30cd48c1324b6311e730a4802ca16fe4e6a4152
Robert Guthrie <rob@guthrie.nz>
2019-11-22 12:03:07 +1300

    ensure old_client param works

--- config/initializers/browser.rb
+++ config/initializers/browser.rb
@@ -1,8 +1,9 @@
 Rails.configuration.middleware.use Browser::Middleware do
+  debugger
   redirect_to '/417' if !request.params['old_client'] &&
                         !request.xhr? &&
-                        browser.ie? ||
+                        (browser.ie? ||
                         (browser.chrome? && browser.version.to_i < 50) ||
                         (browser.safari? && browser.version.to_i < 12) ||
-                        (browser.edge?   && browser.version.to_i < 17)
+                        (browser.edge?   && browser.version.to_i < 17))
 end
