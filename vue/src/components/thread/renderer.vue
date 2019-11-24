aeac5c942da864540dc1a257901a18d364aa08f1
Robert Guthrie <rob@guthrie.nz>
2019-11-25 07:23:13 +1300

    seems great now

--- vue/src/components/thread/renderer.vue
+++ vue/src/components/thread/renderer.vue
@@ -3,7 +3,7 @@ import Records from '@/shared/services/records'
 import EventBus from '@/shared/services/event_bus'
 import RecordLoader from '@/shared/services/record_loader'
 import EventHeights from '@/shared/services/event_heights'
-import { reverse, filter, compact, clone, debounce, range, min, max, map, keys, first, last, sortedUniq, sortBy, difference, isEqual, without } from 'lodash'
+import { reverse, groupBy, filter, compact, clone, debounce, range, min, max, map, keys, first, last, sortedUniq, sortBy, difference, isEqual, without } from 'lodash'
 
 export default
   props:
@@ -25,60 +25,56 @@ export default
 
   data: ->
     eventsBySlot: {}
-    firstItem: null
-    lastItem: null
     visibleSlots: []
     missingItems: []
-    renderedItems: []
+    focus: null
     slots: []
-    padding: 2
+    padding: parseInt(screen.height/40) || 20
 
   methods:
+    grouped: (slots) ->
+      groupBy slots, (slot) -> parseInt(slot * 0.1)
+
     renderSlots: ->
       return if @parentEvent.childCount == 0
-      return if @visibleSlots.length == 0 and !@focalEvent
 
       # we can let the browser garbage collect if eventsBySlot is too big
-      # @eventsBySlot = {} if @slots.length > 1000
+      # @eventsBySlot = {} if @slots.length > 1000 # no! just set all existing slots to null so page stays in same place.
       # may want to re scroll after this action.
 
       if @focalEvent
-        focalPosition = @eventOrParent(@focalEvent).position
-
-        @firstItem = max([1, focalPosition - @padding])
-        @lastItem = min([focalPosition + @padding, @parentEvent.childCount])
-        console.log {focalPosition, @padding}
+        @focus = @eventOrParent(@focalEvent).position
       else
-        @firstItem = max(compact [1, first(@visibleSlots) - @padding])
-        @lastItem = min([last(@visibleSlots) + @padding, @parentEvent.childCount])
+        @focus = @visibleSlots[parseInt(@visibleSlots.length / 2)] || 1
 
-      # fyi
-      # first([]) -> undefined
-      # min([undefined, 1]) -> 1
-      # max([undefined, 1]) -> 1
+      firstItem = max([1, @focus - @padding])
+      lastItem = min([@focus + @padding, @parentEvent.childCount])
 
-      firstSlot = max([1, @firstItem - (@padding * 2)])
-      lastSlot = min([@parentEvent.childCount, @lastItem + (@padding * 2)])
-      console.log {@firstItem, @lastItem, firstSlot, lastSlot}
+      firstSlot = max([1, firstItem - (@padding * 2)])
+      lastSlot = min([@parentEvent.childCount, lastItem + (@padding * 2)])
 
       presentItems = []
 
       Records.events.collection.chain().
       find(parentId: @parentEvent.id).
-      find(position: {$between: [@firstItem, @lastItem]}).
+      find(position: {$between: [firstItem, lastItem]}).
       simplesort('position').
       data().forEach (event) =>
         presentItems.push(event.position)
         @eventsBySlot[event.position] = event
 
+      # after items have been added, we should emit "items added"
+        # if they're above visibleSlots, then we need to issue a hold on! alert
+
       range(firstSlot, lastSlot+1).forEach (slot) =>
         @eventsBySlot[slot] = null unless @eventsBySlot.hasOwnProperty(slot)
 
-      @missingItems = sortBy difference(range(@firstItem, @lastItem+1), presentItems)
+      @missingItems = sortBy difference(range(firstItem, lastItem+1), presentItems)
 
       @slots = sortBy map(keys(@eventsBySlot), Number)
 
     slotVisible: (isVisible, slot) ->
+      # idea: if adding a slot which is not continuous with the other slots, remove the other slots?
       slot = parseInt(slot)
       if isVisible
         @visibleSlots = sortedUniq(sortBy(@visibleSlots.concat([slot])))
@@ -118,28 +114,38 @@ export default
 </script>
 <template lang="pug">
 .thread-renderer.mb-2
-  div(v-if="parentEvent.depth == 0")
-    | depth {{parentEvent.depth}}
-    | position {{parentEvent.position}}
+  //- div.thread-renderer__stats.caption(v-if="parentEvent.depth == 0")
+    //- | depth {{parentEvent.depth}}
+    //- | position {{parentEvent.position}}
+    | focus {{focus}}
+    | padding {{padding}}
+    br
     | childCount {{parentEvent.childCount}}
     | isFocusing {{(focalEvent && true) || false}}
-    | firstItem {{firstItem}}
-    | lastItem {{lastItem}}
-    | missingItems {{missingItems}}
+    br
+    | missingItems
+    p.my-0(v-for="slots in grouped(missingItems)") {{slots}}
+    br
     | visibleSlots {{visibleSlots}}
-    | renderedItems {{renderedItems}}
-    | slots {{slots}}
+    br
+    | slots
+    p.my-0(v-for="slots in grouped(slots)") {{slots}}
   .thread-item-slot(v-for="slot in adjustedSlots" :key="slot" v-observe-visibility="{callback: (isVisible) => slotVisible(isVisible, slot)}" )
     thread-item-wrapper(:parent-id="parentEvent.id" :event="eventsBySlot[slot]" :position="parseInt(slot)" :focal-event="focalEvent" :is-returning="isReturning")
-  div(v-if="parentEvent.depth == 0")
-    | depth {{parentEvent.depth}}
-    | position {{parentEvent.position}}
-    | childCount {{parentEvent.childCount}}
-    | isFocusing {{(focalEvent && true) || false}}
-    | firstItem {{firstItem}}
-    | lastItem {{lastItem}}
-    | missingItems {{missingItems}}
-    | visibleSlots {{visibleSlots}}
-    | renderedItems {{renderedItems}}
-    | slots {{slots}}
 </template>
+
+<style lang="sass">
+.thread-renderer__stats
+  z-index: 1000
+  right: 0px
+  position: fixed
+  bottom: 0px
+  height: 300px
+  width: 256px
+  // opacity: 0.9
+  border: 1px solid #eee
+  background-color: #ccc
+  overflow-y: scroll
+
+
+</style>
