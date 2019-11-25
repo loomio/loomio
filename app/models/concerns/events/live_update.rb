b12493c46fc04f38698b90f3f660f13e55469715
Robert Guthrie <rob@guthrie.nz>
2019-11-26 06:47:39 +1300

    null check for eventable in live update

--- app/models/concerns/events/live_update.rb
+++ app/models/concerns/events/live_update.rb
@@ -6,6 +6,7 @@ module Events::LiveUpdate
 
   # send client live updates
   def notify_clients!
+    return unless eventable
     eventable.groups.each do |group|
       MessageChannelService.publish_data(event_collection, to: group.message_channel)
     end
