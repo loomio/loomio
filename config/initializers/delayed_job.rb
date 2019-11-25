c68d11dc356e408ea1b468258dbfc28b24bf7735
Robert Guthrie <rob@guthrie.nz>
2019-11-26 06:39:55 +1300

    1 hour delayed job max time

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -2,4 +2,4 @@ Delayed::Worker.default_priority = 10
 Delayed::Worker.max_attempts = ENV.fetch('MAX_JOB_ATTEMPTS', 2).to_i
 Delayed::Worker.delay_jobs = ENV.fetch('RUN_JOBS_IN_BACKGROUND', Rails.env.production?)
 Delayed::Worker.destroy_failed_jobs = (!ENV.fetch('KEEP_FAILED_JOBS', false))
-Delayed::Worker.max_run_time = 5.minutes
+Delayed::Worker.max_run_time = 1.hour
