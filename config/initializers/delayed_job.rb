7147546e6d2b2c2e1ce048736f21b61e8af7b02b
Robert Guthrie <rob@guthrie.nz>
2019-11-25 21:06:46 +1300

    add delayed jobs indexes

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,4 +1,5 @@
 Delayed::Worker.default_priority = 10
-Delayed::Worker.max_attempts = ENV.fetch('MAX_JOB_ATTEMPTS', 3).to_i
+Delayed::Worker.max_attempts = ENV.fetch('MAX_JOB_ATTEMPTS', 2).to_i
 Delayed::Worker.delay_jobs = ENV.fetch('RUN_JOBS_IN_BACKGROUND', Rails.env.production?)
 Delayed::Worker.destroy_failed_jobs = (!ENV.fetch('KEEP_FAILED_JOBS', false))
+Delayed::Worker.max_run_time = 5.minutes
