f099dff5060e884cb4f6041bb0b6a09e069f86fa
Robert Guthrie <rob@guthrie.nz>
2019-11-25 17:02:20 +1300

    link tag to list of other discussions tagged same

--- vue/src/components/tags/display.vue
+++ vue/src/components/tags/display.vue
@@ -7,5 +7,5 @@ export default
 </script>
 <template lang="pug">
 .thread-tags
-  v-chip.ml-1(small outlined color="accent" v-for="tag in tags" :key="tag") {{ tag }}
+  v-chip.ml-1(small outlined color="accent" v-for="tag in tags" :key="tag" :to="{path: urlFor(discussion.group()), query: {tag: tag}}") {{ tag }}
 </template>
