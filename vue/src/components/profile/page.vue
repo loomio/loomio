86b5579a948e5a2739fe586810efd5f7f90ee540
Gregory Kan <kan.gregory@gmail.com>
2019-11-27 13:41:10 +1300

    add button to change profile pic

--- vue/src/components/profile/page.vue
+++ vue/src/components/profile/page.vue
@@ -83,8 +83,9 @@ v-content
                   v-text-field#user-email-field.profile-page__email-input(:label="$t('profile_page.email_label')" required='ng-required', v-model='user.email')
                   validation-errors(:subject='user', field='email')
 
-                v-flex.profile-page__avatar(justify-center @click="changePicture()")
+                .profile-page__avatar.d-flex.flex-column.justify-center.align-center.mx-12(@click="changePicture()")
                   user-avatar(:user='originalUser' size='featured' :no-link="true")
+                  v-btn(color="accent" @click="changePicture" v-t="'profile_page.change_picture_link'")
 
               lmo-textarea(:model='user' field="shortBio" :label="$t('profile_page.short_bio_label')" :placeholder="$t('profile_page.short_bio_placeholder')")
               validation-errors(:subject='user', field='shortBio')
