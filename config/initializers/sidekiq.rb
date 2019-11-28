ece404ecd3d598d4eb58b51eebd800cccf624997
Robert Guthrie <rob@guthrie.nz>
2019-11-28 13:38:11 +1300

    going on sidekiq on travis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,11 @@
 Sidekiq.default_worker_options = { 'backtrace' => true }
 Sidekiq::Extensions.enable_delay!
 
+redis_url = (ENV['REDIS_QUEUE_URL'] || ENV.fetch('REDIS_URL', 'redis://localhost:6379'))+"/1"
 Sidekiq.configure_server do |config|
-  config.redis = { url: (ENV['REDIS_QUEUE_URL'] || ENV['REDIS_URL'])+"/1" }
+  config.redis = { url: redis_url }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: (ENV['REDIS_QUEUE_URL'] || ENV['REDIS_URL'])+"/1" }
+  config.redis = { url: redis_url }
 end
