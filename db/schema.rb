9f37bccf9532b972593bd616faad0bd7ef32299c
Gregory Kan <kan.gregory@gmail.com>
2019-12-02 12:07:30 +1300

    migration to backfill group_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_033332) do
+ActiveRecord::Schema.define(version: 2019_12_01_225942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
