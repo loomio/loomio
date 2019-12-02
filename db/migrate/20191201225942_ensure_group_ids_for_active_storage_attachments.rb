cd0ee31f7e7ab7c58bb84fe68ac9b49fca5dfc53
Gregory Kan <kan.gregory@gmail.com>
2019-12-02 16:32:34 +1300

    fix migration; some attachments have missing records

--- db/migrate/20191201225942_ensure_group_ids_for_active_storage_attachments.rb
+++ db/migrate/20191201225942_ensure_group_ids_for_active_storage_attachments.rb
@@ -1,7 +1,7 @@
 class EnsureGroupIdsForActiveStorageAttachments < ActiveRecord::Migration[5.2]
   def change
     ActiveStorage::Attachment.where(group_id: nil).find_each do |attachment|
-      attachment.record.update_attachments!
+      attachment.record.update_attachments! if attachment.record
     end
   end
 end
