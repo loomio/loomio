2402a11c7d1f4d425112f0999578762907e91ac0
Gregory Kan <kan.gregory@gmail.com>
2019-12-02 13:44:38 +1300

    use the concern method

--- db/migrate/20191201225942_ensure_group_ids_for_active_storage_attachments.rb
+++ db/migrate/20191201225942_ensure_group_ids_for_active_storage_attachments.rb
@@ -1,7 +1,7 @@
 class EnsureGroupIdsForActiveStorageAttachments < ActiveRecord::Migration[5.2]
   def change
     ActiveStorage::Attachment.where(group_id: nil).find_each do |attachment|
-      attachment.update(group_id: attachment.record.group.id) if attachment.record && attachment.record.group
+      attachment.record.update_attachments!
     end
   end
 end
