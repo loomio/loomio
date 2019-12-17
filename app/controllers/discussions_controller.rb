5b5fcedb6b7bcbed6501fe12cadcde34f32defa6
Gregory Kan <kan.gregory@gmail.com>
2019-12-18 10:55:10 +1300

    broken into multiple templates

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -6,7 +6,6 @@ class DiscussionsController < ApplicationController
 
   def export
     @discussion = load_and_authorize(:discussion, :show)
-    @info = DiscussionEmailInfo.new(recipient: current_user, event: @discussion.created_event, action_name: 'new_discussion')
     byebug
     respond_to do |format|
       format.html
