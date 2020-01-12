e52e01975566d23c9fe576854750bf2fdd74692f
Robert Guthrie <rob@guthrie.nz>
2020-01-12 16:03:17 +1300

    remove cohorts

--- app/models/cohort.rb
+++
@@ -1,8 +0,0 @@
-class Cohort < ApplicationRecord
-  has_many :groups
-
-  def organisations
-    groups.where('parent_id IS NULL')
-  end
-
-end
--- app/models/formal_group.rb
+++ app/models/formal_group.rb
@@ -50,7 +50,6 @@ class FormalGroup < Group
   has_many :public_comment_documents,    through: :public_comments,    source: :documents
   has_many :tags, foreign_key: :group_id
 
-  belongs_to :cohort
   belongs_to :default_group_cover
   belongs_to :subscription
 
