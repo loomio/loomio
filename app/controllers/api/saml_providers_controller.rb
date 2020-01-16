7d6038b01e5ad94af82c3799b1242082c5013363
Robert Guthrie <rguthrie@gmail.com>
2020-01-16 18:52:17 +1300

    vaidate idp metadata url and more (#6210)
    * vaidate idp metadata url and more
    
    * fix

--- app/controllers/api/saml_providers_controller.rb
+++ app/controllers/api/saml_providers_controller.rb
@@ -2,7 +2,7 @@ class API::SamlProvidersController < API::RestfulController
   def index
     if group = find_group
       saml_provider = SamlProvider.find_by!(group_id: group.id)
-      render json: { saml_provider_id:  saml_provider.id }
+      render json: { saml_provider_id:  saml_provider.id, idp_metadata_url: saml_provider.idp_metadata_url }
     else
       render json: {}, status: 404
     end
@@ -10,7 +10,18 @@ class API::SamlProvidersController < API::RestfulController
 
   def create
     group = load_and_authorize(:group, :set_saml_provider)
+
+    # this will raise 404 if the metadata url is incorrect
+    OneLogin::RubySaml::IdpMetadataParser.new.parse_remote(params[:idp_metadata_url])
+
     SamlProvider.create(group: group, idp_metadata_url: params[:idp_metadata_url])
+    group.memberships.update_all(saml_session_expires_at: nil)
+    render json: { success: :ok }
+  end
+
+  def destroy
+    group = load_and_authorize(:group, :set_saml_provider)
+    SamlProvider.where(group: group).destroy_all
     render json: { success: :ok }
   end
 
