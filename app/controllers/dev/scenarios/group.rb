2dca6ebd42503b996532c2fa026d5773e2429650
Robert Guthrie <rob@guthrie.nz>
2020-01-16 17:00:49 +1300

    fix

--- app/controllers/dev/scenarios/group.rb
+++ app/controllers/dev/scenarios/group.rb
@@ -124,8 +124,8 @@ module Dev::Scenarios::Group
   def setup_saml_group
     @group = FormalGroup.create!(name: 'Dirty Dancing Shoes', handle: 'dirty-dancing-shoes', group_privacy: params[:privacy] || 'secret')
     @group.create_subscription(plan: 'pp-pro-annual')
-    # provider_url = "https://saml_provider.example.com"
-    provider_url = 'https://app.onelogin.com/saml/metadata/c5690a10-4e33-4a57-9389-30dd92996629'
+    provider_url = "https://saml_provider.example.com"
+    # provider_url = 'https://app.onelogin.com/saml/metadata/c5690a10-4e33-4a57-9389-30dd92996629'
     patrick.update(email: params[:email]) if params[:email]
 
     SamlProvider.create(group: @group, idp_metadata_url: provider_url)
