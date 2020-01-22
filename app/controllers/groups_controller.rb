1e4c7c6e035b76ef65a689cb9e343e118822c891
Robert Guthrie <rob@guthrie.nz>
2020-01-22 21:43:31 +1300

    start on group page

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,17 @@
 class GroupsController < ApplicationController
   include UsesMetadata
   include LoadAndAuthorize
-  layout false
+
+  def show
+    if !current_user.is_logged_in? or params[:export]
+      @group = load_and_authorize(:group, :show)
+      respond_to do |format|
+        format.html
+      end
+    else
+      boot_app
+    end
+  end
 
   def export
     @exporter = GroupExporter.new(load_and_authorize(:formal_group, :export))
