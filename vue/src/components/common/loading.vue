58c038582ce151ff82dc0a60ad5bf368268b2c2b
Robert Guthrie <rguthrie@gmail.com>
2020-01-25 00:52:14 +1300

    use SSR content as loading placeholder (more consisently) (#6244)
    * i think it's ready
    
    * fix spec

--- vue/src/components/common/loading.vue
+++ vue/src/components/common/loading.vue
@@ -1,15 +1,26 @@
 <script lang="coffee">
+import { hasContent, insertContent, removeContent } from '@/shared/services/ssr_content'
+
 export default
   props:
     until: null
     diameter:
       type: Number
       default: 30
+
+  data: ->
+    canDisplay: hasContent()
+
+  mounted: ->
+    insertContent('#loading-placeholder')
+
+  watch:
+    until: (val) -> removeContent() if val
 </script>
 
 <template lang="pug">
 div
-  v-layout.my-4.page-loading(justify-center v-if="!until")
-    v-progress-circular(indeterminate color='primary')
+  v-layout.my-4.page-loading#loading-placeholder(justify-center v-if="!until")
+    v-progress-circular(v-if="!canDisplay" indeterminate color='primary')
   slot(v-if="until")
 </template>
