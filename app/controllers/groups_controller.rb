9c8964bc0a4b2f8771acf98ba7178bd4cee5534a
Gregory Kan <kan.gregory@gmail.com>
2020-01-29 15:17:28 +1300

    fix pages count logic

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,11 +2,19 @@ class GroupsController < ApplicationController
   def index
     @groups = Queries::ExploreGroups.new.search_for(params[:q]).where.not(name: nil).order('groups.memberships_count DESC')
     @total = @groups.count
-    limit = params.fetch(:limit, 50)
-    @pages = (@total < limit) ? 1 : (@total / limit) + 1
+    limit = 2
+    if @total < limit
+      @pages = 1
+    else
+      if @total % limit > 0
+        @pages = @total / limit + 1
+      else
+        @pages = @total / limit
+      end
+    end
     @page = params.fetch(:page, 1).to_i.clamp(1, @pages)
-    @offset = @page == 1 ? 0 : @page * limit
-    @groups.limit(limit).offset(@offset)
+    @offset = @page == 1 ? 0 : ((@page - 1) * limit)
+    @groups = @groups.limit(limit).offset(@offset)
   end
 
   def export
