11ea30b7fd1df08e1ed06a72d82583a7acc075fd
Gregory Kan <kan.gregory@gmail.com>
2020-02-05 14:49:31 +1300

    memoize

--- app/extras/queries/group_stats.rb
+++ app/extras/queries/group_stats.rb
@@ -1,42 +1,29 @@
 class Queries::GroupStats
-  def self.visits_count(group)
-    GroupVisit.where(group_id: group.id).count
+  def self.visits_count(group_id)
+    @visits_count ||= GroupVisit.where(group_id: group_id).count
   end
 
-  def self.comments_count(group)
-    Discussion.where(group_id: group.id).sum { |d| d.comments.count }
+  def self.comments_count(group_ids)
+    @comments_count ||= Discussion.where(group_id: group_ids).sum { |d| d.comments.count }
   end
 
-  def self.discussions_count(group)
-    Discussion.where(group_id: group.id).count
+  def self.discussions_count(group_ids)
+    @discussions_count ||= Discussion.where(group_id: group_ids).count
   end
 
-  def self.polls_count(group)
-    Poll.where(group_id: group.id).count
+  def self.polls_count(group_ids)
+    @polls_count ||= Poll.where(group_id: group_ids).count
   end
 
-  def self.stances_count(group)
-    Poll.where(group_id: group.id).sum(:stances_count)
+  def self.stances_count(group_ids)
+    @stances_count ||= Poll.where(group_id: group_ids).sum(:stances_count)
   end
 
-  def self.visits_count_including_subgroups(group)
-    OrganisationVisit.where(organisation_id: group.id).count
+  def self.org_visits_count(group)
+    @org_visits_count ||= OrganisationVisit.where(organisation_id: group.id).count
   end
 
-  def self.comments_count_including_subgroups(group)
-    Discussion.where(group_id: group.id_and_subgroup_ids).sum { |d| d.comments.count }
+  def self.poll_types_count(group_ids)
+    @poll_types_count ||= Poll.where(group_id: group_ids).group(:poll_type).count
   end
-
-  def self.discussions_count_including_subgroups(group)
-    Discussion.where(group_id: group.id_and_subgroup_ids).count
-  end
-
-  def self.polls_count_including_subgroups(group)
-    Poll.where(group_id: group.id_and_subgroup_ids).count
-  end
-
-  def self.stances_count_including_subgroups(group)
-    Poll.where(group_id: group.id_and_subgroup_ids).sum(:stances_count)
-  end
-
 end
