e071af4f8e226f4bf703492412feba54ce2183c5
Gregory Kan <kan.gregory@gmail.com>
2020-02-05 15:06:46 +1300

    don't memoize; save results to variable

--- app/extras/queries/group_stats.rb
+++ app/extras/queries/group_stats.rb
@@ -1,29 +1,29 @@
 class Queries::GroupStats
   def self.visits_count(group_id)
-    @visits_count ||= GroupVisit.where(group_id: group_id).count
+    GroupVisit.where(group_id: group_id).count
   end
 
   def self.comments_count(group_ids)
-    @comments_count ||= Discussion.where(group_id: group_ids).sum { |d| d.comments.count }
+    Discussion.where(group_id: group_ids).sum { |d| d.comments.count }
   end
 
   def self.discussions_count(group_ids)
-    @discussions_count ||= Discussion.where(group_id: group_ids).count
+    Discussion.where(group_id: group_ids).count
   end
 
   def self.polls_count(group_ids)
-    @polls_count ||= Poll.where(group_id: group_ids).count
+    Poll.where(group_id: group_ids).count
   end
 
   def self.stances_count(group_ids)
-    @stances_count ||= Poll.where(group_id: group_ids).sum(:stances_count)
+    Poll.where(group_id: group_ids).sum(:stances_count)
   end
 
   def self.org_visits_count(group)
-    @org_visits_count ||= OrganisationVisit.where(organisation_id: group.id).count
+    OrganisationVisit.where(organisation_id: group.id).count
   end
 
   def self.poll_types_count(group_ids)
-    @poll_types_count ||= Poll.where(group_id: group_ids).group(:poll_type).count
+    Poll.where(group_id: group_ids).group(:poll_type).count
   end
 end
