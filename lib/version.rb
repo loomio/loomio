4f24f33c9e13d83f2c8e880a6f01e123f90e6d83
Gregory Kan <kan.gregory@gmail.com>
2020-02-10 10:36:18 +1300

    overhaul version system, simplify

--- lib/version.rb
+++ lib/version.rb
@@ -1,19 +1,7 @@
 module Loomio
   module Version
     def self.current
-      [major, minor, patch].join('.')
-    end
-
-    def self.major
-      File.read("lib/version/major").strip
-    end
-
-    def self.minor
-      File.read("lib/version/minor").strip
-    end
-
-    def self.patch
-      File.read("lib/version/patch").strip
+      "2.1.0"
     end
   end
 end
