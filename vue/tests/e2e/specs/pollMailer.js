d1f987808369fd7667a4f92b344390856d42b0a3
Gregory Kan <kan.gregory@gmail.com>
2020-02-12 12:09:45 +1300

    prevent suite from running automatically, as this might have like 100 individual tests

--- vue/tests/e2e/specs/pollMailer.js
+++ vue/tests/e2e/specs/pollMailer.js
@@ -2,6 +2,8 @@ require('coffeescript/register')
 pageHelper = require('../helpers/pageHelper.coffee')
 
 module.exports = {
+  '@disabled': true,
+  
   'proposal_poll_announced': (test) => {
     page = pageHelper(test)
 
