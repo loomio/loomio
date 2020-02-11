bcedea6cc237bb5407f51a2b185f0e5633c32bd9
Gregory Kan <kan.gregory@gmail.com>
2020-02-12 11:50:48 +1300

    added poll email helper; refactored poll mailer

---
+++ app/helpers/poll_email_helper.rb
@@ -0,0 +1,81 @@
+module PollEmailHelper
+  include Routing
+
+  # def login_token(redirect_path: poll_path(@poll))
+  #   @token ||= @recipient.login_tokens.create!(redirect: redirect_path)
+  # end
+
+  def login_token(recipient, redirect_path)
+    recipient.login_tokens.create!(redirect: redirect_path)
+  end
+
+  def actor(event)
+    @actor ||= if event.eventable.is_a?(Stance)
+      event.eventable.participant_for_client
+    else
+      event.user || LoggedOutUser.new
+    end
+  end
+
+  def recipient_stance(recipient, poll)
+    poll.stances.latest.find_by(participant: recipient)
+  end
+
+  def poll_options(poll)
+    poll.ordered_poll_options
+  end
+
+  # def poll_type(poll)
+  #   poll.poll_type
+  # end
+
+  def undecided(poll)
+    poll.undecided
+  end
+
+  def undecided_max
+    20
+  end
+
+  def time_zone(recipient, poll)
+    recipient.time_zone || poll.time_zone
+  end
+
+  def formatted_time_zone(recipient, poll)
+    ActiveSupport::TimeZone[time_zone].to_s if time_zone(recipient, poll)
+  end
+
+  # def outcome(poll)
+  #   poll.current_outcome
+  # end
+
+  def choice_img(poll)
+    prefix = poll.multiple_choice ? 'check' : 'radio'
+    "poll_mailer/#{prefix}_off.png"
+  end
+
+  def target_url(args = {}, poll, recipient)
+    membership = membership(poll, recipient)
+    args.merge!(membership_token: membership.token) if membership
+    polymorphic_url(poll, poll_mailer_utm_hash(args))
+  end
+
+  def unsubscribe_url(poll, recipient, action_name)
+    poll_unsubscribe_url poll, poll_mailer_utm_hash(action_name).merge(unsubscribe_token: recipient.unsubscribe_token)
+  end
+
+  def poll_mailer_utm_hash(args = {}, action_name)
+    {
+      utm_medium: 'email',
+      utm_campaign: 'poll_mailer',
+      utm_source: action_name
+    }.merge(args)
+  end
+
+  private
+
+  def membership(poll, recipient)
+    poll.guest_group.memberships.find_by(user: recipient)
+  end
+
+end
--- app/mailers/poll_mailer.rb
+++ app/mailers/poll_mailer.rb
@@ -1,4 +1,7 @@
 class PollMailer < BaseMailer
+  helper PollEmailHelper
+  include PollEmailHelper
+
   REPLY_DELIMITER = "--"
 
   %w(poll_created poll_announced poll_edited
@@ -14,34 +17,36 @@ class PollMailer < BaseMailer
   private
 
   def send_poll_email(recipient_id, event_id, action_name)
-    recipient = User.find_by!(id: recipient_id)
-    event = Event.find_by!(id: event_id)
-    return if User::BOT_EMAILS.values.include?(recipient.email)
+    @recipient = User.find_by!(id: recipient_id)
+    @event = Event.find_by!(id: event_id)
+    @action_name = action_name
+    @poll = @event.eventable.poll
+    return if User::BOT_EMAILS.values.include?(@recipient.email)
     headers = {
       "Precedence":               :bulk,
       "X-Auto-Response-Suppress": :OOF,
       "Auto-Submitted":           :"auto-generated"
     }
 
-    @info = PollEmailInfo.new(
-      recipient:   recipient,
-      event:       event,
-      action_name: action_name
-    )
+    # @info = PollEmailInfo.new(
+    #   recipient:   recipient,
+    #   event:       event,
+    #   action_name: action_name
+    # )
 
-    if event.eventable.respond_to?(:calendar_invite) && event.eventable.calendar_invite
+    if @event.eventable.respond_to?(:calendar_invite) && @event.eventable.calendar_invite
       attachments['meeting.ics'] = {
         content_type:              'text/calendar',
         content_transfer_encoding: 'base64',
-        content:                   Base64.encode64(event.eventable.calendar_invite)
+        content:                   Base64.encode64(@event.eventable.calendar_invite)
       }
     end
 
     send_single_mail(
-      locale:        recipient.locale,
-      to:            recipient.email,
-      subject_key:   event.email_subject_key || "poll_mailer.#{@info.poll_type}.subject.#{@info.action_name}",
-      subject_params: { title: @info.poll.title, actor: @info.actor.name },
+      locale:        @recipient.locale,
+      to:            @recipient.email,
+      subject_key:   @event.email_subject_key || "poll_mailer.#{@poll.poll_type}.subject.#{@action_name}",
+      subject_params: { title: @poll.title, actor: actor(@event) },
       layout:        'base_mailer'
     )
   end
