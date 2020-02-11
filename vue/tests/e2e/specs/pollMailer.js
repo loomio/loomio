891b30b2a14dfd79ad09113750946caee16f1f83
Gregory Kan <kan.gregory@gmail.com>
2020-02-12 12:19:35 +1300

    poll_poll_announced test

--- vue/tests/e2e/specs/pollMailer.js
+++ vue/tests/e2e/specs/pollMailer.js
@@ -3,12 +3,19 @@ pageHelper = require('../helpers/pageHelper.coffee')
 
 module.exports = {
   '@disabled': true,
-  
+
   'proposal_poll_announced': (test) => {
     page = pageHelper(test)
 
     page.loadPathNoApp('test_proposal_poll_created_email', { controller: 'polls' })
-    page.expectText('.thread-mailer__subject', "Patrick Swayze invited you to vote on a proposal")
+    page.expectText('.poll-mailer__subject', "invited you to vote on a proposal")
+  },
+
+  'poll_poll_announced': (test) => {
+    page = pageHelper(test)
+
+    page.loadPathNoApp('test_poll_poll_created_email', { controller: 'polls' })
+    page.expectText('.poll-mailer__subject', "invited you to vote in a poll")
   },
 
 }
