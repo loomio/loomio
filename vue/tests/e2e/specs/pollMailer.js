3b6b520eb5b87e65c4bdab42335da97d3f47cb2a
Gregory Kan <kan.gregory@gmail.com>
2020-02-12 14:17:36 +1300

    poll_announced tests and refactor for all poll types

--- vue/tests/e2e/specs/pollMailer.js
+++ vue/tests/e2e/specs/pollMailer.js
@@ -18,4 +18,39 @@ module.exports = {
     page.expectText('.poll-mailer__subject', "invited you to vote in a poll")
   },
 
+  'count_poll_announced': (test) => {
+    page = pageHelper(test)
+
+    page.loadPathNoApp('test_count_poll_created_email', { controller: 'polls' })
+    page.expectText('.poll-mailer__subject', "invited you to participate in a count")
+  },
+
+  'dot_vote_poll_announced': (test) => {
+    page = pageHelper(test)
+
+    page.loadPathNoApp('test_dot_vote_poll_created_email', { controller: 'polls' })
+    page.expectText('.poll-mailer__subject', "invited you to participate in a dot vote")
+  },
+
+  'meeting_poll_announced': (test) => {
+    page = pageHelper(test)
+
+    page.loadPathNoApp('test_meeting_poll_created_email', { controller: 'polls' })
+    page.expectText('.poll-mailer__subject', "invited you to participate in a time poll")
+  },
+
+  'ranked_choice_poll_announced': (test) => {
+    page = pageHelper(test)
+
+    page.loadPathNoApp('test_ranked_choice_poll_created_email', { controller: 'polls' })
+    page.expectText('.poll-mailer__subject', "invited you to rank options in a poll")
+  },
+
+  'score_poll_announced': (test) => {
+    page = pageHelper(test)
+
+    page.loadPathNoApp('test_score_poll_created_email', { controller: 'polls' })
+    page.expectText('.poll-mailer__subject', "invited you to vote in a score poll")
+  },
+
 }
