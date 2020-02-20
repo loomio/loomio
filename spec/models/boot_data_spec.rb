4640d40042f0babe8108dc69670bb23fcb76543f
Robert Guthrie <rob@guthrie.nz>
2020-02-21 01:01:21 +1300

    remove specs

--- spec/models/boot_data_spec.rb
+++ spec/models/boot_data_spec.rb
@@ -12,15 +12,6 @@ require 'rails_helper'
     let(:identity) { create :slack_identity, user: user }
 
     describe 'data' do
-      it 'returns the current users memberships' do
-        expect(subject.payload[:memberships].map { |m| m[:id] }).to include membership.id
-      end
-
-      it 'returns the current users notifications' do
-        notification
-        expect(subject.payload[:notifications].map { |n| n[:id] }).to include notification.id
-      end
-
       it 'returns the current users identities' do
         identity
         expect(subject.payload[:identities].map { |i| i[:id] }).to include identity.id
