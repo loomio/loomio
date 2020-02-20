eece070beb73c6514452cdf9eee99aaca6694126
Robert Guthrie <rob@guthrie.nz>
2020-02-20 20:36:13 +1300

    better migration

--- db/migrate/20200220024259_remove_type_from_groups.rb
+++ db/migrate/20200220024259_remove_type_from_groups.rb
@@ -1,5 +1,7 @@
 class RemoveTypeFromGroups < ActiveRecord::Migration[5.2]
   def change
-    remove_column :groups, :type
+    Membership.joins(:group).where(:'groups.type' => "GuestGroup").delete_all
+    Group.where(type: 'GuestGroup').delete_all
+    remove_column :groups, :type, :string
   end
 end
