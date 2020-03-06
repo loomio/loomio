afded39201cbe5648254d6d391016b6a3b853db9
Gregory Kan <kan.gregory@gmail.com>
2020-03-06 18:19:04 +1300

    all the actions!

--- app/admin/group_surveys.rb
+++ app/admin/group_surveys.rb
@@ -1,6 +1,8 @@
 ActiveAdmin.register GroupSurvey do
   permit_params :group_id, :category, :location, :size, :declaration, :purpose, :usage, :referrer, :role, :website, :misc
 
+  actions :index, :show, :new, :edit, :update, :create
+  
   index do
     column :id
     # column 'Group Id', :group_id
