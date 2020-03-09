323684a83de311495a3956728901828cbbd3626c
Gregory Kan <kan.gregory@gmail.com>
2020-03-10 11:41:05 +1300

    reduce expense of this page; better report is on metabase now

--- app/admin/group_surveys.rb
+++ app/admin/group_surveys.rb
@@ -1,6 +1,5 @@
 ActiveAdmin.register GroupSurvey do
-  includes :group, :subscription
-
+  includes :group
   permit_params :group_id, :category, :location, :size, :declaration, :purpose, :usage, :referrer, :role, :website, :misc
 
   actions :index, :show, :new, :edit, :update, :create
@@ -9,14 +8,18 @@ ActiveAdmin.register GroupSurvey do
     column :id
     column :group_id
     column 'Group Name' do |survey|
-      link_to(survey.group.name, admin_group_path(survey.group)) if survey.group
+      link_to(survey.group.name, admin_group_path(survey.group))
     end
-    column 'Subscription Plan' do |survey|
-      link_to(survey.subscription.plan, admin_subscription_path(survey.subscription)) if survey.subscription
-    end
-    column 'Subscription State' do |survey|
-      link_to(survey.subscription.state, admin_subscription_path(survey.subscription)) if survey.subscription
-    end
+    # column 'Subscription Plan' do |survey|
+    #   group = Group.find(survey.group_id)
+    #   subscription = Subscription.find(group.subscription_id)
+    #   link_to(subscription.plan, admin_subscription_path(subscription))
+    # end
+    # column 'Subscription State' do |survey|
+    #   group = Group.find(survey.group_id)
+    #   subscription = Subscription.find(group.subscription_id)
+    #   link_to(subscription.state, admin_subscription_path(subscription))
+    # end
     column :category
     column :location
     column :size
@@ -30,31 +33,5 @@ ActiveAdmin.register GroupSurvey do
     column :created_at
     column :updated_at
     actions
-  end
-
-  csv do
-    column :id
-    column :group_id
-    column 'Group Name' do |survey|
-      survey.group.name  if survey.group
-    end
-    column 'Subscription Plan' do |survey|
-      survey.subscription.plan if survey.subscription
-    end
-    column 'Subscription State' do |survey|
-      survey.subscription.state if survey.subscription
-    end
-    column :category
-    column :location
-    column :size
-    column :declaration
-    column :purpose
-    column :usage
-    column :referrer
-    column :role
-    column :website
-    column :misc
-    column :created_at
-    column :updated_at
   end
 end
