7233492456dd0f7d2b0999efa144546dfbcd6ccf
Gregory Kan <kan.gregory@gmail.com>
2020-03-09 16:53:35 +1300

    eager load group and subscriptions

--- app/admin/group_surveys.rb
+++ app/admin/group_surveys.rb
@@ -1,24 +1,21 @@
 ActiveAdmin.register GroupSurvey do
+  includes :group, :subscription
+
   permit_params :group_id, :category, :location, :size, :declaration, :purpose, :usage, :referrer, :role, :website, :misc
 
   actions :index, :show, :new, :edit, :update, :create
 
   index do
     column :id
-    # column 'Group Id', :group_id
+    column :group_id
     column 'Group Name' do |survey|
-      group = Group.find(survey.group_id)
-      link_to(group.name, admin_group_path(group))
+      link_to(survey.group.name, admin_group_path(survey.group)) if survey.group
     end
     column 'Subscription Plan' do |survey|
-      group = Group.find(survey.group_id)
-      subscription = Subscription.find(group.subscription_id)
-      link_to(subscription.plan, admin_subscription_path(subscription))
+      link_to(survey.subscription.plan, admin_subscription_path(survey.subscription)) if survey.subscription
     end
     column 'Subscription State' do |survey|
-      group = Group.find(survey.group_id)
-      subscription = Subscription.find(group.subscription_id)
-      link_to(subscription.state, admin_subscription_path(subscription))
+      link_to(survey.subscription.state, admin_subscription_path(survey.subscription)) if survey.subscription
     end
     column :category
     column :location
@@ -33,5 +30,31 @@ ActiveAdmin.register GroupSurvey do
     column :created_at
     column :updated_at
     actions
+  end
+
+  csv do
+    column :id
+    column :group_id
+    column 'Group Name' do |survey|
+      survey.group.name  if survey.group
+    end
+    column 'Subscription Plan' do |survey|
+      survey.subscription.plan if survey.subscription
+    end
+    column 'Subscription State' do |survey|
+      survey.subscription.state if survey.subscription
+    end
+    column :category
+    column :location
+    column :size
+    column :declaration
+    column :purpose
+    column :usage
+    column :referrer
+    column :role
+    column :website
+    column :misc
+    column :created_at
+    column :updated_at
   end
 end
