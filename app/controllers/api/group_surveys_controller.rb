359addf1f22a2d0a0bacf3e641b773aeb749760d
Gregory Kan <kan.gregory@gmail.com>
2020-03-09 13:04:09 +1300

    return success

--- app/controllers/api/group_surveys_controller.rb
+++ app/controllers/api/group_surveys_controller.rb
@@ -2,5 +2,6 @@ class API::GroupSurveysController < API::RestfulController
 
   def create
     service.create(params: resource_params, actor: current_user)
+    render json: { success: :ok }
   end
 end
