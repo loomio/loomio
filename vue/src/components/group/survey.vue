ea30209483224ea01641fef8d643053a71596ab7
Gregory Kan <kan.gregory@gmail.com>
2020-03-09 14:40:42 +1300

    e2es

--- vue/src/components/group/survey.vue
+++ vue/src/components/group/survey.vue
@@ -50,36 +50,36 @@ export default
 </script>
 
 <template lang="pug">
-v-card.group-form
+v-card.group-survey
   submit-overlay(:value='submitting')
   v-card-title
     h1.headline(v-t="'group_survey.title'")
     p.body-1.mt-4(v-t="'group_survey.subtitle'")
   v-card-text
     v-form(ref="form")
-      v-text-field(v-model='survey.location' :label="$t('group_survey.location')" :rules="[rules.required]")
+      v-text-field.group-survey__location(v-model='survey.location' :label="$t('group_survey.location')" :rules="[rules.required]")
 
-      v-radio-group(v-model='survey.category' :label="$t('group_survey.category_question')" :rules="[rules.required]")
-        v-radio(v-for='category in categories' :key='category' :value='category' :aria-label='category' :label="$t('group_survey.categories.' + category)")
+      v-radio-group.group-survey__category(v-model='survey.category' :label="$t('group_survey.category_question')" :rules="[rules.required]")
+        v-radio(v-for='category in categories' :key='category' :value='category' :aria-label='category' :label="$t('group_survey.categories.' + category)" :class="'group-survey__category-' + category")
 
-      v-radio-group(v-model='survey.size' :label="$t('group_survey.size_question')" :rules="[rules.required]")
-        v-radio(v-for='size in sizes' :key='size' :value='size' :aria-label='size' :label="$t('group_survey.sizes.' + size)")
+      v-radio-group.group-survey__size(v-model='survey.size' :label="$t('group_survey.size_question')" :rules="[rules.required]")
+        v-radio(v-for='size in sizes' :key='size' :value='size' :aria-label='size' :label="$t('group_survey.sizes.' + size)" :class="'group-survey__size-' + size")
 
-      v-radio-group(v-model='usage' :label="$t('group_survey.usage')" multiple :rules="[rules.required]")
-        v-radio(v-for='use in uses' :key='use' :value='use' :aria-label='use' :label="$t('group_survey.uses.' + use)")
+      v-radio-group.group-survey__usage(v-model='usage' :label="$t('group_survey.usage')" multiple :rules="[rules.required]")
+        v-radio(v-for='use in uses' :key='use' :value='use' :aria-label='use' :label="$t('group_survey.uses.' + use)" :class="'group-survey__usage-' + use")
 
-      v-radio-group(v-model='survey.referrer' :label="$t('group_survey.referrer')" :rules="[rules.required]")
-        v-radio(v-for='referrer in referrers' :key='referrer' :value='referrer' :aria-label='referrer' :label="$t('group_survey.referrers.' + referrer)")
+      v-radio-group.group-survey__referrer(v-model='survey.referrer' :label="$t('group_survey.referrer')" :rules="[rules.required]")
+        v-radio(v-for='referrer in referrers' :key='referrer' :value='referrer' :aria-label='referrer' :label="$t('group_survey.referrers.' + referrer)" :class="'group-survey__referrer-' + referrer")
 
-      v-text-field(v-model='survey.role' :label="$t('group_survey.role')" :rules="[rules.required]")
+      v-text-field.group-survey__role(v-model='survey.role' :label="$t('group_survey.role')" :rules="[rules.required]")
 
-      v-text-field(v-model='survey.website' :label="$t('group_survey.website')" :rules="[rules.required]")
+      v-text-field.group-survey__website(v-model='survey.website' :label="$t('group_survey.website')" :rules="[rules.required]")
 
-      v-textarea(v-model='survey.misc' :label="$t('group_survey.anything_else')")
+      v-textarea.group-survey__misc(v-model='survey.misc' :label="$t('group_survey.anything_else')")
 
   v-card-actions
     v-spacer
-    v-btn.group-form__submit-button(:disabled="submitting" color="primary" @click='submit()' v-t="'common.action.save'")
+    v-btn.group-survey__submit-button(:disabled="submitting" color="primary" @click='submit()' v-t="'common.action.save'")
 </template>
 <style>
 .v-text-field__details {
