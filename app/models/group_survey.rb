0e4f3d40bee3c719336384bc3262bd830b7032fe
Gregory Kan <kan.gregory@gmail.com>
2020-03-11 09:37:26 +1300

    why are the plugin specs running old code

--- app/models/group_survey.rb
+++ app/models/group_survey.rb
@@ -1,4 +1,5 @@
 class GroupSurvey < ApplicationRecord
   belongs_to :group, class_name: 'FormalGroup', foreign_key: :group_id
+  
   has_one :subscription, through: :group, source: :subscription
 end
