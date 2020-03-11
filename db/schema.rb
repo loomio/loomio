d534bfe5833a422a8061fb4780c5a033c427d6bd
Gregory Kan <kan.gregory@gmail.com>
2020-03-12 11:46:40 +1300

    add gclid to visits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_074536) do
+ActiveRecord::Schema.define(version: 2020_03_11_223004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -809,6 +809,7 @@ ActiveRecord::Schema.define(version: 2020_03_04_074536) do
     t.string "utm_content"
     t.string "utm_campaign"
     t.datetime "started_at"
+    t.string "gclid"
     t.index ["user_id"], name: "index_visits_on_user_id"
   end
 
