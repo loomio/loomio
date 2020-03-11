188272c11287b9f1aed8fda036fdb1cf700451c3
Gregory Kan <kan.gregory@gmail.com>
2020-03-12 12:10:35 +1300

    add gclid to initializer track_visit

--- app/models/visit.rb
+++ app/models/visit.rb
@@ -1,10 +1,4 @@
 class Visit < ApplicationRecord
   has_many :ahoy_events, class_name: "Ahoy::Event"
   belongs_to :user
-
-  before_create :set_gclid
-
-  def set_gclid
-    self.gclid = landing_params["gclid"]
-  end
 end
