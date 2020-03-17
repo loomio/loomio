778b79dacfda8ec4efe52181df117f7e4358151c
Gregory Kan <gregorykan@users.noreply.github.com>
2020-03-18 12:25:43 +1300

    Password manager stuff (#6393)
    * add autocomplete attributes to email and password fields - password managers should be able to pick these up
    
    * add autocomplete attribute to password reset field

--- vue/src/components/auth/email_form.vue
+++ vue/src/components/auth/email_form.vue
@@ -30,7 +30,7 @@ export default
 <template lang="pug">
 .auth-email-form(@keyup.ctrl.enter="submit()" @keydown.meta.enter.stop.capture="submit()" @keydown.enter="submit()")
   .auth-email-form__email
-    v-text-field#email.lmo-primary-form-input(outlined name='email' type='email' :placeholder="$t('auth_form.email_placeholder')" v-model='email')
+    v-text-field#email.lmo-primary-form-input(outlined name='email' type='email' :placeholder="$t('auth_form.email_placeholder')" v-model='email' autocomplete="username email")
     validation-errors(:subject='user' field='email')
     v-btn.auth-email-form__submit(color="primary" @click='submit()' :disabled='!email' v-t="'auth_form.continue_with_email'")
 </template>
