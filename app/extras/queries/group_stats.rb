6638e41b19023b420d978653ca32f50a93cb9c4a
Gregory Kan <kan.gregory@gmail.com>
2020-03-30 16:02:44 +1300

    fix: reinstate visits data for admin group page and SSR group stats page

--- app/extras/queries/group_stats.rb
+++ app/extras/queries/group_stats.rb
@@ -1,7 +1,8 @@
 class Queries::GroupStats
-  # def self.visits_count(group_id)
-  #   GroupVisit.where(group_id: group_id).count
-  # end
+  def self.visits_count(group_id)
+    # GroupVisit.where(group_id: group_id).count
+    Ahoy::Event.where("properties -> 'groupId' is not null and properties ->> 'groupId' = '#{group_id}'").pluck(:visit_id).uniq.count
+  end
 
   def self.comments_count(group_ids)
     Discussion.where(group_id: group_ids).sum { |d| d.comments.count }
@@ -19,9 +20,10 @@ class Queries::GroupStats
     Poll.where(group_id: group_ids).sum(:stances_count)
   end
 
-  # def self.org_visits_count(group)
-  #   OrganisationVisit.where(organisation_id: group.id).count
-  # end
+  def self.org_visits_count(group)
+    # OrganisationVisit.where(organisation_id: group.id).count
+    Ahoy::Event.where("properties -> 'organisationId' is not null and properties ->> 'organisationId' = '#{group.id}'").pluck(:visit_id).uniq.count
+  end
 
   def self.poll_types_count(group_ids)
     Poll.where(group_id: group_ids).group(:poll_type).count
