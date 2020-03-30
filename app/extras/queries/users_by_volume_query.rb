09814a46c1801438c8690e5187bff3b86e448049
Robert Guthrie <rob@guthrie.nz>
2020-03-31 08:54:43 +1300

    ensure latest stance

--- app/extras/queries/users_by_volume_query.rb
+++ app/extras/queries/users_by_volume_query.rb
@@ -16,7 +16,7 @@ class Queries::UsersByVolumeQuery
     User.active.distinct.
       joins("LEFT OUTER JOIN discussion_readers dv ON dv.discussion_id = #{model.discussion_id || 0} AND dv.user_id = users.id").
       joins("LEFT OUTER JOIN memberships m ON m.user_id = users.id AND m.group_id = #{model.group_id || 0}").
-      joins("LEFT OUTER JOIN stances s ON s.participant_id = users.id AND s.poll_id = #{model.poll_id || 0}")
+      joins("LEFT OUTER JOIN stances s ON s.participant_id = users.id AND s.poll_id = #{model.poll_id || 0} AND s.latest = TRUE")
       where('(m.id IS NOT NULL) OR
              (dv.id IS NOT NULL and dv.revoked_at IS NULL AND dv.inviter_id IS NOT NULL) OR
              (s.id IS NOT NULL and s.recoved_at IS NULL AND s.inviter_id IS NOT NULL)').
