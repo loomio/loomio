cb869d1a0bd7b3e7fab1201786ad16625cfdc4cf
Robert Guthrie <rob@guthrie.nz>
2020-03-30 16:33:01 +1300

    fix users by volume query

--- app/extras/queries/users_by_volume_query.rb
+++ app/extras/queries/users_by_volume_query.rb
@@ -13,16 +13,13 @@ class Queries::UsersByVolumeQuery
 
   def self.users_by_volume(model, operator, volume)
     return User.none if model.nil?
-    rel = User.active.distinct
-    if model.is_a?(Group)
-      rel.joins(:memberships)
-         .where("memberships.group_id": model.id)
-         .where("memberships.volume #{operator} ?", volume)
-    else
-      rel.joins_discussion_readers(model)
-         .joins_formal_memberships(model)
-         .where("fm.id is NOT NULL")
-         .where("coalesce(dr.volume, fm.volume, 2) #{operator} :volume", volume: volume)
-    end
+    User.active.distinct.
+      joins("LEFT OUTER JOIN discussion_readers dv ON dv.discussion_id = #{model.discussion_id || 0} AND dv.user_id = users.id").
+      joins("LEFT OUTER JOIN memberships m ON m.user_id = users.id AND m.group_id = #{model.group_id || 0}").
+      joins("LEFT OUTER JOIN stances s ON s.participant_id = users.id AND s.poll_id = #{model.poll_id || 0}")
+      where('(m.id IS NOT NULL) OR
+             (dv.id IS NOT NULL and dv.revoked_at IS NULL AND dv.inviter_id IS NOT NULL) OR
+             (s.id IS NOT NULL and s.recoved_at IS NULL AND s.inviter_id IS NOT NULL)').
+      where("coalesce(s.volume, dr.volume, m.volume, 2) #{operator} :volume", volume: volume)
   end
 end
