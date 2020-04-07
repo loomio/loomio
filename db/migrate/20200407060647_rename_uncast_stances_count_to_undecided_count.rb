63663ac7f8f92a8dd60b30b59856b82f089e48f4
Robert Guthrie <rob@guthrie.nz>
2020-04-07 19:23:40 +1200

    better way to write that

--- db/migrate/20200407060647_rename_uncast_stances_count_to_undecided_count.rb
+++ db/migrate/20200407060647_rename_uncast_stances_count_to_undecided_count.rb
@@ -1,6 +1,6 @@
 class RenameUncastStancesCountToUndecidedCount < ActiveRecord::Migration[5.2]
   def change
-    return if ENV['CANONICAL_HOST'] == 'www.loomio.org'
+    return if column_exists? :polls, :undecided_count
     rename_column :polls, :uncast_stances_count, :undecided_count
   end
 end
