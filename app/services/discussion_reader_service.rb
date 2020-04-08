a71a2f5dbc0bf3d576871b25fb821e926a98e8da
Robert Guthrie <rob@guthrie.nz>
2020-04-09 10:25:27 +1200

    handle case of existing discussion reader

--- app/services/discussion_reader_service.rb
+++ app/services/discussion_reader_service.rb
@@ -2,5 +2,11 @@ class DiscussionReaderService
   def self.redeem(discussion_reader: , actor: )
     actor.ability.authorize! :redeem, discussion_reader
     discussion_reader.update(user: actor, accepted_at: Time.zone.now)
+  rescue ActiveRecord::RecordNotUnique
+    DiscusionReader.find_by(discussion_id: discussion_reader.discussion_id,
+                            user_id: actor.id).
+                    update(inviter_id: discussion_reader.inviter_id,
+                           accepted_at: Time.zone.now)
+    discussion_reader.destroy
   end
 end
