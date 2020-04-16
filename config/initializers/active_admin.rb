1dadc2d0c799bfd179362541c0d4f65fdff1ee3a
Robert Guthrie <rob@guthrie.nz>
2020-04-17 06:27:28 +1200

    fix logout from admin panel

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -3,6 +3,7 @@ ActiveAdmin.setup do |config|
   config.authentication_method = :authenticate_admin_user!
   config.current_user_method = :current_user
   config.logout_link_path = :destroy_user_session_path
+  config.logout_link_method = :delete
   config.root_to = 'groups#index'
   config.batch_actions = true
   config.comments = false
