93f9a69b77e1765b2a2bafa1ef55212da277abdc
Robert Guthrie <rob@guthrie.nz>
2020-04-18 08:52:43 +1200

    fix redirect for guest group memberships

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -9,12 +9,19 @@ class MembershipsController < ApplicationController
 
   def show
     session[:pending_membership_token] = membership.token
-    redirect_to back_to_url || polymorphic_url(membership.group)
+    redirect_to back_to_url || polymorphic_url(target_for(membership))
   rescue ActiveRecord::RecordNotFound
     redirect_to join_url(Group.find_by!(token: params[:token]))
   end
 
   private
+
+  def target_for(membership)
+    group_id = membership.group_id
+    Group.find_by(id: group_id) ||
+    Discussion.find_by(guest_group_id: group_id) ||
+    Poll.find_by(guest_group_id: group_id)
+  end
 
   def membership
     @membership ||= Membership.find_by!(token: params[:token])
