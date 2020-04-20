447b5098026eccb516e95860c523a4fcd2700585
Robert Guthrie <rguthrie@gmail.com>
2020-04-21 10:29:02 +1200

    Update group_query.rb

--- app/queries/group_query.rb
+++ app/queries/group_query.rb
@@ -6,8 +6,8 @@ class GroupQuery
   def self.visible_to(user: , chain: start, show_public: false)
     chain.published
     .joins("LEFT OUTER JOIN memberships m on m.group_id = groups.id AND m.user_id = #{user.id || 0}")
-    .joins("LEFT OUTER JOIN memberships pm on m.group_id = groups.parent_id AND m.user_id = #{user.id || 0}")
-    .where("is_visible_to_parent_members = true OR
+    .joins("LEFT OUTER JOIN memberships pm on pm.group_id = groups.parent_id AND pm.user_id = #{user.id || 0}")
+    .where("#{'groups.is_visible_to_public = true OR ' if show_public}
             (m.id IS NOT NULL AND memberships.archived_at is not null) OR
             (pm.id IS NOT NULL AND pm.is_visible_to_parent_members = true))")
   end
