c84745c101cf04c90a4f07f60939d9150b5aaba4
Robert Guthrie <rob@guthrie.nz>
2020-04-20 17:26:24 +1200

    fix poll query spec

--- app/queries/poll_query.rb
+++ app/queries/poll_query.rb
@@ -18,14 +18,14 @@ class PollQuery
 
     chain = chain.where('polls.group_id IN (:group_ids)', group_ids: group_ids) if group_ids.any?
     chain = chain.joins("LEFT OUTER JOIN discussions on discussions.id = polls.discussion_id") if show_public
-    chain.joins("LEFT OUTER JOIN memberships m ON m.group_id = polls.group_id AND m.user_id = #{user.id || 0}")
-         .joins("LEFT OUTER JOIN discussion_readers dr ON dr.discussion_id = polls.discussion_id AND (dr.user_id = #{user.id || 0} #{or_discussion_reader_token})")
-         .joins("LEFT OUTER JOIN stances s ON s.poll_id = polls.id AND (s.participant_id = #{user.id || 0} #{or_stance_token})")
-         .joins("LEFT OUTER JOIN groups g on g.id = polls.group_id").where("g.archived_at IS NULL")
-         .where("#{'discussions.private = FALSE OR polls.anyone_can_participate = TRUE OR ' if show_public}
-                 (m.id IS NOT NULL AND m.archived_at IS NULL) OR
-                 (dr.id IS NOT NULL AND dr.revoked_at IS NULL AND dr.inviter_id IS NOT NULL) OR
-                 (s.id IS NOT NULL AND s.revoked_at IS NULL)")
+    chain = chain.joins("LEFT OUTER JOIN memberships m ON m.group_id = polls.group_id AND m.user_id = #{user.id || 0}")
+                 .joins("LEFT OUTER JOIN discussion_readers dr ON dr.discussion_id = polls.discussion_id AND (dr.user_id = #{user.id || 0} #{or_discussion_reader_token})")
+                 .joins("LEFT OUTER JOIN stances s ON s.poll_id = polls.id AND (s.participant_id = #{user.id || 0} #{or_stance_token})")
+                 .joins("LEFT OUTER JOIN groups g on g.id = polls.group_id").where("g.archived_at IS NULL")
+                 .where("#{'discussions.private = FALSE OR polls.anyone_can_participate = TRUE OR ' if show_public}
+                         (m.id IS NOT NULL AND m.archived_at IS NULL) OR
+                         (dr.id IS NOT NULL AND dr.revoked_at IS NULL AND dr.inviter_id IS NOT NULL) OR
+                         (s.id IS NOT NULL AND s.revoked_at IS NULL)")
     chain
   end
 
