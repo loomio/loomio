45e916c535e7e3f3e0aa16a1192e642f38406b80
Robert Guthrie <rob@guthrie.nz>
2020-04-22 08:45:26 +1200

    remove routes for group identity

--- config/routes.rb
+++ config/routes.rb
@@ -75,8 +75,6 @@ Loomio::Application.routes.draw do
       end
     end
 
-    resources :group_identities, only: [:create, :destroy]
-
     resources :memberships, only: [:index, :create, :update, :destroy] do
       collection do
         post :add_to_subgroup
