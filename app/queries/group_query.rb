8195efc7f68c73a99b4f8bd3efd1fec6f4bbaedc
Robert Guthrie <rob@guthrie.nz>
2020-04-21 14:11:34 +1200

    fix group query with spec

--- app/queries/group_query.rb
+++ app/queries/group_query.rb
@@ -3,12 +3,12 @@ class GroupQuery
     Group.includes(:subscription, :creator)
   end
 
-  def self.visible_to(user: , chain: start, show_public: false)
+  def self.visible_to(user: LoggedOutUser.new, chain: start, show_public: false)
     chain.published
     .joins("LEFT OUTER JOIN memberships m on m.group_id = groups.id AND m.user_id = #{user.id || 0}")
     .joins("LEFT OUTER JOIN memberships pm on pm.group_id = groups.parent_id AND pm.user_id = #{user.id || 0}")
     .where("#{'groups.is_visible_to_public = true OR ' if show_public}
-            (m.id IS NOT NULL AND m.archived_at is not null) OR
+            (m.id IS NOT NULL AND m.archived_at is null) OR
             (pm.id IS NOT NULL AND groups.is_visible_to_parent_members = true)")
   end
 end
