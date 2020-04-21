873017e7c9e643d54327b8c476e50c715ab456da
Robert Guthrie <rob@guthrie.nz>
2020-04-22 07:54:51 +1200

    remove group identity serializer

--- app/serializers/group_identity_serializer.rb
+++
@@ -1,7 +0,0 @@
-class GroupIdentitySerializer < ActiveModel::Serializer
-  embed :ids, include: true
-  attributes :id, :custom_fields, :group_id
-
-  has_one :group, serializer: Full::GroupSerializer
-  has_one :identity, serializer: IdentitySerializer
-end
