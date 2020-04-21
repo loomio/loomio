9ddfc60068b4d7472779cb9ad3933e28fdd7d8de
Gregory Kan <kan.gregory@gmail.com>
2020-04-22 10:18:16 +1200

    inherit from application serializer

--- app/serializers/stance_choice_serializer.rb
+++ app/serializers/stance_choice_serializer.rb
@@ -1,6 +1,4 @@
-class StanceChoiceSerializer < ActiveModel::Serializer
-  embed :ids, include: true
-
+class StanceChoiceSerializer < ApplicationSerializer
   attributes :id, :score, :created_at, :stance_id, :rank, :rank_or_score, :poll_option_id
 
   has_one :poll_option
