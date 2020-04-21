473314dea611030d3883a56660bba017913839c6
Robert Guthrie <rob@guthrie.nz>
2020-04-22 07:09:27 +1200

    folloup

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,55 +1,55 @@
 class ApplicationSerializer < ActiveModel::Serializer
   embed :ids, include: true
 
-  def exclude_record?(type)
+  def exclude_type?(type)
     !Array((scope || {})[:exclude_types]).include?(type)
   end
 
   def include_discussion?
-    exclude_record?('discussion')
+    exclude_type?('discussion')
   end
 
   def include_group?
-    exclude_record?('group')
+    exclude_type?('group')
   end
 
   def include_eventable?
-    exclude_record?(object.eventable_type.downcase)
+    exclude_type?(object.eventable_type.downcase)
   end
 
   def include_poll_options?
-    exclude_record?('poll_option')
+    exclude_type?('poll_option')
   end
 
   def include_stances?
-    exclude_record?('stance')
+    exclude_type?('stance')
   end
 
   def include_stance_choices?
-    exclude_record?('stance_choice')
+    exclude_type?('stance_choice')
   end
 
   def include_participant?
-    exclude_record?('user')
+    exclude_type?('user')
   end
   
   def include_user?
-    exclude_record?('user')
+    exclude_type?('user')
   end
 
   def include_author?
-    exclude_record?('user')
+    exclude_type?('user')
   end
 
   def include_outcome?
-    exclude_record?('outcome')
+    exclude_type?('outcome')
   end
 
   def include_current_outcome?
-    exclude_record?('outcome')
+    exclude_type?('outcome')
   end
 
   def include_outcomes?
-    exclude_record?('outcome')
+    exclude_type?('outcome')
   end
 end
