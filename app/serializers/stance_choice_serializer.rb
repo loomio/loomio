a6b269c7494d195e2441c77fab3e87166397d950
Gregory Kan <kan.gregory@gmail.com>
2020-04-22 10:17:40 +1200

    include poll_option_id

--- app/serializers/stance_choice_serializer.rb
+++ app/serializers/stance_choice_serializer.rb
@@ -1,7 +1,7 @@
 class StanceChoiceSerializer < ActiveModel::Serializer
   embed :ids, include: true
 
-  attributes :id, :score, :created_at, :stance_id, :rank, :rank_or_score
+  attributes :id, :score, :created_at, :stance_id, :rank, :rank_or_score, :poll_option_id
 
   has_one :poll_option
 end
