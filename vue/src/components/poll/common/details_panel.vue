e675dd01a407e2f83761d4f750b32c218a38795e
Robert Guthrie <rob@guthrie.nz>
2020-05-05 14:03:21 +1200

    fix spacing on poll card

--- vue/src/components/poll/common/details_panel.vue
+++ vue/src/components/poll/common/details_panel.vue
@@ -10,9 +10,9 @@ export default
   v-subheader(v-t="'poll_common.details'" v-if='poll.outcome()')
   .poll-common-details-panel__started-by
     span(v-t="{ path: 'poll_card.started_by', args: { name: poll.authorName() } }")
-    span(aria-hidden='true') ·
+    mid-dot
     poll-common-closing-at(:poll='poll')
-    span(v-if='poll.anonymous') ·
+    mid-dot(v-if='poll.anonymous')
     span(v-if='poll.anonymous', md-colors="{color: 'primary-600', 'border-color': 'primary-600'}" v-t="'common.anonymous'")
   formatted-text.poll-common-details-panel__details(:model="poll" column="details")
   attachment-list(:attachments="poll.attachments")
