1d90bf264319f4da77f518d17ddf6954bbb012ec
Robert Guthrie <rguthrie@gmail.com>
2020-05-15 20:52:02 +1200

    convert paperclip attachments to activestorage attachments in parallel (#6547)
    * first pass of generating activestorage records
    
    * convert paperclip attachments to activestorage in parallel

--- app/models/poll_poll_option.rb
+++
@@ -1,4 +0,0 @@
-class PollPollOption < ApplicationRecord
-  belongs_to :poll, required: true
-  belongs_to :poll_option, required: true
-end
--- app/models/theme.rb
+++
@@ -1,8 +0,0 @@
-class Theme < ApplicationRecord
-  has_attached_file :pages_logo
-  has_attached_file :app_logo
-  validates_attachment_content_type :pages_logo, :content_type => /\Aimage\/.*\Z/
-  validates_attachment_content_type :app_logo, :content_type => /\Aimage\/.*\Z/
-
-  validates_presence_of :name
-end
