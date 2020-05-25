453b257bbc0809b4533c32766b651d4de37cd45a
Robert Guthrie <rob@guthrie.nz>
2020-05-26 09:41:00 +1200

    ok

--- db/migrate/20200521231650_drop_user_from_anonymous_stances_and_events.rb
+++ db/migrate/20200521231650_drop_user_from_anonymous_stances_and_events.rb
@@ -1,13 +1,9 @@
 class DropUserFromAnonymousStancesAndEvents < ActiveRecord::Migration[5.2]
   def change
-    poll_ids = Poll.where(anonymous: true).where("closed_at is not null").pluck(:id)
-    stance_ids = Stance.where(poll_id: poll_ids).pluck(:id)
-    puts "total polls #{Poll.count}, stances #{Stance.count}"
-    puts "anonymous polls #{poll_ids.length}, anonymous stances #{stance_ids.length}"
-    puts "Anonymous stances with nil participant_id: #{Stance.where(poll_id: poll_ids, participant_id: nil).count}"
-    puts "Anonymous stances with participant_id: #{Stance.where(poll_id: poll_ids).where("participant_id is not null").count}"
-    updated_stances_count = Stance.where(poll_id: poll_ids).update_all(participant_id: nil)
-    updated_events_count = Event.where(kind: "stance_created", eventable_id: stance_ids, eventable_type: "Stance").update_all(user_id: nil)
-    puts "updated stances, events", updated_stances_count, updated_events_count
+    closed_anonymous_poll_ids = Poll.where(anonymous: true).where("closed_at is not null").pluck(:id)
+    Stance.where(poll_id: closed_anonymous_poll_ids).update_all(participant_id: nil)
+
+    anonymous_stance_ids = Stance.joins(:poll).where('polls.anonymous': true).pluck(:id)
+    Event.where(kind: "stance_created", eventable_id: anonymous_stance_ids, eventable_type: "Stance").update_all(user_id: nil)
   end
 end
