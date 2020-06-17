7ca8e90a690ea31fee9ca7bb671b7c7beb297ae9
Robert Guthrie <rguthrie@gmail.com>
2020-06-17 15:29:42 +1200

    update pinned to false for discaded comments (#6657)
    * update pinned to false for discaded comments
    
    * schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_02_220450) do
+ActiveRecord::Schema.define(version: 2020_06_17_030449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
