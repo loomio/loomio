e3cd45d06b69e2c99f1eb45b533078c2f1df251a
Loomio Deploy <dev@dev.loomio.io>
2020-06-23 02:20:41 +0000

    remove event from notification serializer

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,7 +1,6 @@
 class NotificationSerializer < ApplicationSerializer
   attributes :id, :viewed, :created_at, :url, :kind, :translation_values, :actor_id, :event_id
   has_one :actor, serializer: AuthorSerializer, root: :users
-  has_one :event, serializer: Events::BaseSerializer, root: :events
 
   def kind
     if object.kind == "announcement_created"
