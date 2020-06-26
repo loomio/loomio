af41f3933743da231500492b39b5c4d43f8d4c98
Robert Guthrie <rob@guthrie.nz>
2020-06-27 11:26:23 +1200

    fix spec

--- app/models/ability/attachment.rb
+++ app/models/ability/attachment.rb
@@ -2,11 +2,11 @@ module Ability::Attachment
   def initialize(user)
     super(user)
 
-    can :show, Attachment do |attachment|
+    can :show, ::Attachment do |attachment|
       user.groups.exists?(attachment.group_id)
     end
 
-    can :destroy, Attachment do |attachment|
+    can :destroy, ::Attachment do |attachment|
       user.adminable_groups.exists?(attachment.group_id)
     end
   end
