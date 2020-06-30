96a671e3859d2d53bd6dc0fcffd46c2961ad2edd
Gregory Kan <kan.gregory@gmail.com>
2020-07-01 11:45:19 +1200

    add new "desired feature" question to group survey

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_030449) do
+ActiveRecord::Schema.define(version: 2020_06_30_233309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -317,6 +317,7 @@ ActiveRecord::Schema.define(version: 2020_06_17_030449) do
     t.text "misc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "desired_feature"
     t.index ["group_id"], name: "index_group_surveys_on_group_id"
   end
 
