634b84267d20415a39074eaf8945e062e63f7dee
Robert Guthrie <rob@guthrie.nz>
2020-07-22 11:13:12 +1200

    fix sql and remove notifications too

--- db/migrate/20200418054222_copy_inviter_ids_to_discussion_readers.rb
+++ db/migrate/20200418054222_copy_inviter_ids_to_discussion_readers.rb
@@ -7,11 +7,16 @@ class CopyInviterIdsToDiscussionReaders < ActiveRecord::Migration[5.2]
              FROM discussions, memberships
              WHERE discussion_readers.discussion_id = discussions.id AND
                    memberships.group_id = discussions.guest_group_id"
+
     execute "DELETE FROM memberships WHERE user_id IS NULL"
     execute "VACUUM FULL memberships"
-    execute "DELETE FROM events
-             USING events e
-             LEFT OUTER JOIN memberships m ON e.eventable_type = 'Membership' and e.eventable_id = m.id
-             WHERE m.id IS NULL"
+
+    execute "CREATE TEMP TABLE membership_event_ids (id INT)"
+    execute "INSERT INTO membership_event_ids (id) select events.id from events LEFT OUTER JOIN memberships ON events.eventable_id = memberships.id where eventable_type='Membership' and memberships.id is null"
+    execute "DELETE FROM events where id in (SELECT id from membership_event_ids)"
+
+    execute "CREATE TEMP TABLE notification_ids (id INT)"
+    execute "INSERT INTO notification_ids (id) SELECT notifications.id FROM notifications LEFT OUTER JOIN events on notifications.event_id = events.id WHERE events.id is null"
+    execute "DELETE FROM notifications WHERE id IN (select id from notification_ids)"
   end
 end
