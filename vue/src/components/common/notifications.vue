b10b658e380f91ed359502cbec1ab3a40cb18082
Robert Guthrie <rob@guthrie.nz>
2020-07-29 11:31:40 +1200

    fix notifciations opening twice

--- vue/src/components/common/notifications.vue
+++ vue/src/components/common/notifications.vue
@@ -32,19 +32,19 @@ export default
 
 </script>
 <template lang="pug">
-v-menu.notifications(offset-y bottom v-model="open")
+v-menu.notifications(offset-y bottom)
   template(v-slot:activator="{on}")
     v-btn.notifications__button(icon v-on="on" :aria-label="$t('navbar.notifications')")
       v-badge(color="accent" v-model="unread.length")
         template(v-slot:badge)
           span.notifications__activity {{unread.length}}
         v-icon mdi-bell
-
-        v-sheet.notifications__dropdown.py-2
-          div(v-if ="notifications.length > 0" v-for="notification in notifications", :key="notification.id")
-            notification(:notification="notification", :unread="unreadIds.includes(notification.id)")
-          v-layout.align-center.justify-center(v-if="notifications.length == 0")
-            span.py-3.px-3(v-t="'notifications.no_notifications'")
+        //-
+        //- v-sheet.notifications__dropdown.py-2
+        //-   div(v-if ="notifications.length > 0" v-for="notification in notifications", :key="notification.id")
+        //-     notification(:notification="notification", :unread="unreadIds.includes(notification.id)")
+        //-   v-layout.align-center.justify-center(v-if="notifications.length == 0")
+        //-     span.py-3.px-3(v-t="'notifications.no_notifications'")
 
   v-sheet.notifications__dropdown
     v-list(v-if="notifications.length > 0" dense)
@@ -52,7 +52,7 @@ v-menu.notifications(offset-y bottom v-model="open")
         v-list-item-avatar
           user-avatar(v-if="n.actor()" :user="n.actor()" size="thirtysix")
         v-list-item-content
-          v-list-item-title.notification__content.body-2
+          v-list-item-title.notification__content
             span(v-html="$t(n.path(), n.args())")
             space
             span(aria-hidden='true') ·
