e866a631bdbce852d0570b2e2c168548d08127e1
Robert Guthrie <rob@guthrie.nz>
2020-08-02 05:58:50 +1200

    fix blowup

--- app/workers/populate_missing_position_keys_worker.rb
+++ app/workers/populate_missing_position_keys_worker.rb
@@ -2,8 +2,7 @@ class PopulateMissingPositionKeysWorker
   include Sidekiq::Worker
 
   def perform(event_id, limit)
-    Event.where("discussion_id is not null and position_key is null and events.id >= ?", event_id).
-          includes(parent: [:parent]).order('events.id').limit(limit).each do |event|
+    Event.where("discussion_id is not null and position_key is null and events.id >= ?", event_id).order('events.id').limit(limit).each do |event|
       event.set_position_and_position_key!
     end
   end
