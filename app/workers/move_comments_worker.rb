f089223ee70518fd91cb67ed38780b7988b7a86f
Gregory Kan <kan.gregory@gmail.com>
2020-08-03 13:30:32 +1200

    let message channel service call as_json

--- app/workers/move_comments_worker.rb
+++ app/workers/move_comments_worker.rb
@@ -68,6 +68,6 @@ class MoveCommentsWorker
 
     ActiveStorage::Attachment.where(record: all_events.map(&:eventable)).update_all(group_id: target_discussion.group_id)
 
-    MessageChannelService.publish_records(Events::DiscussionItemsSerializer.new(target_discussion).as_json, group_ids: [target_discussion.group.id])
+    MessageChannelService.publish_records(Events::DiscussionItemsSerializer.new(target_discussion), group_ids: [target_discussion.group.id])
   end
 end
