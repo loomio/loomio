4bd6c5bb017d7ab3a5b986d7260dad4359c7edbe
Robert Guthrie <rob@guthrie.nz>
2020-08-05 12:02:41 +1200

    fix live update for own stance

--- app/services/message_channel_service.rb
+++ app/services/message_channel_service.rb
@@ -1,10 +1,10 @@
 class MessageChannelService
-  def self.publish_models(models, serializer: nil, root: nil, group_ids: [], user_ids: [])
+  def self.publish_models(models, serializer: nil, scope: {}, root: nil, group_ids: [], user_ids: [])
     models = Array(models)
     model = models.first
     serializer ||= model.is_a?(Event) ? Events::BaseSerializer : "#{model.class}Serializer".constantize
     root       ||= model.is_a?(Event) ? 'events' : model.class.to_s.pluralize.downcase
-    data       =   ActiveModel::ArraySerializer.new(models, each_serializer: serializer, root: root).as_json
+    data       =   ActiveModel::ArraySerializer.new(models, scope: scope, each_serializer: serializer, root: root).as_json
     publish_serialized_records(data, group_ids: group_ids, user_ids: user_ids)
   end
 
