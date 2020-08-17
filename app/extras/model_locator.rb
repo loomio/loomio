f780d2d586010ae6f04927f83d7141da4c6f2cac
Robert Guthrie <rob@guthrie.nz>
2020-08-18 10:40:01 +1200

    fix model locator

--- app/extras/model_locator.rb
+++ app/extras/model_locator.rb
@@ -2,17 +2,16 @@ ModelLocator = Struct.new(:model, :params) do
 
   def locate
     return nil unless defined?(resource_class)
+
     if model.to_sym == :user
       resource_class.verified.find_by(username: params[:id] || params[:username]) || resource_class.friendly.find(params[:id] || params[:user_id])
     elsif model.to_sym == :group
-      (model_key && resource_class.find_by(key: model_key)) ||
-      (model_id && resource_class.find_by(id: model_id)) ||
+      resource_class.find_by(id: key_or_id) ||
+      resource_class.find_by(key: key_or_id) ||
       resource_class.where.not(handle: nil).find_by(handle: params[:id])
     elsif resource_class.respond_to?(:friendly)
-      key_or_id = model_key ? model_key : model_id
       resource_class.friendly.find key_or_id
     else
-      key_or_id = model_key ? model_key : model_id
       resource_class.find key_or_id
     end
   end
@@ -23,14 +22,8 @@ ModelLocator = Struct.new(:model, :params) do
 
   private
 
-  def model_key
-    # strip overloaded id's that chargify gives us in the form of
-    # key-number
-    (params[:"#{model}_key"] ||  params[:key]).to_s.split('-')[0]
-  end
-
-  def model_id
-    (params[:"#{model}_id"] || params[:id]).to_s.split('-')[0]
+  def key_or_id
+    (params[:"#{model}_id"] || params[:"#{model}_key"] ||  params[:key] || params[:id]).to_s.split('-')[0]
   end
 
   def resource_class
