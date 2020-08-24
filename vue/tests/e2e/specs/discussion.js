ef08e5bebeea3c63859bd561f0537f0db751c890
Robert Guthrie <rob@guthrie.nz>
2020-08-24 14:10:56 +1200

    fix e2es

--- vue/tests/e2e/specs/discussion.js
+++ vue/tests/e2e/specs/discussion.js
@@ -209,7 +209,7 @@ module.exports = {
 
     page.loadPath('setup_discussion')
     page.click('.html-editor__expand')
-    page.click('i.mdi-markdown')
+    page.click('i.mdi-language-markdown-outline')
     page.acceptConfirm()
     page.fillIn('.comment-form .lmo-textarea textarea', '@jennifer')
     page.expectText('.suggestion-list', 'Jennifer Grey')
