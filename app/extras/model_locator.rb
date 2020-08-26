0699ab8ccf4f07f6e690ca1c02bb565183c04d41
Robert Guthrie <rob@guthrie.nz>
2020-08-27 11:22:44 +1200

    fix find by friendly id

--- app/extras/model_locator.rb
+++ app/extras/model_locator.rb
@@ -6,8 +6,7 @@ ModelLocator = Struct.new(:model, :params) do
     if model.to_sym == :user
       resource_class.verified.find_by(username: params[:id] || params[:username]) || resource_class.friendly.find(params[:id] || params[:user_id])
     elsif model.to_sym == :group
-      resource_class.find_by(id: key_or_id) ||
-      resource_class.find_by(key: key_or_id) ||
+      resource_class.send(:first_by_friendly_id, key_or_id) ||
       resource_class.where.not(handle: nil).find_by(handle: params[:id])
     elsif resource_class.respond_to?(:friendly)
       resource_class.friendly.find key_or_id
