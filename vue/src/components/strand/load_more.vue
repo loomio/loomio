6cb205808ffb72da63360a8b0a5c5f8ec9bf793f
Robert Guthrie <rob@guthrie.nz>
2020-09-06 11:11:48 +1200

    strand load more

---
+++ vue/src/components/strand/load_more.vue
@@ -0,0 +1,10 @@
+<script lang="coffee">
+export default
+  props:
+    label: Object
+</script>
+
+<template lang="pug">
+.strand-item__load-more
+  v-btn.action-button(text block depressed v-t="label" @click="$emit('click')")
+</style>
