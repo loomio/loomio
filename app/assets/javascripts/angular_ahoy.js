8ced15ac5e9e4685955b3daee05659356e20b7a5
Gregory Kan <kan.gregory@gmail.com>
2020-09-09 11:43:06 +1200

    track visits and events to marketing pages

--- app/assets/javascripts/angular_ahoy.js
+++ app/assets/javascripts/angular_ahoy.js
@@ -1,1 +1,21 @@
 //= require ahoy
+
+ahoy.configure({
+  // urlPrefix: "",
+  visitsUrl: "/ahoy/visits",
+  eventsUrl: "/ahoy/events",
+  // page: null,
+  // platform: "Web",
+  // useBeacon: true,
+  // startOnReady: true,
+  // trackVisits: true,
+  // cookies: true,
+  cookieDomain: "loomio.org",
+  // headers: {},
+  // visitParams: {},
+  // withCredentials: false,
+  // visitDuration: 4 * 60, // 4 hours
+  // visitorDuration: 2 * 365 * 24 * 60 // 2 years
+})
+
+ahoy.trackAll()
