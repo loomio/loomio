2ef55f5b0f1945173e6cf47e51269786fcf8ca15
Gregory Kan <kan.gregory@gmail.com>
2020-09-17 11:50:51 +1200

    migration to populate attachment records with corresponding group id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_15_201352) do
+ActiveRecord::Schema.define(version: 2020_09_16_234818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
