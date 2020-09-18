f95234015fb17a73f5c479c54e5bd95ecc8dabf2
Robert Guthrie <rob@guthrie.nz>
2020-09-19 03:12:47 +1200

    tooltip is overkill

--- vue/src/components/discussion/privacy_badge.vue
+++ vue/src/components/discussion/privacy_badge.vue
@@ -7,29 +7,26 @@ export default
 </script>
 
 <template lang="pug">
-v-tooltip(bottom)
-  template(v-slot:activator="{ on, attrs }")
-    v-chip.context-panel__discussion-privacy(v-bind="attrs" v-on="on" label outlined aria-label="$t('discussion_form.visible_to')")
-      template(v-if='discussion.visibleTo == "discussion"')
-        v-avatar(left tile)
-          v-icon(small color="grey darken-2") mdi-lock-outline
-        span(v-t="'discussion_form.visible_to_discussion'")
-      template(v-if='discussion.visibleTo == "group"')
-        v-avatar(left tile)
-          v-icon(small color="grey darken-2") mdi-account-group
-        span(v-t="{path: 'discussion_form.visible_to_group', args: {group: discussion.group().name}}")
-      template.mr-1(v-if='discussion.visibleTo == "parent_group"')
-        v-avatar(left tile)
-          v-icon(small color="grey darken-2") mdi-domain
-        span(v-t="{path: 'discussion_form.visible_to_parent_group', args: {group: discussion.group().parent().name}}")
-      template.mr-1(v-if='discussion.visibleTo == "public"')
-        v-avatar(left tile)
-          v-icon(small color="grey darken-2") mdi-earth
-        span(v-t="'discussion_form.visible_to_public'")
-      template(v-if='!noGroup && discussion.visibleTo != "discussion"')
-        mid-dot
-        router-link(:to="urlFor(discussion.group())") {{discussion.group().name}}
-  span(v-t="'discussion_form.visible_to_'+discussion.visibleTo+'_tooltip'")
+v-chip.context-panel__discussion-privacy(label outlined :title="$t('discussion_form.visible_to_'+discussion.visibleTo+'_tooltip')")
+  template(v-if='discussion.visibleTo == "discussion"')
+    v-avatar(left tile)
+      v-icon(small color="grey darken-2") mdi-lock-outline
+    span(v-t="'discussion_form.visible_to_discussion'")
+  template(v-if='discussion.visibleTo == "group"')
+    v-avatar(left tile)
+      v-icon(small color="grey darken-2") mdi-account-group
+    span(v-t="{path: 'discussion_form.visible_to_group', args: {group: discussion.group().name}}")
+  template.mr-1(v-if='discussion.visibleTo == "parent_group"')
+    v-avatar(left tile)
+      v-icon(small color="grey darken-2") mdi-domain
+    span(v-t="{path: 'discussion_form.visible_to_parent_group', args: {group: discussion.group().parent().name}}")
+  template.mr-1(v-if='discussion.visibleTo == "public"')
+    v-avatar(left tile)
+      v-icon(small color="grey darken-2") mdi-earth
+    span(v-t="'discussion_form.visible_to_public'")
+  template(v-if='!noGroup && discussion.visibleTo != "discussion"')
+    mid-dot
+    router-link(:to="urlFor(discussion.group())") {{discussion.group().name}}
 </template>
 
 <style lang="sass">
