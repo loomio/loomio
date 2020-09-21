1fbfb30c0b5708bef95041966e75471445ba6965
Robert Guthrie <rob@guthrie.nz>
2020-09-21 14:04:34 +1200

    make recipients notifications count it's own component

--- vue/src/components/common/recipients_autocomplete.vue
+++ vue/src/components/common/recipients_autocomplete.vue
@@ -3,9 +3,13 @@ import EventBus from '@/shared/services/event_bus'
 import Records from '@/shared/services/records'
 import Session from '@/shared/services/session'
 import {map, debounce, without, filter, uniq, uniqBy} from 'lodash'
+import RecipientsNotificationsCount from '@/components/common/recipients_notifications_count'
 
 export default
-  # emits: query, recipients
+  components: {
+    RecipientsNotificationsCount
+  }
+
   props:
     autofocus: Boolean
     group: Object
@@ -134,33 +138,35 @@ export default
 </script>
 
 <template lang="pug">
-v-autocomplete(
-  :autofocus="autofocus"
-  multiple
-  chips
-  return-object
-  auto-select-first
-  hide-no-data
-  hide-selected
-  v-model='recipients'
-  @change="query= null"
-  :search-input.sync="query"
-  item-text='name'
-  :loading="loading"
-  :label="label"
-  :placeholder="placeholder"
-  :items='searchResults')
-  template(v-slot:selection='data')
-    v-chip.chip--select-multi(:value='data.selected' close @click:close='remove(data.item)')
-      v-icon.mr-1(v-if="data.item.type == 'email'" small) mdi-email-outline
-      v-icon.mr-1(v-if="data.item.type == 'group'" small) mdi-account-group
-      user-avatar.mr-1(v-if="data.item.type == 'user'" :user="data.item.user" size="small" no-link)
-      span {{ data.item.name }}
-  template(v-slot:item='data')
-    v-list-item-avatar
-      v-icon(v-if="data.item.type == 'email'" small) mdi-email-outline
-      v-icon.mr-1(v-if="data.item.type == 'group'" small) mdi-account-group
-      user-avatar(v-if="data.item.type == 'user'" :user="data.item.user" size="small" no-link)
-    v-list-item-content.announcement-chip__content
-      v-list-item-title(v-html='data.item.name')
+div
+  v-autocomplete(
+    :autofocus="autofocus"
+    multiple
+    chips
+    return-object
+    auto-select-first
+    hide-no-data
+    hide-selected
+    v-model='recipients'
+    @change="query= null"
+    :search-input.sync="query"
+    item-text='name'
+    :loading="loading"
+    :label="label"
+    :placeholder="placeholder"
+    :items='searchResults')
+    template(v-slot:selection='data')
+      v-chip.chip--select-multi(:value='data.selected' close @click:close='remove(data.item)')
+        v-icon.mr-1(v-if="data.item.type == 'email'" small) mdi-email-outline
+        v-icon.mr-1(v-if="data.item.type == 'group'" small) mdi-account-group
+        user-avatar.mr-1(v-if="data.item.type == 'user'" :user="data.item.user" size="small" no-link)
+        span {{ data.item.name }}
+    template(v-slot:item='data')
+      v-list-item-avatar
+        v-icon(v-if="data.item.type == 'email'" small) mdi-email-outline
+        v-icon.mr-1(v-if="data.item.type == 'group'" small) mdi-account-group
+        user-avatar(v-if="data.item.type == 'user'" :user="data.item.user" size="small" no-link)
+      v-list-item-content.announcement-chip__content
+        v-list-item-title(v-html='data.item.name')
+  recipients-notifications-count(:recipients="recipients")
 </template>
