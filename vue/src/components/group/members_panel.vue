d4fa01d0915d2b6de51cd37708c33fcc423feef3
Gregory Kan <kan.gregory@gmail.com>
2020-10-13 15:11:12 +1300

    revert this but keep table code handy for now

--- vue/src/components/group/members_panel.vue
+++ vue/src/components/group/members_panel.vue
@@ -174,39 +174,58 @@ export default
       shareable-link-modal(v-if='canAddMembers' :group="group")
       v-btn.group-page__requests-tab(:to="urlFor(group, 'members/requests')" v-t="'members_panel.requests'")
 
-    div(v-if="loader.status == 403")
-      p.pa-4.text-center(v-t="'error_page.forbidden'")
-    div(v-else)
-      p.pa-4.text-center(v-if="!memberships.length" v-t="'common.no_results_found'")
-      v-simple-table(v-else)
-        template(v-slot:default)
-          tbody
-            tr(v-for="membership in memberships" :key="membership.id")
-              td.shrink
-                user-avatar(:user='membership.user()' size='32')
-              td
+    v-card(outlined)
+      div(v-if="loader.status == 403")
+        p.pa-4.text-center(v-t="'error_page.forbidden'")
+      div(v-else)
+        p.pa-4.text-center(v-if="!memberships.length" v-t="'common.no_results_found'")
+        v-list(v-else three-line)
+          v-list-item(v-for="membership in memberships" :key="membership.id")
+            v-list-item-avatar(size='48')
+              router-link(:to="urlFor(membership.user())")
+                user-avatar(:user='membership.user()' size='48')
+            v-list-item-content
+              v-list-item-title
                 router-link(:to="urlFor(membership.user())") {{ membership.user().name }}
-                span(v-if="membership.title")
-                  mid-dot
-                  span {{membership.title}}
-                span(v-if="membership.user().shortBio")
-                  space
-                  span.caption.grey--text {{ membership.user().simpleBio() }}
-              td.shrink(v-if="$route.query.subgroups") {{membership.group().name}}
-              td.shrink
+                space
+                span.caption(v-if="$route.query.subgroups") {{membership.group().name}}
+                space
+                span.title.caption {{membership.title}}
+                space
                 v-chip(v-if="membership.admin" small outlined label v-t="'members_panel.admin'")
-              td.shrink
-                membership-dropdown(:membership="membership")
-      v-layout(justify-center)
-        v-btn.my-2(outlined color='accent' v-if="showLoadMore" :loading="loader.loading" @click="loader.fetchRecords()" v-t="'common.action.load_more'")
+              v-list-item-subtitle
+                span(v-if="membership.groupId != group.id" v-t="{path: 'members_panel.only_in_subgroups', args: {name: membership.group().name}}")
+                span(v-if="membership.acceptedAt") {{ (membership.user().shortBio || '').replace(/<\/?[^>]+(>|$)/g, "") }}
+                span(v-if="!membership.acceptedAt && membership.inviter()" v-t="{path: 'members_panel.invited_by_name', args: {name: membership.inviter().name}}")
+            v-list-item-action
+              membership-dropdown(v-if="membership.groupId == group.id" :membership="membership")
+        v-layout(justify-center)
+          v-btn.my-2(outlined color='accent' v-if="showLoadMore" :loading="loader.loading" @click="loader.fetchRecords()" v-t="'common.action.load_more'")
+
+    //- div(v-if="loader.status == 403")
+    //-   p.pa-4.text-center(v-t="'error_page.forbidden'")
+    //- div(v-else)
+    //-   p.pa-4.text-center(v-if="!memberships.length" v-t="'common.no_results_found'")
+    //-   v-simple-table(v-else)
+    //-     template(v-slot:default)
+    //-       tbody
+    //-         tr(v-for="membership in memberships" :key="membership.id")
+    //-           td.shrink
+    //-             user-avatar(:user='membership.user()' size='32')
+    //-           td
+    //-             router-link(:to="urlFor(membership.user())") {{ membership.user().name }}
+    //-             span(v-if="membership.title")
+    //-               mid-dot
+    //-               span {{membership.title}}
+    //-             span(v-if="membership.user().shortBio")
+    //-               space
+    //-               span.caption.grey--text {{ membership.user().simpleBio() }}
+    //-           td.shrink(v-if="$route.query.subgroups") {{membership.group().name}}
+    //-           td.shrink
+    //-             v-chip(v-if="membership.admin" small outlined label v-t="'members_panel.admin'")
+    //-           td.shrink
+    //-             membership-dropdown(:membership="membership")
+    //-   v-layout(justify-center)
+    //-     v-btn.my-2(outlined color='accent' v-if="showLoadMore" :loading="loader.loading" @click="loader.fetchRecords()" v-t="'common.action.load_more'")
 
 </template>
-
-<style lang="sass">
-.members-panel
-  .grow
-    width: 100%
-  .shrink
-    width: 0.1%
-    white-space: nowrap
-</style>
