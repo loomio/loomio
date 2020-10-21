f450ca46e11cc6a82cb6076783251eebb70db7e2
Gregory Kan <kan.gregory@gmail.com>
2020-10-21 16:30:50 +1300

    remove old seen by/history modal and endpoint

--- vue/src/components/thread/context_panel.vue
+++ vue/src/components/thread/context_panel.vue
@@ -77,9 +77,6 @@ export default
           span.nowrap.context-panel__discussion-privacy.context-panel__discussion-privacy--public(v-show='!discussion.private')
             i.mdi.mdi-earth
             span(v-t="'common.privacy.public'")
-        span(v-show='discussion.seenByCount > 0')
-          mid-dot
-          a.context-panel__seen_by_count(v-t="{ path: 'thread_context.seen_by_count', args: { count: discussion.seenByCount } }"  @click="openSeenByModal()")
         span.context-panel__fork-details(v-if='discussion.forkedEvent() && discussion.forkedEvent().discussion()')
           mid-dot
           span(v-t="'thread_context.forked_from'")
