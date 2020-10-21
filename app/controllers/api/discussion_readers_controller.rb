3db69a43245166d6dde5dc8d7034081abcba3f50
Gregory Kan <kan.gregory@gmail.com>
2020-10-21 14:16:03 +1300

    fix query

--- app/controllers/api/discussion_readers_controller.rb
+++ app/controllers/api/discussion_readers_controller.rb
@@ -17,6 +17,6 @@ class API::DiscussionReadersController < API::RestfulController
     @accessible_records ||= DiscussionReader.includes(:user)
       .joins("LEFT OUTER JOIN memberships m ON m.user_id = discussion_readers.user_id")
       .where("(m.group_id = :group_id AND m.accepted_at IS NOT NULL AND archived_at IS NULL)
-              OR (discussion_readers.inviter_id IS NOT NULL AND discussion_readers.revoked_at IS NOT NULL)", group_id: @discussion.group_id)
+              OR (discussion_readers.inviter_id IS NOT NULL AND discussion_readers.revoked_at IS NULL)", group_id: @discussion.group_id)
   end
 end
