f309f3674040eac0c4581a6812b080b4ad00d5da
Robert Guthrie <rob@guthrie.nz>
2020-10-27 04:13:54 +1300

    drop clone

--- vue/src/components/poll/common/outcome_modal.vue
+++ vue/src/components/poll/common/outcome_modal.vue
@@ -28,20 +28,15 @@ export default
   data: ->
     options: []
     bestOption: null
-    clone: null
     isDisabled: false
     searchResults: []
     query: ''
     recipients: []
-    groups: []
     users: []
 
-
   created: ->
-    @clone = @outcome.clone()
-
     if @datesAsOptions()
-      @options = map @clone.poll().pollOptions(), (option) ->
+      @options = map @outcome.poll().pollOptions(), (option) ->
         id:        option.id
         value:     exact(parseISO(option.name))
         attendees: option.stances().length
@@ -49,10 +44,10 @@ export default
       @bestOption = head sortBy @options, (option) ->
         -1 * option.attendees # sort descending, so the best option is first
 
-      Vue.set(@clone, 'calendarInvite', true)
+      Vue.set(@outcome, 'calendarInvite', true)
 
-      @clone.pollOptionId = @outcome.pollOptionId or @bestOption.id
-      @clone.customFields.event_summary = @clone.customFields.event_summary or @clone.poll().title
+      @outcome.pollOptionId = @outcome.pollOptionId or @bestOption.id
+      @outcome.customFields.event_summary = @outcome.customFields.event_summary or @outcome.poll().title
 
   mounted: ->
     @fetchMemberships()
@@ -64,8 +59,8 @@ export default
     findUsers: ->
       chain = Records.users.collection.chain()
 
-      if @clone.groupId
-        chain = chain.find(id: {$in: @clone.group().memberIds()})
+      if @outcome.groupId
+        chain = chain.find(id: {$in: @outcome.group().memberIds()})
 
       chain = chain.find(id: {$nin: [Session.user().id]})
 
@@ -83,55 +78,55 @@ export default
       @users = @findUsers()
 
     submit: ->
-      @clone.customFields.should_send_calendar_invite = @clone.calendarInvite
-      @clone.customFields.event_description = @clone.statement if @datesAsOptions()
+      @outcome.customFields.should_send_calendar_invite = @outcome.calendarInvite
+      @outcome.customFields.event_description = @outcome.statement if @datesAsOptions()
 
-      if @clone.isNew()
+      if @outcome.isNew()
         actionName = "created"
       else
         actionName = "updated"
 
-      @clone.save()
+      @outcome.save()
       .then (data) =>
         Flash.success("poll_common_outcome_form.outcome_#{actionName}")
         @closeModal()
 
-      .catch onError(@clone)
+      .catch onError(@outcome)
 
     datesAsOptions: ->
-      fieldFromTemplate @clone.poll().pollType, 'dates_as_options'
+      fieldFromTemplate @outcome.poll().pollType, 'dates_as_options'
 
     newRecipients: (val) ->
       @recipients = val
-      @clone.recipientAudience = (val.find((i) -> i.type=='audience') || {}).id
-      @clone.recipientUserIds = map filter(val, (o) -> o.type == 'user'), 'id'
-      @clone.recipientEmails = map filter(val, (o) -> o.type == 'email'), 'name'
+      @outcome.recipientAudience = (val.find((i) -> i.type=='audience') || {}).id
+      @outcome.recipientUserIds = map filter(val, (o) -> o.type == 'user'), 'id'
+      @outcome.recipientEmails = map filter(val, (o) -> o.type == 'email'), 'name'
 
   computed:
-    model: -> @clone
+    model: -> @outcome
     audiences: ->
       ret = []
       if @recipients.length == 0
-        if @clone.group()
+        if @outcome.group()
           ret.push
             id: 'group'
-            name: @clone.group().name
-            size: @clone.group().acceptedMembershipsCount
-        if @clone.poll().discussionId
+            name: @outcome.group().name
+            size: @outcome.group().acceptedMembershipsCount
+        if @outcome.poll().discussionId
           ret.push
             id: 'discussion_group'
             name: @$t('announcement.audiences.discussion_group')
-            size: @clone.poll().discussion().membersCount
-        if @clone.poll().stancesCount > 1
+            size: @outcome.poll().discussion().membersCount
+        if @outcome.poll().stancesCount > 1
           ret.push
             id: 'voters'
-            name: @$t('announcement.audiences.voters', pollType: @clone.poll().translatedPollType())
-            size: @clone.poll().stancesCount
-        if @clone.poll().participantsCount > 1
+            name: @$t('announcement.audiences.voters', pollType: @outcome.poll().translatedPollType())
+            size: @outcome.poll().stancesCount
+        if @outcome.poll().participantsCount > 1
           ret.push
             id: 'participants'
             name: @$t('announcement.audiences.participants')
-            size: @clone.poll().participantsCount
+            size: @outcome.poll().participantsCount
 
       ret.filter (a) => a.name.match(new RegExp(@query, 'i'))
 
@@ -139,19 +134,19 @@ export default
 
 <template lang="pug">
 v-card.poll-common-outcome-modal(@keyup.ctrl.enter="submit()" @keydown.meta.enter.stop.capture="submit()")
-  submit-overlay(:value='clone.processing')
+  submit-overlay(:value='outcome.processing')
   v-card-title
     h1.headline
-      span(v-if='clone.isNew()' v-t="'poll_common_outcome_form.new_title'")
-      span(v-if='!clone.isNew()' v-t="'poll_common_outcome_form.update_title'")
+      span(v-if='outcome.isNew()' v-t="'poll_common_outcome_form.new_title'")
+      span(v-if='!outcome.isNew()' v-t="'poll_common_outcome_form.update_title'")
     v-spacer
     dismiss-modal-button(:close="close")
   .poll-common-outcome-form.px-4
     p Wrap up the process by entering and outcome and notiyfing people about it (fixme)
     //- p outcome.pollId {{outcome.pollId}}
-    p audience: {{clone.recipientAudience}}
-    p userIds: {{clone.recipientUserIds}}
-    p userEmails: {{clone.recipientEmails}}
+    p audience: {{outcome.recipientAudience}}
+    p userIds: {{outcome.recipientUserIds}}
+    p userEmails: {{outcome.recipientEmails}}
     //- p audiences: {{audiences}}
     //- p {{recipients.length}}
     recipients-autocomplete(
@@ -167,18 +162,18 @@ v-card.poll-common-outcome-modal(@keyup.ctrl.enter="submit()" @keydown.meta.ente
 
     .poll-common-calendar-invite(v-if='datesAsOptions()')
       .poll-common-calendar-invite__checkbox.poll-common-checkbox-option
-        v-checkbox(v-model='clone.calendarInvite' :label="$t('poll_common_calendar_invite.calendar_invite')")
-      .poll-common-calendar-invite__form(v-if='clone.calendarInvite')
+        v-checkbox(v-model='outcome.calendarInvite' :label="$t('poll_common_calendar_invite.calendar_invite')")
+      .poll-common-calendar-invite__form(v-if='outcome.calendarInvite')
         .poll-common-calendar-invite--pad-top
-          v-select.lmo-flex__grow(v-model='clone.pollOptionId' :items="options" item-value="id" item-text="value" :label="$t('poll_common_calendar_invite.poll_option_id')")
+          v-select.lmo-flex__grow(v-model='outcome.pollOptionId' :items="options" item-value="id" item-text="value" :label="$t('poll_common_calendar_invite.poll_option_id')")
         .poll-common-calendar-invite--pad-top
-          v-text-field.poll-common-calendar-invite__summary(type='text' :placeholder="$t('poll_common_calendar_invite.event_summary_placeholder')" v-model='clone.customFields.event_summary' :label="$t('poll_common_calendar_invite.event_summary')")
-          validation-errors(:subject='clone', field='event_summary')
-          v-text-field.poll-common-calendar-invite__location(type='text' :placeholder="$t('poll_common_calendar_invite.location_placeholder')" v-model='clone.customFields.event_location' :label="$t('poll_common_calendar_invite.location')")
-          //- v-textarea.md-input.poll-common-calendar-invite__description(type='text' :placeholder="$t('poll_common_calendar_invite.event_description_placeholder')" v-model='clone.customFields.event_description' :label="$t('poll_common_calendar_invite.event_description')")
+          v-text-field.poll-common-calendar-invite__summary(type='text' :placeholder="$t('poll_common_calendar_invite.event_summary_placeholder')" v-model='outcome.customFields.event_summary' :label="$t('poll_common_calendar_invite.event_summary')")
+          validation-errors(:subject='outcome', field='event_summary')
+          v-text-field.poll-common-calendar-invite__location(type='text' :placeholder="$t('poll_common_calendar_invite.location_placeholder')" v-model='outcome.customFields.event_location' :label="$t('poll_common_calendar_invite.location')")
+          //- v-textarea.md-input.poll-common-calendar-invite__description(type='text' :placeholder="$t('poll_common_calendar_invite.event_description_placeholder')" v-model='outcome.customFields.event_description' :label="$t('poll_common_calendar_invite.event_description')")
 
-    lmo-textarea.poll-common-outcome-form__statement.lmo-primary-form-input(:model='clone' field='statement' :label="$t('poll_common.statement')" :placeholder="$t('poll_common_outcome_form.statement_placeholder')")
+    lmo-textarea.poll-common-outcome-form__statement.lmo-primary-form-input(:model='outcome' field='statement' :label="$t('poll_common.statement')" :placeholder="$t('poll_common_outcome_form.statement_placeholder')")
       template(v-slot:actions)
         v-btn.poll-common-outcome-form__submit(color="primary" @click='submit()' v-t="'common.action.save'")
-    validation-errors(:subject="clone" field="statement")
+    validation-errors(:subject="outcome" field="statement")
 </template>
