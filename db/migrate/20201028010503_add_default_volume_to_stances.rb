6adcc4f7148e138ab43a3b503f6ddd8f1e3d40b6
Robert Guthrie <rob@guthrie.nz>
2020-10-30 13:46:43 +1300

    fix migrations

--- db/migrate/20201028010503_add_default_volume_to_stances.rb
+++ db/migrate/20201028010503_add_default_volume_to_stances.rb
@@ -1,5 +1,12 @@
 class AddDefaultVolumeToStances < ActiveRecord::Migration[5.2]
   def change
+    max = Stance.order('stances.id desc').limit(1).pluck(:id).first
+    i = 0
+    step = 10000
+    while i < max do
+      execute("update stances set volume = 2 where volume is null and id > #{i} and id <= #{i+step}")
+      i = i + step
+    end
     change_column :stances, :volume, :integer, default: 2, null: false
   end
 end
