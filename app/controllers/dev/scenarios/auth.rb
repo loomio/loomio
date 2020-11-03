da3de3ca4d22abfbb0feee3cbeb98b917238f3b5
Gregory Kan <kan.gregory@gmail.com>
2020-11-03 15:55:12 +1300

    reload?

--- app/controllers/dev/scenarios/auth.rb
+++ app/controllers/dev/scenarios/auth.rb
@@ -10,7 +10,12 @@ module Dev::Scenarios::Auth
 
   def setup_invitation_email_to_user_with_password
     group = create_group
+    another_group = saved fake_group
     user = saved fake_user(password: nil)
+    another_group.add_member! user
+    another_group.add_member! group.creator
+    user.reload
+    group.creator.reload
     params = {recipient_user_ids: [user.id], message: "click accept,
     please
     thanks" }
