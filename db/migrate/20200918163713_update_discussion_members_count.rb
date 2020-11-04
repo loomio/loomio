b5ec2850b2d3a1bf52c39aae8ac01039bd4964ed
Robert Guthrie <rob@guthrie.nz>
2020-11-04 16:53:14 +1300

    faster migration strategy

--- db/migrate/20200918163713_update_discussion_members_count.rb
+++ db/migrate/20200918163713_update_discussion_members_count.rb
@@ -1,6 +1,5 @@
 class UpdateDiscussionMembersCount < ActiveRecord::Migration[5.2]
   def change
-    return if ENV['CANONICAL_HOST'] == 'www.loomio.org'
     execute('UPDATE discussions d SET members_count = (SELECT count(id) FROM discussion_readers dr where dr.discussion_id = d.id) WHERE members_count IS NULL')
   end
 end
--- db/migrate/20200918172224_add_announceable_members_count_to_discussions.rb
+++ db/migrate/20200918172224_add_announceable_members_count_to_discussions.rb
@@ -1,30 +1,30 @@
 class AddAnnounceableMembersCountToDiscussions < ActiveRecord::Migration[5.2]
   def change
-    return if ENV['CANONICAL_HOST'] == 'www.loomio.org'
-
-    execute 'CREATE TEMP table reader_temp (reader_id INT, user_id INT, group_id INT, volume INT)'
-    execute 'CREATE INDEX reader_id_idx ON reader_temp (reader_id)'
-    execute 'INSERT INTO reader_temp (
-        SELECT dr.id as reader_id,
-               dr.user_id as user_id,
-               d.group_id as group_id
-        FROM discussion_readers dr LEFT JOIN discussions d on dr.discussion_id = d.id
-      )'
-
-    execute 'UPDATE reader_temp t SET volume = (
-      SELECT volume FROM memberships WHERE user_id = t.user_id and group_id = t.group_id
-    )'
-
-    max = DiscussionReader.order('id desc').limit(1).pluck(:id).first
-    i = 0
-    step = 10000
-    while i < max do
-      execute(
-        "UPDATE discussion_readers dr SET volume = (
-          SELECT volume FROM reader_temp WHERE reader_id = dr.id
-        ) where dr.volume is null and dr.id > #{i} and dr.id <= #{i+step}")
-      i = i + step
-    end
-
+    # return if ENV['CANONICAL_HOST'] == 'www.loomio.org'
+    #
+    # execute 'CREATE TEMP table reader_temp (reader_id INT, user_id INT, group_id INT, volume INT)'
+    # execute 'CREATE INDEX reader_id_idx ON reader_temp (reader_id)'
+    # execute 'INSERT INTO reader_temp (
+    #     SELECT dr.id as reader_id,
+    #            dr.user_id as user_id,
+    #            d.group_id as group_id
+    #     FROM discussion_readers dr LEFT JOIN discussions d on dr.discussion_id = d.id
+    #   )'
+    #
+    # execute 'UPDATE reader_temp t SET volume = (
+    #   SELECT volume FROM memberships WHERE user_id = t.user_id and group_id = t.group_id
+    # )'
+    #
+    # max = DiscussionReader.order('id desc').limit(1).pluck(:id).first
+    # i = 0
+    # step = 10000
+    # while i < max do
+    #   execute(
+    #     "UPDATE discussion_readers dr SET volume = (
+    #       SELECT volume FROM reader_temp WHERE reader_id = dr.id
+    #     ) where dr.volume is null and dr.id > #{i} and dr.id <= #{i+step}")
+    #   i = i + step
+    # end
+    #
   end
 end
