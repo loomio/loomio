c3e666d337fefc018ac6182d2a0d459454a8bd13
Robert Guthrie <rob@guthrie.nz>
2020-11-08 17:05:08 +1300

    gosh

--- app/services/throttle_service.rb
+++ app/services/throttle_service.rb
@@ -9,7 +9,7 @@ module ThrottleService
   end
 
   def self.can?(key: 'default-key', id: 1, max: 100 , inc: 1, per: 'hour')
-    raise "Throttle per is not hour or day: #{per}"  unless ['hour', 'day'].include? per
+    raise "Throttle per is not hour or day: #{per}"  unless ['hour', 'day'].include? per.to_s
     k = "THROTTLE-#{per.upcase}-#{key}-#{id}"
     Redis::Counter.new(k).increment(inc)
     Redis::Counter.new(k).value <= ENV.fetch('THROTTLE_MAX_'+key, max)
