38fc422af66d9280a6f46386e457fdc98d4c1be0
Robert Guthrie <rob@guthrie.nz>
2020-11-25 06:36:51 +1300

    add sentry-ruby sample rate

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,6 @@
 Sentry.init do |config|
   config.dsn = ENV['SENTRY_PUBLIC_DSN']
-  # config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+  config.breadcrumbs_logger = [:sentry_logger]
   config.send_default_pii = true
+  config.traces_sample_rate = ENV.fetch('SENTRY_SAMPLE_RATE', '0.1').to_f
 end
