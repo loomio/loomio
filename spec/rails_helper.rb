de1db90b6898795e45a3d0c1e54e2dd43c36d591
Robert Guthrie <rob@guthrie.nz>
2020-12-03 14:31:13 +1300

    log redis flushes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,6 +57,7 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 
   config.before(:each) do
+    puts "==== flushing redis ===="
     redis.flushall            # clean before run
     stub_request(:get, /\.chargifypay.com/).
       to_return(status: 200, body: '{"subscription":{"product":{"handle":"test-handle"}}}', headers: {})
