44ba5f446fd3f9d0f35d0565d11b51e456181d60
Robert Guthrie <rob@guthrie.nz>
2020-12-07 18:33:34 +1300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_183328) do
+ActiveRecord::Schema.define(version: 2020_12_07_052702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -354,7 +354,6 @@ ActiveRecord::Schema.define(version: 2020_11_24_183328) do
     t.boolean "members_can_add_members", default: true, null: false
     t.string "membership_granted_upon", default: "approval", null: false
     t.boolean "members_can_edit_discussions", default: true, null: false
-    t.boolean "motions_can_be_edited", default: false, null: false
     t.string "cover_photo_file_name", limit: 255
     t.string "cover_photo_content_type", limit: 255
     t.integer "cover_photo_file_size"
@@ -400,10 +399,11 @@ ActiveRecord::Schema.define(version: 2020_11_24_183328) do
     t.jsonb "info", default: {}, null: false
     t.integer "new_threads_max_depth", default: 2, null: false
     t.boolean "new_threads_newest_first", default: false, null: false
-    t.boolean "admins_can_edit_user_content", default: false, null: false
+    t.boolean "admins_can_edit_user_content", default: true, null: false
     t.boolean "listed_in_explore", default: false, null: false
     t.string "secret_token", default: -> { "gen_random_uuid()" }
     t.string "content_locale"
+    t.boolean "members_can_add_guests", default: true, null: false
     t.index ["archived_at"], name: "index_groups_on_archived_at", where: "(archived_at IS NULL)"
     t.index ["category_id"], name: "index_groups_on_category_id"
     t.index ["cohort_id"], name: "index_groups_on_cohort_id"
