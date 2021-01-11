d389c3e00a315c84323e27008e77d65244737de8
Robert Guthrie <rob@guthrie.nz>
2021-01-12 12:28:39 +1300

    remove integrations table

--- db/migrate/20210111130334_drop_integrations_add_to_webhooks.rb
+++ db/migrate/20210111130334_drop_integrations_add_to_webhooks.rb
@@ -1,9 +1,9 @@
 class DropIntegrationsAddToWebhooks < ActiveRecord::Migration[5.2]
   def change
     drop_table :integrations
-    add_column :token, :string
-    add_column :author_id, :integer
-    add_column :actor_id, :integer
-    add_column :permissions, :integer, array: true, default: [], null: false
+    add_column :webhooks, :token, :string
+    add_column :webhooks, :author_id, :integer
+    add_column :webhooks, :actor_id, :integer
+    add_column :webhook, :permissions, :integer, array: true, default: [], null: false
   end
 end
