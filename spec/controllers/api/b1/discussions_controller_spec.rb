3c9d285b7a9bc9ac1045530fcd44b914a8cdc02a
Robert Guthrie <rob@guthrie.nz>
2021-01-13 13:16:35 +1300

    ensure that you cant create a discussion in another persons gropu

--- spec/controllers/api/b1/discussions_controller_spec.rb
+++ spec/controllers/api/b1/discussions_controller_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 describe API::B1::DiscussionsController do
   let(:group) { create :group }
+  let(:bad_group) { create :group }
   let(:admin) { group.admins.first }
+  let(:bot) { User.create(name: 'bot user', email: 'bot@example.com', email_verified: false) }
 
   describe 'create' do
     it 'happy case' do
-      bot = User.create!(name: 'bot user', email: 'bot@example.com', email_verified: false)
       webhook = Webhook.create(
         group_id: group.id,
         actor_id: bot.id,
@@ -24,7 +25,6 @@ describe API::B1::DiscussionsController do
     end
 
     it 'missing permission' do
-      bot = User.create!(name: 'bot user', email: 'bot@example.com', email_verified: false)
       webhook = Webhook.create(
         group_id: group.id,
         actor_id: bot.id,
@@ -37,13 +37,26 @@ describe API::B1::DiscussionsController do
     end
 
     it 'incorrect key' do
-      Webhook.create(
+      webhook = Webhook.create(
         group_id: group.id,
-        actor_id: group.admins.first.id,
+        actor_id: bot.id,
         author_id: group.admins.first.id,
-        name: 'group admin bot'
+        name: 'group admin bot',
+        permissions: ['create_discussion']
       )
       post :create, params: { title: 'test', group_id: group.id, api_key: 1234 }
+      expect(response.status).to eq 403
+    end
+
+    it 'bad group id' do
+      webhook = Webhook.create(
+        group_id: group.id,
+        actor_id: bot.id,
+        author_id: group.admins.first.id,
+        name: 'group admin bot',
+        permissions: ['create_discussion']
+      )
+      post :create, params: { title: 'test', group_id: bad_group.id, api_key: webhook.token }
       expect(response.status).to eq 403
     end
   end
