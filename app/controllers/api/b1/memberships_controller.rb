f9403b07d0d5eafebcbbf787eec1984d16d43ae9
Robert Guthrie <rob@guthrie.nz>
2021-01-15 11:54:24 +1300

    permission check for read_memberships

--- app/controllers/api/b1/memberships_controller.rb
+++ app/controllers/api/b1/memberships_controller.rb
@@ -1,5 +1,6 @@
 class API::B1::MembershipsController < API::B1::BaseController
   def index
+    raise CanCan::AccessDenied unless current_webhook.permissions.include?('read_memberships')
     instantiate_collection
     respond_with_collection
   end
