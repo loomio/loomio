7c4c6322c8a0b6c8514f2ea62d1f4e137cc89340
Robert Guthrie <rob@guthrie.nz>
2021-02-04 11:14:46 +1300

    validate user name length

--- app/models/user.rb
+++ app/models/user.rb
@@ -64,6 +64,7 @@ class User < ApplicationRecord
   validates_uniqueness_of :email, conditions: -> { where(email_verified: true) }, if: :email_verified?
   validates_uniqueness_of :username, if: :email_verified
   before_validation :generate_username, if: :email_verified
+  validates_length_of :name, maximum: 100
   validates_length_of :username, maximum: 30
   validates_length_of :short_bio, maximum: 5000
   validates_format_of :username, with: /\A[a-z0-9]*\z/, message: I18n.t(:'user.error.username_must_be_alphanumeric')
