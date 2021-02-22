13ddf1f66078568d404055e59cda49f2a68fc0fe
Robert Guthrie <rob@guthrie.nz>
2021-02-23 09:56:43 +1300

    why dont this work?

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -56,9 +56,9 @@ class UserService
 
   # this is for user accounts deactivated with the older method
   def self.reactivate(user_id)
-    user = User.where("deactivated_at is not null").find(user_id)
-    Membership.where(user_id: user_id).update_all(archived_at: nil)
-    group_ids = Membership.where(user_id: user_id).pluck(:group_id)
+    user = User.find(user_id)
+    Membership.where(user_id: user.id).update_all(archived_at: nil)
+    group_ids = Membership.where(user_id: user.id).pluck(:group_id)
     Group.where(id: group_ids).map(&:update_memberships_count)
     user.update(deactivated_at: nil)
   end
