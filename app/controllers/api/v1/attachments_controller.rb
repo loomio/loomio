2e093e7430383ff87553f482734627eddeba1853
Robert Guthrie <rob@guthrie.nz>
2021-03-18 16:50:20 +1300

    fix files panel, fix delete

--- app/controllers/api/v1/attachments_controller.rb
+++ app/controllers/api/v1/attachments_controller.rb
@@ -2,7 +2,9 @@ class API::V1::AttachmentsController < API::V1::RestfulController
   def index
     # Group.find_by(params[:group_id).id_and_subgroup_ids
     group = current_user.groups.find_by!(id: params[:group_id])
-    self.collection = AttachmentQuery.find(group.id_and_subgroup_ids, params[:q], (params[:per] || 50), (params[:from] || 0))
+    group_ids = current_user.group_ids.intersection(group.id_and_subgroup_ids)
+    self.collection = AttachmentQuery.find(group_ids, params[:q], (params[:per] || 20), (params[:from] || 0))
+    self.collection_count = AttachmentQuery.find(group_ids, params[:q], 1000, 0).count
     respond_with_collection
   end
 
