80c06ad3cbd62c973c3713dd7feea5718dd54f20
Robert Guthrie <rob@guthrie.nz>
2021-04-08 16:23:46 +1200

    fix add poll options

--- vue/src/components/poll/common/add_option_modal.vue
+++ vue/src/components/poll/common/add_option_modal.vue
@@ -12,6 +12,7 @@ export default
     close: Function
 
   data: ->
+    originalLength: @poll.pollOptionNames.length
     isDisabled: false
 
   methods:
@@ -40,6 +41,6 @@ v-card.poll-common-add-option-modal
     poll-meeting-form-options-field(:poll="poll" v-if="poll.pollType == 'meeting'" add-options-only)
   v-card-actions
     v-spacer
-    v-btn.poll-add-option__submit(color="primary" :disabled="!poll.isModified()" @click='submit()' :loading="poll.processing")
+    v-btn.poll-add-option__submit(color="primary" :disabled="poll.pollOptionNames.length == originalLength" @click='submit()' :loading="poll.processing")
       span(v-t="'poll_common_add_option.form.add_options'")
 </template>
