3a2640f62690d07dfd1db44814aba11a668969b5
Robert Guthrie <rob@guthrie.nz>
2021-04-16 15:33:36 +1200

    filter empty strings

--- app/workers/add_heading_ids_worker.rb
+++ app/workers/add_heading_ids_worker.rb
@@ -10,7 +10,7 @@ class AddHeadingIdsWorker
       Stance => 'reason',
       Group => 'description'
     }.each_pair do |model, field|
-      model.where("#{field}_format": 'html').where("#{field} is not null").find_each do |r|
+      model.where("#{field}_format": 'html').where("#{field} is not null and #{field} != ''").find_each do |r|
         model.where(id: r.id).update_all(field => HasRichText::add_heading_ids(r[field]))
       end
     end
