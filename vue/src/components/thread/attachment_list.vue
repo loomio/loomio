9f38cbf8a9a01e819f2b3bf8c151ed4f09aa9ff9
Robert Guthrie <rob@guthrie.nz>
2021-05-05 11:08:08 +1200

    update attachment styles

--- vue/src/components/thread/attachment_list.vue
+++ vue/src/components/thread/attachment_list.vue
@@ -1,45 +1,14 @@
 <script lang="coffee">
 import EventBus       from '@/shared/services/event_bus'
 import AbilityService from '@/shared/services/ability_service'
-import prettyBytes from 'pretty-bytes'
+import AttachmentListItem from './attachment_list_item'
 
 export default
+  components: {AttachmentListItem}
   props:
     attachments: [Array, Object]
-    edit:
-      default: null
-      type: Function
-  methods:
-    prettifyBytes: (s) -> prettyBytes(s)
 </script>
 <template lang="pug">
 .attachment-list
-  .attachment-list__item(v-for="attachment in attachments" :key="attachment.id")
-    a.attachment-list__preview(v-if="attachment.preview_url" :href='attachment.download_url', target='_blank')
-      img(:src="attachment.preview_url")
-    .attachment-list__item-details
-      a(:href="attachment.download_url") {{ attachment.filename }}
-      v-btn.ml-2(v-if="edit" icon :aria-label="$t('common.action.delete')" @click='edit')
-        v-icon(size="medium") mdi-delete
-
+  attachment-list-item(v-for="attachment in attachments" :key="attachment.id" :attachment="attachment")
 </template>
-<style lang="sass">
-.attachment-list__item
-  display: flex
-  margin: 8px 0
-  line-height: 32px
-  background: #f6f6f6
-  border-radius: 2px
-
-.attachment-list__item-details
-  padding: 0 4px
-  display: flex
-  flex-direction: row
-  align-items: center
-
-.attachment-list__preview img
-  max-height: 128px
-  max-width: 100%
-.attachment-list__preview
-  width: 128px
-</style>
---
+++ vue/src/components/thread/attachment_list_item.vue
@@ -0,0 +1,60 @@
+<script lang="coffee">
+import prettyBytes from 'pretty-bytes'
+export default
+  props:
+    attachment: Object
+
+  data: ->
+    backgroundSize: 'contain'
+    backgroundPosition: 'center'
+
+  mounted: ->
+    @setBackgroundSize()
+
+  methods:
+    prettifyBytes: (s) -> prettyBytes(s)
+    setBackgroundSize: ->
+      return unless @attachment.preview_url
+      url = @attachment.preview_url
+      img = new Image();
+      that = @
+      img.onload = ->
+        if (Math.abs(@width - @height) < @width/3)
+          that.backgroundSize = 'contain'
+          that.backgroundPosition = 'center'
+        else
+          that.backgroundSize = 'cover'
+          that.backgroundPosition = '0 5%'
+      img.src = @attachment.preview_url
+
+  watch:
+    'attachment.preview_url': 'setBackgroundSize'
+</script>
+<template lang="pug">
+v-card.mt-3(outlined).attachment-list-item-link
+  a(:href="attachment.download_url" target="_blank")
+    v-card-title
+      v-icon.mr-2(small) mdi-{{attachment.icon}}
+      span.text--secondary
+        |{{ attachment.filename }}
+        space
+        span.text-caption
+          |{{ prettifyBytes(attachment.byte_size) }}
+    .attachment-list-item-image(v-if="attachment.preview_url" :style="{'background-image': 'url('+attachment.preview_url+')', 'background-size': backgroundSize, 'background-position': backgroundPosition}")
+</template>
+
+<style lang="sass">
+.attachment-list-item-link:hover
+  background-color: #ededed
+
+.link-preview__hostname
+  word-break: break-word
+
+.attachment-list-item-image
+  background-repeat: no-repeat
+  height: 160px
+  overflow: none
+  // background-position: center
+  // max-width: 512px
+  // margin: 0 auto
+</style>
