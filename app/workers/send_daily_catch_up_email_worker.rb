81d15553b0eec5c4c0fa6428e17fd5de18f9876b
Robert Guthrie <rob@guthrie.nz>
2021-05-05 15:45:16 +1200

    catchup every second day

--- app/workers/send_daily_catch_up_email_worker.rb
+++ app/workers/send_daily_catch_up_email_worker.rb
@@ -9,9 +9,15 @@ class SendDailyCatchUpEmailWorker
       if Time.find_zone(zone)
         time_in_zone = DateTime.now.in_time_zone(zone)
         if time_in_zone.hour == 6
+          days = [7, time_in_zone.wday, (time_in_zone.wday % 2 == 1) ? 8 : nil].compact
           User.active.verified.where(time_zone: zone)
-          .where('email_catch_up_day = 7 or email_catch_up_day = ?', time_in_zone.wday).find_each do |user|
-            period = user.email_catch_up_day == 7 ? 'daily' : 'weekly'
+          .where(email_catch_up_day: days).find_each do |user|
+            period = case user.email_catch_up_day
+              when 8 then 'other'
+              when 7 then 'daily'
+              else
+                'weekly'
+              end
             UserMailer.delay(queue: :low).catch_up(user.id, nil, period)
           end
         end
