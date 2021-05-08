c23bc8c7de227cba730d6d05e7dfac816dccf5b9
Robert Guthrie <rob@guthrie.nz>
2021-05-08 13:31:02 +1200

    vtfy system

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
-Rails.application.config.assets.precompile += %w(basic/all.css
+Rails.application.config.assets.precompile += %w(basic/all.css vtfy/all.css
   active_admin.css slack.css active_admin.js active_admin/print.css angular_ahoy.js
-  marketing.js email.css poll_mailer.css marketing/index.scss admin.css admin.js marketing/marketing.js
+  marketing.js email.css vuetify.css poll_mailer.css marketing/index.scss admin.css admin.js marketing/marketing.js
   *.png *.jpg *.jpeg *.gif *.svg)
