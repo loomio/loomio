e8a873a0da066a549c2566a92a81b40d9f950f8b
Robert Guthrie <rob@guthrie.nz>
2021-05-25 17:03:59 +1200

    wtf mode

--- vue/src/components/tasks/page.vue
+++ vue/src/components/tasks/page.vue
@@ -5,6 +5,8 @@ import Session            from '@/shared/services/session'
 import EventBus           from '@/shared/services/event_bus'
 import AbilityService     from '@/shared/services/ability_service'
 
+import {groupBy} from 'lodash'
+
 export default
   data: ->
     records: {}
@@ -29,9 +31,18 @@ export default
 <template lang="pug">
 v-main
   v-container.dashboard-page.max-width-1024
-    h1.display-1.my-4(tabindex="-1" v-observe-visibility="{callback: titleVisible}" v-t="'tasks.tasks'")
-    v-card.mb-3
-      v-list
-        v-list-item(v-for="task in tasks" :key="task.id")
-          v-list-item-title {{task.name}}
+    h1.display-1.my-4(tabindex="-1" v-observe-visibility="{callback: titleVisible}" v-t="'tasks.your_tasks'")
+
+    loading(v-if="Object.keys(records).length == 0")
+    template(v-for="(tasks, recordKey) in tasksByRecordKey")
+      v-card.mb-3
+        v-card-title
+          router-link(:to="urlFor(records[recordKey])") {{records[recordKey].discussion().title}}
+        v-list(subheader)
+          v-list-item(v-for="task in tasks" :key="task.id")
+            v-list-item-action
+              v-btn(color="accent" icon @click="toggleDone(task)")
+                v-icon(v-if="task.done") mdi-checkbox-marked
+                v-icon(v-else) mdi-checkbox-blank-outline
+            v-list-item-title {{task.name}}
 </template>
