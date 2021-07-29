529ba6b525785b0e36b62426018378c9a56d3ba3
Robert Guthrie <rob@guthrie.nz>
2021-07-30 11:31:29 +1200

    ok

--- vue/src/components/poll/common/percent_voted.vue
+++ vue/src/components/poll/common/percent_voted.vue
@@ -4,6 +4,9 @@ export default
     poll: Object
 </script>
 
-<template>
-  <span class="text--secondary" v-if="poll.closingAt" v-t="{ path: 'poll_common_percent_voted.sentence', args: { numerator: poll.decidedVotersCount, denominator: poll.votersCount, percent: poll.castStancesPct } }"></span>
+<template lang="pug">
+p.text--secondary.my-2(
+  v-if="poll.closingAt"
+  v-t="{ path: 'poll_common_percent_voted.sentence', args: { numerator: poll.decidedVotersCount, denominator: poll.votersCount, percent: poll.castStancesPct } }"
+)
 </template>
