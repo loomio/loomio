7cc4fc741b23efb11d761cf408e3ebb9ab9ab079
Robert Guthrie <rob@guthrie.nz>
2021-08-06 07:41:46 +1200

    dont upset other polls with the reset migration

--- db/migrate/20210805145847_reset_priority_for_old_proposal_poll_options.rb
+++ db/migrate/20210805145847_reset_priority_for_old_proposal_poll_options.rb
@@ -1,8 +1,8 @@
 class ResetPriorityForOldProposalPollOptions < ActiveRecord::Migration[6.0]
   def change
-    PollOption.joins(:poll).where('polls.poll_type': 'proposal', name: 'agree').update_all(priority: 0)
-    PollOption.joins(:poll).where('polls.poll_type': 'proposal', name: 'abstain').update_all(priority: 1)
-    PollOption.joins(:poll).where('polls.poll_type': 'proposal', name: 'disagree').update_all(priority: 2)
-    PollOption.joins(:poll).where('polls.poll_type': 'proposal', name: 'block').update_all(priority: 3)
+    # PollOption.joins(:poll).where('polls.poll_type': 'proposal', name: 'agree', priority: 0).update_all(priority: 0)
+    PollOption.joins(:poll).where('polls.poll_type': 'proposal', name: 'abstain', priority: 0).update_all(priority: 1)
+    PollOption.joins(:poll).where('polls.poll_type': 'proposal', name: 'disagree', priority: 0).update_all(priority: 2)
+    PollOption.joins(:poll).where('polls.poll_type': 'proposal', name: 'block', priority: 0).update_all(priority: 3)
   end
 end
