f88c33045a84d7654e1ec7af9601c01fefc4813b
Robert Guthrie <rob@guthrie.nz>
2021-08-10 11:22:45 +1200

    try to ensure we dont double send.

--- app/workers/send_daily_catch_up_email_worker.rb
+++ app/workers/send_daily_catch_up_email_worker.rb
@@ -3,15 +3,12 @@ class SendDailyCatchUpEmailWorker
   sidekiq_options retry: false, backtrace: true
 
   def perform
-    # email users who have day = 7 and hour = 6
-    # and email users with day = today and hour = 6
-    User.distinct.pluck(:time_zone).each do |zone|
+    User.distinct.pluck(:time_zone).uniq.each do |zone|
       if Time.find_zone(zone)
         time_in_zone = DateTime.now.in_time_zone(zone)
         if time_in_zone.hour == 6
           days = [7, time_in_zone.wday, (time_in_zone.wday % 2 == 1) ? 8 : nil].compact
-          User.active.verified.where(time_zone: zone)
-          .where(email_catch_up_day: days).find_each do |user|
+          User.distinct.active.verified.where(time_zone: zone).where(email_catch_up_day: days).find_each do |user|
             period = case user.email_catch_up_day
               when 8 then 'other'
               when 7 then 'daily'
