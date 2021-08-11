45269bf86d21a93b85e9d359514a578b97498772
Robert Guthrie <rob@guthrie.nz>
2021-08-11 15:58:59 +1200

    gosh I'm bad at releases

--- db/migrate/20210801040439_update_poll_stance_data.rb
+++ db/migrate/20210801040439_update_poll_stance_data.rb
@@ -1,9 +1,4 @@
 class UpdatePollStanceData < ActiveRecord::Migration[6.0]
   def change
-    put "migrating poll data, please wait"
-    Poll.find_each do |poll|
-      poll.stances.each(&:update_option_scores!)
-      poll.update_counts!
-    end
   end
 end
--- db/migrate/20210809001931_fix_anonymous_stances.rb
+++ db/migrate/20210809001931_fix_anonymous_stances.rb
@@ -1,10 +1,10 @@
 class FixAnonymousStances < ActiveRecord::Migration[6.0]
   def change
     return if ENV['CANONICAL_HOST'] == 'www.loomio.org'
-    poll_ids = Poll.where(anonymous: true).where("closed_at is not null").pluck(:id)
-    Stance.where(poll_id: poll_ids).update_all(latest: false)
-    Stance.where(poll_id: poll_ids).where('cast_at is not null').update_all(latest: true)
-    Stance.where(poll_id: poll_ids).where('cast_at is not null').find_each(&:update_option_scores!)
-    Poll.where(id: poll_ids).find_each(&:update_counts!)
+    put "migrating poll data, please wait"
+    Poll.find_each do |poll|
+      poll.stances.each(&:update_option_scores!)
+      poll.update_counts!
+    end
   end
 end
