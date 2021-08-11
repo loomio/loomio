1aa392751a46bf8716378ffcfdd6b76e74e28641
Robert Guthrie <rob@guthrie.nz>
2021-08-11 21:34:39 +1200

    damnit

--- db/migrate/20210809001931_fix_anonymous_stances.rb
+++ db/migrate/20210809001931_fix_anonymous_stances.rb
@@ -1,7 +1,7 @@
 class FixAnonymousStances < ActiveRecord::Migration[6.0]
   def change
     return if ENV['CANONICAL_HOST'] == 'www.loomio.org'
-    put "migrating poll data, please wait"
+    puts "migrating poll data, please wait"
     Poll.find_each do |poll|
       poll.stances.each(&:update_option_scores!)
       poll.update_counts!
