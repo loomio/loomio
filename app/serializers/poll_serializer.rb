6f4c37c4dea1ba424ff8c62cfdc787a1f79af741
Robert Guthrie <rob@guthrie.nz>
2021-08-14 20:36:06 +1200

    allow long vote reasons with boolean on poll

--- app/serializers/poll_serializer.rb
+++ app/serializers/poll_serializer.rb
@@ -21,6 +21,7 @@ class PollSerializer < ApplicationSerializer
              :group_id,
              :hide_results_until_closed,
              :key,
+             :allow_long_reason,
              :multiple_choice,
              :notify_on_closing_soon,
              :poll_type,
