f3d72c7a3e2951bc552a15cdb1c1c95fadfca0a6
Robert Guthrie <rob@guthrie.nz>
2021-08-18 17:32:35 +1200

    ok

--- app/controllers/api/v1/boot_controller.rb
+++ app/controllers/api/v1/boot_controller.rb
@@ -4,7 +4,7 @@ class API::V1::BootController < API::V1::RestfulController
     EventBus.broadcast('boot_site', current_user)
   end
 
-  # temp
+  # temp1
   def version
     render json: {
       version: Loomio::Version.current,
