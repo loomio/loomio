df5d390d6c306575f247476d1965c7e90cff1e91
Robert Guthrie <rob@guthrie.nz>
2021-08-27 11:42:07 +1200

    add migrate paperclip attachments migration

---
+++ db/migrate/20210826234109_migrate_paperclip_attachments.rb
@@ -0,0 +1,24 @@
+class MigratePaperclipAttachments < ActiveRecord::Migration[6.0]
+  def up
+    Rails.application.eager_load!
+    models = ActiveRecord::Base.descendants.reject(&:abstract_class?)
+
+    models.each do |model|
+      attachments = model.column_names.map do |c|
+        if c =~ /(.+)_file_name$/
+          $1
+        end
+      end.compact
+
+      next if attachments.blank?
+
+      model.find_each.each do |instance|
+        puts instance.to_s
+        attachments.each do |attachment|
+          next if instance.send(attachment).path.blank? or ActiveStorage::Attachment.where(record_type: instance.class.to_s, record_id: instance.id, name: attachment).exists?
+          MigrateAttachmentWorker.perform_async(instance.class.to_s, instance.id, attachment)
+        end
+      end
+    end
+  end
+end
