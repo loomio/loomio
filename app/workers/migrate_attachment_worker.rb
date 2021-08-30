9f02b7bb5fc2b4565341aaa7c33a35d413fcb6ab
Robert Guthrie <rob@guthrie.nz>
2021-08-30 22:08:20 +1200

    dont put up with nil

--- app/workers/migrate_attachment_worker.rb
+++ app/workers/migrate_attachment_worker.rb
@@ -4,6 +4,10 @@ class MigrateAttachmentWorker
   def perform(record_type, record_id, name)
     model = record_type.constantize.find(record_id)
     paperclip = Paperclip::Attachment.new(name, model)
-    model.send(name).attach(io: open(paperclip.url), filename: File.basename(paperclip.url))
+    if paperclip.url == '/uploaded_avatars/original/missing.png'
+      model.update_columns("#{name}_file_name": nil)
+    else
+      model.send(name).attach(io: open(paperclip.url), filename: File.basename(paperclip.url))
+    end
   end
 end
