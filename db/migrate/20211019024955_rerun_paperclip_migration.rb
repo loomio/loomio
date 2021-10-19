b2b7f015e3c00f5ce2381e99e8e09e2d33990b25
Robert Guthrie <rob@guthrie.nz>
2021-10-20 08:27:15 +1300

    skip for loomio.org

--- db/migrate/20211019024955_rerun_paperclip_migration.rb
+++ db/migrate/20211019024955_rerun_paperclip_migration.rb
@@ -1,5 +1,6 @@
 class RerunPaperclipMigration < ActiveRecord::Migration[6.1]
   def change
+    return if ENV['CANONICAL_HOST'] == 'www.loomio.org'
     MigrateEventsService.migrate_paperclip
   end
 end
