fb5368275b6278b03bda72a3983d41b5b01c4cd7
Robert Guthrie <rob@guthrie.nz>
2021-10-23 13:36:54 +1300

    fix discussino generation specs

--- app/controllers/dev/discussions_controller.rb
+++ app/controllers/dev/discussions_controller.rb
@@ -8,42 +8,34 @@ class Dev::DiscussionsController < Dev::BaseController
   end
 
   def test_some_read
-    BaseMailer.skip do
-      discussion = create_discussion_with_nested_comments
-      EventService.repair_thread(discussion.id)
-      discussion.author.experienced!('betaFeatures')
-      sign_in discussion.author
-      read_ids = discussion.items.order(sequence_id: :asc).limit(5).pluck(:sequence_id)
-      DiscussionReader.for_model(discussion, discussion.author).viewed!(read_ids)
-      redirect_to discussion_url(discussion)
-    end
+    discussion = create_discussion_with_nested_comments
+    EventService.repair_thread(discussion.id)
+    discussion.author.experienced!('betaFeatures')
+    sign_in discussion.author
+    read_ids = discussion.items.order(sequence_id: :asc).limit(5).pluck(:sequence_id)
+    DiscussionReader.for_model(discussion, discussion.author).viewed!(read_ids)
+    redirect_to discussion_url(discussion)
   end
 
   def test_most_read
-    BaseMailer.skip do
-      discussion = create_discussion_with_nested_comments
-      sign_in discussion.author
-      read_ids = discussion.items.order(sequence_id: :asc).limit(5).pluck(:sequence_id)
-      DiscussionReader.for_model(discussion, discussion.author).viewed!(read_ids)
-      redirect_to discussion_url(discussion)
-    end
+    discussion = create_discussion_with_nested_comments
+    sign_in discussion.author
+    read_ids = discussion.items.order(sequence_id: :asc).limit(5).pluck(:sequence_id)
+    DiscussionReader.for_model(discussion, discussion.author).viewed!(read_ids)
+    redirect_to discussion_url(discussion)
   end
 
   def test_all_read
-    BaseMailer.skip do
-      discussion = create_discussion_with_nested_comments
-      sign_in discussion.author
-      read_ids = discussion.items.order(sequence_id: :asc).pluck(:sequence_id)
-      DiscussionReader.for_model(discussion, discussion.author).viewed!(read_ids)
-      redirect_to discussion_url(discussion)
-    end
+    discussion = create_discussion_with_nested_comments
+    sign_in discussion.author
+    read_ids = discussion.items.order(sequence_id: :asc).pluck(:sequence_id)
+    DiscussionReader.for_model(discussion, discussion.author).viewed!(read_ids)
+    redirect_to discussion_url(discussion)
   end
 
   def test_sampled_comments
-    BaseMailer.skip do
-      discussion = create_discussion_with_sampled_comments
-      sign_in discussion.author
-      redirect_to discussion_url(discussion)
-    end
+    discussion = create_discussion_with_sampled_comments
+    sign_in discussion.author
+    redirect_to discussion_url(discussion)
   end
 end
