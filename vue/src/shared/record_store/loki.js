3c2d111719c88f56289e4ce2d1b6a254d16f3288
Robert Guthrie <rob@guthrie.nz>
2021-11-03 21:37:49 +1300

    ok

--- vue/src/shared/record_store/loki.js
+++ vue/src/shared/record_store/loki.js
@@ -1,3 +1,24 @@
+
+// here is a test script I used to understand if I got the sorting right
+// https://rawgit.com/techfort/LokiJS/master/examples/sandbox/LokiSandbox.htm
+// testdb = new loki('test.db');
+// events = testdb.addCollection('events', {indices: ['key']});
+//
+// input = [
+//   {key: '001'},
+//   {key: '001-001'},
+//   {key: '001-002'},
+//   {key: '002'},
+//   {key: '002-001'},
+// ]
+// events.insert(input)
+//
+// keys = events.chain().simplesort('key',  { useJavascriptSorting: true }).data().map( o => o.key)
+// logText('result')
+// logObject(keys)
+// logText('desired result')
+// logObject(input.map( o => o.key))
+
 import loki from 'lokijs'
 
 var cmp = function(a, b) {
