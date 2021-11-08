b4e80397cada84c6124b4a2101252dca42347533
Robert Guthrie <rob@guthrie.nz>
2021-11-08 15:21:31 +1300

    handle no discussion ids in admin group page stats

--- app/extras/queries/admin_group_page.rb
+++ app/extras/queries/admin_group_page.rb
@@ -13,7 +13,11 @@ class Queries::AdminGroupPage
 
   def self.thread_events_per_day(group)
     ids = Discussion.where(group_id: group.id_and_subgroup_ids).pluck(:id)
-    "select date_trunc('day', created_at) date, count(id) from events where discussion_id IN (#{ids.join(',')}) group by date order by date"
+    if ids.length
+      "select date_trunc('day', created_at) date, count(id) from events where discussion_id IN (#{ids.join(',')}) group by date order by date"
+    else
+      "select date_trunc('day', created_at) date, count(id) from events where discussion_id = 0 group by date order by date"
+    end
   end
 
   def self.execute(sql)
