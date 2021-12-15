3416c5123d8b85d27dfa8ad3428a6681bc6bd60b
Robert Guthrie <rob@guthrie.nz>
2021-12-15 16:45:31 +1300

    deduplicated stance-created, stance-updated and add a timestamp

--- vue/src/components/strand/item/stance_updated.vue
+++ vue/src/components/strand/item/stance_updated.vue
@@ -1,41 +1,14 @@
 <script lang="coffee">
-import Session        from '@/shared/services/session'
-import StanceService        from '@/shared/services/stance_service'
-import AbilityService from '@/shared/services/ability_service'
-import openModal from '@/shared/helpers/open_modal'
+import StanceUpdated from '@/components/strand/item/stance_created.vue'
 
 export default
+  components: {StanceUpdated}
   props:
     event: Object
     eventable: Object
     collapsed: Boolean
-
-  computed:
-    poll: -> @eventable.poll()
-    showResults: -> @poll.showResults()
-    actions: -> StanceService.actions(@eventable)
-
-    componentType:  ->
-      if @event.actor()
-        'router-link'
-      else
-        'div'
 </script>
 
 <template lang="pug">
-
-section.strand-item__stance-created.stance-updated(id="'comment-'+ eventable.id" :event="event")
-  template(v-if="eventable.singleChoice()")
-    .d-flex
-      component(:is="componentType" :to="event.actor() && urlFor(event.actor())") {{event.actorName()}}
-      space
-      poll-common-stance-choice(v-if="showResults" :poll="poll" :stance-choice="eventable.stanceChoice()")
-  .poll-common-stance(v-if="showResults && !collapsed")
-    v-layout(v-if="!eventable.singleChoice()" wrap align-center)
-      strand-item-headline(:event="event" :eventable="eventable")
-      poll-common-stance-choices(:stance="eventable")
-    formatted-text.poll-common-stance-created__reason(:model="eventable" column="reason")
-    link-previews(:model="eventable")
-    attachment-list(:attachments="eventable.attachments")
-  action-dock(:model='eventable' :actions='actions' small)
+stance-updated(:event="event" :eventable="eventable" :collapsed="collapsed")
 </template>
