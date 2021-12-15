ef065f025e905be543f5ce4588fa77bfc5e7c6cf
Robert Guthrie <rob@guthrie.nz>
2021-12-15 16:47:30 +1300

    remove 0 value bar bits

--- vue/src/components/poll/common/chart/bar.vue
+++ vue/src/components/poll/common/chart/bar.vue
@@ -15,6 +15,7 @@ export default
     div.rounded.text-truncate.pa-2(
       v-for="option in options"
       :key="option.id"
+      v-if="option.rawScorePercent() > 0"
       :style="{width: option.rawScorePercent()+'%', 'background-color': option.color}"
     )
       span {{option.optionName()}} {{option.scorePercent()}}%
