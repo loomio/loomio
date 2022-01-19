ec5aeefbdb07029c75c3f3a02bf12021bf1b63b5
Robert Guthrie <rob@guthrie.nz>
2022-01-19 23:14:56 +1300

    use email to deliver customer support request messages

--- app/services/contact_message_service.rb
+++ app/services/contact_message_service.rb
@@ -1,13 +1,12 @@
 class ContactMessageService
   def self.create(contact_message:, actor:)
-    client = Clients::OsTicket.new if ENV['OS_TICKET_HOST']
-    client = Clients::Zammad.new if ENV['ZAMMAD_HOST']
-    if client && contact_message.valid?
-      request = client.post_message(contact_message.name,
-                                    contact_message.email,
-                                    contact_message.subject,
-                                    contact_message.message)
-      EventBus.broadcast('contact_message_create', contact_message, actor)
+    if contact_message.valid?
+      BaseMailer.delay.contact_message(
+        contact_message.name,
+        contact_message.email,
+        contact_message.subject,
+        contact_message.message
+      )
     else
       raise "failed to send a contact message. name: #{contact_message.name}, #{contact_message.email}, #{contact_message.subject}"
     end
