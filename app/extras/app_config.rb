c40e740a47473a7ddeb4418d7de506990970271d
Robert Guthrie <rob@guthrie.nz>
2022-01-20 10:43:54 +1300

    add app_config features trials

--- app/extras/app_config.rb
+++ app/extras/app_config.rb
@@ -91,6 +91,7 @@ class AppConfig
       env: Rails.env,
       subscriptions:              !!ENV.fetch('CHARGIFY_API_KEY', false),
       demos:                      ENV.fetch('FEATURES_DEMO_GROUPS', false),
+      trials:                     ENV.fetch('FEATURES_TRIALS', false),
       email_login:                !ENV['FEATURES_DISABLE_EMAIL_LOGIN'],
       create_user:                !ENV['FEATURES_DISABLE_CREATE_USER'],
       create_group:               !ENV['FEATURES_DISABLE_CREATE_GROUP'],
