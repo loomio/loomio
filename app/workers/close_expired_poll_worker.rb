1250540fa47035762a6e3164368e85e678b6de05
Robert Guthrie <rob@guthrie.nz>
2022-02-20 17:05:32 +1300

    close expired polls in background jobs

---
+++ app/workers/close_expired_poll_worker.rb
@@ -0,0 +1,10 @@
+class CloseExpiredPollWorker
+  include Sidekiq::Worker
+
+  def perform(poll_id)
+    poll = Poll.find(poll_id)
+    return if poll.closed_at
+    do_closing_work(poll: poll)
+    Events::PollExpired.publish!(poll)
+  end
+end
