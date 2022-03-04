6f390a817cd8d4ae3cdcd19c943553f40bbcaf84
Robert Guthrie <rob@guthrie.nz>
2022-03-05 11:10:36 +1300

    fix migration

--- db/migrate/20220304213244_convert_poll_stances_in_discussion.rb
+++ db/migrate/20220304213244_convert_poll_stances_in_discussion.rb
@@ -1,4 +1,4 @@
-class ConvertPollInStancesInDiscussion < ActiveRecord::Migration[6.1]
+class ConvertPollStancesInDiscussion < ActiveRecord::Migration[6.1]
   def change
     Poll.where(stances_in_discussion: false).
          where("discussion_id is not null").pluck(:id).each do |poll_id|
