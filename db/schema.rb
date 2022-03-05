d65ece86b687404564e7500b82563d23d0bc5a8a
Robert Guthrie <rob@guthrie.nz>
2022-03-05 19:01:28 +1300

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_04_235224) do
+ActiveRecord::Schema.define(version: 2022_03_05_055732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
