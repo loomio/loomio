cba92161774728e3d7b883ac192996db33265f7f
Justman10000 <49990629+Justman10000@users.noreply.github.com>
2022-07-29 14:34:18 +0200

    if expanted

--- docker_start.sh
+++ docker_start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
-mkdir -p tmp/pids
-if [ "$TASK" = "worker" ];
-then
+if ! [ -d tmp/pids ]; then
+  mkdir -p tmp/pids
+fi
+if [ "$TASK" = "worker" ]; then
   bundle exec sidekiq
 else
   bundle exec puma -C config/puma.rb;
