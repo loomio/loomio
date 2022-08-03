6debe33a228058f4262a7effb2e9d8070d107139
Robert Guthrie <rob@guthrie.nz>
2022-08-03 17:57:51 +1200

    ok

--- vue/tests/e2e/specs/poll.js
+++ vue/tests/e2e/specs/poll.js
@@ -347,7 +347,6 @@ module.exports = {
     page.click('.decision-tools-card__poll-type--proposal')
     page.fillIn('.poll-common-form-fields__title input', 'A new proposal')
     page.fillIn('.poll-common-form-fields .lmo-textarea div[contenteditable=true]', 'Some details')
-    page.click('.poll-common-form__settings-tab')
     page.click('.poll-common-settings__specified-voters-only')
     page.click('.poll-common-form__submit')
 
