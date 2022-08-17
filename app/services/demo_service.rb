5d3a78b2692c02c785cf78f3bf2842f471002f37
Robert Guthrie <rob@guthrie.nz>
2022-08-17 22:27:14 +1200

    fix demoservice

--- app/services/demo_service.rb
+++ app/services/demo_service.rb
@@ -3,22 +3,18 @@ class DemoService
 		return unless ENV['FEATURES_DEMO_GROUPS']
 
 		demo = Demo.where('demo_handle is not null').last
-		ids = Redis::List.new('demo_group_ids').value
 
-		expected = 3
-		remaining = Group.where(id: ids).where('created_at < ?', 24.hours.ago).count
+		expected = 2
+		remaining = Redis::List.new('demo_group_ids').value.size
 
 		(expected - remaining).times do
 			group = RecordCloner.new(recorded_at: demo.recorded_at).create_clone_group(demo.group)
-			ids.push group.id
+			Redis::List.new('demo_group_ids').push(group.id)
 		end
-
-		# Group.where(id: ids).where('created_at < ?', 24.hours.ago).delete_all
 	end
 
 	def self.take_demo(actor)
-		ids = Redis::List.new('demo_group_ids')
-		group = Group.find(ids.shift)
+		group = Group.find(Redis::List.new('demo_group_ids').shift)
     group.creator = actor
     group.subscription = Subscription.new(plan: 'demo', owner: actor)
     group.add_member! actor
