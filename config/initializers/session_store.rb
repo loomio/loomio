f1e844abc6ea2df1e59b28b6ec30bf5b8692ef6f
Robert Guthrie <rob@guthrie.nz>
2022-09-16 18:02:37 +1200

    config/session_store remove domain setting

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,1 @@
-# Be sure to restart your server when you modify this file.
-Loomio::Application.config.session_store :cookie_store, key: '_loomio', domain: ENV.fetch('SESSION_DOMAIN', ENV['CANONICAL_HOST']), secure: (ENV['FORCE_SSL'].to_i == 1)
+Loomio::Application.config.session_store :cookie_store, key: '_loomio', secure: (ENV['FORCE_SSL'].to_i == 1)
