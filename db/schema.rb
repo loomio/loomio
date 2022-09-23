875bd8e177767804587b4613cce59f32fbdb7de7
Robert Guthrie <rob@guthrie.nz>
2022-09-23 17:07:46 +1200

    db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_10_102929) do
+ActiveRecord::Schema.define(version: 2022_09_23_041404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
@@ -607,6 +607,15 @@ ActiveRecord::Schema.define(version: 2022_08_10_102929) do
     t.text "key", null: false
     t.integer "id", null: false
     t.integer "counter", default: 0
+  end
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
   end
 
   create_table "poll_options", id: :serial, force: :cascade do |t|
