7e4bf9257f20d712bff426e14ffd2ebeca7319a1
Robert Guthrie <rob@guthrie.nz>
2022-10-25 16:43:48 +1300

    MembershipSerializer: ensure that inviter can see email address if users they've invited

--- app/serializers/membership_serializer.rb
+++ app/serializers/membership_serializer.rb
@@ -11,6 +11,28 @@ class MembershipSerializer < ApplicationSerializer
              :accepted_at
 
   has_one :group, serializer: GroupSerializer, root: :groups
-  has_one :user, serializer: UserSerializer, root: :users
+  has_one :user_without_email, serializer: UserSerializer, root: :users, key: :user_id
+  has_one :user_with_email, serializer: UserWithEmailSerializer, root: :users, key: :user_id
   has_one :inviter, serializer: AuthorSerializer, root: :users
+
+  def include_user_without_email?
+    !include_user_with_email?
+  end
+
+  def include_user_with_email?
+    scope && (
+      object.user_id == scope[:current_user_id] ||
+      object.inviter_id == scope[:current_user_id] ||
+      scope[:current_user_is_admin]
+    )
+  end
+
+  def user_without_email
+    user
+  end
+
+  def user_with_email
+    user
+  end
+
 end
