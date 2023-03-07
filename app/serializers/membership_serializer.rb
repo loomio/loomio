040dba29979e00f83cbb23b862fb2e5fbf7e16a2
Robert Guthrie <rob@guthrie.nz>
2023-03-07 16:34:33 +1300

    Group Members panel: always show member email addresses if you're an admin

--- app/serializers/membership_serializer.rb
+++ app/serializers/membership_serializer.rb
@@ -8,31 +8,21 @@ class MembershipSerializer < ApplicationSerializer
              :experiences,
              :title,
              :created_at,
-             :accepted_at
+             :accepted_at,
+             :user_email
 
   has_one :group, serializer: GroupSerializer, root: :groups
-  has_one :user_without_email, serializer: UserSerializer, root: :users, key: :user_id
-  has_one :user_with_email, serializer: UserWithEmailSerializer, root: :users, key: :user_id
+  has_one :user, serializer: UserSerializer, root: :users, key: :user_id
   has_one :inviter, serializer: AuthorSerializer, root: :users
 
-  def include_user_without_email?
-    !include_user_with_email?
+  def user_email
+    (cache_fetch(:users_by_id, object.user_id) { object.user }).email
   end
 
-  def include_user_with_email?
+  def include_user_email?
     scope && (
-      object.user_id == scope[:current_user_id] ||
       object.inviter_id == scope[:current_user_id] ||
       scope[:current_user_is_admin]
     )
   end
-
-  def user_without_email
-    user
-  end
-
-  def user_with_email
-    user
-  end
-
 end
