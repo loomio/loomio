9bef9771d07a320431674d352350d0bfbfe8b387
Robert Guthrie <rob@guthrie.nz>
2023-03-24 13:06:23 +1300

    fix

--- app/services/translation_service.rb
+++ app/services/translation_service.rb
@@ -5,7 +5,7 @@ class TranslationService
 
   GOOGLE_LOCALES = %w[af sq am ar hy as ay az bm eu be bn bho bs bg ca ceb zh-CN zh zh-TW co hr cs da dv doi nl en eo et ee fil fi fr fy gl ka de el gn gu ht ha haw he iw hi hmn hu is ig ilo id ga it ja jv jw kn kk km rw gom ko kri ku ckb ky lo la lv ln lt lg lb mk mai mg ms ml mt mi mr mni-Mtei lus mn my ne no ny or om ps fa pl pt pa qu ro ru sm sa gd nso sr st sn sd si sk sl so es su sw sv tl tg ta tt te th ti ts tr tk ak uk ur ug uz vi cy xh yi yo zu]
 
-  def locale_for_google(locale)
+  def self.locale_for_google(locale)
     locale = locale.downcase.gsub("_", "-")
     return locale if GOOGLE_LOCALES.include?(locale)
     locale.split("-")[0]
