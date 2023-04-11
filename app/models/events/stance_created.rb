733eb5c66633911c839ba137107195e9262e7c34
Robert Guthrie <rob@guthrie.nz>
2023-04-11 18:29:13 +1200

    ok

--- app/models/events/stance_created.rb
+++ app/models/events/stance_created.rb
@@ -6,7 +6,7 @@ class Events::StanceCreated < Event
   include Events::Notify::Chatbots
 
   def self.publish!(stance)
-    NotificationService.delay.mark_as_read("Poll", stance.poll_id, stance.participant_id)
+    GenericWorker.perform_async('NotificationService', 'mark_as_read', "Poll", stance.poll_id, stance.participant_id)
 
     super stance,
           user: stance.participant.presence,
