f4b2b1d92e83152cab174a28977276841aae205c
Robert Guthrie <rob@guthrie.nz>
2023-04-12 14:11:33 +1200

    fix Sidekiq redis connection

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,7 +3,6 @@ Sidekiq.default_job_options = { 'backtrace' => true }
 sidekiq_redis_url = (ENV['REDIS_QUEUE_URL'] || ENV.fetch('REDIS_URL', 'redis://localhost:6379/0'))
 channels_redis_url = (ENV['REDIS_CACHE_URL'] || ENV.fetch('REDIS_URL', 'redis://localhost:6379/0'))
 
-MAIN_REDIS_POOL = ConnectionPool.new(size: ENV.fetch('REDIS_POOL_SIZE', 30), timeout: 5) { Redis.new(url: sidekiq_redis_url) }
 CACHE_REDIS_POOL = ConnectionPool.new(size: ENV.fetch('REDIS_POOL_SIZE', 30), timeout: 5) { Redis.new(url: channels_redis_url) }
 
 Redis::Objects.redis = CACHE_REDIS_POOL
@@ -13,10 +12,10 @@ if !Rails.env.production? && !ENV['USE_SIDEKIQ']
   Sidekiq::Testing.inline!
 else
   Sidekiq.configure_server do |config|
-    config.redis = MAIN_REDIS_POOL
+    config.redis = { url: sidekiq_redis_url }
   end
 
   Sidekiq.configure_client do |config|
-    config.redis = MAIN_REDIS_POOL
+    config.redis = { url: sidekiq_redis_url }
   end
 end
