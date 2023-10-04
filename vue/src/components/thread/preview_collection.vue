59a77f17e9f363f0d7830b9a9ec7a3da3f76a74b
Robert Guthrie <rob@guthrie.nz>
2023-10-04 17:57:14 +1300

    restore thread-preview-collection

---
+++ vue/src/components/thread/preview_collection.vue
@@ -0,0 +1,20 @@
+<script lang="coffee">
+import {slice, orderBy} from 'lodash'
+export default
+  props:
+    threads: Array
+    limit:
+      type: Number
+      default: 25
+    order:
+      type: String
+      default: '-lastActivityAt'
+  computed:
+    orderedThreads: ->
+      slice(orderBy(@threads, @order), 0, @limit)
+</script>
+
+<template lang="pug">
+v-list.thread-previews(two-line)
+  thread-preview(v-for="thread in orderedThreads", :key="thread.id", :thread="thread")
+</template>
