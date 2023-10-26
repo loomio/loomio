855f23e32f6fd5971b5309bd9d4610e363082e4f
Robert Guthrie <rob@guthrie.nz>
2023-10-27 09:53:05 +1300

    fix migration

--- db/migrate/20230807023000_migrate_template_discussions_to_discussion_templates.rb
+++ db/migrate/20230807023000_migrate_template_discussions_to_discussion_templates.rb
@@ -1,5 +1,5 @@
 class MigrateTemplateDiscussionsToDiscussionTemplates < ActiveRecord::Migration[7.0]
   def change
-    ConvertDiscussionTemplatesWorker.perform_async
+    ConvertDiscussionTemplatesWorker.perform_later
   end
 end
