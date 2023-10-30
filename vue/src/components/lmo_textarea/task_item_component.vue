23a92e559d86f7934ee5af9ed082216cf8ba8811
Robert Guthrie <rob@guthrie.nz>
2023-10-31 10:32:49 +1300

    tune task item

--- vue/src/components/lmo_textarea/task_item_component.vue
+++ vue/src/components/lmo_textarea/task_item_component.vue
@@ -74,7 +74,7 @@ node-view-wrapper(as="li")
   //- input.flex-shrink-0(style="z-index: 2300" type="checkbox" :checked="node.attrs.checked" @change="onCheckboxChange")
   v-simple-checkbox(contenteditable="false" color="accent" :ripple="false" type="checkbox" :value="checked" @click="onCheckboxChange")
   node-view-content(as="span" :class="{'task-item-text': true, 'task-item-is-empty': isEmpty}" :data-placeholder="$t('tasks.task_placeholder')")
-  v-chip.ml-2(v-if="hasMention" contenteditable="false" color="accent" x-small @click="openModal")
+  v-chip.ml-2(v-if="hasMention" contenteditable="false" color="primary" x-small @click="openModal")
     | 📅
     span.ml-1(v-if="node.attrs.dueOn") {{node.attrs.dueOn}}
     span.ml-1(v-else v-t="'tasks.add_due_date'")
@@ -101,5 +101,10 @@ node-view-wrapper(as="li")
   color: #ced4da
   pointer-events: none
   height: 0
+
+.theme--dark
+  .task-item-text.task-item-is-empty::before
+    color: rgba(255, 255, 255, 0.2)
+
 
 </style>
