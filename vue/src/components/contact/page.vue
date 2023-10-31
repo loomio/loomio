079851aa51d6ecf61b1e67f473246f653a60e56c
Robert Guthrie <rob@guthrie.nz>
2023-10-31 21:06:33 +1300

    make contact form nicer

--- vue/src/components/contact/page.vue
+++ vue/src/components/contact/page.vue
@@ -20,7 +20,7 @@ export default {
 
   mounted() {
     EventBus.$emit('currentComponent', {
-      titleKey: 'contact_message_form.contact_us',
+      titleKey: 'contact_message_form.alt_title',
       page: 'inboxPage'
     });
   },
@@ -56,36 +56,34 @@ export default {
 
 <template lang="pug">
 v-main
-  v-container.contact-page.d-flex.justify-center
+  v-container.contact-page
     v-card.contact-form(v-show='!submitted').max-width-800.px-0.px-sm-3
       submit-overlay(:value='message.processing')
-      v-card-title
-        p.text-heading(tabindex="-1" v-t="'contact_message_form.alt_title'")
-      v-card-subtitle(v-t="'contact_message_form.title'")
-      v-card-text
-        p(v-html="$t('contact_message_form.this_is_for')")
-        p(v-html="$t('contact_message_form.read_the_manual', { link: helpLink })")
-        p
-        div(v-if='!isLoggedIn')
-          label(v-t="")
-          v-text-field(:label="$t('contact_message_form.name_label')" :placeholder="$t('contact_message_form.name_placeholder')" v-model='message.name')
-          validation-errors(:subject='message', field='name')
-        div(v-if='!isLoggedIn')
-          v-text-field(:label="$t('contact_message_form.email_label')" :placeholder="$t('contact_message_form.email_placeholder')", v-model='message.email')
-          validation-errors(:subject='message', field='email')
+      v-card-title.mx-0.px-0
+        p.text-heading.mx-0(v-t="'contact_message_form.title'")
 
-        v-text-field(:label="$t('contact_message_form.subject_label')" :placeholder="$t('contact_message_form.subject_placeholder')", v-model='message.subject')
-        validation-errors(:subject='message', field='subject')
+      p(v-t="'contact_message_form.respond_asap'")
+      p(v-t="'contact_message_form.happy_to_help_with'")
 
-        v-textarea(:label="$t('contact_message_form.message_label')" v-model='message.message', :placeholder="$t('contact_message_form.message_placeholder')")
-        v-alert(type="info" v-if="needMessage")
-          span(v-t="'contact_message_form.need_message'")
-        validation-errors(:subject='message', field='message')
+      div(v-if='!isLoggedIn')
+        v-text-field(:label="$t('contact_message_form.name_label')" :placeholder="$t('contact_message_form.name_placeholder')" v-model='message.name')
+        validation-errors(:subject='message', field='name')
+      div(v-if='!isLoggedIn')
+        v-text-field(:label="$t('contact_message_form.email_label')" :placeholder="$t('contact_message_form.email_placeholder')", v-model='message.email')
+        validation-errors(:subject='message', field='email')
 
-        p.text--secondary(v-html="$t('contact_message_form.contact_us_email', { email: contactEmail })")
-        v-card-actions
-          v-spacer
-          v-btn(color="primary" @click='submit' v-t="'contact_message_form.send_message'")
+      v-text-field(:label="$t('contact_message_form.subject_label')" :placeholder="$t('contact_message_form.subject_placeholder')", v-model='message.subject')
+      validation-errors(:subject='message', field='subject')
+
+      v-textarea(:label="$t('contact_message_form.message_label')" v-model='message.message', :placeholder="$t('contact_message_form.message_placeholder')")
+      v-alert(type="info" v-if="needMessage")
+        span(v-t="'contact_message_form.need_message'")
+      validation-errors(:subject='message', field='message')
+
+      p.text--secondary(v-html="$t('contact_message_form.contact_us_email', { email: contactEmail })")
+      v-card-actions
+        v-spacer
+        v-btn(color="primary" @click='submit' v-t="'contact_message_form.send_message'")
 
     v-card.contact-form__success(v-show='submitted')
       v-card-title
