46e81a047ff42accb537396db3b5b2280377d522
Robert Guthrie <rob@guthrie.nz>
2023-11-09 20:06:07 +1300

    detailed_output: false

--- vue/nightwatch.conf.js
+++ vue/nightwatch.conf.js
@@ -1,27 +1,13 @@
 module.exports = {
   // An array of folders (excluding subfolders) where your tests are located;
   // if this is not specified, the test source must be passed as the second argument to the test runner.
+  detailed_output: false,
   skip_testcases_on_fail: false,
   src_folders: ['tests/e2e/specs'],
   output_folder: 'tests/reports',
-
-  // See https://nightwatchjs.org/guide/concepts/page-object-model.html
-  // page_objects_path: ['nightwatch/page-objects'],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
-  // custom_commands_path: ['nightwatch/custom-commands'],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
-  // custom_assertions_path: ['nightwatch/custom-assertions'],
-
-  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
   plugins: [],
-  
-  // See https://nightwatchjs.org/guide/concepts/test-globals.html
   globals_path: '',
-  
   webdriver: {},
-
   test_workers: {
     enabled: false
   },
