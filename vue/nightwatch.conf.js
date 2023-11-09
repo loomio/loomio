a32b5d5b5be15cd4714b6b289c3f8307e93b7ceb
Robert Guthrie <rob@guthrie.nz>
2023-11-09 13:31:27 +1300

    adjust github actions to run e2es

---
+++ vue/nightwatch.conf.js
@@ -0,0 +1,83 @@
+var chromeOptions = ["window-size=1280,1500"]
+
+if (process.env.RAILS_ENV == 'test' || 
+    process.env.CHROME_HEADLESS ) { chromeOptions.push("headless") }
+  
+module.exports = {
+  // An array of folders (excluding subfolders) where your tests are located;
+  // if this is not specified, the test source must be passed as the second argument to the test runner.
+  src_folders: ['tests/e2e/specs'],
+  output_folder: 'tests/reports',
+
+  // See https://nightwatchjs.org/guide/concepts/page-object-model.html
+  // page_objects_path: ['nightwatch/page-objects'],
+
+  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-commands.html
+  // custom_commands_path: ['nightwatch/custom-commands'],
+
+  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-custom-assertions.html
+  // custom_assertions_path: ['nightwatch/custom-assertions'],
+
+  // See https://nightwatchjs.org/guide/extending-nightwatch/adding-plugins.html
+  plugins: [],
+  
+  // See https://nightwatchjs.org/guide/concepts/test-globals.html
+  globals_path: '',
+  
+  webdriver: {},
+
+  test_workers: {
+    enabled: false
+  },
+
+  test_settings: {
+    default: {
+      disable_error_log: false,
+      launch_url: 'http://localhost:8080',
+
+      screenshots: {
+        enabled: false,
+        path: 'screens',
+        on_failure: true
+      },
+
+      desiredCapabilities: {
+        browserName: 'chrome'
+      },
+      
+      webdriver: {
+        start_process: true,
+        server_path: ''
+      },
+      
+    },
+    
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
+          //
+          // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
+          w3c: false,
+          args: [
+            //'--no-sandbox',
+            //'--ignore-certificate-errors',
+            //'--allow-insecure-localhost',
+            //'--headless'
+          ]
+        }
+      },
+
+      webdriver: {
+        start_process: true,
+        server_path: '',
+        cli_args: [
+          // --verbose
+        ]
+      }
+    },
+    
+  },
+  
+};
