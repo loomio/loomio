e1fda1a1137590fe3a3e42e39cb562201626d7c3
Robert Guthrie <rob@loomio.org>
2024-02-22 16:11:11 +1300

    Handle case where user is invited to group but previously was a member (#10471)
    * Handle case where user is invited to group but previously was a member
    
    * remove vestigial memberships when user accepting is not user invited
    
    * only accept pending invitations
    
    * fix for spec testing inviting existing member
    
    * move the deletion of unused membership records further up the method

--- app/services/membership_service.rb
+++ app/services/membership_service.rb
@@ -10,48 +10,52 @@ class MembershipService
     # and we dont want any surprises if they already have some memberships.
     # they may be accepting memberships send to a different email (unverified_user)
 
-    group_ids = membership.group.parent_or_self.id_and_subgroup_ids
+    invited_group_id = membership.group_id
+    existing_group_ids = Membership.where(user_id: actor.id).pluck(:group_id)
+    existing_accepted_group_ids = Membership.active.accepted.where(user_id: actor.id).pluck(:group_id)
+    invited_group_ids = Membership.pending.where(user_id: membership.user_id, group_id: membership.group.parent_or_self.id_and_subgroup_ids).pluck(:group_id)
+
+    # unrevoke any memberships the actor was just invited to
+    Membership.revoked
+    .where(user_id: actor.id, group_id: invited_group_ids)
+    .update(revoked_at: nil, revoker_id: nil, inviter_id: membership.inviter_id, accepted_at: DateTime.now)
 
     # ensure actor has accepted any existing pending memberships to this group
     Membership.pending
-    .where(user_id: actor.id, group_id: group_ids)
-    .update_all(accepted_at: DateTime.now)
+    .where(user_id: actor.id, group_id: invited_group_ids)
+    .update(accepted_at: DateTime.now)
 
-    # cant accept pending memberships to groups I already belong to
-    existing_group_ids = Membership.pending.where(user_id: membership.user_id,
-                                                  group_id: actor.memberships.accepted.pluck(:group_id)).pluck(:group_id)
+    Membership.pending
+    .where(user_id: membership.user_id, group_id: (invited_group_ids - existing_group_ids))
+    .update(user_id: actor.id, accepted_at: DateTime.now)
 
-    Membership.pending.where(
-      user_id: membership.user_id,
-      group_id: (group_ids - existing_group_ids))
-    .update_all(
-      user_id: actor.id,
-      accepted_at: DateTime.now)
+    if (membership.user_id != actor.id)
+      Membership.where(user_id: membership.user_id, group_id: invited_group_ids).destroy_all
+    end
 
-    member_group_ids = actor.group_ids & membership.group.parent_or_self.id_and_subgroup_ids
-
-    group_ids.each do |group_id|
+    invited_group_ids.each do |group_id|
       GenericWorker.perform_async('PollService', 'group_members_added', group_id)
     end
 
     # remove any existing guest access in these groups
     DiscussionReader.joins(:discussion)
-    .where(user_id: actor.id, 'discussions.group_id': member_group_ids, guest: true)
+    .where(user_id: actor.id, 'discussions.group_id': invited_group_ids, guest: true)
     .update_all(guest: false, revoked_at: nil, revoker_id: nil)
 
     Stance.joins(:poll)
-    .where(participant_id: actor.id, 'polls.group_id': member_group_ids)
+    .where(participant_id: actor.id, 'polls.group_id': invited_group_ids)
     .update_all(guest: false)
 
     # unrevoke any votes on active polls
     Stance.joins(:poll)
     .where(participant_id: actor.id)
-    .where('polls.group_id': member_group_ids)
+    .where('polls.group_id': invited_group_ids)
     .where('stances.revoked_at is not null')
     .where('polls.closed_at is null')
     .update_all(revoked_at: nil, revoker_id: nil)
 
-    membership.reload
+    return if existing_accepted_group_ids.include?(invited_group_id)
+    membership = Membership.find_by!(group_id: invited_group_id, user_id: actor.id)
     Events::InvitationAccepted.publish!(membership) if notify && membership.accepted_at
   end
 
