9958883c28f1a14b8e36fd364fb47b2debc9c9fa
Robert Guthrie <rob@guthrie.nz>
2024-02-29 11:18:28 +1300

    up the headless window size

--- vue/nightwatch.conf.js
+++ vue/nightwatch.conf.js
@@ -37,7 +37,7 @@ module.exports = {
           // More info on Chromedriver: https://sites.google.com/a/chromium.org/chromedriver/
           // w3c:false tells Chromedriver to run using the legacy JSONWire protocol (not required in Chrome 78)
           w3c: false,
-          args: ['headless', 'window-size=1280,2400']
+          args: ['headless', 'window-size=1280,4800']
         }
       },
 
