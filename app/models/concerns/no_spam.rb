a8e54e4122ba8248f3b25d57ca86f5ac5a74c944
Robert Guthrie <rob@guthrie.nz>
2024-03-05 15:15:57 +1300

    tidy up spam_regex handling

--- app/models/concerns/no_spam.rb
+++ app/models/concerns/no_spam.rb
@@ -1,9 +1,11 @@
 module NoSpam
   # eg:
   # SPAM_REGEX="(diide\.com|gusronk\.com|appnox\.com|akxpert\.com)"
+  SPAM_REGEX = Regexp.new(ENV.fetch('SPAM_REGEX', "(diide\.com|gusronk\.com)"), 'i')
+  
   def no_spam_for(*fields)
     Array(fields).each do |field|
-      validates field, format: { without: Regexp.new(ENV['SPAM_REGEX']), message: "no spam" } if ENV['SPAM_REGEX']
+      validates field, format: { without: SPAM_REGEX, message: "no spam" }
     end
   end
 end
