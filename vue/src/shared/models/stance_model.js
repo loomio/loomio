71633ddc9cfbfea7cab30d3235970a8e65363ec8
Robert Guthrie <rob@guthrie.nz>
2024-03-11 12:01:48 +1300

    fix bug with pollOptionIds and lodash's map, which was breaking edit vote

--- vue/src/shared/models/stance_model.js
+++ vue/src/shared/models/stance_model.js
@@ -96,7 +96,7 @@ export default class StanceModel extends BaseModel {
   }
 
   pollOptionIds() {
-    return map(Object.keys(this.optionScores), parseInt);
+    return Object.keys(this.optionScores).map(k => parseInt(k))
   }
 
   pollOptions() {
