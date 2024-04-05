5c21f4ea44707091e68306f137f52677dad50f5b
Robert Guthrie <rob@guthrie.nz>
2024-04-05 14:36:07 +1300

    remove all work from the deactivate memberships migration, so that kamal does not kill the process during deploy

--- db/migrate/20240405000007_revoke_memberships_for_deactivated_users.rb
+++ db/migrate/20240405000007_revoke_memberships_for_deactivated_users.rb
@@ -1,10 +1,5 @@
 class RevokeMembershipsForDeactivatedUsers < ActiveRecord::Migration[7.0]
   def change
-    User.where.not(deactivated_at: nil).find_each do |user|
-      group_ids = Membership.active.where(user_id: user.id).pluck(:group_id)
-      if group_ids.count > 0
-        GenericWorker.perform_async("MembershipService", "revoke_by_id", group_ids, user.id, user.id, user.deactivated_at)
-      end
-    end
+    RevokeMembershipsOfDeactivatedUsersWorker.perform_async
   end
 end
