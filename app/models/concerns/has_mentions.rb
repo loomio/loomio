30a546f4f69baccc14deafefc29ce47cc11fcc79
Jerinkc <jerinkc22@gmail.com>
2024-07-02 23:48:12 +0530

    Remove author from mentioned users

--- app/models/concerns/has_mentions.rb
+++ app/models/concerns/has_mentions.rb
@@ -31,12 +31,14 @@ module HasMentions
     mentioned_usernames_result = mentioned_usernames
     mentioned_collections = mentioned_usernames_result & %w[group thread]
 
-    members.where("users.username in (:usernames) or
-                   users.id in (:ids)
-                   #{ 'or m.id IS NOT NULL' if mentioned_collections.include? 'group' }
-                   #{ 'or dr.id IS NOT NULL' if mentioned_collections.include? 'thread' }",
-                  usernames: mentioned_usernames_result,
-                  ids: mentioned_user_ids)
+    chain = members.where("users.username in (:usernames) or
+                          users.id in (:ids)",
+                          usernames: mentioned_usernames_result,
+                          ids: mentioned_user_ids)
+    chain.where('m.id IS NOT NULL AND users.id <> ?', self.author&.id) if mentioned_collections.include? 'group'
+    chain.where('dr.id IS NOT NULL AND users.id <> ?', self.author&.id) if mentioned_collections.include? 'thread'
+
+    chain
   end
 
   # users mentioned in the text, but not yet sent notifications
