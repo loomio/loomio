90f59817dd15b5fc6e303977ef0c416fa2cf8213
Jerinkc <jerinkc22@gmail.com>
2024-07-05 19:29:44 +0530

    Remove migration to create collections and moved to helper mentionable_collection.rb

---
+++ app/models/mentionable_collection.rb
@@ -0,0 +1,64 @@
+class MentionableCollection
+  private_class_method :new # use MentionableCollection.[collection_name] to initialize
+
+  attr_reader :name, :collection
+
+  COLLECTIONS = User::COLLECTIONS
+  @@instance = {}
+
+  def initialize(collection)
+    @name = collection
+
+    initialize_collection
+  end
+
+  def translate
+    I18n.t(:"user.mentionable_collection.#{name}", default: nil )
+  end
+
+  COLLECTIONS.each do |collection|
+    define_singleton_method(collection) do
+      @@instance[collection] ||= new(collection)
+    end
+  end
+
+  def initialize_collection
+    return if @collection
+
+    translated_name = translate
+    existing_collections = User.where('users.name = :translated_name AND users.collection = false OR
+                                        users.name <> :translated_name AND users.email = :email',
+                                        translated_name:,
+                                        email: "#{name}@loomio")
+
+    existing_collections.each do |user|
+      if user.email == "#{name}@loomio"
+        user.destroy! # Prevents collection creation error on locale changes
+        next
+      end
+
+      random_num = SecureRandom.random_number(9000) + 1000
+      user.update!(name: "#{user.name}#{random_num}")
+    end
+
+    @collection = User.find_by(name: translated_name) ||
+                  User.create!(email: "#{name}@loomio",
+                              name: translated_name,
+                              password: SecureRandom.hex(20),
+                              email_verified: true,
+                              collection: true,
+                              avatar_kind: :gravatar)
+  end
+
+  def self.encased_all
+    COLLECTIONS.map{ |c| send(c) }.freeze
+  end
+
+  def self.all
+    COLLECTIONS.map{ |c| send(c).collection }.freeze
+  end
+
+  def self.pluck(*args)
+    all.pluck(*args)
+  end
+end
