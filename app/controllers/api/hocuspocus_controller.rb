a2cb3a4be1cf8de168ed7693a2aeb29d6f3a9639
Robert Guthrie <rob@guthrie.nz>
2024-07-18 11:33:56 +1200

    add tests for hocuspocus auth controller

--- app/controllers/api/hocuspocus_controller.rb
+++ app/controllers/api/hocuspocus_controller.rb
@@ -1,23 +1,30 @@
 class API::HocuspocusController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
-  TYPES = %w[comment discussion poll stance outcome pollTemplate discussionTemplate group user]
+  RECORD_TYPES = %w[comment discussion poll stance outcome pollTemplate discussionTemplate group user]
 
   def create
-    id, secret_token = params[:user_secret].split(',')
-    user = User.active.find_by!(id: id, secret_token: secret_token)
+    user_id, secret_token = params[:user_secret].split(',')
+    user = User.active.find_by!(id: user_id, secret_token: secret_token)
 
-    type, id = params[:document_name].split('-')
-    raise "invalid record type #{type}" unless TYPES.include?(type)
+    record_type, record_id, user_id_if_new = params[:document_name].split('-')
 
-    if id == 'new'
-      head :ok
+    raise "invalid record type #{record_type}" unless RECORD_TYPES.include?(record_type)
+
+    if record_id == 'new'
+      if user_id_if_new.to_i == user.id
+        head :ok
+      else
+        head :unauthorized
+      end
     else
-      if user.can? :update, type.camelize.constantize.find(id.to_i)
+      if user.can? :update, record_type.camelize.constantize.find(record_id.to_i)
         head :ok
       else
         head :unauthorized
       end
     end
+  rescue ActiveRecord::RecordNotFound
+    head :unauthorized
   end
 end
