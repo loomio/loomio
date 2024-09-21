c3142b5d88e9bee140563709fc28975627ffe625
Robert Guthrie <rob@guthrie.nz>
2024-09-21 13:58:43 +1200

    fix move thread form

--- vue/src/components/thread/move_thread_form.vue
+++ vue/src/components/thread/move_thread_form.vue
@@ -5,15 +5,19 @@ import { I18n }           from '@/i18n';
 import EventBus from '@/shared/services/event_bus';
 import Flash from '@/shared/services/flash';
 import { filter } from 'lodash-es';
+import WatchRecords from '@/mixins/watch_records';
+import UrlFor from '@/mixins/url_for';
 
 export default {
+  mixins: [WatchRecords, UrlFor],
   props: {
     discussion: Object
   },
   data() {
     return {
       targetGroup: null,
-      availableGroups: []
+      availableGroups: [],
+      loading: false
     };
   },
   created() {
@@ -21,12 +25,13 @@ export default {
     this.watchRecords({
       collections: ['groups', 'memberships'],
       query: store => {
-        this.availableGroups = filter(Session.user().groups(), group => group.id !== this.discussion.groupId);
+        this.availableGroups = Session.user().groups();
       }
     });
   },
   methods: {
     submit() {
+      this.loading = true
       this.discussion.move().then(data => {
         Flash.success('move_thread_form.messages.success', { name: this.discussion.group().name });
         const discussionKey = data.discussions[0].key;
@@ -34,7 +39,7 @@ export default {
           EventBus.$emit('closeModal');
           this.$router.push(`/d/${discussionKey}`);
         });
-      }).catch(() => true);
+      }).catch(() => true).finally(() => this.loading = false);
     },
 
     updateTarget() {
@@ -52,19 +57,21 @@ export default {
 };
 </script>
 <template lang="pug">
-v-card.move-thread-form
+v-card.move-thread-form(:title="$t('move_thread_form.title')")
   submit-overlay(:value='discussion.processing')
-  v-card-title
-    h1.text-h5(tabindex="-1" v-t="'move_thread_form.title'")
-    v-spacer
+  template(v-slot:append)
     dismiss-modal-button
   v-card-text
-    v-select#group-dropdown.move-thread-form__group-dropdown(v-model='discussion.groupId' :required='true' @change='updateTarget()' :items='availableGroups' item-value='id' item-text='fullName' :label="$t('move_thread_form.body')")
-      template(v-slot:item='data')
-        v-list-item-content.group-dropdown-item
-          v-list-item-title.group-dropdown-item-group-name
-            span {{ data.item.fullName }}
+    v-select#group-dropdown.move-thread-form__group-dropdown(
+      v-model='discussion.groupId'
+      :required='true'
+      @change='updateTarget()'
+      :items='availableGroups'
+      item-value='id'
+      item-title='fullName'
+      :label="$t('move_thread_form.body')")
   v-card-actions
     v-spacer
-    v-btn.move-thread-form__submit(color="primary" type='button' v-t="'move_thread_form.confirm'" @click='moveThread()')
+    v-btn.move-thread-form__submit(:loading="loading" color="primary" variant="tonal" @click='moveThread()')
+      span( v-t="'move_thread_form.confirm'")
 </template>
