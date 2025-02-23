26323c84e0ad771215850de3090098dd40be3fbf
Hamsterarsch <git.7uxl1@slmails.com>
2025-02-23 01:03:43 +0100

    added toggle hide all button with confirm modal, moved task help to top, fixed weird no tasks message layout

--- vue/src/components/tasks/page.vue
+++ vue/src/components/tasks/page.vue
@@ -5,6 +5,7 @@ import Session            from '@/shared/services/session';
 import Flash              from '@/shared/services/flash';
 import EventBus           from '@/shared/services/event_bus';
 import AbilityService     from '@/shared/services/ability_service';
+import confirm_modal from "@/components/common/confirm_modal.vue";
 
 import {groupBy} from 'lodash-es';
 
@@ -20,6 +21,7 @@ export default {
         {name: 'hidden', filter: task => task.hidden}
       ],
       activeFilter: 0,
+      dialog: false,
     };
   },
 
@@ -70,6 +72,16 @@ export default {
       });
     },
 
+    toggleHideVisible() {
+      let visibleTasks = this.tasks.filter(this.filterTabs[this.activeFilter].filter);
+      if(visibleTasks)
+      {
+        visibleTasks.forEach(task => {
+          task.toggleHidden();
+        });
+      }
+    },
+
     setFilter(tabId) { this.activeFilter = tabId; },
 
   },
@@ -88,8 +100,30 @@ export default {
 <template lang="pug">
 v-main
   v-container.dashboard-page.max-width-1024.px-0.px-sm-3
-    h1.text-h4.my-4(tabindex="-1" v-observe-visibility="{callback: titleVisible}" v-t="'tasks.your_tasks'")
-    
+
+    v-container()
+      v-row(:align="'center'")
+        h1.text-h4.my-4(tabindex="-1" v-observe-visibility="{callback: titleVisible}" v-t="'tasks.your_tasks'")
+        v-spacer
+        v-col(:align="'right'")
+          v-chip.mb-2(outlined @click="dialog = true")
+            common-icon.mr-2(name="mdi-eye-off")
+            span(v-t="'tasks.tasks_toggle_hide_all'")
+
+          v-chip.mb-2.ml-3(outlined href="https://help.loomio.org/en/user_manual/threads/thread_admin/tasks.html" target="_blank")
+            common-icon.mr-2(name="mdi-help-circle-outline")
+            span(v-t="'common.help'")
+            span :
+            space
+            span(v-t="'tasks.tasks'")
+    template()
+      v-dialog(v-model="dialog")
+        v-card
+          v-card-title(v-t="'tasks.tasks_toggle_hide_all_confirm.title'")
+          v-card-text(v-t="'tasks.tasks_toggle_hide_all_confirm.description'")
+          v-card-actions
+            v-btn(@click="dialog = false; toggleHideVisible()" v-t="'tasks.tasks_toggle_hide_all_confirm.button'")
+
     v-divider.mt-4
     v-tabs(
       background-color="transparent"
@@ -120,12 +154,6 @@ v-main
                 common-icon(name="mdi-arrow-right")
 
 
-    p(v-if="!loading && filteredTasks.length == 0" v-t="'tasks.no_tasks_assigned'")
-    .d-flex.justify-center
-      v-chip(outlined href="https://help.loomio.org/en/user_manual/threads/thread_admin/tasks.html" target="_blank")
-        common-icon.mr-2(name="mdi-help-circle-outline")
-        span(v-t="'common.help'")
-        span :
-        space
-        span(v-t="'tasks.tasks'")
+    p(style="margin: 2Opx" v-if="!loading && filteredTasks.length == 0" v-t="'tasks.no_tasks_assigned'")
+
 </template>
