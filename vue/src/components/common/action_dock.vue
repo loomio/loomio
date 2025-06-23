92e3b8bf966e5792e79f96edfa1257ecae04d3b4
Robert Guthrie <rob@guthrie.nz>
2025-06-23 22:08:50 +1200

    tweek action dock a little more

--- vue/src/components/common/action_dock.vue
+++ vue/src/components/common/action_dock.vue
@@ -44,7 +44,7 @@ section.d-flex.flex-wrap.align-center.action-dock.pb-1(style="margin-left: -6px"
   .action-dock__action(v-for='(action, name) in rightActions' :key="name")
     reaction-input.action-dock__button--react(:model="model" v-if="name == 'react'", :size="size")
     action-button(v-if="name != 'react'" :action="action" :name="name" :size="size" :nameArgs="action.nameArgs && action.nameArgs()")
-  action-menu(v-if="menuActions", :actions='menuActions', :menuIcon="menuIcon" :size="size" icon :name="$t('action_dock.more_actions')")
+  action-menu(v-if="menuActions" :actions='menuActions' :menuIcon="menuIcon" :size="size" icon :name="$t('action_dock.more_actions')")
   v-spacer(v-if="left")
   reaction-display(:model="model" v-if="left && actions.react" :canEdit="actions.react.canPerform()" :size="size")
 </template>
@@ -55,7 +55,7 @@ section.d-flex.flex-wrap.align-center.action-dock.pb-1(style="margin-left: -6px"
 
 .lmo-action-dock-wrapper
   .action-dock__action, .action-menu
-    opacity: 0.5
+    opacity: 0.66
   &:hover .action-dock__action, &:hover .action-menu
     opacity: 1
 
