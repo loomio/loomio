41d340176f467d61105b206edbe7268fdce88234
Robert Guthrie <rob@guthrie.nz>
2025-07-03 14:53:46 +1200

    handle very long poll titles and show group name on poll preview

--- vue/src/components/poll/common/preview.vue
+++ vue/src/components/poll/common/preview.vue
@@ -1,18 +1,11 @@
 <script lang="js">
-import Session from '@/shared/services/session';
-import TemplateBadge from '@/components/poll/common/template_badge';
 import LmoUrlService from '@/shared/services/lmo_url_service';
 
 export default {
-  components: {TemplateBadge},
   props: {
     poll: Object,
-    displayGroupName: Boolean
-  },
-  methods: {
-    showGroupName() {
-      return this.displayGroupName && this.poll.groupId;
-    }
+    displayGroupName: Boolean,
+    fullName: Boolean
   },
   computed: {
     link() { return LmoUrlService.discussionPoll(this.poll); },
@@ -29,18 +22,19 @@ v-list-item.poll-common-preview(:to='link')
     span {{poll.title}}
     tags-display.ml-1(:tags="poll.tags" :group="poll.group()" size="x-small")
   v-list-item-subtitle
-    span(v-t="{ path: 'poll_common_collapsed.by_who', args: { name: poll.authorName() } }")
+    span(v-if='displayGroupName && poll.groupId')
+      span(v-if="fullName") {{ poll.group().fullName }}
+      span(v-if="!fullName") {{ poll.group().name }}
+    template(v-else)
+      span(v-t="{ path: 'poll_common_collapsed.by_who', args: { name: poll.authorName() } }")
     space
     span ·
     space
-    span(v-if='displayGroupName && poll.groupId')
-      span {{ poll.group().name }}
-      space
-      span ·
-      space
     poll-common-closing-at(:poll='poll' approximate)
 </template>
 <style lang="sass">
 .poll-common-preview .v-avatar
   overflow: visible! important
+.poll-common-preview .v-list-item-title
+  white-space: wrap
 </style>
