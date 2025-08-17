dc5821bfc4a66b5093cdc0a2a3d36c86e6aace52
Robert Guthrie <rob@guthrie.nz>
2025-08-17 15:54:07 +1200

    hide the new toc nav stuff until it's finished

--- vue/src/components/strand/toc_nav.vue
+++ vue/src/components/strand/toc_nav.vue
@@ -197,13 +197,13 @@ export default {
 <template lang="pug">
 v-navigation-drawer.lmo-no-print.disable-select.thread-sidebar(v-if="discussion" v-model="open" :permanent="$vuetify.display.mdAndUp"  app fixed location="right" clipped color="background" floating)
   template(v-if="items.length > 1")
-    v-list(nav density="compact" :lines="false")
-      v-list-subheader(v-t="'strand_nav.jump_to'")
-      v-list-item(nav :prepend-icon="mdiArrowUpThin" :title="$t('strand_nav.top')" @click="scrollToTop")
-      v-list-item(:prepend-icon="mdiMessageBadgeOutline" :title="$t('strand_nav.unread')" @click="scrollToUnread" :to="baseUrl+'/'+loader.firstUnreadSequenceId()" v-if="loader.firstUnreadSequenceId()")
-      v-list-item(:prepend-icon="mdiLightningBolt" :title="$t('strand_nav.newest')" @click="scrollToNewest" :to="baseUrl+'/'+discussion.lastSequenceId()")
-      v-list-item(:prepend-icon="mdiArrowDownThin" :title="$t('strand_nav.bottom')" @click="scrollToBottom")
-      v-list-subheader(v-t="'strand_nav.timeline'")
+    //v-list(nav density="compact" :lines="false")
+    //  v-list-subheader(v-t="'strand_nav.jump_to'")
+    //  v-list-item(nav :prepend-icon="mdiArrowUpThin" :title="$t('strand_nav.top')" @click="scrollToTop")
+    //  v-list-item(:prepend-icon="mdiMessageBadgeOutline" :title="$t('strand_nav.unread')" @click="scrollToUnread" :to="baseUrl+'/'+loader.firstUnreadSequenceId()" v-if="loader.firstUnreadSequenceId()")
+    //  v-list-item(:prepend-icon="mdiLightningBolt" :title="$t('strand_nav.newest')" @click="scrollToNewest" :to="baseUrl+'/'+discussion.lastSequenceId()")
+    //  v-list-item(:prepend-icon="mdiArrowDownThin" :title="$t('strand_nav.bottom')" @click="scrollToBottom")
+    //  v-list-subheader(v-t="'strand_nav.timeline'")
     div.strand-nav__toc
       //- | {{items}}
       router-link.strand-nav__entry.text-caption(
