299417f45f0409f010d93affca3ad230d1f81a71
Robert Guthrie <rob@guthrie.nz>
2025-09-24 22:58:13 +1200

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_29_033545) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_22_103715) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "hstore"
