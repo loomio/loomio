98a91bac9b8211d970fc24469d455adf0dc98da7
Robert Guthrie <rob@loomio.org>
2026-02-16 00:01:21 +1300

    Update practice thread text: cleaner title, description, subtitle, and intro

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_15_105544) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_15_110103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "hstore"
