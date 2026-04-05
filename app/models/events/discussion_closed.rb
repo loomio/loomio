f901559269fb141100ed3e03389394a6bdf665cc
Robert Guthrie <rob@loomio.org>
2026-04-05 22:34:20 +1200

    remove dead code in discussion_closed event

--- app/models/events/discussion_closed.rb
+++ app/models/events/discussion_closed.rb
@@ -1,10 +1,3 @@
 class Events::DiscussionClosed < Event
-  include Events::LiveUpdate
-
-  def self.publish!(discussion, actor)
-    super discussion,
-          user: actor,
-          topic: discussion.topic,
-          created_at: discussion.topic.closed_at
-  end
+  # we no longer created discussion closed events. but legacy items may exist
 end
