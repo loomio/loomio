4c5682c815f2bd6cf2fc973967cd9479ece8c3af
Robert Guthrie <rob@loomio.org>
2026-05-18 13:32:32 +1200

    update to script setup mostly to burn tokens

--- vue/src/components/common/active_time_ago.vue
+++ vue/src/components/common/active_time_ago.vue
@@ -1,16 +1,9 @@
-<script lang="js">
+<script setup lang="js">
 import { approximate } from '@/shared/helpers/format_time';
 
-export default {
-  props: {
-    date: Date
-  },
-
-  methods: {
-    approximate
-  }
-};
-
+defineProps({
+  date: Date
+});
 </script>
 <template lang="pug">
 span(v-t="{path: 'common.active_time_ago', args: {time_ago: approximate(date)}}")
