720f696c0627e39fa89813cef115e5f2e64f87fc
Robert Guthrie <rob@loomio.org>
2026-05-22 00:03:11 +1200

    Fix topics locked_at index migration

--- db/migrate/20260319084208_add_indexes_to_topics.rb
+++ db/migrate/20260319084208_add_indexes_to_topics.rb
@@ -2,7 +2,7 @@ class AddIndexesToTopics < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def change
-    add_index :topics, :last_activity_at, order: :desc, algorithm: :concurrently
-    add_index :topics, :closed_at, algorithm: :concurrently
+    add_index :topics, :last_activity_at, order: :desc, algorithm: :concurrently, if_not_exists: true
+    add_index :topics, :locked_at, algorithm: :concurrently, if_not_exists: true
   end
 end
