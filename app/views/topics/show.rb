0c4c7d6a8bd1eebbc77c2914907c305af494eebd
Robert Guthrie <rob@loomio.org>
2026-06-30 01:45:02 +1200

    Fix N+1 avatar queries in Topics::Show (DiscussionsController#show)
    Preload comment author and stance participant avatar attachments in a
    fixed number of queries instead of one per item.

--- app/views/topics/show.rb
+++ app/views/topics/show.rb
@@ -82,22 +82,31 @@ class Views::Topics::Show < Views::Application::Layout
 
   def render_activity_panel
     div(class: "activity-panel") do
-      items = @topic.items
+      scope = @topic.items
         .includes(:eventable, :user)
         .order("position_key #{@topic.newest_first ? 'desc' : 'asc'}")
         .where(kind: %w[new_comment poll_created stance_created stance_updated])
 
-      items.limit(@pagination[:limit]).offset(@pagination[:offset]).each do |item|
-        if item.eventable.present?
-          render Views::Topics::TopicItem.new(item: item, current_user: @recipient)
-        end
+      total = scope.count
+      items = scope.limit(@pagination[:limit]).offset(@pagination[:offset]).to_a
+      preload_item_avatars(items)
+
+      items.each do |item|
+        render Views::Topics::TopicItem.new(item: item, current_user: @recipient) if item.eventable.present?
       end
 
-      if (@pagination[:offset] + @pagination[:limit]) < items.count
+      if (@pagination[:offset] + @pagination[:limit]) < total
         a(href: "?offset=#{@pagination[:offset] + @pagination[:limit]}&limit=#{@pagination[:limit]}&export=1") do
           plain t("common.action.load_more")
         end
       end
     end
+  end
+
+  def preload_item_avatars(items)
+    comments = items.filter_map { |i| i.eventable if i.kind == 'new_comment' }
+    stances  = items.filter_map { |i| i.eventable if i.kind.in?(%w[stance_created stance_updated]) }
+    ActiveRecord::Associations::Preloader.new(records: comments, associations: {user: {uploaded_avatar_attachment: :blob}}).call if comments.any?
+    ActiveRecord::Associations::Preloader.new(records: stances, associations: {participant: {uploaded_avatar_attachment: :blob}}).call if stances.any?
   end
 end
