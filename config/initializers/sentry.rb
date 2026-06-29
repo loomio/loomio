af788860b76cc7cbdd923088963cbec9d25eb5ab
Robert Guthrie <rob@loomio.org>
2026-06-29 16:08:30 +1200

    Use presence convention for SENTRY_ENABLE_LOGS boolean env; document convention in AGENTS.md

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,5 +4,5 @@ Sentry.init do |config|
   config.send_default_pii = true
   config.traces_sample_rate = ENV.fetch('SENTRY_SAMPLE_RATE', 0.1).to_f
   config.profiles_sample_rate = ENV.fetch('SENTRY_PROFILES_SAMPLE_RATE', 1.0).to_f # this is relative to traces_sample_rate
-  config.enable_logs = ENV.fetch("SENTRY_ENABLE_LOGS", "false") == "true"
+  config.enable_logs = ENV["SENTRY_ENABLE_LOGS"].present?
 end
