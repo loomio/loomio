22e87793062c54792175a562bb5b256248ba2028
Robert Guthrie <rob@loomio.org>
2026-07-06 21:31:10 +1200

    fix tag deadlock

--- app/workers/update_group_and_org_tags_worker.rb
+++ app/workers/update_group_and_org_tags_worker.rb
@@ -1,4 +1,6 @@
 class UpdateGroupAndOrgTagsWorker < ApplicationJob
+  retry_on ActiveRecord::Deadlocked, wait: :polynomially_longer, attempts: 5
+
   def perform(group_id)
     TagService.update_group_and_org_tags(group_id)
   end
