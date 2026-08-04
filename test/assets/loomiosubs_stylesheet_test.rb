f47e7d70a1a495c8bd8e3da85c120891becb362c
Robert Guthrie <rob@loomio.org>
2026-08-05 11:02:37 +1200

    Tidy subscription pricing and enforce Pro limits

--- test/assets/loomiosubs_stylesheet_test.rb
+++ test/assets/loomiosubs_stylesheet_test.rb
@@ -7,6 +7,8 @@ class LoomiosubsStylesheetTest < ActiveSupport::TestCase
     assert_includes css, ".btn--ink"
     assert_includes css, AppConfig.theme[:primary_color]
     assert_includes css, AppConfig.theme[:accent_color]
+    assert_match(/\.price-box\s*\{[^}]*border:\s*2px solid var\(--loomio-primary-color\)/m, css)
+    assert_match(/\.ribbon-grey[^}]*background:\s*var\(--loomio-primary-color\)/m, css)
     refute_includes css, "#00BDD4"
     refute_includes css, "#F08B00"
   end
