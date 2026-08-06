e82d9d5a343f30c26cf99ade7a59a394901e05ae
Robert Guthrie <rob@loomio.org>
2026-08-07 10:49:58 +1200

    Require vote reasons when disagreeing

---
+++ docs/user_manual/changelog/2026-08-07_vote_reasons_for_disagreement.md
@@ -0,0 +1,7 @@
+# Require vote reasons when disagreeing
+
+Proposal authors can now require a vote reason when someone selects an option marked Disagree or Block, while leaving reasons optional for Agree and Abstain.
+
+Choose **Required when disagreeing** under **Vote reason** when starting or editing a poll. The setting follows the option's voting icon, so it continues to work when an option is renamed, such as changing Disagree to Objection.
+
+Loomio's built-in templates that include disagreement options now use this setting by default. Coordinators can change it to Required, Optional, or Disabled for an individual poll or template.
--- vue/src/components/poll/common/form.vue
+++ vue/src/components/poll/common/form.vue
@@ -217,9 +217,10 @@ const allowAnonymous = computed(() => !props.poll.config().prevent_anonymous);
 
 const stanceReasonRequiredItems = computed(() => [
   {title: I18n.global.t('poll_common_form.stance_reason_required'), value: 'required'},
+  props.poll.pollType === 'proposal' && {title: I18n.global.t('poll_common_form.stance_reason_required_when_disagreeing'), value: 'required_when_disagreeing'},
   {title: I18n.global.t('poll_common_form.stance_reason_optional'), value: 'optional'},
   {title: I18n.global.t('poll_common_form.stance_reason_disabled'), value: 'disabled'}
-]);
+].filter(Boolean));
 
 const titlePath = computed(() => {
   return (props.poll.isNew() && 'action_dock.new_poll_type') || 'action_dock.edit_poll_type';
