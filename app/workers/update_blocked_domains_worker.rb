0c219d97f922dd4a851e96ad51d07a3d3e9c023e
Robert Guthrie <rob@loomio.org>
2026-09-06 00:07:57 +1200

    Preserve the blocklist when replacement fails
    Download and validate the replacement before deleting existing domains, then replace them in one transaction.

--- app/workers/update_blocked_domains_worker.rb
+++ app/workers/update_blocked_domains_worker.rb
@@ -2,14 +2,15 @@ class UpdateBlockedDomainsWorker < ApplicationJob
   def perform
     puts "updating blocked domains"
     hostsfile = 'https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn/hosts'
-    BlockedDomain.delete_all
-    URI.open(hostsfile, 'r').each do |line|
-      next unless line.starts_with?('0.0.0.0 ')
-      domain = line.split(" ")[1]
-      BlockedDomain.create(name: domain)
+    domains = URI.open(hostsfile, 'r') do |file|
+      file.each_line.filter_map { |line| line.split[1] if line.start_with?('0.0.0.0 ') }.uniq
+    end
+    raise "Downloaded blocklist contains no domains" if domains.empty?
+
+    BlockedDomain.transaction(requires_new: true) do
+      BlockedDomain.delete_all
+      domains.each { |domain| BlockedDomain.create!(name: domain) }
     end
     puts "updating blocked domains completed"
   end
 end
-
-
