36fa416c0c116ed2a3780e00bc46dcd6868dc4d1
Robert Guthrie <rob@loomio.org>
2026-09-17 14:07:06 +1200

    Keep group follow resources type-consistent

--- app/controllers/api/v1/group_follows_controller.rb
+++ app/controllers/api/v1/group_follows_controller.rb
@@ -2,23 +2,23 @@ class Api::V1::GroupFollowsController < Api::V1::RestfulController
   before_action :require_current_user
 
   def create
-    self.resource = GroupFollow.new(group: Group.find(params.require(:group_id)))
-    GroupFollowService.create(group_follow: resource, actor: current_user)
-    respond_with_group
+    self.resource = GroupFollowService.create(
+      group_follow: GroupFollow.new(group: Group.find(params.require(:group_id))),
+      actor: current_user
+    )
+    respond_with_group(resource.group)
   end
 
   def destroy
     self.resource = current_user.group_follows.find_by!(group_id: params.require(:group_id))
     group = resource.group
     GroupFollowService.destroy(group_follow: resource, actor: current_user)
-    self.resource = group
-    respond_with_resource(serializer: GroupSerializer, root: :groups)
+    respond_with_group(group)
   end
 
   private
 
-  def respond_with_group
-    self.resource = resource.group
-    respond_with_resource(serializer: GroupSerializer, root: :groups)
+  def respond_with_group(group)
+    respond_with_collection(records: [ group ], serializer: GroupSerializer, root: :groups)
   end
 end
