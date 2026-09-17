ae88e9d5091b22fa5a8eb4f133ba86fc24677dd9
Robert Guthrie <rob@loomio.org>
2026-09-17 13:49:46 +1200

    Stabilize trial expiry mailer test

--- test/mailers/group_mailer_test.rb
+++ test/mailers/group_mailer_test.rb
@@ -6,23 +6,26 @@ class GroupMailerTest < ActionMailer::TestCase
     recipient = users(:admin)
     usage = GroupUsageSummary.for(group)
 
-    email = GroupMailer.expired_trial_deletion_warning(group.id, recipient.id)
-    body = AppConfig.stub(:group_deletion_delay_days, 45) { email.body.decoded }
+    travel_to Time.zone.local(2026, 9, 17, 12) do
+      group.subscription.update!(expires_at: 60.days.ago)
+      email = GroupMailer.expired_trial_deletion_warning(group.id, recipient.id)
+      body = AppConfig.stub(:group_deletion_delay_days, 45) { email.body.decoded }
 
-    assert_equal "Your Loomio group is scheduled for deletion", email.subject
-    assert_includes body, "trial expired 60 days ago"
-    assert_includes body, "Unless you contact us, it will be deleted in 45 days"
-    assert_includes body, "Your group is unavailable while it is marked for deletion"
-    assert_includes body, "it is not too late to restart"
-    assert_includes body, "request a trial extension, upgrade to a paid subscription"
-    assert_includes body, "or need more time to export its data"
-    assert_includes body, "Subgroups: #{usage[:subgroups]}"
-    assert_includes body, "Current members: #{usage[:members]}"
-    assert_includes body, "Discussions: #{usage[:discussions]}"
-    assert_includes body, "Polls: #{usage[:polls]}"
-    assert_includes body, "Comments: #{usage[:comments]}"
-    assert_includes body, "permanently deleted"
-    refute_includes body, "https://help.loomio.org/en/user_manual/groups/data_export/"
+      assert_equal "Your Loomio group is scheduled for deletion", email.subject
+      assert_includes body, "trial expired 60 days ago"
+      assert_includes body, "Unless you contact us, it will be deleted in 45 days"
+      assert_includes body, "Your group is unavailable while it is marked for deletion"
+      assert_includes body, "it is not too late to restart"
+      assert_includes body, "request a trial extension, upgrade to a paid subscription"
+      assert_includes body, "or need more time to export its data"
+      assert_includes body, "Subgroups: #{usage[:subgroups]}"
+      assert_includes body, "Current members: #{usage[:members]}"
+      assert_includes body, "Discussions: #{usage[:discussions]}"
+      assert_includes body, "Polls: #{usage[:polls]}"
+      assert_includes body, "Comments: #{usage[:comments]}"
+      assert_includes body, "permanently deleted"
+      refute_includes body, "https://help.loomio.org/en/user_manual/groups/data_export/"
+    end
   end
 
   test "requested deletion warning identifies the requestor" do
